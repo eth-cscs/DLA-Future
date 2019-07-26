7fd8573582403cef2a177348270f448c8d003be0
Alberto Invernizzi <invernizzi@cscs.ch>
2019-07-26 15:08:55 +0200

    automatic load for NS3C modules

---
+++ cmake/NS3C.cmake
@@ -0,0 +1,19 @@
+#
+# NS3C
+#
+# Copyright (c) 2018-2019, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Loads all scripts available in CMAKE_MODULE_PATH with filename "NS3C_*.cmake"
+foreach(module_folder IN LISTS CMAKE_MODULE_PATH)
+  file(GLOB cmake_modules "${module_folder}/NS3C_*.cmake")
+
+  foreach(script_filepath IN LISTS cmake_modules)
+    get_filename_component(script_name ${script_filepath} NAME_WE)
+    include(${script_name})
+  endforeach()
+endforeach()

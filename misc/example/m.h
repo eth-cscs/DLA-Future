81d89b15cfcbce153d533e4d53ed54917a9958fe
Raffaele Solcà <rasolca@cscs.ch>
2019-09-04 16:11:25 +0200

    Updated proof of concept with new const correctness method

--- misc/example/m.h
+++ misc/example/m.h
@@ -8,31 +8,66 @@
 #include <memory>
 
 template <class El>
-class Tile {
-  public:
-  Tile(El* ptr) : ptr_(ptr) {}
+class Tile : public Tile<const El> {
+public:
+  Tile(El* ptr) : Tile<const El>(ptr), ptr_(ptr) {}
 
   Tile(const Tile&) = delete;
-  Tile(Tile&& rhs) : ptr_(rhs.ptr_) {
-    //std::cout << "Move" << std::endl;
+  Tile(Tile&& rhs) : Tile<const El>(std::move(rhs)), ptr_(rhs.ptr_) {
+    // std::cout << "Move" << std::endl;
     rhs.ptr_ = nullptr;
   }
 
-  El& operator()() {
+  El& operator()() const {
     return *ptr_;
+  }
+
+private:
+  El* ptr_;
+};
+
+template <class El>
+class Tile<const El> {
+public:
+  Tile(const El* ptr) : ptr_(ptr) {}
+
+  Tile(const Tile&) = delete;
+  Tile(Tile&& rhs) : ptr_(rhs.ptr_) {
+    // std::cout << "Move" << std::endl;
+    rhs.ptr_ = nullptr;
   }
 
   const El& operator()() const {
     return *ptr_;
   }
 
-  private:
-  El* ptr_;
+private:
+  const El* ptr_;
 };
 
-template <class Type>
-class Wrapper {
-  public:
+template <class El>
+class Wrapper : public Wrapper<const El> {
+  using Type = Tile<El>;
+  using Wrapper<const El>::t_;
+
+public:
+  Wrapper(Type&& t, hpx::promise<Type>&& p) : Wrapper<const El>(std::move(t), std::move(p)) {}
+
+  Wrapper(const Wrapper&) = delete;
+  Wrapper(Wrapper&& rhs) = default;
+
+  const Type& get() const {
+    return t_;
+  }
+};
+
+template <class El>
+class Wrapper<const El> {
+  friend Wrapper<El>;
+  using Type = Tile<El>;
+  using ConstType = Tile<const El>;
+
+public:
   Wrapper(Type&& t, hpx::promise<Type>&& p) : t_(std::move(t)), p_(std::move(p)), valid_(true) {}
 
   ~Wrapper() {
@@ -46,14 +81,11 @@ class Wrapper {
     rhs.valid_ = false;
   }
 
-  Type& get() {
-    return t_;
-  }
-  const Type& get() const {
+  const ConstType& get() const {
     return t_;
   }
 
-  private:
+private:
   Type t_;
   hpx::promise<Type> p_;
   bool valid_;
@@ -64,14 +96,17 @@ class MatrixRW;
 template <class Type>
 class MatrixRead;
 
-template <class Type>
+template <class El>
 class ConstMatrix {
-  public:
+  using Type = Tile<El>;
+  using ConstType = Tile<const El>;
+
+public:
   ConstMatrix(std::array<hpx::future<Type>, 4>&& f) : f_(std::move(f)) {}
 
   ConstMatrix(ConstMatrix&& rhs) : f_(std::move(rhs.f_)), s_(std::move(rhs.s_)){};
 
-  hpx::shared_future<Wrapper<Type>> read(int i) const {
+  hpx::shared_future<Wrapper<const El>> read(int i) const {
     // if the shared future is not valid (i.e. the previous task modified the tile)
     // a new shared future is created. when all the shared future are destroyed, the Wrapper object
     // is destroyed as well and the future is set allowing write operation to go on.
@@ -81,20 +116,21 @@ class ConstMatrix {
       hpx::promise<Type> p;
       f_[i] = p.get_future();
       s_[i] = std::move(fut.then(hpx::launch::sync, [p = std::move(p)](hpx::future<Type>&& fut) mutable {
-        return Wrapper(std::move(fut.get()), std::move(p));
+        return Wrapper<const El>(std::move(fut.get()), std::move(p));
       }));
     }
     return s_[i];
   }
 
-  MatrixRead<Type> block_read();
+  MatrixRead<El> block_read();
 
-  protected:
+protected:
   // used for building RW matrix.
-  ConstMatrix(std::array<hpx::future<Type>, 4>&& f, std::array<hpx::shared_future<Wrapper<Type>>, 4>&& s)
+  ConstMatrix(std::array<hpx::future<Type>, 4>&& f,
+              std::array<hpx::shared_future<Wrapper<const El>>, 4>&& s)
       : f_(std::move(f)), s_(std::move(s)) {}
   // used for building read-only matrix.
-  ConstMatrix(std::array<hpx::shared_future<Wrapper<Type>>, 4>&& s) : f_(), s_(std::move(s)) {
+  ConstMatrix(std::array<hpx::shared_future<Wrapper<const El>>, 4>&& s) : f_(), s_(std::move(s)) {
     for (std::size_t i = 0; i < s_.size(); ++i) {
       if (!s_[i].valid()) {
         std::cerr << "ERROR: Invalid shared future!" << std::endl;
@@ -104,51 +140,57 @@ class ConstMatrix {
   }
 
   mutable std::array<hpx::future<Type>, 4> f_;
-  mutable std::array<hpx::shared_future<Wrapper<Type>>, 4> s_;
+  mutable std::array<hpx::shared_future<Wrapper<const El>>, 4> s_;
 };
 
-template <class Type>
-class Matrix : public ConstMatrix<Type> {
-  protected:
-  using ConstMatrix<Type>::f_;
-  using ConstMatrix<Type>::s_;
+template <class El>
+class Matrix : public ConstMatrix<El> {
+  using Type = Tile<El>;
 
-  public:
-  Matrix(std::array<hpx::future<Type>, 4>&& f) : ConstMatrix<Type>(std::move(f)) {}
+protected:
+  using ConstMatrix<El>::f_;
+  using ConstMatrix<El>::s_;
 
-  Matrix(Matrix&& rhs) : ConstMatrix<Type>(std::move(rhs)){};
+public:
+  Matrix(std::array<hpx::future<Type>, 4>&& f) : ConstMatrix<El>(std::move(f)) {}
 
-  // Create a new future for i-th tile which will be set as ready when the Wrapper object included in the returned future is destroyed.
-  hpx::future<Wrapper<Type>> operator()(int i) {
+  Matrix(Matrix&& rhs) : ConstMatrix<El>(std::move(rhs)){};
+
+  // Create a new future for i-th tile which will be set as ready when the Wrapper object included in the
+  // returned future is destroyed.
+  hpx::future<Wrapper<El>> operator()(int i) {
     auto fut = std::move(f_[i]);
     hpx::promise<Type> p;
     f_[i] = p.get_future();
     s_[i] = {};
     return fut.then(hpx::launch::sync, [p = std::move(p)](hpx::future<Type>&& fut) mutable {
-      return Wrapper(std::move(fut.get()), std::move(p));
+      return Wrapper<El>(std::move(fut.get()), std::move(p));
     });
   }
 
-  MatrixRW<Type> block();
+  MatrixRW<El> block();
 
-  protected:
+protected:
   // used for building RW matrix.
-  Matrix(std::array<hpx::future<Type>, 4>&& f, std::array<hpx::shared_future<Wrapper<Type>>, 4>&& s)
-      : ConstMatrix<Type>(std::move(f), std::move(s)) {}
+  Matrix(std::array<hpx::future<Type>, 4>&& f, std::array<hpx::shared_future<Wrapper<const El>>, 4>&& s)
+      : ConstMatrix<El>(std::move(f), std::move(s)) {}
 };
 
-template <class Type>
-class MatrixRW : public Matrix<Type> {
-  protected:
-  using Matrix<Type>::f_;
-  using Matrix<Type>::s_;
+template <class El>
+class MatrixRW : public Matrix<El> {
+  using Type = Tile<El>;
 
-  public:
-  MatrixRW(std::array<hpx::future<Type>, 4>&& f, std::array<hpx::shared_future<Wrapper<Type>>, 4>&& s,
+protected:
+  using Matrix<El>::f_;
+  using Matrix<El>::s_;
+
+public:
+  MatrixRW(std::array<hpx::future<Type>, 4>&& f,
+           std::array<hpx::shared_future<Wrapper<const El>>, 4>&& s,
            std::array<std::unique_ptr<hpx::promise<Type>>, 4>&& p,
-           std::array<std::unique_ptr<hpx::promise<Wrapper<Type>>>, 4>&& sp,
-           std::array<hpx::shared_future<Wrapper<Type>>, 4> sf)
-      : Matrix<Type>(std::move(f), std::move(s)), p_(std::move(p)), sp_(std::move(sp)), sf_(sf) {}
+           std::array<std::unique_ptr<hpx::promise<Wrapper<const El>>>, 4>&& sp,
+           std::array<hpx::shared_future<Wrapper<const El>>, 4> sf)
+      : Matrix<El>(std::move(f), std::move(s)), p_(std::move(p)), sp_(std::move(sp)), sf_(sf) {}
 
   ~MatrixRW() {
     for (std::size_t i = 0; i < p_.size(); ++i) {
@@ -163,7 +205,7 @@ class MatrixRW : public Matrix<Type> {
       s_[i] = sf_[i];
       f_[i].then(hpx::launch::sync,
                  [p = std::move(p_[i]), sp = std::move(sp_[i])](hpx::future<Type>&& fut) mutable {
-                   sp->set_value(Wrapper(std::move(fut.get()), std::move(*p)));
+                   sp->set_value(Wrapper<const El>(std::move(fut.get()), std::move(*p)));
                  });
       p_[i] = nullptr;
       sp_[i] = nullptr;
@@ -175,19 +217,19 @@ class MatrixRW : public Matrix<Type> {
     s_[i] = {};
   }
 
-  private:
+private:
   std::array<std::unique_ptr<hpx::promise<Type>>, 4> p_;
-  std::array<std::unique_ptr<hpx::promise<Wrapper<Type>>>, 4> sp_;
-  std::array<hpx::shared_future<Wrapper<Type>>, 4> sf_;
+  std::array<std::unique_ptr<hpx::promise<Wrapper<const El>>>, 4> sp_;
+  std::array<hpx::shared_future<Wrapper<const El>>, 4> sf_;
 };
 
-template <class Type>
-class MatrixRead : public ConstMatrix<Type> {
-  protected:
-  using ConstMatrix<Type>::s_;
+template <class El>
+class MatrixRead : public ConstMatrix<El> {
+protected:
+  using ConstMatrix<El>::s_;
 
-  public:
-  MatrixRead(std::array<hpx::shared_future<Wrapper<Type>>, 4>&& s) : ConstMatrix<Type>(std::move(s)) {}
+public:
+  MatrixRead(std::array<hpx::shared_future<Wrapper<const El>>, 4>&& s) : ConstMatrix<El>(std::move(s)) {}
 
   MatrixRead(MatrixRead&&) = default;
 
@@ -199,39 +241,39 @@ class MatrixRead : public ConstMatrix<Type> {
 
   void done(int i) {
     if (s_[i].valid()) {
-      //std::cout << "done " << i << std::endl;
+      // std::cout << "done " << i << std::endl;
       s_[i] = {};
     }
   }
 };
 
-template <class Type>
-MatrixRW<Type> Matrix<Type>::block() {
+template <class El>
+MatrixRW<El> Matrix<El>::block() {
   // Create a new future for each tile. The i-th future will be set as ready when the done(i)
   // method of MatrixRW is called or the MatrixRW object is destroyed.
   // The current futures and shared futures are moved to the MatrixRW object.
   std::array<std::unique_ptr<hpx::promise<Type>>, 4> p;
-  std::array<std::unique_ptr<hpx::promise<Wrapper<Type>>>, 4> sp;
+  std::array<std::unique_ptr<hpx::promise<Wrapper<const El>>>, 4> sp;
 
   std::array<hpx::future<Type>, 4> f = std::move(f_);
-  std::array<hpx::shared_future<Wrapper<Type>>, 4> s = std::move(s_);
+  std::array<hpx::shared_future<Wrapper<const El>>, 4> s = std::move(s_);
 
   for (std::size_t i = 0; i < f_.size(); ++i) {
     p[i] = std::make_unique<hpx::promise<Type>>();
-    sp[i] = std::make_unique<hpx::promise<Wrapper<Type>>>();
+    sp[i] = std::make_unique<hpx::promise<Wrapper<const El>>>();
     f_[i] = std::move(p[i]->get_future());
     s_[i] = sp[i]->get_future();
   }
-  return MatrixRW<Type>(std::move(f), std::move(s), std::move(p), std::move(sp), s_);
+  return MatrixRW<El>(std::move(f), std::move(s), std::move(p), std::move(sp), s_);
 }
 
-template <class Type>
-MatrixRead<Type> ConstMatrix<Type>::block_read() {
+template <class El>
+MatrixRead<El> ConstMatrix<El>::block_read() {
   // Create if not already available the shared future for the tiles and store a copy of them
   // in the new MatrixRead object.
-  std::array<hpx::shared_future<Wrapper<Type>>, 4> s;
+  std::array<hpx::shared_future<Wrapper<const El>>, 4> s;
   for (std::size_t i = 0; i < f_.size(); ++i) {
     s[i] = std::move(read(i));
   }
-  return MatrixRead<Type>(std::move(s));
+  return MatrixRead<El>(std::move(s));
 }
--- misc/example/test.cpp
+++ misc/example/test.cpp
@@ -6,7 +6,7 @@
 #include "hpx/include/parallel_executors.hpp"
 
 // Subtract 1 to tile value.
-void work0(Wrapper<Tile<int>>&& i, int index) {
+void work0(const Wrapper<int>&& i, int index) {
   auto& i_ = i.get()();
   std::cout << out("start", i_, -1, index);
   sleep(5);
@@ -15,7 +15,7 @@ void work0(Wrapper<Tile<int>>&& i, int index) {
 }
 
 // Sum j to tile value.
-void work(Wrapper<Tile<int>>&& i, int j, int index) {
+void work(const Wrapper<int>&& i, int j, int index) {
   auto& i_ = i.get()();
   std::cout << out("start", i_, j, index);
   sleep(1);
@@ -25,7 +25,7 @@ void work(Wrapper<Tile<int>>&& i, int j, int index) {
 
 // Sum (plus = true)/Subtract the value of the tile j to the value of tile i.
 template <bool plus = true>
-void work2(Wrapper<Tile<int>>&& i, const Wrapper<Tile<int>>& j, int index) {
+void work2(const Wrapper<int>&& i, const Wrapper<const int>& j, int index) {
   auto& i_ = i.get()();
   auto& j_ = j.get()();
   std::cout << out("start2", i_, j_, index);
@@ -46,38 +46,36 @@ void foo() {
 
   std::array<int, 4> a = {0, 1, 2, 3};
   std::array<int, 4> b = {0, 1, 2, 3};
-  std::array<hpx::future<Type>, 4> fa = {
-      hpx::make_ready_future<Type>(&a[0]), hpx::make_ready_future<Type>(&a[1]),
-      hpx::make_ready_future<Type>(&a[2]), hpx::make_ready_future<Type>(&a[3])};
+  std::array<hpx::future<Type>, 4> fa = {hpx::make_ready_future<Type>(&a[0]),
+                                         hpx::make_ready_future<Type>(&a[1]),
+                                         hpx::make_ready_future<Type>(&a[2]),
+                                         hpx::make_ready_future<Type>(&a[3])};
 
-  std::array<hpx::future<Type>, 4> fb = {
-      hpx::make_ready_future<Type>(&b[0]), hpx::make_ready_future<Type>(&b[1]),
-      hpx::make_ready_future<Type>(&b[2]), hpx::make_ready_future<Type>(&b[3])};
+  std::array<hpx::future<Type>, 4> fb = {hpx::make_ready_future<Type>(&b[0]),
+                                         hpx::make_ready_future<Type>(&b[1]),
+                                         hpx::make_ready_future<Type>(&b[2]),
+                                         hpx::make_ready_future<Type>(&b[3])};
 
-  Matrix<Type> ma(std::move(fa));
-  Matrix<Type> mb(std::move(fb));
+  Matrix<int> ma(std::move(fa));
+  Matrix<int> mb(std::move(fb));
   // end of matrices setup
 
   // execute some operation on matrix B asynchronously.
-  MatrixRW<Type> mb1 = mb.block();
+  MatrixRW<int> mb1 = mb.block();
   hpx::async([mb1 = std::move(mb1)]() mutable {
     hpx::dataflow(hpx::util::unwrapping(work0), std::move(mb1(0)), 1);
   });
 
   // execute some operation on matrix A asynchronously. B is constant (read-only).
-  MatrixRW<Type> ma1 = ma.block();
-  MatrixRead<Type> mb2 = mb.block_read();
+  MatrixRW<int> ma1 = ma.block();
+  MatrixRead<int> mb2 = mb.block_read();
   hpx::async([ma1 = std::move(ma1), mb2 = std::move(mb2), &HP]() mutable {
     for (std::size_t i = 1; i < 4; ++i) {
-      hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(0)), std::move(ma1.read(i)),
-                    100 + i);
+      hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(0)), std::move(ma1.read(i)), 100 + i);
     }
-    {
-      hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(2)), std::move(ma1.read(3)), 200);
-    }
+    { hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(2)), std::move(ma1.read(3)), 200); }
     for (std::size_t i = 0; i < 4; ++i) {
-      hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(i)), std::move(mb2.read(i)),
-                    300 + i);
+      hpx::dataflow(hpx::util::unwrapping(work2<>), std::move(ma1(i)), std::move(mb2.read(i)), 300 + i);
       hpx::dataflow(HP, hpx::util::unwrapping(work), std::move(ma1(1)), i * 100, 400 + i);
     }
   });

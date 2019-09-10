646e3a2cb23de26a1d2ce145cd1a04290c880c1b
Alberto Invernizzi <invernizzi@cscs.ch>
2019-09-10 16:04:02 +0200

    constrain ceilDiv params type + doc update

--- include/dlaf/util_math.h
+++ include/dlaf/util_math.h
@@ -22,21 +22,19 @@ namespace util {
 /// It accepts both signed and unsigned integer types, but as precondition states both values must
 /// not be negative (additionally, @p den must not be zero either)
 ///
-/// @tparam IntNumType and @tparam IntDenType have to be integer types.
+/// @tparam IntType must be a signed or unsigned integer type.
 /// @param num
 /// @param den
-/// @return ceiled division of type @a IntNumType
+/// @return ceiled division of type @a IntType
 ///
 /// @pre @a num >= 0 and @a den > 0.
-/// @pre @a num + @a den - 1 must not overflow @a IntNumType max value
+/// @pre @a num + @a den - 1 must not overflow @a IntType max value
+template <typename IntType>
+constexpr auto ceilDiv(const IntType num, const IntType den)
 #ifdef DLAF_DOXYGEN
-template <typename IntNumType, typename IntDenType>
-constexpr IntNumType ceilDiv(const IntNumType num, const IntDenType den);
+    ;
 #else
-template <typename IntNumType, typename IntDenType>
-constexpr
-std::enable_if_t<std::is_integral<IntNumType>::value && std::is_integral<IntDenType>::value, IntNumType>
-ceilDiv(const IntNumType num, const IntDenType den) {
+    -> std::enable_if_t<std::is_integral<IntType>::value, IntType> {
   return (num + den - 1) / den;
 }
 #endif

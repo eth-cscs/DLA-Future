8180a7f928055b0ea1ce4f13fb556ef3e6cf0182
Alberto Invernizzi <invernizzi@cscs.ch>
2019-09-11 11:16:30 +0200

    Implement basic test for CommunicatorGrid

--- test/unit/communication/test_communicator_grid.cpp
+++ test/unit/communication/test_communicator_grid.cpp
@@ -13,13 +13,95 @@
 #include <mpi.h>
 #include <gtest/gtest.h>
 
-TEST(CommunicatorGrid, basic) {
-  using namespace dlaf::comm;
+using namespace dlaf::comm;
 
-  Communicator world;
-  CommunicatorGrid grid(world, computeGridDims(world.size()));
+TEST(CommunicatorGrid, ConstructorWithParams) {
+  Communicator world(MPI_COMM_WORLD);
+
+  auto grid_dims = computeGridDims(NUM_MPI_RANKS);
+  int nrows = grid_dims[0];
+  int ncols = grid_dims[1];
+
+  CommunicatorGrid grid(world, nrows, ncols);
 
   EXPECT_EQ(grid.rows() * grid.cols(), NUM_MPI_RANKS);
+  EXPECT_EQ(grid.rows(), nrows);
+  EXPECT_EQ(grid.cols(), ncols);
+}
 
-  // std::cout << "(" << grid.rank().row() << "; " << grid.rank().col() << ")" << std::endl;
+TEST(CommunicatorGrid, ConstructorWithArray) {
+  Communicator world(MPI_COMM_WORLD);
+
+  auto grid_dims = computeGridDims(NUM_MPI_RANKS);
+  CommunicatorGrid grid(world, grid_dims);
+
+  EXPECT_EQ(grid.rows() * grid.cols(), NUM_MPI_RANKS);
+  EXPECT_EQ(grid.rows(), grid_dims[0]);
+  EXPECT_EQ(grid.cols(), grid_dims[1]);
+}
+
+TEST(CommunicatorGrid, ConstructorIncomplete) {
+  static_assert(NUM_MPI_RANKS > 1, "There must be at least 2 ranks");
+
+  Communicator world(MPI_COMM_WORLD);
+  CommunicatorGrid incomplete_grid(world, NUM_MPI_RANKS - 1, 1);
+
+  if (world.rank() != NUM_MPI_RANKS - 1) { // ranks in the grid
+    EXPECT_EQ(incomplete_grid.rows(), NUM_MPI_RANKS - 1);
+    EXPECT_EQ(incomplete_grid.cols(), 1);
+
+    EXPECT_NE(incomplete_grid.all(), MPI_COMM_NULL);
+    EXPECT_NE(incomplete_grid.row(), MPI_COMM_NULL);
+    EXPECT_NE(incomplete_grid.col(), MPI_COMM_NULL);
+
+    auto rank_row = world.rank() / incomplete_grid.cols();
+    auto rank_col = world.rank() % incomplete_grid.cols();
+
+    EXPECT_EQ(rank_row, incomplete_grid.rank().row());
+    EXPECT_EQ(rank_col, incomplete_grid.rank().col());
+
+    EXPECT_EQ(rank_row, incomplete_grid.row().rank());
+    EXPECT_EQ(rank_col, incomplete_grid.col().rank());
+  }
+  else {  // last rank is not in the grid
+    EXPECT_EQ(incomplete_grid.rows(), MPI_UNDEFINED);
+    EXPECT_EQ(incomplete_grid.cols(), MPI_UNDEFINED);
+
+    EXPECT_EQ(incomplete_grid.all(), MPI_COMM_NULL);
+    EXPECT_EQ(incomplete_grid.row(), MPI_COMM_NULL);
+    EXPECT_EQ(incomplete_grid.col(), MPI_COMM_NULL);
+
+    EXPECT_EQ(incomplete_grid.rank().row(), -1);
+    EXPECT_EQ(incomplete_grid.rank().col(), -1);
+
+    EXPECT_EQ(incomplete_grid.row().rank(), MPI_UNDEFINED);
+    EXPECT_EQ(incomplete_grid.col().rank(), MPI_UNDEFINED);
+  }
+}
+
+TEST(CommunicatorGrid, Rank) {
+  auto grid_dims = computeGridDims(NUM_MPI_RANKS);
+
+  auto grid_area = 1;
+  for(auto dim : grid_dims) {
+    ASSERT_NE(dim, 1);
+    grid_area *= dim;
+  }
+  ASSERT_EQ(grid_area, NUM_MPI_RANKS);
+
+  Communicator world(MPI_COMM_WORLD);
+  CommunicatorGrid complete_grid(world, grid_dims);
+
+  EXPECT_EQ(complete_grid.rows(), grid_dims[0]);
+  EXPECT_EQ(complete_grid.cols(), grid_dims[1]);
+
+  auto rank_row = world.rank() / complete_grid.cols();
+  auto rank_col = world.rank() % complete_grid.cols();
+
+  EXPECT_EQ(rank_row, complete_grid.rank().row());
+  EXPECT_EQ(rank_col, complete_grid.rank().col());
+
+  // TODO equivalent call...redundancy?!
+  EXPECT_EQ(rank_row, complete_grid.row().rank());
+  EXPECT_EQ(rank_col, complete_grid.col().rank());
 }

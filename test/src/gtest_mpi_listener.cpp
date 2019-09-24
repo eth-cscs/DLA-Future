af3fdcf93df0509cc7e0a93e365bf04e560bfb37
Alberto Invernizzi <invernizzi@cscs.ch>
2019-09-24 09:52:28 +0200

    fix problem with multiple failure per test
    now it can manage multiple failure messages per test

--- test/src/gtest_mpi_listener.cpp
+++ test/src/gtest_mpi_listener.cpp
@@ -27,6 +27,8 @@ void MPIListener::OnTestProgramEnd(const ::testing::UnitTest& unit_test) {
 
 // Called before a test starts.
 void MPIListener::OnTestStart(const ::testing::TestInfo& test_info) {
+  last_test_result_ = "";
+
   if (!isMainRank())
     return;
 }
@@ -36,11 +38,11 @@ void MPIListener::OnTestPartResult(const ::testing::TestPartResult& test_part_re
   std::ostringstream error_description;
 
   error_description
-    << (test_part_result.failed() ? "Failure" : "Success")
+    << "- " << (test_part_result.failed() ? "Failure" : "Success")
     << " in " << test_part_result.file_name() << ":" << test_part_result.line_number() << std::endl
     << test_part_result.summary() << std::endl;
 
-  last_test_result_ = error_description.str();
+  last_test_result_ += error_description.str();
 }
 
 // Called after a test ends.
@@ -59,7 +61,7 @@ void MPIListener::OnTestEnd(const ::testing::TestInfo& test_info) {
     assert(result == results[rank_]);
 
     if (!result)
-      printf("[R%d] %s\n", rank_, last_test_result_.c_str());
+      printf("[R%d]\n%s", rank_, last_test_result_.c_str());
   }
 
   for (int rank = 1; rank < world_size_; rank++) {
@@ -74,7 +76,7 @@ void MPIListener::OnTestEnd(const ::testing::TestInfo& test_info) {
         char rank_error_message[number_amount];
         MPI_Recv(rank_error_message, number_amount, MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        printf("[R%d] %s\n", rank, rank_error_message);
+        printf("[R%d]\n%s", rank, rank_error_message);
       }
     }
     else if(rank_ == rank) {

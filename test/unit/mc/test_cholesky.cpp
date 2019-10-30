7229a369c376f8f1bcb529cfbefa10bf87eb0fd8
l.quercia <lara.querciagrossa@gmail.com>
2019-10-30 16:20:58 +0100

    Wrote beginning of tests for Correctness and for NonPositiveDefiniteException

--- test/unit/mc/test_cholesky.cpp
+++ test/unit/mc/test_cholesky.cpp
@@ -31,7 +31,7 @@ std::vector<GlobalElementSize> rectangular_sizes({{10, 20}, {50, 20}, {0, 10}, {
 std::vector<TileElementSize> square_block_sizes({{5, 5}, {20, 20}});
 std::vector<TileElementSize> rectangular_block_sizes({{10, 30}, {20, 10}});
 
-TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
+TYPED_TEST(CholeskyTest, Correctness) {
   using Type = TypeParam;
 
   auto el = [](const GlobalElementIndex& index) {
@@ -40,7 +40,6 @@ TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
     return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
   };
 
-  // Check for square sizes
   for (const auto& size : square_sizes) {
     for (const auto& block_size : square_block_sizes) {
       Matrix<Type, Device::CPU> mat(size, block_size);
@@ -50,43 +49,66 @@ TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
       EXPECT_NO_THROW(cholesky_local(mat));
     }
   }
-
-  // Check for rectangular sizes
-  for (const auto& size : rectangular_sizes) {
-    for (const auto& block_size : square_block_sizes) {
-      Matrix<Type, Device::CPU> mat(size, block_size);
-
-      EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
-    }
-  }
 }
 
-TYPED_TEST(CholeskyTest, NoSquareBlockException) {
-  using Type = TypeParam;
+  TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
+    using Type = TypeParam;
 
-  auto el = [](const GlobalElementIndex& index) {
-    SizeType i = index.row();
-    SizeType j = index.col();
-    return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
-  };
+    auto el = [](const GlobalElementIndex& index) {
+      SizeType i = index.row();
+      SizeType j = index.col();
+      return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
+    };
 
-  // Check for square sizes
-  for (const auto& size : square_sizes) {
-    for (const auto& block_size : square_block_sizes) {
-      Matrix<Type, Device::CPU> mat(size, block_size);
+    // Check for square sizes
+    for (const auto& size : square_sizes) {
+      for (const auto& block_size : square_block_sizes) {
+        Matrix<Type, Device::CPU> mat(size, block_size);
 
-      set(mat, el);
+        set(mat, el);
 
-      EXPECT_NO_THROW(cholesky_local(mat));
+        EXPECT_NO_THROW(cholesky_local(mat));
+      }
+    }
+
+    // Check for rectangular sizes
+    for (const auto& size : rectangular_sizes) {
+      for (const auto& block_size : square_block_sizes) {
+        Matrix<Type, Device::CPU> mat(size, block_size);
+
+        EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
+      }
     }
   }
 
-  // Check for rectangular sizes
-  for (const auto& size : square_sizes) {
-    for (const auto& block_size : rectangular_block_sizes) {
-      Matrix<Type, Device::CPU> mat(size, block_size);
+  TYPED_TEST(CholeskyTest, NoSquareBlockException) {
+    using Type = TypeParam;
 
-      EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
+    auto el = [](const GlobalElementIndex& index) {
+      SizeType i = index.row();
+      SizeType j = index.col();
+      return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
+    };
+
+    // Check for square sizes
+    for (const auto& size : square_sizes) {
+      for (const auto& block_size : square_block_sizes) {
+        Matrix<Type, Device::CPU> mat(size, block_size);
+
+        set(mat, el);
+
+        EXPECT_NO_THROW(cholesky_local(mat));
+      }
+    }
+
+    // Check for rectangular sizes
+    for (const auto& size : square_sizes) {
+      for (const auto& block_size : rectangular_block_sizes) {
+        Matrix<Type, Device::CPU> mat(size, block_size);
+
+        EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
+      }
     }
   }
-}
+
+  TYPED_TEST(CholeskyTest, NonPositiveDefiniteException) {}

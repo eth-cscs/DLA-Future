3f1208dd37af55d3a0ae57a0a1a2d237879ee00e
Raffaele Solcà <rasolca@cscs.ch>
2019-11-07 10:51:43 +0100

    Updated daint environment (#81)

--- ci/daint-mc_env.sh
+++ ci/daint-mc_env.sh
@@ -13,13 +13,13 @@ module load intel
 export EASYBUILD_PREFIX=/apps/daint/SSL/rasolca/jenkins/daint-broadwell
 module load EasyBuild-custom/cscs
 
-module load BLASPP/20190829-CrayGNU-18.08
-module load LAPACKPP/20190829-CrayGNU-18.08
+module load BLASPP/20190829-CrayGNU-19.10
+module load LAPACKPP/20190829-CrayGNU-19.10
 if [ "${BUILD_TYPE,,}" = debug ]
 then
-  module load HPX/20190830-CrayGNU-18.08-jemalloc-debug
+  module load HPX/20190830-CrayGNU-19.10-jemalloc-debug
 else
-  module load HPX/20190830-CrayGNU-18.08-jemalloc
+  module load HPX/20190830-CrayGNU-19.10-jemalloc
 fi
 
 export CRAYPE_LINK_TYPE=dynamic

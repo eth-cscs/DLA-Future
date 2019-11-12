7133d957dcc53d28173ca81734475f5aa6d4cb26
l.quercia <lara.querciagrossa@gmail.com>
2019-11-12 15:58:04 +0100

    Added first implementation of Cholesky decomposition using tile operations. Documentation attempt. Tests not implemented yet
    adjusted clang format
    
    added correctness test for cholesky_local (CPU)

--- include/dlaf/mc/cholesky_local.h
+++ include/dlaf/mc/cholesky_local.h
@@ -1,5 +1,4 @@
-//
-// Distributed Linear Algebra with Future (DLAF)
+// Linear Algebra with Future (DLAF)
 //
 // Copyright (c) 2018-2019, ETH Zurich
 // All rights reserved.
@@ -9,9 +8,17 @@
 //
 #pragma once
 
+#include "dlaf/blas_tile.h"
+#include "dlaf/lapack_tile.h"
 #include "dlaf/matrix.h"
+#include "dlaf/matrix/index.h"
+#include "dlaf/matrix/layout_info.h"
+#include "dlaf/util_blas.h"
 #include "dlaf/util_matrix.h"
 //
+#include <exception>
+//
+#include <hpx/runtime/threads/run_as_hpx_thread.hpp>
 #include "hpx/hpx.hpp"
 #include "hpx/include/parallel_executors.hpp"
 #include "hpx/include/threads.hpp"
@@ -21,23 +28,90 @@
 namespace dlaf {
 
 /// @brief Cholesky implementation on local memory
+///
+/// @tparam mat referes to a dlaf::Matrix object
+/// @param uplo specifies whether the matrix is \a Upper or \a Lower triangular
+/// @param side defines whether the matrix is on the \a Left or \a Right side of the unknown matrix
+/// @param op specifies the form of \p mat used in matrix multiplication: \a NoTrans, \a Trans, \a ConjTrans
+/// @param diag describes if the matrix is unit triangular (\a Unit) or not (\a NonUnit)
+///
+/// @throws std::runtime_error if \p uplo = \a Upper decomposition  is chosen (not yet implemented)
 
 template <class T>
-void cholesky_local(Matrix<T, Device::CPU>& mat) {
-  // Setup executors for different task priorities on the default (matrix) pool
+void cholesky_local(Matrix<T, Device::CPU>& mat, blas::Uplo uplo, blas::Side side, blas::Op op,
+                    blas::Diag diag) {
+  /// First, two executors are set up on the default queue: with high and with default priority.
+
+  // Set up executor on the default queue with high priority.
   hpx::threads::scheduled_executor matrix_HP_executor =
       hpx::threads::executors::pool_executor("default", hpx::threads::thread_priority_high);
 
+  // Set up executor on the default queue with default priority.
   hpx::threads::scheduled_executor matrix_normal_executor =
       hpx::threads::executors::pool_executor("default", hpx::threads::thread_priority_default);
 
+  /// Then the matrix and its sublocks are tested to confirm they are square.
   // Check if matrix is square
   util_matrix::check_size_square(mat, "Cholesky", "mat");
   // Check if block matrix is square
   util_matrix::check_blocksize_square(mat, "Cholesky", "mat");
 
-  // Define tile index
-  LocalTileIndex index;
-}
+  // Number of tile (rows = cols)
+  SizeType nrtile = mat.nrTiles().cols();
 
+  // k-loop
+  for (SizeType k = 0; k < nrtile; ++k) {
+    /// A Cholesky decomposition with the tile::potrf function is done on the diagonal tiles.
+    // Cholesky decomposition on mat(k,k) r/w potrf (lapack operation)
+    if (uplo == blas::Uplo::Lower) {
+      hpx::dataflow(hpx::util::unwrapping(tile::potrf<T, Device::CPU>), uplo, std::move(mat({k, k})));
+    }
+    else {
+      throw std::runtime_error("uplo = Upper not yet implemented");
+    }
+
+    // i-loop
+    for (SizeType i = k + 1; i < nrtile; ++i) {
+      /// The panel below each diagonal tile is updated, using tile::trsm.
+      // Update panel mat(i,k) with trsm (blas operation), using data mat.read(k,k)
+      if (uplo == blas::Uplo::Lower && side == blas::Side::Right && op == blas::Op::ConjTrans &&
+          diag == blas::Diag::NonUnit) {
+        T alpha = 1.0;
+        hpx::dataflow(hpx::util::unwrapping(tile::trsm<T, Device::CPU>), side, uplo, op, diag, alpha,
+                      mat.read({k, k}), std::move(mat({i, k})));
+      }
+      else {
+        throw std::runtime_error(
+            "uplo = Upper, side = Left, op = Trans/NoTrans, diag = Unit not yet implemented");
+      }
+    }
+
+    // j-loop
+    for (SizeType j = k + 1; j < nrtile; ++j) {
+      /// Next, also the diagonal tiles of the trailing matrix are updated with tile::herk operation.
+      // Update trailing matrix: diagonal element mat(j,j, reading mat.read(j,k), using herk (blas operation)
+      if (uplo == blas::Uplo::Lower && op == blas::Op::ConjTrans) {
+        BaseType<T> alpha = -1.0;
+        BaseType<T> beta = 1.0;
+        hpx::dataflow(hpx::util::unwrapping(tile::herk<T, Device::CPU>), uplo, blas::Op::NoTrans, alpha,
+                      mat.read({j, k}), beta, std::move(mat({j, j})));
+      }
+      else {
+        throw std::runtime_error("uplo = Upper, diag = Unit not yet implemented");
+      }
+
+      // internal i-loop
+      for (SizeType i = j + 1; i < nrtile; ++i) {
+        /// Finally, the remaining trailing matrix is updated with tile::gemm.
+        // Update remaining trailing matrix mat(i,j), reading mat.read(i,k) and mat.read(j,k), using gemm
+        // (blas operation)
+        T alpha = -1.0;
+        T beta = 1.0;
+        hpx::dataflow(hpx::util::unwrapping(tile::gemm<T, Device::CPU>), blas::Op::NoTrans,
+                      blas::Op::ConjTrans, alpha, mat.read({i, k}), mat.read({j, k}), beta,
+                      std::move(mat({i, j})));
+      }
+    }
+  }
+}
 }
--- test/unit/mc/test_cholesky.cpp
+++ test/unit/mc/test_cholesky.cpp
@@ -26,12 +26,64 @@ class CholeskyTest : public ::testing::Test {};
 
 TYPED_TEST_CASE(CholeskyTest, MatrixElementTypes);
 
-std::vector<GlobalElementSize> square_sizes({{10, 10}, {25, 25}, {0, 0}});
+std::vector<GlobalElementSize> square_sizes({{10, 10}, {25, 25}, {12, 12}, {0, 0}});
 std::vector<GlobalElementSize> rectangular_sizes({{10, 20}, {50, 20}, {0, 10}, {20, 0}});
 std::vector<TileElementSize> square_block_sizes({{5, 5}, {20, 20}});
 std::vector<TileElementSize> rectangular_block_sizes({{10, 30}, {20, 10}});
 
 TYPED_TEST(CholeskyTest, Correctness) {
+  using Type = TypeParam;
+
+  // Note: The tile elements are chosen such that:
+  // - res_ij = 1 / 2^(|i-j|) * exp(I*(-i+j)),
+  // where I = 0 for real types or I is the complex unit for complex types.
+  // Therefore the result should be:
+  // a_ij = Sum_k(res_ik * ConjTrans(res)_kj) =
+  //      = Sum_k(1 / 2^(|i-k| + |j-k|) * exp(I*(-i+j))),
+  // where k = 0 .. min(i,j)
+  // Therefore,
+  // a_ij = (4^(min(i,j)+1) - 1) / (3 * 2^(i+j)) * exp(I*(-i+j)
+  auto el = [](const GlobalElementIndex& index) {
+    SizeType i = index.row();
+    SizeType j = index.col();
+    if (i < j)
+      return TypeUtilities<TypeParam>::element(-9.9, 0.0);
+
+    return TypeUtilities<TypeParam>::polar(std::exp2(-(i + j)) / 3 *
+                                               (std::exp2(2 * (std::min(i, j) + 1)) - 1),
+                                           -i + j);
+  };
+
+  // Analytical results
+  auto res = [](const TileElementIndex& index) {
+    SizeType i = index.row();
+    SizeType j = index.col();
+    if (i < j)
+      return TypeUtilities<TypeParam>::element(-9.9, 0.0);
+
+    return TypeUtilities<TypeParam>::polar(std::exp2(-std::abs(i - j)), -i + j);
+  };
+
+  for (const auto& size : square_sizes) {
+    for (const auto& block_size : square_block_sizes) {
+      // Matrix to undergo Cholesky decomposition
+      Matrix<Type, Device::CPU> mat(size, block_size);
+      set(mat, el);
+
+      // Matrix with analytical results
+      Matrix<Type, Device::CPU> matres(size, block_size);
+      set(matres, res);
+
+      EXPECT_NO_THROW(cholesky_local(mat, blas::Uplo::Lower, blas::Side::Right, blas::Op::ConjTrans,
+                                     blas::Diag::NonUnit));
+
+      CHECK_MATRIX_NEAR(res, mat, 4 * (mat.size().rows() + 1) * TypeUtilities<TypeParam>::error,
+                        4 * (mat.size().rows() + 1) * TypeUtilities<TypeParam>::error);
+    }
+  }
+}
+
+TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
   using Type = TypeParam;
 
   auto el = [](const GlobalElementIndex& index) {
@@ -40,75 +92,61 @@ TYPED_TEST(CholeskyTest, Correctness) {
     return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
   };
 
+  // Check for square sizes
   for (const auto& size : square_sizes) {
     for (const auto& block_size : square_block_sizes) {
       Matrix<Type, Device::CPU> mat(size, block_size);
 
       set(mat, el);
 
-      EXPECT_NO_THROW(cholesky_local(mat));
+      EXPECT_NO_THROW(cholesky_local(mat, blas::Uplo::Lower, blas::Side::Right, blas::Op::ConjTrans,
+                                     blas::Diag::NonUnit));
+    }
+  }
+
+  // Check for rectangular sizes
+  for (const auto& size : rectangular_sizes) {
+    for (const auto& block_size : square_block_sizes) {
+      Matrix<Type, Device::CPU> mat(size, block_size);
+
+      EXPECT_THROW(cholesky_local(mat, blas::Uplo::Lower, blas::Side::Right, blas::Op::ConjTrans,
+                                  blas::Diag::NonUnit),
+                   std::invalid_argument);
     }
   }
 }
 
-  TYPED_TEST(CholeskyTest, NoSquareMatrixException) {
-    using Type = TypeParam;
+TYPED_TEST(CholeskyTest, NoSquareBlockException) {
+  using Type = TypeParam;
 
-    auto el = [](const GlobalElementIndex& index) {
-      SizeType i = index.row();
-      SizeType j = index.col();
-      return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
-    };
+  auto el = [](const GlobalElementIndex& index) {
+    SizeType i = index.row();
+    SizeType j = index.col();
+    return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
+  };
 
-    // Check for square sizes
-    for (const auto& size : square_sizes) {
-      for (const auto& block_size : square_block_sizes) {
-        Matrix<Type, Device::CPU> mat(size, block_size);
+  // Check for square sizes
+  for (const auto& size : square_sizes) {
+    for (const auto& block_size : square_block_sizes) {
+      Matrix<Type, Device::CPU> mat(size, block_size);
 
-        set(mat, el);
+      set(mat, el);
 
-        EXPECT_NO_THROW(cholesky_local(mat));
-      }
-    }
-
-    // Check for rectangular sizes
-    for (const auto& size : rectangular_sizes) {
-      for (const auto& block_size : square_block_sizes) {
-        Matrix<Type, Device::CPU> mat(size, block_size);
-
-        EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
-      }
+      EXPECT_NO_THROW(cholesky_local(mat, blas::Uplo::Lower, blas::Side::Right, blas::Op::ConjTrans,
+                                     blas::Diag::NonUnit));
     }
   }
 
-  TYPED_TEST(CholeskyTest, NoSquareBlockException) {
-    using Type = TypeParam;
+  // Check for rectangular sizes
+  for (const auto& size : square_sizes) {
+    for (const auto& block_size : rectangular_block_sizes) {
+      Matrix<Type, Device::CPU> mat(size, block_size);
 
-    auto el = [](const GlobalElementIndex& index) {
-      SizeType i = index.row();
-      SizeType j = index.col();
-      return TypeUtilities<Type>::element(i + 0.001 * j, j - 0.01 * i);
-    };
-
-    // Check for square sizes
-    for (const auto& size : square_sizes) {
-      for (const auto& block_size : square_block_sizes) {
-        Matrix<Type, Device::CPU> mat(size, block_size);
-
-        set(mat, el);
-
-        EXPECT_NO_THROW(cholesky_local(mat));
-      }
-    }
-
-    // Check for rectangular sizes
-    for (const auto& size : square_sizes) {
-      for (const auto& block_size : rectangular_block_sizes) {
-        Matrix<Type, Device::CPU> mat(size, block_size);
-
-        EXPECT_THROW(cholesky_local(mat), std::invalid_argument);
-      }
+      EXPECT_THROW(cholesky_local(mat, blas::Uplo::Lower, blas::Side::Right, blas::Op::ConjTrans,
+                                  blas::Diag::NonUnit),
+                   std::invalid_argument);
     }
   }
+}
 
-  TYPED_TEST(CholeskyTest, NonPositiveDefiniteException) {}
+TYPED_TEST(CholeskyTest, NonPositiveDefiniteException) {}

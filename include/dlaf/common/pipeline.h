578fcca1f904e96a80a7e13e9354ad00910fbce6
albestro <9337627+albestro@users.noreply.github.com>
2019-12-04 11:08:51 +0100

    Implementation of Pipeline object (#95)
    Pipeline is initialized by moving in an object on which it creates a ready future and the only method it exposes is the call operator Pipeline<T>::operator()() that returns an hpx::future.
    
    Internally, at every call, it chains all the futures, so that they will become ready in a serialized and ordered way. In this chain, the object given at initialization time is moved (i.e. std::move) from future to future as soon as they get ready, by means of the Wrapper object.
    The Wrapper object is used to contain the real object (the moved one) and to make ready the next future as soon as it is destroyed. A reference to the object can be accessed through the call operatorWrapper<T>::operator()().

---
+++ include/dlaf/common/pipeline.h
@@ -0,0 +1,80 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#include <hpx/lcos/future.hpp>
+#include <hpx/lcos/promise.hpp>
+
+namespace dlaf {
+namespace common {
+
+template <class T>
+struct Pipeline {
+public:
+  template <class U>
+  class Wrapper {
+    friend class Pipeline<U>;
+
+    Wrapper(U&& object, hpx::promise<T> next)
+        : object_(std::move(object)), promise_(std::move(next)), valid_(true) {}
+
+  public:
+    Wrapper(Wrapper&& rhs) : object_(std::move(rhs.object_)), promise_(std::move(rhs.promise_)) {
+      std::swap(valid_, rhs.valid_);
+    }
+
+    ~Wrapper() {
+      if (valid_)
+        promise_.set_value(std::move(object_));
+    }
+
+    U& operator()() {
+      return object_;
+    }
+
+    const U& operator()() const {
+      return object_;
+    }
+
+  private:
+    bool valid_ = false;
+    U object_;
+    hpx::promise<U> promise_;
+  };
+
+  Pipeline(T&& object) {
+    future_ = hpx::make_ready_future(std::move(object));
+  }
+
+  ~Pipeline() {
+    if (future_.valid())
+      future_.get();
+  }
+
+  hpx::future<Wrapper<T>> operator()() {
+    auto before_last = std::move(future_);
+
+    hpx::promise<T> promise_next;
+    future_ = promise_next.get_future();
+
+    return before_last.then(hpx::launch::sync,
+                            hpx::util::unwrapping(
+                                [promise_next = std::move(promise_next)](T&& object) mutable {
+                                  return Wrapper<T>{std::move(object), std::move(promise_next)};
+                                }));
+  }
+
+private:
+  hpx::future<T> future_;
+};
+
+}
+}
---
+++ test/unit/common/test_pipeline.cpp
@@ -0,0 +1,54 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#include "dlaf/common/pipeline.h"
+
+#include <gtest/gtest.h>
+
+#include <hpx/lcos/future.hpp>
+
+using namespace dlaf;
+using dlaf::common::Pipeline;
+
+TEST(Pipeline, Basic) {
+  Pipeline<int> serial(26);
+
+  auto checkpoint0 = serial();
+  auto checkpoint1 =
+      checkpoint0.then(hpx::launch::sync,
+                       hpx::util::unwrapping([](auto&& wrapper) { return std::move(wrapper); }));
+
+  auto guard0 = serial();
+  auto guard1 = serial();
+
+  EXPECT_TRUE(checkpoint1.is_ready());
+  EXPECT_FALSE(guard0.is_ready());
+  EXPECT_FALSE(guard1.is_ready());
+
+  checkpoint1.get();
+
+  EXPECT_TRUE(guard0.is_ready());
+  EXPECT_FALSE(guard1.is_ready());
+
+  guard0.get();
+
+  EXPECT_TRUE(guard1.is_ready());
+
+  guard1.get();
+}
+
+TEST(Pipeline, DestructionNoDependency) {
+  Pipeline<int> serial(13);
+}
+
+TEST(Pipeline, DestructionWithDependency) {
+  Pipeline<int> serial(26);
+  auto checkpoint = serial();
+}

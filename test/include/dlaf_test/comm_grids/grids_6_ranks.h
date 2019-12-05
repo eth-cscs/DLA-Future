431b4b415b66fc18fa76482c1df49587df37a5b0
Raffaele Solcà <rasolca@cscs.ch>
2019-12-05 17:46:04 +0100

    Fix lenght of matrix test

--- test/include/dlaf_test/comm_grids/grids_6_ranks.h
+++ test/include/dlaf_test/comm_grids/grids_6_ranks.h
@@ -25,7 +25,6 @@ public:
   virtual void SetUp() override {
     if (comm_grids.empty()) {
       Communicator world(MPI_COMM_WORLD);
-      comm_grids.emplace_back(world, 3, 2, common::Ordering::ColumnMajor);
       comm_grids.emplace_back(world, 3, 2, common::Ordering::RowMajor);
       comm_grids.emplace_back(world, 2, 3, common::Ordering::ColumnMajor);
 

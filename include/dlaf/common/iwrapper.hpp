106a976d06be18b8668cbcc1381630106f3b2011
Alberto Invernizzi <invernizzi@cscs.ch>
2019-12-05 13:45:00 +0100

    add license

--- include/dlaf/common/iwrapper.hpp
+++ include/dlaf/common/iwrapper.hpp
@@ -1,3 +1,13 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
 #pragma once
 
 #include <hpx/lcos/promise.hpp>

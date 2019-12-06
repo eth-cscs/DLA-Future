549dc5a8e2e3ad3bae4a65ec44e78c75a5344dfe
albestro <9337627+albestro@users.noreply.github.com>
2019-12-06 15:01:13 +0100

    Pipeline Object (#108) (#95)
    Pipeline is initialized by moving in an object on which it creates a ready future and the only method it exposes is the call operator Pipeline<T>::operator()() that returns an hpx::future.
    
    Internally, at every call, it chains all the futures, so that they will become ready in a serialized and ordered way. In this chain, the object given at initialization time is moved (i.e. std::move) from future to future as soon as they get ready, by means of the Wrapper object.
    The Wrapper object is used to contain the real object (the moved one) and to make ready the next future as soon as it is destroyed. A reference to the object can be accessed through the call operatorWrapper<T>::operator()().

---
+++ include/dlaf/common/pipeline.h
@@ -0,0 +1,97 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#include <hpx/lcos/future.hpp>
+#include <hpx/lcos/promise.hpp>
+
+namespace dlaf {
+namespace common {
+
+/// Pipeline takes ownership of a given object and manages the access to this resource by serializing
+/// calls. Anyone that requires access to the underlying resource will get an hpx::future, which is the
+/// way to register to the queue. All requests are serialized and served in the same order they arrive.
+///
+/// The mechanism for auto-releasing the resource and passing it to the next user works thanks to the
+/// internal Wrapper object. This Wrapper contains the real resource, and it will do what is needed to
+/// unlock the next user as soon as the Wrapper is destroyed.
+template <class T>
+struct Pipeline {
+public:
+  /// Wrapper is the object that manages the auto-release mechanism
+  template <class U>
+  class Wrapper {
+    friend class Pipeline<U>;
+
+    /// Create a wrapper
+    /// @param object	the resource to wrap (the wrapper becomes the owner of the resource)
+    /// @param next	the promise that has to be set on destruction
+    Wrapper(U&& object, hpx::promise<T> next) : object_(std::move(object)), promise_(std::move(next)) {}
+
+  public:
+    /// Trivial move constructor (that invalidates the status of the source object)
+    Wrapper(Wrapper&& rhs) = default;
+
+    /// This is where the "magic" happens!
+    ///
+    /// If the wrapper is still valid, set the promise to unlock the next future
+    ~Wrapper() {
+      if (promise_.valid())
+        promise_.set_value(std::move(object_));
+    }
+
+    /// Get a reference to the internal object
+    U& operator()() {
+      return object_;
+    }
+
+    /// Get a reference to the internal object
+    const U& operator()() const {
+      return object_;
+    }
+
+  private:
+    U object_;                 ///< the wrapped object! it is actually owned by the wrapper
+    hpx::promise<U> promise_;  ///< promise containing the shared state that will unlock the next user
+  };
+
+  /// Create a Pipeline by moving in the resource (it takes the ownership)
+  Pipeline(T&& object) {
+    future_ = hpx::make_ready_future(std::move(object));
+  }
+
+  /// On destruction it waits that all users have finished using it
+  ~Pipeline() {
+    if (future_.valid())
+      future_.get();
+  }
+
+  /// Enqueue for the resource
+  /// @return a future that will become ready as soon as the previous user release the resource
+  hpx::future<Wrapper<T>> operator()() {
+    auto before_last = std::move(future_);
+
+    hpx::promise<T> promise_next;
+    future_ = promise_next.get_future();
+
+    return before_last.then(hpx::launch::sync,
+                            hpx::util::unwrapping(
+                                [promise_next = std::move(promise_next)](T&& object) mutable {
+                                  return Wrapper<T>{std::move(object), std::move(promise_next)};
+                                }));
+  }
+
+private:
+  hpx::future<T> future_;  ///< This contains always the "tail" of the queue of futures
+};
+
+}
+}

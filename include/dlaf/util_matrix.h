65c54e0b77581433820b253e2946d9933ae3aed8
Sely85 <44648667+Sely85@users.noreply.github.com>
2019-12-18 18:45:03 +0100

    Distributed Cholesky(#114)
    Added distributed Cholesky implementation.

--- include/dlaf/util_matrix.h
+++ include/dlaf/util_matrix.h
@@ -22,8 +22,7 @@ namespace util_matrix {
 /// @tparam Matrix refers to a dlaf::Matrix object
 /// @throws std::invalid_argument if the matrix is not squared
 template <class Matrix>
-void assert_size_square(const Matrix& matrix, std::string function, std::string mat_name) {
-  assert(matrix.size().isValid());
+void assertSizeSquare(const Matrix& matrix, std::string function, std::string mat_name) {
   if (matrix.size().rows() != matrix.size().cols())
     throw std::invalid_argument(function + ": " + "Matrix " + mat_name + " is not square.");
 }
@@ -33,11 +32,34 @@ void assert_size_square(const Matrix& matrix, std::string function, std::string 
 /// @tparam Matrix refers to a dlaf::Matrix object
 /// @throws std::invalid_argument if the matrix block is not squared
 template <class Matrix>
-void assert_blocksize_square(const Matrix& matrix, std::string function, std::string mat_name) {
-  assert(matrix.size().isValid());
+void assertBlocksizeSquare(const Matrix& matrix, std::string function, std::string mat_name) {
   if (matrix.blockSize().rows() != matrix.blockSize().cols())
     throw std::invalid_argument(function + ": " + "Block size in matrix " + mat_name +
                                 " is not square.");
+}
+
+/// @brief Verify if dlaf::Matrix is distributed on a (1x1) grid (i.e. if it is a local matrix).
+///
+/// @tparam Matrix refers to a dlaf::Matrix object
+/// @throws std::invalid_argument if the matrix is not local
+template <class Matrix>
+void assertLocalMatrix(const Matrix& matrix, std::string function, std::string mat_name) {
+  if (matrix.distribution().commGridSize() != comm::Size2D{1, 1})
+    throw std::invalid_argument(function + ": " + "Matrix " + mat_name + " is not local.");
+}
+
+/// @brief Verify that the matrix is distributed according to the given communicator grid.
+///
+/// @tparam Matrix refers to a dlaf::Matrix object
+/// @throws std::invalid_argument if the matrix is not distributed correctly
+template <class Matrix>
+void assertMatrixDistributedOnGrid(const comm::CommunicatorGrid& grid, const Matrix& matrix,
+                                   std::string function, std::string mat_name, std::string grid_name) {
+  if ((matrix.distribution().commGridSize() != grid.size()) &&
+      (matrix.distribution().rankIndex() != grid.rank()))
+    throw std::invalid_argument(function + ": " + "The matrix " + mat_name +
+                                " is not distributed according to the communicator grid " + grid_name +
+                                ".");
 }
 
 }

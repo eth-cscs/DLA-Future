5ecd277f60093c1baa3fe6b2a938f75d45534703
lara <lara.querciagrossa@gmail.com>
2020-03-02 17:39:50 +0100

    Expanded comments on functions in util_generic_blas.h file

--- test/include/dlaf_test/matrix/util_generic_blas.h
+++ test/include/dlaf_test/matrix/util_generic_blas.h
@@ -17,29 +17,39 @@
 
 #include "dlaf_test/util_types.h"
 
+/// @file
+
 namespace dlaf {
 namespace matrix {
 namespace test {
 
 using namespace dlaf_test;
 
-/// @brief Returns el_op_a, el_b, res_b for side = Left.
+/// Returns elements of the three matrices, solving the triangular matrix equation
+/// op(A) B = X (with A on side == Left).
+///
+/// The tile elements of A matrix (el_op_a) are chosen such that:
+///   op(A)_ik = (i+1) / (k+.5) * exp(I*(2*i-k)) for the referenced elements
+///   op(A)_ik = -9.9 otherwise.
+///
+/// The elements of B (el_b) should be:
+/// B_ij = (Sum_k op(A)_ik * res_Xkj) / alpha
+///      = (op(A)_ii * res_Xij + (kk-1) * gamma) / alpha,
+/// where gamma = (i+1) / (j+2) * exp(I*(2*i+j)),
+///       kk = i+1 if op(a) is an lower triangular matrix, or
+///       kk = m-i if op(a) is an lower triangular matrix.
+/// Therefore
+/// B_ij = (res_Xij + (kk-1) * gamma) / alpha, if diag == Unit
+/// B_ij = kk * gamma / alpha, otherwise.
+///
+/// Finally, the X matrix elements (res_b) are computed as
+///   res_Xkj = (k+.5) / (j+2) * exp(I*(k+j)),
+/// where I = 0 for real types or I is the complex unit for complex types.
+/// These data are typically used to check whether the result of the equation
+/// performed with any algorithm is consistent with the computed values.
+///
 template <class ElementIndex, class T>
 auto getLeftTriangularSystem(blas::Uplo uplo, blas::Op op, blas::Diag diag, T alpha, SizeType m) {
-  // Note: The tile elements are chosen such that:
-  // - op(a)_ik = (i+1) / (k+.5) * exp(I*(2*i-k)) for the referenced elements
-  //   op(a)_ik = -9.9 otherwise,
-  // - res_kj = (k+.5) / (j+2) * exp(I*(k+j)),
-  // where I = 0 for real types or I is the complex unit for complex types.
-  // Therefore the elements of b should be:
-  // b_ij = (Sum_k op(a)_ik * res_kj) / alpha
-  //      = (op(a)_ii * res_ij + (kk-1) * gamma) / alpha,
-  // where gamma = (i+1) / (j+2) * exp(I*(2*i+j)),
-  //       kk = i+1 if op(a) is an lower triangular matrix, or
-  //       kk = m-i if op(a) is an lower triangular matrix.
-  // Therefore
-  // b_ij = (res_ij + (kk-1) * gamma) / alpha, if diag == Unit
-  // b_ij = kk * gamma / alpha, otherwise.
   bool op_a_lower = false;
   if ((uplo == blas::Uplo::Lower && op == blas::Op::NoTrans) ||
       (uplo == blas::Uplo::Upper && op != blas::Op::NoTrans))
@@ -79,24 +89,31 @@ auto getLeftTriangularSystem(blas::Uplo uplo, blas::Op op, blas::Diag diag, T al
   return std::make_tuple(el_op_a, el_b, res_b);
 }
 
-/// @brief Returns el_op_a, el_b, res_b for side = Right.
+/// Returns elements of the three matrices, solving the triangular matrix equation
+/// B op(A) = X (with A on side == right)
+///
+/// The tile elements of A matrix (el_op_a) are chosen such that:
+///   op(A)_kj = (j+1) / (k+.5) * exp(I*(2*j-k)) for the referenced elements
+///   op(A)_kj = -9.9 otherwise.
+///
+/// The elements of B (el_b) should be:
+/// B_ij = (Sum_k res_Xik * op(A)_kj) / alpha
+///      = (res_Xij * op(A)_jj + (kk-1) * gamma) / alpha,
+/// where gamma = (j+1) / (i+2) * exp(I*(i+2*j)),
+///       kk = j+1 if op(a) is an upper triangular matrix, or
+///       kk = m-j if op(a) is an upper triangular matrix.
+/// Therefore
+/// B_ij = (res_Xij + (kk-1) * gamma) / alpha, if diag == Unit
+/// B_ij = kk * gamma / alpha, otherwise.
+///
+/// Finally, the X matrix elements (res_b) are computed as
+///   res_Xik = (k+.5) / (i+2) * exp(I*(i+k)),
+/// where I = 0 for real types or I is the complex unit for complex types.
+/// These data are typically used to check whether the result of the equation
+/// performed with any algorithm is consistent with the computed values.
+///
 template <class ElementIndex, class T>
 auto getRightTriangularSystem(blas::Uplo uplo, blas::Op op, blas::Diag diag, T alpha, SizeType n) {
-  // Note: The tile elements are chosen such that:
-  // - res_ik = (k+.5) / (i+2) * exp(I*(i+k)),
-  // - op(a)_kj = (j+1) / (k+.5) * exp(I*(2*j-k)) for the referenced elements
-  //   op(a)_kj = -9.9 otherwise,
-  // where I = 0 for real types or I is the complex unit for complex types.
-  // Therefore the elements of b should be:
-  // b_ij = (Sum_k res_ik * op(a)_kj) / alpha
-  //      = (res_ij * op(a)_jj + (kk-1) * gamma) / alpha,
-  // where gamma = (j+1) / (i+2) * exp(I*(i+2*j)),
-  //       kk = j+1 if op(a) is an upper triangular matrix, or
-  //       kk = m-j if op(a) is an upper triangular matrix.
-  // Therefore
-  // b_ij = (res_ij + (kk-1) * gamma) / alpha, if diag == Unit
-  // b_ij = kk * gamma / alpha, otherwise.
-
   bool op_a_lower = false;
   if ((uplo == blas::Uplo::Lower && op == blas::Op::NoTrans) ||
       (uplo == blas::Uplo::Upper && op != blas::Op::NoTrans))

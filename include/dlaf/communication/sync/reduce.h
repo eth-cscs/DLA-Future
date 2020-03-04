449c878d435d66d28f83677db51ae4b4334b507d
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-04 09:00:26 +0100

    Basic implementation of reduce and test

--- include/dlaf/communication/sync/reduce.h
+++ include/dlaf/communication/sync/reduce.h
@@ -13,6 +13,7 @@
 /// @file
 
 #include <mpi.h>
+#include "dlaf/common/data_descriptor.h"
 #include "dlaf/communication/communicator.h"
 #include "dlaf/communication/message.h"
 
@@ -20,17 +21,82 @@ namespace dlaf {
 namespace comm {
 namespace sync {
 
-/// MPI_Reduce wrapper
+namespace internal {
+namespace reduce {
+
+/// @brief MPI_Reduce wrapper (sender side)
+/// MPI Reduce(see MPI documentation for additional info)
+/// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
+template <class DataIn, class DataOut>
+void collector(Communicator& communicator, MPI_Op reduce_operation, DataIn input, DataOut output) {
+  using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
+
+  common::BufferWithMemory<T> tmp_mem_input;
+  common::BufferWithMemory<T> tmp_mem_output;
+
+  common::DataDescriptor<const T> tmp_in = input;
+  common::DataDescriptor<T> tmp_out = output;
+
+  // if input is not contiguous, copy it in a contiguous temporary buffer
+  if (!input.is_contiguous()) {
+    tmp_in = tmp_mem_input = common::create_temporary_buffer(tmp_in);
+    common::copy(input, tmp_mem_input);
+  }
+
+  // if output is not contiguous, create an intermediate buffer
+  if (!output.is_contiguous())
+    tmp_out = tmp_mem_output = common::create_temporary_buffer(tmp_out);
+
+  auto&& message_input = comm::make_message(std::move(tmp_in));
+  auto&& message_output = comm::make_message(DataOut(tmp_out));
+
+  MPI_Reduce(message_input.data(), message_output.data(), message_input.count(),
+             message_input.mpi_type(), reduce_operation, communicator.rank(), communicator);
+
+  // if output was not contiguous, copy it back!
+  if (!output.is_contiguous())
+    common::copy(tmp_out, output);
+}
+
+/// @brief MPI_Reduce wrapper (receiver side)
 /// MPI Reduce(see MPI documentation for additional info)
 /// @param rank_root  the rank that will collect the result in output
 /// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
-template <class T>
-void reduce(int rank_root, Communicator& communicator, MPI_Op reduce_operation, Message<T>&& input,
-            Message<T>&& output) {
-  MPI_Reduce(input.data(), output.data(), input.count(), input.mpi_type(), reduce_operation, rank_root,
-             communicator);
+template <class DataIn>
+void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operation, DataIn input) {
+  using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
+
+  common::BufferWithMemory<T> tmp_mem_input;
+
+  common::DataDescriptor<const T> tmp_in = input;
+
+  // if input is not contiguous, copy it in a contiguous temporary buffer
+  if (!input.is_contiguous()) {
+    tmp_in = tmp_mem_input = common::create_temporary_buffer(tmp_in);
+    common::copy(input, tmp_mem_input);
+  }
+
+  auto&& message_input = comm::make_message(std::move(tmp_in));
+
+  MPI_Reduce(message_input.data(), nullptr, message_input.count(), message_input.mpi_type(),
+             reduce_operation, rank_root, communicator);
 }
 
+}
+}
+
+/// @brief MPI_Reduce wrapper
+/// MPI Reduce(see MPI documentation for additional info)
+/// @param rank_root  the rank that will collect the result in output
+/// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
+template <class DataIn, class DataOut>
+void reduce(const int rank_root, Communicator& communicator, MPI_Op reduce_operation, DataIn input,
+            DataOut output) {
+  if (rank_root == communicator.rank())
+    internal::reduce::collector(communicator, reduce_operation, input, output);
+  else
+    internal::reduce::participant(rank_root, communicator, reduce_operation, input);
+}
 }
 }
 }
--- test/unit/communication/test_reduce.cpp
+++ test/unit/communication/test_reduce.cpp
@@ -14,6 +14,7 @@
 #include <gtest/gtest.h>
 
 #include "dlaf_test/helper_communicators.h"
+#include "dlaf_test/util_types.h"
 
 using namespace dlaf;
 using namespace dlaf_test;
@@ -27,8 +28,7 @@ TEST_F(ReduceTest, Basic) {
   int value = 1;
   int result = 0;
 
-  sync::reduce(root, world, MPI_SUM, make_message(common::make_data(&value, 1)),
-               make_message(common::make_data(&result, 1)));
+  sync::reduce(root, world, MPI_SUM, common::make_data(&value, 1), common::make_data(&result, 1));
 
   // check that the root rank has the reduced results
   if (world.rank() == root) {
@@ -47,8 +47,7 @@ TEST_F(ReduceTest, Multiple) {
   int input[N] = {1, 2, 3};
   int reduced[N];
 
-  sync::reduce(root, world, MPI_SUM, make_message(common::make_data(input, N)),
-               make_message(common::make_data(reduced, N)));
+  sync::reduce(root, world, MPI_SUM, common::make_data(input, N), common::make_data(reduced, N));
 
   // check that the root rank has the reduced results
   if (world.rank() == root) {
@@ -59,4 +58,201 @@ TEST_F(ReduceTest, Multiple) {
   // check that input has not been modified
   for (std::size_t index = 0; index < N; ++index)
     EXPECT_EQ(index + 1, input[index]);
+}
+
+TEST_F(ReduceTest, ContiguousToContiguous) {
+  using TypeParam = int;
+
+  const int N = 10;
+  const TypeParam value = 13;
+
+  TypeParam data_A[N];
+  TypeParam data_B[N];
+
+  for (auto i = 0; i < N; ++i)
+    data_A[i] = value;
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(data_A, N);
+  auto&& message_output = common::make_data(data_B, N);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+
+  if (root == world.rank())
+    for (auto i = 0; i < N; ++i)
+      EXPECT_EQ(value * NUM_MPI_RANKS, data_B[i]);
+}
+
+TEST_F(ReduceTest, ConstContiguousToContiguous) {
+  using TypeParam = int;
+
+  const int N = 10;
+  const TypeParam value = 13;
+
+  TypeParam data_A[N];
+  const TypeParam* const_data_A = data_A;
+  TypeParam data_B[N];
+
+  for (auto i = 0; i < N; ++i)
+    data_A[i] = value;
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(const_data_A, N);
+  auto&& message_output = common::make_data(data_B, N);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+
+  if (root == world.rank())
+    for (auto i = 0; i < N; ++i)
+      EXPECT_EQ(value * NUM_MPI_RANKS, data_B[i]);
+}
+
+TEST_F(ReduceTest, StridedToContiguous) {
+  using TypeParam = int;
+
+  const TypeParam value = 13;
+
+  // 3 blocks, 2 elements each, with a distance of 5 elements between start of each block
+  // E E - - - E E - - - E E    (without padding at the end)
+  const std::size_t nblocks = 3;
+  const std::size_t block_size = 2;
+  const std::size_t block_distance = 5;
+
+  const std::size_t memory_footprint = (nblocks - 1) * block_distance + block_size;
+  TypeParam data_strided[memory_footprint];
+
+  const int N = nblocks * block_size;
+  TypeParam data_contiguous[N];
+
+  for (int i_block = 0; i_block < nblocks; ++i_block)
+    for (int i_element = 0; i_element < block_size; ++i_element) {
+      auto mem_pos = i_block * block_distance + i_element;
+      data_strided[mem_pos] = dlaf_test::TypeUtilities<TypeParam>::element(value, 0);
+    }
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(data_strided, nblocks, block_size, block_distance);
+  auto&& message_output = common::make_data(data_contiguous, N);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+
+  if (root == world.rank())
+    for (auto i = 0; i < N; ++i)
+      EXPECT_EQ(value * NUM_MPI_RANKS, data_contiguous[i]);
+}
+
+TEST_F(ReduceTest, ConstStridedToContiguous) {
+  using TypeParam = int;
+
+  const TypeParam value = 13;
+
+  // 3 blocks, 2 elements each, with a distance of 5 elements between start of each block
+  // E E - - - E E - - - E E    (without padding at the end)
+  const std::size_t nblocks = 3;
+  const std::size_t block_size = 2;
+  const std::size_t block_distance = 5;
+
+  const std::size_t memory_footprint = (nblocks - 1) * block_distance + block_size;
+  TypeParam data_strided[memory_footprint];
+  const TypeParam* const_data_strided = data_strided;
+
+  const int N = nblocks * block_size;
+  TypeParam data_contiguous[N];
+
+  for (int i_block = 0; i_block < nblocks; ++i_block)
+    for (int i_element = 0; i_element < block_size; ++i_element) {
+      auto mem_pos = i_block * block_distance + i_element;
+      data_strided[mem_pos] = dlaf_test::TypeUtilities<TypeParam>::element(value, 0);
+    }
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(const_data_strided, nblocks, block_size, block_distance);
+  auto&& message_output = common::make_data(data_contiguous, N);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+
+  if (root == world.rank())
+    for (auto i = 0; i < N; ++i)
+      EXPECT_EQ(value * NUM_MPI_RANKS, data_contiguous[i]);
+}
+
+TEST_F(ReduceTest, ContiguousToStrided) {
+  using TypeParam = int;
+
+  const TypeParam value = 13;
+
+  // 3 blocks, 2 elements each, with a distance of 5 elements between start of each block
+  // E E - - - E E - - - E E    (without padding at the end)
+  const std::size_t nblocks = 3;
+  const std::size_t block_size = 2;
+  const std::size_t block_distance = 5;
+
+  const std::size_t memory_footprint = (nblocks - 1) * block_distance + block_size;
+  TypeParam data_strided[memory_footprint];
+
+  const int N = nblocks * block_size;
+  TypeParam data_contiguous[N];
+
+  for (auto i = 0; i < N; ++i)
+    data_contiguous[i] = dlaf_test::TypeUtilities<TypeParam>::element(value, 0);
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(data_contiguous, N);
+  auto&& message_output = common::make_data(data_strided, nblocks, block_size, block_distance);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+  if (world.rank() == root) {
+    for (int i_block = 0; i_block < nblocks; ++i_block)
+      for (int i_element = 0; i_element < block_size; ++i_element) {
+        auto mem_pos = i_block * block_distance + i_element;
+        EXPECT_EQ(value * NUM_MPI_RANKS, data_strided[mem_pos]);
+      }
+  }
+}
+
+TEST_F(ReduceTest, ConstContiguousToStrided) {
+  using TypeParam = int;
+
+  const TypeParam value = 13;
+
+  // 3 blocks, 2 elements each, with a distance of 5 elements between start of each block
+  // E E - - - E E - - - E E    (without padding at the end)
+  const std::size_t nblocks = 3;
+  const std::size_t block_size = 2;
+  const std::size_t block_distance = 5;
+
+  const std::size_t memory_footprint = (nblocks - 1) * block_distance + block_size;
+  TypeParam data_strided[memory_footprint];
+
+  const int N = nblocks * block_size;
+  TypeParam data_contiguous[N];
+  const TypeParam* const_data_contiguous = data_contiguous;
+
+  for (auto i = 0; i < N; ++i)
+    data_contiguous[i] = dlaf_test::TypeUtilities<TypeParam>::element(value, 0);
+
+  const int root = 0;
+  auto& communicator = world;
+  auto&& message_input = common::make_data(const_data_contiguous, N);
+  auto&& message_output = common::make_data(data_strided, nblocks, block_size, block_distance);
+  MPI_Op op = MPI_SUM;
+
+  sync::reduce(root, communicator, op, std::move(message_input), std::move(message_output));
+  if (world.rank() == root) {
+    for (int i_block = 0; i_block < nblocks; ++i_block)
+      for (int i_element = 0; i_element < block_size; ++i_element) {
+        auto mem_pos = i_block * block_distance + i_element;
+        EXPECT_EQ(value * NUM_MPI_RANKS, data_strided[mem_pos]);
+      }
+  }
 }

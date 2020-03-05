8fb429467bd0e3bf6b261bab2c1d06da84ab8a0a
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-05 14:31:31 +0100

    first step towards compiler warnings

---
+++ cmake/DLAF_AddTargetWarnings.cmake
@@ -0,0 +1,27 @@
+#
+# Distributed Linear Algebra with Future (DLAF)
+#
+# Copyright (c) 2018-2019, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+macro(target_add_warnings target_name)
+  if(NOT TARGET ${target_name})
+    message(SEND_ERROR
+      "${target_name} is not a target."
+      "Cannot add warnings to it.")
+  endif()
+
+  target_compile_options(${target_name}
+    PRIVATE
+      -Wall
+      -Wextra
+      -Wnon-virtual-dtor
+      -Wunused
+      -Woverloaded-virtual
+      -Wconversion
+      -pedantic-errors)
+endmacro()

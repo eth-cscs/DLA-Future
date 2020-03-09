d7e328d30d059644729aee0097233be9821ca713
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-09 14:48:26 +0100

    fixes for cholesky miniapp

--- miniapp/miniapp_cholesky.cpp
+++ miniapp/miniapp_cholesky.cpp
@@ -27,10 +27,10 @@ using namespace dlaf;
 using T = double;
 
 struct options_t {
-  int64_t m;
-  int64_t mb;
-  int64_t grid_rows;
-  int64_t grid_cols;
+  int m;
+  int mb;
+  int grid_rows;
+  int grid_cols;
   int64_t nruns;
   bool do_check;
 };
@@ -99,8 +99,8 @@ int hpx_main(hpx::program_options::variables_map& vm) {
 
     double gigaflops;
     {
-      double n = matrix.size().rows();
-      double add_mul = n * n * n / 6;
+      auto n = static_cast<std::size_t>(matrix.size().rows());
+      auto add_mul = static_cast<std::size_t>(n * n * n / 6);
       gigaflops = total_ops<T>(add_mul, add_mul) / elapsed_time / 1e9;
     }
 
@@ -138,16 +138,16 @@ int main(int argc, char** argv) {
   // clang-format off
   desc_commandline.add_options()
     ("matrix-size",
-     value<int64_t>()->default_value(4096),
+     value<int>()->default_value(4096),
      "Matrix size.")
     ("block-size",
-     value<int64_t>()->default_value(256),
+     value<int>()->default_value(256),
      "Block cyclic distribution size.")
     ("grid-rows",
-     value<int64_t>()->default_value(1),
+     value<int>()->default_value(1),
      "Number of row processes in the 2D communicator.")
     ("grid-cols",
-     value<int64_t>()->default_value(1),
+     value<int>()->default_value(1),
      "Number of column processes in the 2D communicator.")
     ("nruns",
      value<int64_t>()->default_value(1),
@@ -182,15 +182,15 @@ int main(int argc, char** argv) {
 
 options_t check_options(hpx::program_options::variables_map& vm) {
   options_t opts = {
-      .m = vm["matrix-size"].as<int64_t>(),
-      .mb = vm["block-size"].as<int64_t>(),
+      vm["matrix-size"].as<int>(),
+      vm["block-size"].as<int>(),
 
-      .grid_rows = vm["grid-rows"].as<int64_t>(),
-      .grid_cols = vm["grid-cols"].as<int64_t>(),
+      vm["grid-rows"].as<int>(),
+      vm["grid-cols"].as<int>(),
 
-      .nruns = vm["nruns"].as<int64_t>(),
+      vm["nruns"].as<int64_t>(),
 
-      .do_check = vm["check-result"].as<bool>(),
+      vm["check-result"].as<bool>(),
   };
 
   if (opts.m <= 0)

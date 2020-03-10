5fe8f39dca56aed87e82f691245d1929cca00ebd
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-10 11:01:51 +0100

    trying to manage -Wconversion compiler differences

--- cmake/DLAF_AddTargetWarnings.cmake
+++ cmake/DLAF_AddTargetWarnings.cmake
@@ -15,6 +15,9 @@ macro(target_add_warnings target_name)
       "Cannot add warnings to it.")
   endif()
 
+  set(IS_COMPILER_CLANG $<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>)
+  set(IS_COMPILER_GCC $<CXX_COMPILER_ID:GCC>)
+
   target_compile_options(${target_name}
     PRIVATE
       -Wall
@@ -22,11 +25,29 @@ macro(target_add_warnings target_name)
       -Wnon-virtual-dtor
       -Wunused
       -Woverloaded-virtual
-      -Wconversion
+      # Conversions
+      $<${IS_COMPILER_GCC}:
+        -Wsign-conversion
+        -Wfloat-conversion>
+      $<${IS_COMPILER_CLANG}:
+        -Wbitfield-enum-conversion,
+        -Wbool-conversion,
+        -Wconstant-conversion,
+        -Wenum-conversion,
+        -Wfloat-conversion,
+        -Wimplicit-float-conversion,
+        -Wimplicit-int-conversion,
+        -Wint-conversion,
+        -Wliteral-conversion,
+        -Wnon-literal-null-conversion,
+        -Wnull-conversion,
+        -Wshorten-64-to-32,
+        -Wsign-conversion,
+        -Wstring-conversion>
       -pedantic-errors
 
       # googletest macro problem
       # must specify at least one argument for '...' parameter of variadic macro
-      $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:-Wno-gnu-zero-variadic-macro-arguments>
+      $<${IS_COMPILER_CLANG}:-Wno-gnu-zero-variadic-macro-arguments>
     )
 endmacro()

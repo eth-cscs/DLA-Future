3d9b66eb81d19dd9e8629026d5bf501787ec392f
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-27 18:27:52 +0100

    streaming macros test

--- include/dlaf/common/assert.h
+++ include/dlaf/common/assert.h
@@ -10,27 +10,52 @@
 
 #pragma once
 
-#include <cstdio>
+#include <exception>
+#include <iostream>
+#include <sstream>
 
-#define DLAF_TEST(condition, message, ...)                           \
-  if (!(condition)) {                                                \
-    std::fprintf(stderr, "%s:%d\n", DLAF_SOURCE_LOCATION, __LINE__); \
-    std::fprintf(stderr, "%s\n", #condition);                        \
-    std::fprintf(stderr, message, ##__VA_ARGS__);                    \
-    std::fprintf(stderr, "%s\n", "");                                \
-    std::terminate();                                                \
+namespace dlaf {
+namespace common {
+namespace internal {
+
+struct assert_helper {
+  assert_helper(const char* category, const char* condition, const char* location, int line) {
+    message_ << "failed " << category << " " << condition << " at " << location << ":" << line
+             << std::endl;
   }
 
+  ~assert_helper() {
+    std::cerr << message_.str() << std::endl;
+    std::terminate();
+  }
+
+  template <class T>
+  assert_helper& operator<<(const T& part) {
+    message_ << part;
+    return *this;
+  }
+
+  std::stringstream message_;
+};
+
+}
+}
+}
+
+#define DLAF_TEST(category, condition) \
+  if (!(condition))                    \
+  dlaf::common::internal::assert_helper(category, #condition, DLAF_SOURCE_LOCATION, __LINE__)
+
 #ifndef NDEBUG
-#define DLAF_ASSERT(condition, message, ...) DLAF_TEST(condition, message, ##__VA_ARGS__)
+#define DLAF_ASSERT(condition) DLAF_TEST("assert", condition)
 #else
-#define DLAF_ASSERT(condition, message, ...)
+#define DLAF_ASSERT(condition) DLAF_TEST("none", true)
 #endif
 
 #ifdef DLAF_ENABLE_DEVASSERT
-#define DLAF_DEVASSERT(condition, message, ...) DLAF_TEST(condition, message, ##__VA_ARGS__)
+#define DLAF_DEVASSERT(condition) DLAF_TEST("dev assert", condition)
 #else
-#define DLAF_DEVASSERT(condition, message, ...)
+#define DLAF_DEVASSERT(condition) DLAF_TEST("none", true)
 #endif
 
-#define DLAF_PRECONDITION(condition, message, ...) DLAF_TEST(condition, message, ##__VA_ARGS__)
+#define DLAF_PRECONDITION(condition) DLAF_TEST("precondition", condition)

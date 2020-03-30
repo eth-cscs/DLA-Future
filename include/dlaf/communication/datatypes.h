e2ca1fa18140b455e81a79fa305e014b0f978868
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-03-30 09:27:48 +0200

    Improve formatting

--- include/dlaf/communication/datatypes.h
+++ include/dlaf/communication/datatypes.h
@@ -28,21 +28,21 @@ template <typename T>
 struct mpi_datatype<const T> : public mpi_datatype<T> {};
 
 // clang-format off
-template<> MPI_Datatype mpi_datatype<char>::type() { return MPI_CHAR; }
-template<> MPI_Datatype mpi_datatype<short>::type() { return MPI_SHORT; }
-template<> MPI_Datatype mpi_datatype<int>::type() { return MPI_INT; }
-template<> MPI_Datatype mpi_datatype<long>::type() { return MPI_LONG; }
-template<> MPI_Datatype mpi_datatype<long long>::type() { return MPI_LONG_LONG; }
-template<> MPI_Datatype mpi_datatype<unsigned char>::type() { return MPI_UNSIGNED_CHAR; }
-template<> MPI_Datatype mpi_datatype<unsigned short>::type() { return MPI_UNSIGNED_SHORT; }
-template<> MPI_Datatype mpi_datatype<unsigned int>::type() { return MPI_UNSIGNED; }
-template<> MPI_Datatype mpi_datatype<unsigned long>::type() { return MPI_UNSIGNED_LONG; }
-template<> MPI_Datatype mpi_datatype<unsigned long long>::type() { return MPI_UNSIGNED_LONG_LONG; }
-template<> MPI_Datatype mpi_datatype<float>::type() { return MPI_FLOAT; };
-template<> MPI_Datatype mpi_datatype<double>::type() { return MPI_DOUBLE; };
-template<> MPI_Datatype mpi_datatype<bool>::type() { return MPI_CXX_BOOL; };
-template<> MPI_Datatype mpi_datatype<std::complex<float>>::type() { return MPI_CXX_FLOAT_COMPLEX; }
-template<> MPI_Datatype mpi_datatype<std::complex<double>>::type() { return MPI_CXX_DOUBLE_COMPLEX; }
+template<> MPI_Datatype mpi_datatype<char>                 ::type() { return MPI_CHAR;               }
+template<> MPI_Datatype mpi_datatype<short>                ::type() { return MPI_SHORT;              }
+template<> MPI_Datatype mpi_datatype<int>                  ::type() { return MPI_INT;                }
+template<> MPI_Datatype mpi_datatype<long>                 ::type() { return MPI_LONG;               }
+template<> MPI_Datatype mpi_datatype<long long>            ::type() { return MPI_LONG_LONG;          }
+template<> MPI_Datatype mpi_datatype<unsigned char>        ::type() { return MPI_UNSIGNED_CHAR;      }
+template<> MPI_Datatype mpi_datatype<unsigned short>       ::type() { return MPI_UNSIGNED_SHORT;     }
+template<> MPI_Datatype mpi_datatype<unsigned int>         ::type() { return MPI_UNSIGNED;           }
+template<> MPI_Datatype mpi_datatype<unsigned long>        ::type() { return MPI_UNSIGNED_LONG;      }
+template<> MPI_Datatype mpi_datatype<unsigned long long>   ::type() { return MPI_UNSIGNED_LONG_LONG; }
+template<> MPI_Datatype mpi_datatype<float>                ::type() { return MPI_FLOAT;              }
+template<> MPI_Datatype mpi_datatype<double>               ::type() { return MPI_DOUBLE;             }
+template<> MPI_Datatype mpi_datatype<bool>                 ::type() { return MPI_CXX_BOOL;           }
+template<> MPI_Datatype mpi_datatype<std::complex<float>>  ::type() { return MPI_CXX_FLOAT_COMPLEX;  }
+template<> MPI_Datatype mpi_datatype<std::complex<double>> ::type() { return MPI_CXX_DOUBLE_COMPLEX; }
 // clang-format on
 
 }

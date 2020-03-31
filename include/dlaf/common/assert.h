833e0eb9d57fb6a9c9e1074272e97d086e34c6e8
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-31 09:39:47 +0200

    open to dlaf tests without message

--- include/dlaf/common/assert.h
+++ include/dlaf/common/assert.h
@@ -13,24 +13,26 @@
 #include <exception>
 #include <iostream>
 
-#define DLAF_TEST(category, condition, message)                                                 \
-  if (!(condition)) {                                                                           \
-    std::cerr << "failed " category " " #condition << '\n' "at " << DLAF_SOURCE_LOCATION << ":" \
-              << __LINE__ << '\n'                                                               \
-              << message << std::endl;                                                          \
-    std::terminate();                                                                           \
+#include "dlaf/common/utils.h"
+
+#define DLAF_TEST(category, condition, ...)                                                   \
+  if (!(condition)) {                                                                         \
+    std::cerr << "failed " category " " #condition << "\n at " << DLAF_SOURCE_LOCATION << ":" \
+              << __LINE__ << '\n'                                                             \
+              << dlaf::common::concat(__VA_ARGS__) << std::endl;                              \
+    std::terminate();                                                                         \
   }
 
 #ifndef NDEBUG
-#define DLAF_ASSERT(condition, message) DLAF_TEST("assertion", condition, message)
+#define DLAF_ASSERT(condition, ...) DLAF_TEST("assertion", condition, __VA_ARGS__)
 #else
-#define DLAF_ASSERT(condition, message)
+#define DLAF_ASSERT(condition, ...)
 #endif
 
 #ifdef DLAF_ENABLE_DEVASSERT
-#define DLAF_DEVASSERT(condition, message) DLAF_TEST("dev assert", condition, message)
+#define DLAF_DEVASSERT(condition, ...) DLAF_TEST("dev assert", condition, __VA_ARGS__)
 #else
-#define DLAF_DEVASSERT(condition, message)
+#define DLAF_DEVASSERT(condition, ...)
 #endif
 
-#define DLAF_PRECONDITION(condition, message) DLAF_TEST("precondition", condition, message)
+#define DLAF_PRECONDITION(condition, ...) DLAF_TEST("precondition", condition, __VA_ARGS__)
---
+++ include/dlaf/common/utils.h
@@ -0,0 +1,31 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#include <string>
+#include <sstream>
+
+namespace dlaf {
+namespace common {
+
+std::string concat() {
+    return "";
+}
+
+template <class T, class ...Ts>
+std::string concat(T&& first, Ts&& ... args) {
+    std::stringstream ss;
+    ss << first << " " << concat(std::forward<Ts>(args)...);
+    return ss.str();
+}
+
+}
+}

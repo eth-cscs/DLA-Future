608b4b0d1ea9273fe76424781c059bf286b198f6
Alberto Invernizzi <invernizzi@cscs.ch>
2020-04-03 18:00:20 +0200

    add basic doc for dlaf::common::concat

--- include/dlaf/common/utils.h
+++ include/dlaf/common/utils.h
@@ -46,10 +46,17 @@ struct source_location {
 
 }
 
+/// Return an empty string
+///
+/// This is just the fundamental step of the recursive algorithm
 inline std::string concat() {
   return "";
 }
 
+/// Join a list of heterogenous parameters into a string
+///
+/// Given a list of parameters for which a valid std::ostream& operator<<(std::ostream&, const T&) exists,
+/// it returns a std::string with all parameters joined using a space between each one
 template <class T, class... Ts>
 std::string concat(const T&& first, const Ts&&... args) {
   std::ostringstream ss;

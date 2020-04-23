08d7813b3c20bfd0679becb5c8a942517cf4147c
Raffaele Solcà <rasolca@cscs.ch>
2020-04-23 17:34:43 +0200

    Updated sarusify.sh to allow to run all the tests even in case of failure.

--- ci/sarusify.sh
+++ ci/sarusify.sh
@@ -23,11 +23,29 @@ while IFS= read -r TEST_COMMAND; do
         # replace absolute paths with basenames
         if [[ $var == /root/DLA-Future-build/* ]]; then
             TEST_EXECUTABLES="$var"$'\n'"$TEST_EXECUTABLES"
-            FILTERED_COMMAND="$FILTERED_COMMAND\"$(basename "$var")\" "
+            EXECUTABLE=$(basename "$var")
+            FILTERED_COMMAND="$FILTERED_COMMAND\"$EXECUTABLE\" "
         else
             FILTERED_COMMAND="$FILTERED_COMMAND$arg "
         fi
     done
 
+    # Add check if test passed. Otherwise add it to the list of failed tests.
+    FILTERED_COMMAND="$FILTERED_COMMAND"$'\n'"if [ $? -ne 0 ]; then FAILED=\${FAILED}\"$EXECUTABLE \"; fi"
+
     SARUS_TEST_COMMANDS="$FILTERED_COMMAND"$'\n'"$SARUS_TEST_COMMANDS"
 done <<< "$TEST_COMMANDS"
+
+REPORT_COMMAND="if [[ -z \$FAILED ]]
+then
+ echo \"Test Succeded\"
+ exit(0)
+else
+  for TEST in \$FAILED
+  do
+    printf "Test %s FAILED\n" \$TEST
+  done
+  exit(1)
+fi"
+
+SARUS_TEST_COMMANDS="$SARUS_TEST_COMMANDS"$'\n'"$REPORT_COMMAND"

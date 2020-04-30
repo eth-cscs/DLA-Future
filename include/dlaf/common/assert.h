6a159e87754289957490e6062e6f12652b8a0c32
Alberto Invernizzi <invernizzi@cscs.ch>
2020-04-30 12:22:23 +0200

    workaround: use newline as parameter of the variadic parameter

--- include/dlaf/common/assert.h
+++ include/dlaf/common/assert.h
@@ -24,17 +24,17 @@
 /// Message composition is lazily evaluated and it will not add any overhead if the condition is true.
 ///
 /// **This check cannot be disabled**
-#define DLAF_CHECK_WITH_ORIGIN(category, origin, condition, ...)                    \
-  if (!(condition)) {                                                               \
-    std::cerr << "[ERROR] " << origin << std::endl                                  \
-              << dlaf::common::concat(#condition, ' ', ##__VA_ARGS__) << std::endl; \
-    std::terminate();                                                               \
+#define DLAF_CHECK_WITH_ORIGIN(category, origin, condition, ...)     \
+  if (!(condition)) {                                                \
+    std::cerr << "[ERROR] " << origin << std::endl                   \
+              << dlaf::common::concat(#condition, ' ', __VA_ARGS__); \
+    std::terminate();                                                \
   }
 
 /// This macro is a shortcut for #DLAF_CHECK_WITH_ORIGIN, it sets automatically the origin to the line
 /// from where this macro is used
-#define DLAF_CHECK(category, condition, ...) \
-  DLAF_CHECK_WITH_ORIGIN(category, (SOURCE_LOCATION()), condition, ##__VA_ARGS__)
+#define DLAF_CHECK(category, ...) \
+  DLAF_CHECK_WITH_ORIGIN(category, (SOURCE_LOCATION()), __VA_ARGS__, '\n')
 
 #ifdef DLAF_ASSERT_HEAVY_ENABLE
 /// **THIS MACRO MUST BE USED WHEN THE CHECK IS NEEDED FOR DEBUGGING PURPOSES THAT HAS
@@ -47,7 +47,7 @@
 ///
 /// If the switch **DLAF_ASSERT_HEAVY_ENABLE** is not defined, this check will not
 /// be performed and it will not add any overhead, nor for the condition evaluation, nor for the message
-#define DLAF_ASSERT_HEAVY(...) DLAF_CHECK("HEAVY", __VA_ARGS__)
+#define DLAF_ASSERT_HEAVY(...) DLAF_CHECK("HEAVY", __VA_ARGS__, '\n')
 #else
 #define DLAF_ASSERT_HEAVY(...)
 #endif
@@ -67,7 +67,7 @@
 ///
 /// If the switch **DLAF_ASSERT_MODERATE_ENABLE** is not defined, this check
 /// will not be performed and it will not add any overhead, nor for the condition evaluation, nor for the message
-#define DLAF_ASSERT_MODERATE(...) DLAF_CHECK("MODERATE", __VA_ARGS__)
+#define DLAF_ASSERT_MODERATE(...) DLAF_CHECK("MODERATE", __VA_ARGS__, '\n')
 #else
 #define DLAF_ASSERT_MODERATE(...)
 #endif
@@ -87,7 +87,7 @@
 ///
 /// If the switch **DLAF_ASSERT_ENABLE** is not defined, this check will not be performed and it will not
 /// add any overhead, nor for the condition evaluation, nor for the message
-#define DLAF_ASSERT_WITH_ORIGIN(origin, ...) DLAF_CHECK_WITH_ORIGIN("", origin, __VA_ARGS__)
+#define DLAF_ASSERT_WITH_ORIGIN(origin, ...) DLAF_CHECK_WITH_ORIGIN("", origin, __VA_ARGS__, '\n')
 
 /// **THIS MACRO MUST BE USED WHEN THE CHECK IS NEEDED TO ENSURE A CONDITION THAT HAVE
 /// VERY LOW IMPACT ON PERFORMANCES.**
@@ -103,7 +103,7 @@
 ///
 /// If the switch **DLAF_ASSERT_ENABLE** is not defined, this check will not
 /// be performed and it will not add any overhead, nor for the condition evaluation, nor for the message
-#define DLAF_ASSERT(...) DLAF_ASSERT_WITH_ORIGIN((SOURCE_LOCATION()), __VA_ARGS__)
+#define DLAF_ASSERT(...) DLAF_ASSERT_WITH_ORIGIN((SOURCE_LOCATION()), __VA_ARGS__, '\n')
 #else
 #define DLAF_ASSERT_WITH_ORIGIN(origin, ...)
 

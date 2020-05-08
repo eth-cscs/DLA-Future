3a0f1c17e5ab895c430d58c3cb6406875b8c4258
Alberto Invernizzi <invernizzi@cscs.ch>
2020-05-08 16:06:55 +0200

    start putting in place norm_max and its test

---
+++ include/dlaf/utility/mc.h
@@ -0,0 +1,58 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#pragma once
+
+#include <blas.hh>
+#include "dlaf/communication/communicator_grid.h"
+#include "dlaf/utility/internal.h"
+#include "dlaf/matrix.h"
+
+namespace dlaf {
+
+template <>
+struct Utility<Backend::MC> {
+  /// Cholesky factorization which computes the factorization of an Hermitian positive
+  /// definite matrix A.
+  ///
+  /// The factorization has the form A=LL^H (uplo = Lower) or A = U^H U (uplo = Upper),
+  /// where L is a lower and U is an upper triangular matrix
+  /// @param grid is the communicator grid on which the matrix A has been distributed.
+  /// @param uplo specifies if the elements of the Hermitian matrix to be referenced are the elements in
+  /// the lower or upper triangular part.
+  /// @param mat_a on entry it contains the triangular matrix A, on exit the matrix elements
+  /// are overwritten with the elements of the Cholesky factor. Only the tiles of the matrix
+  /// which contain the upper or the lower triangular part (depending on the value of uplo).
+  /// @pre mat_a has a square size
+  /// @pre mat_a has a square block size
+  /// @pre mat_a is distributed according to grid.
+  template <class T>
+  static void norm_max(comm::CommunicatorGrid grid, blas::Uplo uplo, Matrix<T, Device::CPU>& mat_a);
+};
+
+}
+
+#include "dlaf/utility/norm_max/mc.tpp"
+//
+///// ---- ETI
+//namespace dlaf {
+//
+//#define DLAF_CHOLESKY_ETI(KWORD, DATATYPE)                                                            \
+//  KWORD template void Factorization<Backend::MC>::cholesky<DATATYPE>(comm::CommunicatorGrid,          \
+//                                                                     blas::Uplo,                      \
+//                                                                     Matrix<DATATYPE, Device::CPU>&); \
+//  KWORD template void Factorization<Backend::MC>::cholesky<DATATYPE>(blas::Uplo,                      \
+//                                                                     Matrix<DATATYPE, Device::CPU>&);
+//
+//DLAF_CHOLESKY_ETI(extern, float)
+//DLAF_CHOLESKY_ETI(extern, double)
+//DLAF_CHOLESKY_ETI(extern, std::complex<float>)
+//DLAF_CHOLESKY_ETI(extern, std::complex<double>)
+//
+//}
---
+++ test/unit/utility/mc/test_norm_max.cpp
@@ -0,0 +1,96 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include "dlaf/utility/mc.h"
+
+#include <gtest/gtest.h>
+
+#include "dlaf/communication/communicator_grid.h"
+#include "dlaf/matrix.h"
+#include "dlaf_test/comm_grids/grids_6_ranks.h"
+#include "dlaf_test/matrix/util_matrix.h"
+#include "dlaf_test/util_types.h"
+
+using namespace dlaf;
+using namespace dlaf::comm;
+using namespace dlaf::matrix;
+using namespace dlaf::matrix::test;
+using namespace dlaf_test;
+using namespace testing;
+
+::testing::Environment* const comm_grids_env =
+    ::testing::AddGlobalTestEnvironment(new CommunicatorGrid6RanksEnvironment);
+
+template <typename Type>
+class NormMaxDistributedTest : public ::testing::Test {
+public:
+  const std::vector<CommunicatorGrid>& commGrids() {
+    return comm_grids;
+  }
+};
+
+TYPED_TEST_SUITE(NormMaxDistributedTest, ::testing::Types<float>); // TODO fix for complex MatrixElementTypes
+
+const std::vector<LocalElementSize> square_sizes({{10, 10}, {25, 25}, {12, 12}, {0, 0}});
+const std::vector<TileElementSize> square_block_sizes({{3, 3}, {5, 5}});
+
+GlobalElementSize globalTestSize(const LocalElementSize& size) {
+  return {size.rows(), size.cols()};
+}
+
+TYPED_TEST(NormMaxDistributedTest, Correctness) {
+  //// Note: The tile elements are chosen such that:
+  //// - res_ij = 1 / 2^(|i-j|) * exp(I*(-i+j)),
+  //// where I = 0 for real types or I is the complex unit for complex types.
+  //// Therefore the result should be:
+  //// a_ij = Sum_k(res_ik * ConjTrans(res)_kj) =
+  ////      = Sum_k(1 / 2^(|i-k| + |j-k|) * exp(I*(-i+j))),
+  //// where k = 0 .. min(i,j)
+  //// Therefore,
+  //// a_ij = (4^(min(i,j)+1) - 1) / (3 * 2^(i+j)) * exp(I*(-i+j))
+  //auto el = [](const GlobalElementIndex& index) {
+  //  SizeType i = index.row();
+  //  SizeType j = index.col();
+  //  if (i < j)
+  //    return TypeUtilities<TypeParam>::element(-9.9, 0.0);
+
+  //  return TypeUtilities<TypeParam>::polar(std::exp2(-(i + j)) / 3 *
+  //                                             (std::exp2(2 * (std::min(i, j) + 1)) - 1),
+  //                                         -i + j);
+  //};
+
+  //// Analytical results
+  //auto res = [](const GlobalElementIndex& index) {
+  //  SizeType i = index.row();
+  //  SizeType j = index.col();
+  //  if (i < j)
+  //    return TypeUtilities<TypeParam>::element(-9.9, 0.0);
+
+  //  return TypeUtilities<TypeParam>::polar(std::exp2(-std::abs(i - j)), -i + j);
+  //};
+
+  for (const auto& comm_grid : this->commGrids()) {
+    for (const auto& size : square_sizes) {
+      for (const auto& block_size : square_block_sizes) {
+        // Matrix to undergo Cholesky decomposition
+        Index2D src_rank_index(std::max(0, comm_grid.size().rows() - 1),
+                               std::min(1, comm_grid.size().cols() - 1));
+        GlobalElementSize sz = globalTestSize(size);
+        Distribution distribution(sz, block_size, comm_grid.size(), comm_grid.rank(), src_rank_index);
+        Matrix<TypeParam, Device::CPU> mat(std::move(distribution));
+        //set(mat, el);
+
+        Utility<Backend::MC>::norm_max(comm_grid, blas::Uplo::Lower, mat);
+
+        //CHECK_MATRIX_NEAR(res, mat, 4 * (mat.size().rows() + 1) * TypeUtilities<TypeParam>::error,
+        //                  4 * (mat.size().rows() + 1) * TypeUtilities<TypeParam>::error);
+      }
+    }
+  }
+}

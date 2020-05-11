779ab91f4f06fc283eba6fe10fc77d9bcbefd936
Alberto Invernizzi <invernizzi@cscs.ch>
2020-05-11 15:57:59 +0200

    use lapack functions for norm max computation

--- include/dlaf/utility/norm_max/mc/norm_max.h
+++ include/dlaf/utility/norm_max/mc/norm_max.h
@@ -11,6 +11,7 @@
 
 #include <hpx/hpx.hpp>
 #include <hpx/util/unwrap.hpp>
+#include <lapack.hh>
 
 #include "dlaf/common/vector.h"
 #include "dlaf/communication/communicator_grid.h"
@@ -23,42 +24,6 @@ namespace dlaf {
 namespace internal {
 namespace mc {
 
-namespace internal {
-
-template <class T>
-T max_value(const T a, const T b) {
-  return std::max(a, b);
-}
-
-template <class T>
-dlaf::BaseType<T> max_value(const T a, const std::complex<T> b) {
-  return std::max(a, std::abs(b));
-}
-
-template <class T>
-T max_vector(const std::vector<T>& values) {
-  if (values.size() == 0)
-    return std::numeric_limits<T>::min();
-  return *std::max_element(values.begin(), values.end(),
-                           [](const auto& a, const auto& b) { return a < b; });
-}
-
-template <class T>
-dlaf::BaseType<T> max_vector(const std::vector<std::complex<T>>& values) {
-  if (values.size() == 0)
-    return std::numeric_limits<dlaf::BaseType<T>>::min();
-  return std::abs(*std::max_element(values.begin(), values.end(), [](const auto& a, const auto& b) {
-    return std::abs(a) < std::abs(b);
-  }));
-}
-
-template <class T>
-dlaf::BaseType<T> max_vector_dispatcher(const std::vector<T>& values) {
-  return [](const std::vector<T>& values) { return max_vector(values); }(values);
-}
-
-}
-
 // Distributed implementation of Max Norm of a Lower Triangular Matrix
 template <class T>
 dlaf::BaseType<T> norm_max(comm::CommunicatorGrid comm_grid, Matrix<const T, Device::CPU>& matrix) {
@@ -66,9 +31,11 @@ dlaf::BaseType<T> norm_max(comm::CommunicatorGrid comm_grid, Matrix<const T, Dev
   using dlaf::common::make_data;
   using hpx::util::unwrapping;
 
+  using NormT = dlaf::BaseType<T>;
+
   const auto& distribution = matrix.distribution();
 
-  vector<hpx::future<T>> tiles_max;
+  vector<hpx::future<NormT>> tiles_max;
   tiles_max.reserve(distribution.localNrTiles().rows() * distribution.localNrTiles().cols());
 
   // for each tile in local (lower triangular), create a task that finds the max element in the tile
@@ -78,12 +45,14 @@ dlaf::BaseType<T> norm_max(comm::CommunicatorGrid comm_grid, Matrix<const T, Dev
 
     for (SizeType i_loc = i_diag_loc; i_loc < distribution.localNrTiles().rows(); ++i_loc) {
       auto current_tile_max =
-          hpx::dataflow(unwrapping([is_diag = (i_loc == i_diag_loc)](auto&& tile) -> T {
-                          dlaf::BaseType<T> tile_max_value = std::abs(T{0});
-                          for (SizeType j = 0; j < tile.size().cols(); ++j)
-                            for (SizeType i = is_diag ? j : 0; i < tile.size().rows(); ++i)
-                              tile_max_value = internal::max_value(tile_max_value, tile({i, j}));
-                          return tile_max_value;
+          hpx::dataflow(unwrapping([is_diag = (i_loc == i_diag_loc)](auto&& tile) -> NormT {
+                          if (is_diag)
+                            return lapack::lantr(lapack::Norm::Max, lapack::Uplo::Lower,
+                                                 lapack::Diag::NonUnit, tile.size().cols(),
+                                                 tile.size().rows(), tile.ptr(), tile.ld());
+                          else
+                            return lapack::lange(lapack::Norm::Max, tile.size().cols(),
+                                                 tile.size().rows(), tile.ptr(), tile.ld());
                         }),
                         matrix.read(LocalTileIndex{i_loc, j_loc}));
 
@@ -94,17 +63,22 @@ dlaf::BaseType<T> norm_max(comm::CommunicatorGrid comm_grid, Matrix<const T, Dev
   // than it is necessary to reduce max values from all ranks into a single max value for the matrix
 
   // TODO unwrapping can be skipped for optimization reasons
-  dlaf::BaseType<T> local_max_value =
-      hpx::dataflow(unwrapping(internal::max_vector_dispatcher<T>), tiles_max).get();
+  NormT local_max_value = hpx::dataflow(unwrapping([](const auto&& values) {
+                                          if (values.size() == 0)
+                                            return std::numeric_limits<NormT>::min();
+                                          return *std::max_element(values.begin(), values.end());
+                                        }),
+                                        tiles_max)
+                              .get();
 
-  dlaf::BaseType<T> max_value_rows;
+  NormT max_value_rows;
   if (comm_grid.size().cols() > 1)
     dlaf::comm::sync::reduce(0, comm_grid.rowCommunicator(), MPI_MAX, make_data(&local_max_value, 1),
                              make_data(&max_value_rows, 1));
   else
     max_value_rows = local_max_value;
 
-  dlaf::BaseType<T> max_value;
+  NormT max_value;
   if (comm_grid.size().rows() > 1)
     dlaf::comm::sync::reduce(0, comm_grid.colCommunicator(), MPI_MAX, make_data(&max_value_rows, 1),
                              make_data(&max_value, 1));

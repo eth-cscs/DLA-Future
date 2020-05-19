bc687843315b2d2bcb1c4ed04cef222717547c25
Alberto Invernizzi <invernizzi@cscs.ch>
2020-05-19 11:34:32 +0200

    test refactoring for norm max

--- test/unit/utility/mc/test_norm.cpp
+++ test/unit/utility/mc/test_norm.cpp
@@ -41,15 +41,39 @@ public:
 
 TYPED_TEST_SUITE(NormDistributedTest, MatrixElementTypes);
 
-const std::vector<GlobalElementSize> sizes({{10, 10}, {0, 0}});
-const std::vector<TileElementSize> block_sizes({{3, 3}, {5, 5}});
-
 const std::vector<blas::Uplo> blas_uplos({blas::Uplo::Lower, blas::Uplo::Upper, blas::Uplo::General});
+
+// - set a matrix with the given el setter
+// - execute the Utility<Backend::MC>::norm with the given parameters
+// - compare the result with the norm_expected (in case of empty matrix, it compare with 0 instead of
+// expected norm)
+template <class T, class ElementSetter>
+void test_norm(const std::string message, NormT<T> norm_expected, ElementSetter el,
+               CommunicatorGrid comm_grid, Matrix<T, Device::CPU>& matrix, lapack::Norm norm,
+               blas::Uplo uplo) {
+  if (matrix.size().isEmpty())
+    norm_expected = 0;
+
+  set(matrix, el);
+
+  const NormT<T> result = Utility<Backend::MC>::norm(comm_grid, norm, uplo, matrix);
+
+  SCOPED_TRACE(::testing::Message() << lapack::norm2str(norm) << " " << blas::uplo2str(uplo)
+                                    << " size=" << matrix.size() << " grid_size=" << comm_grid.size());
+  SCOPED_TRACE(::testing::Message() << message);
+
+  if (Index2D{0, 0} == comm_grid.rank())
+    EXPECT_NEAR(norm_expected, result, TypeUtilities<NormT<T>>::error);
+}
 
 TYPED_TEST(NormDistributedTest, NormMax) {
   const lapack::Norm norm = lapack::Norm::Max;
 
   const TypeParam value = dlaf_test::TypeUtilities<TypeParam>::element(13, -13);
+  const NormT<TypeParam> norm_expected = std::abs(value);
+
+  const std::vector<GlobalElementSize> sizes({{10, 10}, {0, 0}});
+  const std::vector<TileElementSize> block_sizes({{3, 3}, {5, 5}});
 
   for (const auto& comm_grid : this->commGrids()) {
     for (const auto& size : sizes) {
@@ -63,41 +87,21 @@ TYPED_TEST(NormDistributedTest, NormMax) {
           if (blas::Uplo::Lower != uplo)
             continue;
 
-          const NormT<TypeParam> norm_expected = size.isEmpty() ? 0 : std::abs(value);
+          auto el_L = [size, value](const GlobalElementIndex& index) {
+            if (GlobalElementIndex{size.rows() - 1, 0} == index)
+              return value;
+            return TypeParam(0);
+          };
 
-          {
-            auto el_L = [size, value](const GlobalElementIndex& index) {
-              if (GlobalElementIndex{size.rows() - 1, 0} == index)
-                return value;
-              return TypeParam(0);
-            };
+          test_norm<TypeParam>("lower triangular", norm_expected, el_L, comm_grid, mat, norm, uplo);
 
-            set(mat, el_L);
+          auto el_D = [size, value](const GlobalElementIndex& index) {
+            if (GlobalElementIndex{size.rows() - 1, size.cols() - 1} == index)
+              return value;
+            return TypeParam(0);
+          };
 
-            const NormT<TypeParam> result = Utility<Backend::MC>::norm(comm_grid, norm, uplo, mat);
-
-            SCOPED_TRACE(::testing::Message() << lapack::norm2str(norm) << " Lower Triangular " << size);
-
-            if (Index2D{0, 0} == comm_grid.rank())
-              EXPECT_NEAR(norm_expected, result, TypeUtilities<NormT<TypeParam>>::error);
-          }
-
-          {
-            auto el_D = [size, value](const GlobalElementIndex& index) {
-              if (GlobalElementIndex{size.rows() - 1, size.cols() - 1} == index)
-                return value;
-              return TypeParam(0);
-            };
-
-            set(mat, el_D);
-
-            const NormT<TypeParam> result = Utility<Backend::MC>::norm(comm_grid, norm, uplo, mat);
-
-            SCOPED_TRACE(::testing::Message() << lapack::norm2str(norm) << " Diagonal " << size);
-
-            if (Index2D{0, 0} == comm_grid.rank())
-              EXPECT_NEAR(norm_expected, result, TypeUtilities<NormT<TypeParam>>::error);
-          }
+          test_norm<TypeParam>("diagonal", norm_expected, el_D, comm_grid, mat, norm, uplo);
         }
       }
     }

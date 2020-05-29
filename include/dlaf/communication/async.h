c8fb63b14aab067c1e48ea9b226183f19aac4a58
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-05-29 16:18:09 +0200

    Define a MPI executor

--- include/dlaf/communication/async.h
+++ include/dlaf/communication/async.h
@@ -14,64 +14,18 @@
 #include <utility>
 
 #include <mpi.h>
-#include <hpx/async.hpp>
+
+#include <hpx/include/async.hpp>
 #include <hpx/include/parallel_executors.hpp>
-#include <hpx/include/threads.hpp>
+#include <hpx/include/thread_executors.hpp>
 #include <hpx/lcos/future.hpp>
+#include <hpx/parallel/executors/execution_fwd.hpp>
 #include <hpx/util/yield_while.hpp>
-#ifdef DLAF_WITH_MPI_FUTURES
-#include <hpx/mpi/mpi_executor.hpp>
-#include <hpx/mpi/mpi_future.hpp>
-#endif
 
 namespace dlaf {
 namespace comm {
 
-/// The function only accepts non-blocking MPI routines. The MPI library needs
-/// to support MPI_THREAD_MULTIPLE.
-///
-/// Example usage:
-///
-/// dlaf::comm::async(ex, comm, MPI_Irecv, recv_ptr, num_recv_elems, MPI_DOUBLE, src_rank, tag);
-///
-/// compared to the usual MPI non-blocking call:
-///
-/// MPI_Irecv(recv_ptr, num_recv_elems, MPI_DOUBLE, src_rank, tag, comm, &request);
-///
-/// there are two differences
-///
-/// 1) MPI_Comm (comm) goes after the executor in async()
-/// 2) MPI_Request (request) is omitted as it is handled internally in async()
-///
-/// Note that if starting the communication depends on a previous task, it is often a good idea to call
-/// the `get()` method on a returned future to avoid weird constructs like future<future<>>.
-///
-/// Example:
-///
-/// auto comm_fut = prev_fut.then([=] {
-///   return dlaf::comm::async(...).get();
-/// });
-///
-/// or don't return the future from `dlaf::comm::async()` and just do:
-///
-/// auto comm_fut = prev_fut.then([=] {
-///   dlaf::comm::async(...);
-/// });
-///
-template <class F, class... Ts>
-hpx::future<void> async(hpx::threads::executors::pool_executor& ex, MPI_Comm comm, F&& f,
-                        Ts&&... ts) noexcept {
-  return hpx::async(ex, [=] {
-    MPI_Request req;
-    f(ts..., comm, &req);
-
-    // Yield until non-blocking communication completes.
-    hpx::util::yield_while([&req] {
-      int flag;
-      MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
-      return flag == 0;
-    });
-  });
+// TODO: reference docs from misc/runtimes.md
 
 /// This section is in preparation for MPI_Futures in HPX 1.5. The mechanism for handling MPI
 /// communication is similar but there are notable differences.
@@ -97,10 +51,98 @@ hpx::future<void> async(hpx::threads::executors::pool_executor& ex, MPI_Comm com
 /// global array and polled for completion with `MPI_Testany()` on task completion/yielding for all tasks
 /// for which polling is enabled (note `enable_user_polling()`).
 ///
-#ifdef DLAF_WITH_MPI_FUTURES
-// TODO
-#endif
+
+/// An executor for MPI calls.
+class executor {
+  MPI_Comm comm_;
+  hpx::threads::executors::pool_executor ex_;
+
+public:
+  // Associate the parallel_execution_tag executor tag type as a default with this executor.
+  using execution_category = hpx::parallel::execution::parallel_execution_tag;
+
+  inline executor(MPI_Comm comm) : comm_(comm), ex_("default", hpx::threads::thread_priority_high) {}
+
+  constexpr bool operator==(executor const& rhs) const noexcept {
+    return comm_ == rhs.comm_ && ex_ == rhs.ex_;
+  }
+
+  constexpr bool operator!=(executor const& rhs) const noexcept {
+    return !(*this == rhs);
+  }
+
+  constexpr executor const& context() const noexcept {
+    return *this;
+  }
+
+  /// The function only accepts non-blocking MPI routines. The MPI library needs
+  /// to support MPI_THREAD_MULTIPLE.
+  ///
+  /// Example usage:
+  ///
+  /// ```
+  /// hpx::async(ex, MPI_Irecv, recv_ptr, num_recv_elems, MPI_DOUBLE, src_rank, tag);
+  /// ```
+  /// 
+  /// or
+  /// 
+  /// ```
+  /// ex.async_execute(MPI_Irecv, recv_ptr, num_recv_elems, MPI_DOUBLE, src_rank, tag);
+  ///
+  /// ```
+  //
+  /// compared to the usual MPI non-blocking call:
+  ///
+  /// ```
+  /// MPI_Irecv(recv_ptr, num_recv_elems, MPI_DOUBLE, src_rank, tag, comm, &request);
+  /// ```
+  ///
+  /// there are two differences
+  ///
+  /// 1) MPI_Comm (comm) doesn't need to be specified as it is passed by the executor
+  /// 2) MPI_Request (request) is omitted as it is handled internally in async()
+  ///
+  /// Note that if starting the communication depends on a previous task, it is often a good idea to call
+  /// the `get()` method on a returned future to avoid weird constructs like future<future<>>.
+  ///
+  /// Example:
+  ///
+  /// auto comm_fut = prev_fut.then([=] {
+  ///   return dlaf::comm::async(...).get();
+  /// });
+  ///
+  /// or don't return the future from `dlaf::comm::async()` and just do:
+  ///
+  /// auto comm_fut = prev_fut.then([=] {
+  ///   dlaf::comm::async(...);
+  /// });
+  ///
+  template <typename F, typename... Ts>
+  hpx::future<void> async_execute(F f, Ts... ts) noexcept {
+    return hpx::async(ex_, [comm = comm_, f, ts...] {
+      MPI_Request req;
+      f(ts..., comm, &req);
+
+      // Yield until non-blocking communication completes.
+      hpx::util::yield_while([&req] {
+        int flag;
+        MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
+        return flag == 0;
+      });
+    });
+  }
+};
+
+}
 }
 
+namespace hpx {
+namespace parallel {
+namespace execution {
+
+template <>
+struct is_two_way_executor<dlaf::comm::executor> : std::true_type {};
+
+}
 }
 }

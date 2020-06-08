49d943c5fd2774678089578a67a49e4572dda801
lara <lara.querciagrossa@gmail.com>
2020-06-08 10:15:51 +0200

    small fixed in test

--- test/unit/eigensolver/mc/test_genToStd.cpp
+++ test/unit/eigensolver/mc/test_genToStd.cpp
@@ -32,80 +32,44 @@ class EigensolverGenToStdLocalTest : public ::testing::Test {};
 
 TYPED_TEST_SUITE(EigensolverGenToStdLocalTest, MatrixElementTypes);
 
-TYPED_TEST(EigensolverGenToStdLocalTest, Correctness) {
+TYPED_TEST(EigensolverGenToStdLocalTest, CorrectnessFixed) {
 
   LocalElementSize size = {3,3};
   TileElementSize block_size = {1,1};  
 
-  //// MATRIX A
+  // MATRIX A
     auto el = [](const GlobalElementIndex& index){
       // ColMajor
       static const double values[] = {10.0, 20.0, 30.0, 20.0, 40.0, 60.0, 30.0, 60.0, 90.0};
     return values[index.row()+3*index.col()];
   };
-
   Matrix<double, Device::CPU> mat_a(size, block_size);
   set(mat_a, el);
-
   std::cout << "Matrix A" << std::endl;
   std::cout << "Size " << mat_a.size().rows() << " x " << mat_a.size().cols() << "\n";
   std::cout << "Tile size " << mat_a.blockSize().rows() << " x " << mat_a.blockSize().cols() << "\n";
   std::cout << "Number of tiles " << mat_a.nrTiles().rows()*mat_a.nrTiles().cols()  << "\n";
   std::cout << mat_a << std::endl;
-  
-//  SizeType tottilecol = mat_a.nrTiles().cols();
-//  SizeType tottilerow = mat_a.nrTiles().rows();
-//  for (SizeType i=0; i<tottilerow; ++i) {
-//    for (SizeType j=0; j<tottilecol; ++j) {
-//      auto tile = mat_a(LocalTileIndex(i,j)).get();
-//      for (SizeType ii = 0; ii < tile.size().rows(); ++ii) {
-//	for (SizeType jj = 0; jj < tile.size().cols(); ++jj) {
-//	  std::cout << tile({ii, jj}) << " ";
-//	}
-//      }
-//    }
-//      std::cout << "\n";
-//  }
 
 
-
-  //// MATRIX L
-  
+  // MATRIX L
   auto el_l = [](const GlobalElementIndex& index){
     static const double values[] = {2., 3., 5., 0., 4., 6., 0., 0., 7.};
     return values[index.row()+3*index.col()];
   };
-
   Matrix<double, Device::CPU> mat_l(size, block_size);
   set(mat_l, el_l);
-
   std::cout << "MAT L" << std::endl;
   std::cout << "Size " << mat_l.size().rows() << " x " << mat_l.size().cols() << "\n";
   std::cout << "Tile size " << mat_l.blockSize().rows() << " x " << mat_l.blockSize().cols() << "\n";
   std::cout << "Number of tiles " << mat_l.nrTiles().rows()*mat_l.nrTiles().cols()  << "\n";
   std::cout << mat_l << std::endl;
-  
-//  SizeType tottilecoll = mat_l.nrTiles().cols();
-//  SizeType tottilerowl = mat_l.nrTiles().rows();
-//  for (SizeType i=0; i<tottilerowl; ++i) {
-//    for (SizeType j=0; j<tottilecoll; ++j) {
-//      auto tile = mat_l(LocalTileIndex(i,j)).get();
-//      for (SizeType ii = 0; ii < tile.size().rows(); ++ii) {
-//	for (SizeType jj = 0; jj < tile.size().cols(); ++jj) {
-//	  std::cout << tile({ii, jj}) << " ";
-//	}
-//      }
-//    }
-//    std::cout << "\n";
-//  }
-
 
   // genToStd
   Eigensolver<Backend::MC>::genToStd(mat_a, mat_l);
 
   std::cout << "Result" << std::endl;
   std::cout  << mat_a << std::endl;
-
 
   auto res = [](const GlobalElementIndex& index){
     static const double values[] = {2.5, 0.625, -0.1785714, 20., 0.15625, -0.0446429, 30., 60., 0.0127551};
@@ -114,7 +78,5 @@ TYPED_TEST(EigensolverGenToStdLocalTest, Correctness) {
 
   CHECK_MATRIX_NEAR(res, mat_a, 100000000 * (mat_a.size().rows() + 1) * TypeUtilities<TypeParam>::error,
                     100000000 * (mat_a.size().rows() + 1) * TypeUtilities<TypeParam>::error);
-
-
 }
   

e4bcf1e9a46cd5f14e0166cb79ddf3281425c0fe
Harmen Stoppels <harmenstoppels@gmail.com>
2020-07-10 16:11:40 +0200

    Update the parent commit as well

--- ci/set_github_status.sh
+++ ci/set_github_status.sh
@@ -1,12 +1,26 @@
-#!/bin/bash -l
+#!/bin/bash
 
-set -eux
+function submit() {
+    local commit_status=${1}
+    local commit_sha=${2}
+
+    curl --verbose \
+        --url "https://api.github.com/repos/eth-cscs/DLA-Future/statuses/${commit_sha}" \
+        --header 'Content-Type: application/json' \
+        --header "authorization: Bearer ${GITHUB_TOKEN}" \
+        --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"All Gitlab pipelines\", \"context\": \"ci/gitlab/full-pipeline\" }"
+}
 
 commit_status=${1}
 
-curl --verbose \
-    --request POST \
-    --url "https://api.github.com/repos/eth-cscs/DLA-Future/statuses/${CI_COMMIT_SHA}" \
-    --header 'Content-Type: application/json' \
-    --header "authorization: Bearer ${GITHUB_TOKEN}" \
-    --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"Gitlab\", \"context\": \"ci/gitlab/full-pipeline\" }"
+# always submit for the current commit
+submit "${commit_status}" "$CI_COMMIT_SHA"
+
+# For Bors: get the latest commit before the merge to set the status.
+if [[ $CI_COMMIT_REF_NAME =~ ^(trying|staging)$ ]]; then
+    parent_sha=`git rev-parse --verify -q "$CI_COMMIT_SHA"^2`
+    if [[ $? -eq 0 ]]; then
+        submit "${commit_status}" "${parent_sha}"
+    fi
+fi
+

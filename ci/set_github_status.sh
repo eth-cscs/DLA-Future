74e026a4a06dbcf3933ff21483b71530fd80ad19
Harmen Stoppels <harmenstoppels@gmail.com>
2020-07-10 14:23:44 +0200

    use a fixed image

--- ci/set_github_status.sh
+++ ci/set_github_status.sh
@@ -9,4 +9,4 @@ curl --verbose \
     --url "https://api.github.com/repos/eth-cscs/DLA-Future/statuses/${CI_COMMIT_SHA}" \
     --header 'Content-Type: application/json' \
     --header "authorization: Bearer ${GITHUB_TOKEN}" \
-    --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"Gitlab\", \"context\": \"gitlab-status\" }"
+    --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"Gitlab\", \"context\": \"ci/gitlab/full-pipeline\" }"

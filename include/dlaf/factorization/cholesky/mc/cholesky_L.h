feb0507f84b3823a59a6e5676404e55395f4de64
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-08-21 15:42:58 +0200

    Refactor local Cholesky_L

--- include/dlaf/factorization/cholesky/mc/cholesky_L.h
+++ include/dlaf/factorization/cholesky/mc/cholesky_L.h
@@ -32,59 +32,6 @@ namespace dlaf {
 namespace internal {
 namespace mc {
 
-// Local implementation of Lower Cholesky factorization.
-template <class T>
-void cholesky_L(Matrix<T, Device::CPU>& mat_a) {
-  constexpr auto NonUnit = blas::Diag::NonUnit;
-  constexpr auto ConjTrans = blas::Op::ConjTrans;
-  constexpr auto NoTrans = blas::Op::NoTrans;
-  constexpr auto Right = blas::Side::Right;
-  constexpr auto Lower = blas::Uplo::Lower;
-
-  using hpx::util::unwrapping;
-  using hpx::threads::executors::pool_executor;
-  using hpx::threads::thread_priority_high;
-  using hpx::threads::thread_priority_default;
-
-  // Set up executor on the default queue with high priority.
-  pool_executor executor_hp("default", thread_priority_high);
-  // Set up executor on the default queue with default priority.
-  pool_executor executor_normal("default", thread_priority_default);
-
-  // Number of tile (rows = cols)
-  SizeType nrtile = mat_a.nrTiles().cols();
-
-  for (SizeType k = 0; k < nrtile; ++k) {
-    // Cholesky decomposition on mat_a(k,k) r/w potrf (lapack operation)
-    auto kk = LocalTileIndex{k, k};
-
-    hpx::dataflow(executor_hp, unwrapping(tile::potrf<T, Device::CPU>), Lower, std::move(mat_a(kk)));
-
-    for (SizeType i = k + 1; i < nrtile; ++i) {
-      // Update panel mat_a(i,k) with trsm (blas operation), using data mat_a.read(k,k)
-      hpx::dataflow(executor_hp, unwrapping(tile::trsm<T, Device::CPU>), Right, Lower, ConjTrans,
-                    NonUnit, 1.0, mat_a.read(kk), std::move(mat_a(LocalTileIndex{i, k})));
-    }
-
-    for (SizeType j = k + 1; j < nrtile; ++j) {
-      // Choose queue priority
-      auto trailing_matrix_executor = (j == k + 1) ? executor_hp : executor_normal;
-
-      // Update trailing matrix: diagonal element mat_a(j,j, reading mat_a.read(j,k), using herk (blas operation)
-      hpx::dataflow(trailing_matrix_executor, unwrapping(tile::herk<T, Device::CPU>), Lower, NoTrans,
-                    -1.0, mat_a.read(LocalTileIndex{j, k}), 1.0, std::move(mat_a(LocalTileIndex{j, j})));
-
-      for (SizeType i = j + 1; i < nrtile; ++i) {
-        // Update remaining trailing matrix mat_a(i,j), reading mat_a.read(i,k) and mat_a.read(j,k),
-        // using gemm (blas operation)
-        hpx::dataflow(trailing_matrix_executor, unwrapping(tile::gemm<T, Device::CPU>), NoTrans,
-                      ConjTrans, -1.0, mat_a.read(LocalTileIndex{i, k}),
-                      mat_a.read(LocalTileIndex{j, k}), 1.0, std::move(mat_a(LocalTileIndex{i, j})));
-      }
-    }
-  }
-}
-
 // If the diagonal tile is on this node factorize it
 // Cholesky decomposition on mat_a(k,k) r/w potrf (lapack operation)
 template <class T>
@@ -103,14 +50,22 @@ void trsm_panel_tile(hpx::threads::executors::pool_executor executor_hp,
                 std::move(matrix_tile));
 }
 
-// Compute first tile of the column of the trailing matrix: diagonal element mat_a(j,j),
-// reading mat_a.read(j,k), using herk (blas operation)
 template <class T>
 void herk_trailing_diag_tile(hpx::threads::executors::pool_executor trailing_matrix_executor,
-                             hpx::future<Tile<T, Device::CPU>> matrix_tile,
-                             hpx::shared_future<Tile<const T, Device::CPU>> panel_tile) {
+                             hpx::shared_future<Tile<const T, Device::CPU>> panel_tile,
+                             hpx::future<Tile<T, Device::CPU>> matrix_tile) {
   hpx::dataflow(trailing_matrix_executor, hpx::util::unwrapping(tile::herk<T, Device::CPU>),
                 blas::Uplo::Lower, blas::Op::NoTrans, -1.0, panel_tile, 1.0, std::move(matrix_tile));
+}
+
+template <class T>
+void gemm_trailing_matrix_tile(hpx::threads::executors::pool_executor trailing_matrix_executor,
+                               hpx::shared_future<Tile<const T, Device::CPU>> panel_tile,
+                               hpx::shared_future<Tile<const T, Device::CPU>> col_panel,
+                               hpx::future<Tile<T, Device::CPU>> matrix_tile) {
+  hpx::dataflow(trailing_matrix_executor, hpx::util::unwrapping(tile::gemm<T, Device::CPU>),
+                blas::Op::NoTrans, blas::Op::ConjTrans, -1.0, std::move(panel_tile),
+                std::move(col_panel), 1.0, std::move(matrix_tile));
 }
 
 template <class T>
@@ -126,7 +81,7 @@ void send_tile(hpx::threads::executors::pool_executor executor_hp,
         PromiseExec_t pex = fpex.get();
         comm::bcast(pex.ref(), pex.ref().comm().rank(), fut_tile.get());
       },
-      "send_trailing_panel");
+      "send_tile");
   hpx::dataflow(executor_hp, std::move(send_bcast_f), tile, mpi_task_chain());
 }
 
@@ -150,8 +105,52 @@ hpx::shared_future<Tile<const T, Device::CPU>> recv_tile(
               return std::move(t);
             });
       },
-      "recv_trailing_panel");
+      "recv_tile");
   return hpx::future<ConstTile_t>(hpx::dataflow(executor_hp, std::move(recv_bcast_f), mpi_task_chain()));
+}
+
+// Local implementation of Lower Cholesky factorization.
+template <class T>
+void cholesky_L(Matrix<T, Device::CPU>& mat_a) {
+  using hpx::threads::executors::pool_executor;
+  using hpx::threads::thread_priority_high;
+  using hpx::threads::thread_priority_default;
+
+  // Set up executor on the default queue with high priority.
+  pool_executor executor_hp("default", thread_priority_high);
+  // Set up executor on the default queue with default priority.
+  pool_executor executor_normal("default", thread_priority_default);
+
+  // Number of tile (rows = cols)
+  SizeType nrtile = mat_a.nrTiles().cols();
+
+  for (SizeType k = 0; k < nrtile; ++k) {
+    // Cholesky decomposition on mat_a(k,k) r/w potrf (lapack operation)
+    auto kk = LocalTileIndex{k, k};
+
+    potrf_diag_tile(executor_hp, mat_a(kk));
+
+    for (SizeType i = k + 1; i < nrtile; ++i) {
+      // Update panel mat_a(i,k) with trsm (blas operation), using data mat_a.read(k,k)
+      trsm_panel_tile(executor_hp, mat_a.read(kk), mat_a(LocalTileIndex{i, k}));
+    }
+
+    for (SizeType j = k + 1; j < nrtile; ++j) {
+      // Choose queue priority
+      auto trailing_matrix_executor = (j == k + 1) ? executor_hp : executor_normal;
+
+      // Update trailing matrix: diagonal element mat_a(j,j, reading mat_a.read(j,k), using herk (blas operation)
+      herk_trailing_diag_tile(trailing_matrix_executor, mat_a.read(LocalTileIndex{j, k}),
+                              mat_a(LocalTileIndex{j, j}));
+
+      for (SizeType i = j + 1; i < nrtile; ++i) {
+        // Update remaining trailing matrix mat_a(i,j), reading mat_a.read(i,k) and mat_a.read(j,k),
+        // using gemm (blas operation)
+        gemm_trailing_matrix_tile(trailing_matrix_executor, mat_a.read(LocalTileIndex{i, k}),
+                                  mat_a.read(LocalTileIndex{j, k}), mat_a(LocalTileIndex{i, j}));
+      }
+    }
+  }
 }
 
 // Distributed implementation of Lower Cholesky factorization.
@@ -170,15 +169,14 @@ void cholesky_L(comm::CommunicatorGrid grid, Matrix<T, Device::CPU>& mat_a) {
   pool_executor executor_hp("default", thread_priority_high);
   // Set up executor on the default queue with default priority.
   pool_executor executor_normal("default", thread_priority_default);
-  // Set up MPI executor
+
+  // Set up MPI executor pipelines
   comm::executor executor_mpi_col(grid.colCommunicator());
   comm::executor executor_mpi_row(grid.rowCommunicator());
-
   common::Pipeline<comm::executor> mpi_col_task_chain(std::move(executor_mpi_col));
   common::Pipeline<comm::executor> mpi_row_task_chain(std::move(executor_mpi_row));
 
   const matrix::Distribution& distr = mat_a.distribution();
-
   SizeType nrtile = mat_a.nrTiles().cols();
 
   auto localnrtile_rows = distr.localNrTiles().rows();
@@ -248,7 +246,10 @@ void cholesky_L(comm::CommunicatorGrid grid, Matrix<T, Device::CPU>& mat_a) {
         auto i_local = distr.localTileFromGlobalTile<Coord::Row>(j);
         col_panel = panel[i_local];
         send_tile(executor_hp, mpi_col_task_chain, col_panel);
-        herk_trailing_diag_tile(trailing_matrix_executor, mat_a(GlobalTileIndex{j, j}), col_panel);
+
+        // Compute first tile of the column of the trailing matrix: diagonal element mat_a(j,j),
+        // reading mat_a.read(j,k), using herk (blas operation)
+        herk_trailing_diag_tile(trailing_matrix_executor, col_panel, mat_a(GlobalTileIndex{j, j}));
       }
       else {
         col_panel = recv_tile<T>(executor_hp, mpi_col_task_chain, mat_a.tileSize(GlobalTileIndex(j, k)),
@@ -259,9 +260,8 @@ void cholesky_L(comm::CommunicatorGrid grid, Matrix<T, Device::CPU>& mat_a) {
            i_local < localnrtile_rows; ++i_local) {
         // Update remaining trailing matrix mat_a(i,j), reading mat_a.read(i,k) and mat_a.read(j,k),
         // using gemm (blas operation)
-        hpx::dataflow(trailing_matrix_executor, hpx::util::unwrapping(tile::gemm<T, Device::CPU>),
-                      blas::Op::NoTrans, blas::Op::ConjTrans, -1.0, panel[i_local], col_panel, 1.0,
-                      std::move(mat_a(LocalTileIndex{i_local, j_local})));
+        gemm_trailing_matrix_tile(trailing_matrix_executor, panel[i_local], col_panel,
+                                  mat_a(LocalTileIndex{i_local, j_local}));
       }
     }
   }

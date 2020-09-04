b32f8ce7f9342ea0a5e36346cf2e4274a41b207b
Alberto Invernizzi <invernizzi@cscs.ch>
2020-09-04 13:51:23 +0200

    clang-format

--- miniapp/miniapp_triangular_solver.cpp
+++ miniapp/miniapp_triangular_solver.cpp
@@ -117,8 +117,8 @@ int hpx_main(hpx::program_options::variables_map& vm) {
       std::cout << "[" << run_index << "]"
                 << " " << elapsed_time << "s"
                 << " " << gigaflops << "GFlop/s"
-                << " " << b.size() << " " << b.blockSize() << " " << comm_grid.size()
-                << " " << hpx::get_os_thread_count() << std::endl;
+                << " " << b.size() << " " << b.blockSize() << " " << comm_grid.size() << " "
+                << hpx::get_os_thread_count() << std::endl;
     }
 
     // (optional) run test

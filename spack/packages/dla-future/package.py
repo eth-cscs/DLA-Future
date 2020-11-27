5ee4c3fbce114aac74bf5d1777a388416c1123fe
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-11-27 10:56:45 +0100

    Format with Black / Make sure it's PEP8 compliant

--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -10,54 +10,58 @@ class DlaFuture(CMakePackage, CudaPackage):
     """DLA-Future library: Distributed Linear Algebra with Future"""
 
     homepage = "https://github.com/eth-cscs/DLA-Future/wiki"
-    git      = "https://github.com/eth-cscs/DLA-Future"
+    git = "https://github.com/eth-cscs/DLA-Future"
 
-    maintainers = ['teonnik', 'albestro', 'Sely85']
+    maintainers = ["teonnik", "albestro", "Sely85"]
 
-    version('develop', branch='master')
+    version("develop", branch="master")
 
-    variant('doc', default=False,
-            description='Build documentation.')
+    variant("doc", default=False, description="Build documentation.")
 
-    variant('miniapps', default=False,
-            description='Build miniapps.')
+    variant("miniapps", default=False, description="Build miniapps.")
 
-    depends_on('cmake@3.14:', type='build')
-    depends_on('doxygen', type='build', when='+doc')
+    depends_on("cmake@3.14:", type="build")
+    depends_on("doxygen", type="build", when="+doc")
 
-    depends_on('mpi')
-    depends_on('blaspp')
-    depends_on('lapackpp')
-    depends_on('hpx@1.5.0 cxxstd=14 networking=none')
+    depends_on("mpi")
+    depends_on("blaspp")
+    depends_on("lapackpp")
+    depends_on("hpx@1.5.0 cxxstd=14 networking=none")
 
-    depends_on('hpx build_type=Debug', when='build_type=Debug')
-    depends_on('hpx build_type=Release', when='build_type=Release')
-    depends_on('hpx build_type=RelWithDebInfo', when='build_type=RelWithDebInfo')
+    depends_on("hpx build_type=Debug", when="build_type=Debug")
+    depends_on("hpx build_type=Release", when="build_type=Release")
+    depends_on(
+        "hpx build_type=RelWithDebInfo", when="build_type=RelWithDebInfo"
+    )
 
     def cmake_args(self):
         spec = self.spec
 
         # BLAS/LAPACK
-        if '^mkl' in spec:
-            args = [ self.define('DLAF_WITH_MKL', True) ]
+        if "^mkl" in spec:
+            args = [self.define("DLAF_WITH_MKL", True)]
         else:
             args = [
-                    self.define('DLAF_WITH_MKL', False),
-                    self.define('LAPACK_TYPE', 'Custom'),
-                    self.define('LAPACK_LIBRARY',
-                        ' '.join([spec[dep].libs.ld_flags for dep in ['blas', 'lapack']]))
-                   ]
+                self.define("DLAF_WITH_MKL", False),
+                self.define("LAPACK_TYPE", "Custom"),
+                self.define(
+                    "LAPACK_LIBRARY",
+                    " ".join(
+                        [spec[dep].libs.ld_flags for dep in ["blas", "lapack"]]
+                    ),
+                ),
+            ]
 
         # CUDA
-        args.append(self.define_from_variant('DLAF_WITH_CUDA', 'cuda'))
+        args.append(self.define_from_variant("DLAF_WITH_CUDA", "cuda"))
 
         # DOC
-        args.append(self.define_from_variant('BUILD_DOC', 'doc'))
+        args.append(self.define_from_variant("BUILD_DOC", "doc"))
 
         # TESTs
-        args.append(self.define('DLAF_WITH_TEST', self.run_tests))
+        args.append(self.define("DLAF_WITH_TEST", self.run_tests))
 
         # MINIAPPS
-        args.append(self.define_from_variant('DLAF_BUILD_MINIAPPS', 'miniapps'))
+        args.append(self.define_from_variant("DLAF_BUILD_MINIAPPS", "miniapps"))
 
         return args

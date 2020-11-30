c1373a104633671586e257be1bbd527e18c99f46
lara <lara.querciagrossa@gmail.com>
2020-11-30 16:26:27 +0100

    fixing ASSERT comments in srcs

--- src/communication/communicator_impl.cpp
+++ src/communication/communicator_impl.cpp
@@ -16,14 +16,14 @@ bool is_manageable(MPI_Comm mpi_communicator) noexcept {
 }
 
 CommunicatorImpl::CommunicatorImpl(MPI_Comm mpi_communicator) : comm_(mpi_communicator) {
-  DLAF_ASSERT(comm_ != MPI_COMM_NULL, "A null communicator is used!");
+  DLAF_ASSERT(comm_ != MPI_COMM_NULL, comm_);
   DLAF_MPI_CALL(MPI_Comm_size(comm_, &size_));
   DLAF_MPI_CALL(MPI_Comm_rank(comm_, &rank_));
 }
 
 CommunicatorImpl_Managed::CommunicatorImpl_Managed(MPI_Comm mpi_communicator)
     : CommunicatorImpl(mpi_communicator) {
-  DLAF_ASSERT(is_manageable(comm_), "Passed communicator is not manageable!");
+  DLAF_ASSERT(is_manageable(comm_), is_manageable(comm_), comm_);
 }
 
 CommunicatorImpl_Managed::~CommunicatorImpl_Managed() {

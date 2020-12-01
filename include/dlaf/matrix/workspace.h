43a75142346a58a4630c8673039652b9f8fd4345
Alberto Invernizzi <invernizzi@cscs.ch>
2020-12-01 19:19:38 +0100

    refactoring of workspace
    the idea is to remove VirtualWorkspace and work just with Workspace
    so I moved all the coordinate logic to Workspace in addition to
    functions for workspace "transpose" communication
    
    not yet tested in reduction to band

--- include/dlaf/matrix/workspace.h
+++ include/dlaf/matrix/workspace.h
@@ -12,6 +12,8 @@
 #include "dlaf/common/assert.h"
 #include "dlaf/common/index2d.h"
 #include "dlaf/common/vector.h"
+#include "dlaf/communication/communicator.h"
+#include "dlaf/communication/helpers.h"
 #include "dlaf/matrix/distribution.h"
 #include "dlaf/matrix/matrix.h"
 #include "dlaf/matrix/matrix_base.h"
@@ -19,6 +21,33 @@
 #include "dlaf/util_matrix.h"
 
 namespace dlaf {
+
+namespace internal {
+template <Coord panel_type>
+matrix::Distribution compute_size(const matrix::Distribution& dist_matrix, const LocalTileSize start) {
+  const auto mb = dist_matrix.blockSize().rows();
+  const auto nb = dist_matrix.blockSize().cols();
+
+  const auto panel_size = [&]() -> LocalElementSize {
+    switch (panel_type) {
+      case Coord::Col: {
+        const auto mat_size = dist_matrix.localNrTiles().rows();
+        const auto i_tile = start.rows();
+        return {(mat_size - i_tile) * mb, nb};
+      }
+      case Coord::Row: {
+        const auto mat_size = dist_matrix.localNrTiles().cols();
+        const auto i_tile = start.cols();
+        return {mb, (mat_size - i_tile) * nb};
+      }
+    }
+  }();
+
+  return {panel_size, dist_matrix.blockSize()};
+}
+
+}
+
 namespace matrix {
 
 template <Coord panel_type, class T, Device device>
@@ -27,7 +56,7 @@ struct Workspace;
 // TODO it works just for tile layout
 // TODO the matrix always occupies memory entirely
 template <Coord panel_type, class T, Device device>
-struct Workspace<panel_type, const T, device> : public internal::MatrixBase {
+struct Workspace<panel_type, const T, device> : protected internal::MatrixBase {
   using TileT = Tile<T, device>;
   using ConstTileT = Tile<const T, device>;
 
@@ -35,66 +64,102 @@ struct Workspace<panel_type, const T, device> : public internal::MatrixBase {
     reset();
   }
 
+  // Note:
+  // these 2 next methods allows to use iterate_range2d
+  auto localNrTiles() const {
+    return this->distribution().localNrTiles();
+  }
+
+  auto offset() const {
+    switch (panel_type) {
+      case Coord::Row:
+        return LocalTileIndex{0, offset_.cols()};
+      case Coord::Col:
+        return LocalTileIndex{offset_.rows(), 0};
+    }
+  }
+
+  auto rankIndex() const {
+    return dist_matrix_.rankIndex();
+  }
+
+  auto distribution_matrix() const {
+    return dist_matrix_;
+  }
+
   void set_tile(const LocalTileIndex& index, hpx::shared_future<ConstTileT> new_tile_fut) {
-    DLAF_ASSERT(!is_masked(index), "you cannot set it again");
-    external_[index.row()] = std::move(new_tile_fut);
+    DLAF_ASSERT(index.isIn(dist_matrix_.localNrTiles()), index, dist_matrix_.localNrTiles());
+    DLAF_ASSERT(!is_masked(index), "already set to external", index);
+    external_[panel_index(index)] = std::move(new_tile_fut);
   }
 
+  // Note:
+  // TODO LocalTileIndex must take into account the offset, so to keep association with SoR of the
+  // related matrix.
+  //
+  // You can access tiles by:
+  // - read(LocalTileIndex)           LocalTileIndex in the panel
+  // - TODO read(SizeType)                 Linear local index (like an array)
+  // - TODO read(GlobalTileIndex)     still thinking how it should work
   hpx::shared_future<ConstTileT> read(const LocalTileIndex& index) {
-    return is_masked(index) ? external(index) : internal_.read(index);
+    DLAF_ASSERT(index.isIn(dist_matrix_.localNrTiles()), index, dist_matrix_.localNrTiles());
+    return is_masked(index) ? external_[panel_index(index)] : internal_.read(full_index(index));
   }
 
+  // it is possible to reset the mask for external tiles, so that memory can be easily re-used
   void reset() {
     external_.clear();
   }
 
 protected:
-  static Distribution compute_size(const Distribution& dist_matrix,
-      const LocalTileSize start = {0, 0}) {
-    //DLAF_ASSERT(start.isIn(dist_matrix.nrTiles()), start, dist_matrix.nrTiles());
-
-    const bool is_colpanel = panel_type == Coord::Col;
-
-    const Coord index_type = is_colpanel ? Coord::Row : Coord::Col;
-    const auto mat_size = is_colpanel ? dist_matrix.localNrTiles().rows() : dist_matrix.localNrTiles().cols();
-    const auto i_tile = is_colpanel ? start.rows() : start.cols();
-
-    const auto i_local = dist_matrix.template nextLocalTileFromGlobalTile<index_type>(i_tile);
-    const auto panel_length = mat_size - i_local;
-
-    const auto mb = dist_matrix.blockSize().rows();
-    const auto nb = dist_matrix.blockSize().cols();
-
-    const auto panel_size = is_colpanel ? LocalElementSize{panel_length * mb, nb} : LocalElementSize{mb, panel_length * nb};
-
-    return Distribution(panel_size,
-                        dist_matrix.blockSize());  // TODO transpose size for row panel
-  }
-
-  // Note
-  // TODO Row/Col, Matrix Distribution
-  // If with workspace we identify just a panel of tiles, we can simply
-  // have row/col workspace and internally compute sizes from the matrix
-  // distribution to which they apply to
-  //
-  // TODO sometimes we don't want to have a full matrix panel (e.g. when
-  // working with submatrix, like with trailing matrix)
-  Workspace(matrix::Distribution dist_matrix, LocalTileSize offset) // TODO migrate to Global
-      : MatrixBase(compute_size(std::move(dist_matrix), offset)), internal_(distribution()) {
+  Workspace(matrix::Distribution dist_matrix, LocalTileSize offset)  // TODO migrate to Global
+      : MatrixBase(dlaf::internal::compute_size<panel_type>(dist_matrix, offset)),
+        dist_matrix_(dist_matrix), offset_(offset), internal_(distribution()) {
     // TODO assert not distributed
-    DLAF_ASSERT(nrTiles().cols() == 1 || nrTiles().rows() == 1, nrTiles());
 
-    external_.resize(nrTiles().rows());
+    switch (panel_type) {
+      case Coord::Row:
+        DLAF_ASSERT(nrTiles().rows() == 1, nrTiles());
+        external_.resize(nrTiles().cols());
+        break;
+      case Coord::Col:
+        DLAF_ASSERT(nrTiles().cols() == 1, nrTiles());
+        external_.resize(nrTiles().rows());
+        break;
+    }
+    DLAF_ASSERT_MODERATE(internal_.distribution().localNrTiles().linear_size(),
+                         external_.size() == external_.size(), internal_.nrTiles(), external_.size());
   }
 
-  hpx::shared_future<ConstTileT> external(const LocalTileIndex& index) const {
-    DLAF_ASSERT(index.isIn(distribution().localNrTiles()), index);
-    return external_[index.row()];
+  SizeType panel_index(const LocalTileIndex& index) const {
+    switch (panel_type) {
+      case Coord::Row:
+        return index.col() - offset_.cols();
+      case Coord::Col:
+        return index.row() - offset_.rows();
+    }
   }
 
-  bool is_masked(const LocalTileIndex& index) const {
-    return external(index).valid();
+  LocalTileIndex full_index(LocalTileIndex index) const {
+    DLAF_ASSERT_MODERATE(index.row() == 0 || index.col() == 0, index);
+    index = [&]() -> LocalTileIndex {
+      switch (panel_type) {
+        case Coord::Row:
+          return {0, index.col() - offset_.cols()};
+        case Coord::Col:
+          return {index.row() - offset_.rows(), 0};
+      }
+    }();
+    DLAF_ASSERT(index.isIn(localNrTiles()), index);
+    return index;
   }
+
+  bool is_masked(const LocalTileIndex linear_index) const {
+    return external_[panel_index(linear_index)].valid();
+  }
+
+  Distribution dist_matrix_;
+  LocalTileSize offset_;
 
   ///> Stores the shared_future
   common::internal::vector<hpx::shared_future<ConstTileT>> external_;
@@ -107,21 +172,71 @@ struct Workspace : public Workspace<panel_type, const T, device> {
   using TileT = Tile<T, device>;
   using ConstTileT = Tile<const T, device>;
 
-  Workspace(matrix::Distribution distribution, LocalTileSize start) : Workspace<panel_type, const T, device>(std::move(distribution), std::move(start)) {
-    util::set(internal_, [](auto&&) { return 0; });
-  }
+  Workspace(matrix::Distribution distribution, LocalTileSize start = {0, 0})
+      : Workspace<panel_type, const T, device>(std::move(distribution), std::move(start)) {}
 
   hpx::future<TileT> operator()(const LocalTileIndex& index) {
-    DLAF_ASSERT(!is_masked(index), "read-only access", index);
-    return internal_(index);
+    DLAF_ASSERT(index.isIn(this->dist_matrix_.localNrTiles()), index, this->dist_matrix_.localNrTiles());
+    DLAF_ASSERT(!is_masked(index), "read-only access on external tiles", index);
+    return internal_(this->full_index(index));
   }
 
 protected:
   using BaseT = Workspace<panel_type, const T, device>;
-  using BaseT::external;
   using BaseT::is_masked;
 
   using BaseT::internal_;
 };
+
+template <class T, Device device, class BcastDir, Coord panel_type, class PredicateOwner>
+void share_panel(BcastDir direction, Workspace<panel_type, T, device>& ws, PredicateOwner&& whos_root,
+                 common::Pipeline<comm::CommunicatorGrid>& serial_comm) {
+  using namespace comm::sync;
+
+  const auto rank = [&]() {
+    if (std::is_same<decltype(direction), comm::row_wise>::value)
+      return ws.rankIndex().col();
+    else if (std::is_same<decltype(direction), comm::col_wise>::value)
+      return ws.rankIndex().row();
+  }();
+
+  for (const auto& index : iterate_range2d(ws.offset(), ws.localNrTiles())) {
+    const comm::IndexT_MPI source_rank = whos_root(index).second;
+    if (rank == source_rank)
+      hpx::dataflow(broadcast_send(direction), ws.read(index), serial_comm());
+    else
+      hpx::dataflow(broadcast_recv(direction, source_rank), ws(index), serial_comm());
+  }
+}
+
+template <class T, Device device>
+auto transpose(Workspace<Coord::Col, T, device>& ws_col, Workspace<Coord::Row, T, device>& ws_row) {
+  DLAF_ASSERT(ws_col.distribution_matrix() == ws_row.distribution_matrix(),
+              "they must refer to the same matrix");
+  // DLAF_ASSERT(ws_col.offset() == ws_row.offset(), ws_col.offset(), ws_row.offset()); // TODO do I have
+  // to check for offset?!
+
+  auto whos_root = [dist = ws_col.distribution_matrix()](const LocalTileIndex index) {
+    const auto k = dist.template globalTileFromLocalTile<Coord::Col>(index.col());
+    const auto rank_owner = dist.template rankGlobalTile<Coord::Row>(k);
+    return std::make_pair(k, rank_owner);
+  };
+
+  const auto& dist = ws_col.distribution_matrix();
+  for (const auto& index : iterate_range2d(ws_row.offset(), ws_row.localNrTiles())) {
+    SizeType k;
+    comm::IndexT_MPI rank_owner;
+
+    std::tie(k, rank_owner) = whos_root(index);
+
+    if (dist.rankIndex().row() != rank_owner)
+      continue;
+
+    const auto i = dist.template localTileFromGlobalTile<Coord::Row>(k);
+    ws_row.set_tile(index, ws_col.read({i, 0}));
+  }
+
+  return whos_root;
+}
 }
 }
--- test/unit/matrix/test_workspace.cpp
+++ test/unit/matrix/test_workspace.cpp
@@ -8,6 +8,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
+#include "dlaf/communication/communicator.h"
+#include "dlaf/communication/helpers.h"
 #include "dlaf/matrix/workspace.h"
 
 #include <vector>
@@ -34,7 +36,7 @@ class WorkspaceLocalTest : public ::testing::Test {};
 
 TYPED_TEST_SUITE(WorkspaceLocalTest, MatrixElementTypes);
 
-TYPED_TEST(WorkspaceLocalTest, Basic) {
+TYPED_TEST(WorkspaceLocalTest, BasicColPanel) {
   using hpx::util::unwrapping;
 
   Distribution dist(LocalElementSize{2, 1}, TileElementSize{1, 1});
@@ -45,7 +47,7 @@ TYPED_TEST(WorkspaceLocalTest, Basic) {
   Matrix<TypeParam, Device::CPU> matrix(dist);
   matrix::util::set(matrix, [VALUE](auto&&) { return VALUE; });
 
-  Workspace<TypeParam, Device::CPU> ws(dist);
+  Workspace<Coord::Col, TypeParam, Device::CPU> ws(dist);
 
   ws({0, 0}).then(unwrapping([](auto&& tile) {
     tile({0, 0}) = TypeUtilities<TypeParam>::element(13, 26);
@@ -71,4 +73,208 @@ TYPED_TEST(WorkspaceLocalTest, Basic) {
     EXPECT_EQ(TypeUtilities<TypeParam>::element(13, 26), tile({0, 0}));
   }));
   ws.read({1, 0}).then(unwrapping([VALUE](auto&& tile) { EXPECT_EQ(VALUE, tile({0, 0})); }));
+}
+
+TYPED_TEST(WorkspaceLocalTest, BasicRowPanel) {
+  using hpx::util::unwrapping;
+
+  Distribution dist(LocalElementSize{1, 2}, TileElementSize{1, 1});
+
+  constexpr auto VALUE = TypeUtilities<TypeParam>::element(26, 10);
+
+  // init order matters!
+  Matrix<TypeParam, Device::CPU> matrix(dist);
+  matrix::util::set(matrix, [VALUE](auto&&) { return VALUE; });
+
+  Workspace<Coord::Row, TypeParam, Device::CPU> ws(dist);
+
+  ws({0, 0}).then(unwrapping([](auto&& tile) {
+    tile({0, 0}) = TypeUtilities<TypeParam>::element(13, 26);
+  }));
+  ws({0, 1}).then(unwrapping([](auto&& tile) {
+    tile({0, 0}) = TypeUtilities<TypeParam>::element(5, 10);
+  }));
+  ws.read({0, 0}).then(unwrapping([](auto&& tile) {
+    EXPECT_EQ(TypeUtilities<TypeParam>::element(13, 26), tile({0, 0}));
+  }));
+  ws.read({0, 1}).then(unwrapping([](auto&& tile) {
+    EXPECT_EQ(TypeUtilities<TypeParam>::element(5, 10), tile({0, 0}));
+  }));
+
+  for (const auto& index : common::iterate_range2d(matrix.distribution().localNrTiles()))
+    matrix.read(index).then(unwrapping([VALUE](auto&& tile) { EXPECT_EQ(VALUE, tile({0, 0})); }));
+
+  ws.set_tile({0, 1}, matrix.read(LocalTileIndex{0, 0}));
+
+  // EXPECT_DEATH(ws.set_tile({0, 1}, matrix.read(LocalTileIndex{0, 0})), "you cannot set it again");
+
+  ws.read({0, 0}).then(unwrapping([](auto&& tile) {
+    EXPECT_EQ(TypeUtilities<TypeParam>::element(13, 26), tile({0, 0}));
+  }));
+  ws.read({0, 1}).then(unwrapping([VALUE](auto&& tile) { EXPECT_EQ(VALUE, tile({0, 0})); }));
+}
+
+TYPED_TEST(WorkspaceLocalTest, PopulateCol_RowWise) {
+  using namespace dlaf;
+  using hpx::util::unwrapping;
+
+  constexpr comm::IndexT_MPI grid_rows = 2, grid_cols = 3;
+  constexpr SizeType n = 5, nb = 1;
+
+  static_assert(NUM_MPI_RANKS == grid_rows * grid_cols, "");
+
+  dlaf::comm::Communicator world(MPI_COMM_WORLD);
+  dlaf::comm::CommunicatorGrid grid(world, grid_rows, grid_cols, dlaf::common::Ordering::ColumnMajor);
+
+  const GlobalElementSize matrix_size{n, n};
+  const TileElementSize block_size{nb, nb};
+  dlaf::Matrix<TypeParam, dlaf::Device::CPU> matrix(matrix_size, block_size, grid);
+
+  const auto& dist = matrix.distribution();
+
+  const GlobalTileSize global_offset{2, 2};
+
+  const LocalTileSize at_offset{
+      dist.template nextLocalTileFromGlobalTile<Coord::Row>(global_offset.rows()),
+      dist.template nextLocalTileFromGlobalTile<Coord::Col>(global_offset.cols()),
+  };
+
+  Workspace<Coord::Col, TypeParam, dlaf::Device::CPU> ws_v(dist, at_offset);
+
+  // TODO this part has to be better designed
+  // const auto& ws_dist = ws.distribution();
+
+  // EXPECT_EQ(at_localsize, ws_dist.localNrTiles());
+
+  // EXPECT_EQ(at_localsize.rows(), ws_dist.localNrTiles().rows());
+  // EXPECT_EQ(at_localsize.cols(), ws_dist.localNrTiles().cols());
+
+  // EXPECT_EQ(at_localsize.rows(), ws_dist.nrTiles().rows());
+  // EXPECT_EQ(at_localsize.cols(), ws_dist.nrTiles().cols());
+
+  using TypeUtil = TypeUtilities<TypeParam>;
+  const auto rank = dist.rankIndex();
+
+  // Just access workspace non-transposed tiles
+  // Each rank sets the tile with the index of the global row
+  for (const auto i_w : iterate_range2d(ws_v.offset(), ws_v.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Row>(i_w.row());
+    const auto rank_owner_row = dist.template rankGlobalTile<Coord::Row>(global_row);
+
+    EXPECT_TRUE(rank_owner_row == rank.row());
+
+    hpx::dataflow(unwrapping([global_row](auto&& tile) {
+                    tile({0, 0}) = TypeUtil::element(global_row, 26);
+                  }),
+                  ws_v(i_w));
+    hpx::dataflow(unwrapping([global_row](auto&& tile) {
+                    EXPECT_EQ(TypeUtil::element(global_row, 26), tile({0, 0}));
+                  }),
+                  ws_v.read(i_w));
+  }
+
+  //  const auto rank_row_globaltile = std::mem_fn(&Distribution::rankGlobalTile<Coord::Row>);
+  //  const auto rank_col_globaltile = std::mem_fn(&Distribution::rankGlobalTile<Coord::Col>);
+  //
+  //  // Access both non-transposed and transposed
+  //  for (SizeType global_row = global_offset.rows(); global_row < dist.nrTiles().rows(); ++global_row) {
+  //    const bool is_on_row = rank_row_globaltile(dist, global_row) == rank.row();
+  //    const bool is_on_col = rank_col_globaltile(dist, global_row) == rank.col();
+  //
+  //    if (!is_on_row and !is_on_col)
+  //      continue;
+  //
+  //    // check that at the rows are set according to previous phase
+  //    if (is_on_row)
+  //      hpx::dataflow(unwrapping([=](auto&& tile) {
+  //                      EXPECT_EQ(TypeUtil::element(global_row, 26), tile({0, 0}));
+  //                    }),
+  //                    ws.read(global_row));
+  //
+  //    // then set every cell in the workspace with the negate of the row index
+  //    hpx::dataflow(unwrapping([=](auto&& tile) { tile({0, 0}) = -global_row; }), ws(global_row));
+  //  }
+  //
+  const comm::IndexT_MPI main_column = 0;
+
+  // Just the main column of ranks sets back the non-tranposed workspace
+  for (const auto& i_w : iterate_range2d(ws_v.offset(), ws_v.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Row>(i_w.row());
+    const auto rank_owner_row = dist.template rankGlobalTile<Coord::Row>(global_row);
+
+    EXPECT_TRUE(rank_owner_row == rank.row());
+
+    if (rank.col() == main_column) {
+      hpx::dataflow(unwrapping([global_row](auto&& tile) {
+                      tile({0, 0}) = TypeUtil::element(-global_row, 26);
+                    }),
+                    ws_v(i_w));
+    }
+    else {  // others just checks that the non-tranposed is negated
+      hpx::dataflow(unwrapping([=](auto&& tile) {
+                      EXPECT_EQ(TypeUtil::element(global_row, 26), tile({0, 0}));
+                    }),
+                    ws_v.read(i_w));
+    }
+  }
+
+  // TODO test for masked row (e.g. "v0")
+  common::Pipeline<comm::CommunicatorGrid> serial_comm(std::move(grid));
+
+  share_panel(
+      comm::row_wise{}, ws_v, [&](auto&&) { return std::make_pair(-1, main_column); }, serial_comm);
+
+  for (const auto& i_w : iterate_range2d(ws_v.offset(), ws_v.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Row>(i_w.row());
+    const auto rank_owner_row = dist.template rankGlobalTile<Coord::Row>(global_row);
+
+    EXPECT_TRUE(rank_owner_row == rank.row());
+
+    hpx::dataflow(unwrapping([=](auto&& tile) {
+                    EXPECT_EQ(TypeUtil::element(-global_row, 26), tile({0, 0}));
+                  }),
+                  ws_v.read(i_w));
+  }
+
+  Workspace<Coord::Row, TypeParam, dlaf::Device::CPU> ws_h(dist, at_offset);
+  auto whos_root = transpose(ws_v, ws_h);
+
+  // Check that the row panel has the info (just in locally available ones)
+  for (const auto& i_w : iterate_range2d(ws_h.offset(), ws_h.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Col>(i_w.col());
+    const auto rank_owner_row = dist.template rankGlobalTile<Coord::Row>(global_row);
+
+    if (rank_owner_row != rank.row())
+      continue;
+
+    hpx::dataflow(unwrapping([=](auto&& tile) {
+                    EXPECT_EQ(TypeUtil::element(-global_row, 26), tile({0, 0}));
+                  }),
+                  ws_h.read(i_w));
+  }
+
+  share_panel(comm::col_wise{}, ws_h, whos_root, serial_comm);
+
+  // Check that the row panel has the informations
+  for (const auto& i_w : iterate_range2d(ws_h.offset(), ws_h.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Col>(i_w.col());
+
+    hpx::dataflow(unwrapping([=](auto&& tile) {
+                    EXPECT_EQ(TypeUtil::element(-global_row, 26), tile({0, 0}));
+                  }),
+                  ws_h.read(i_w));
+  }
+
+  // Just checking that nothing changed in the column panel
+  for (const auto& i_w : iterate_range2d(ws_v.offset(), ws_v.localNrTiles())) {
+    const auto global_row = dist.template globalTileFromLocalTile<Coord::Row>(i_w.row());
+    const auto rank_owner_row = dist.template rankGlobalTile<Coord::Row>(global_row);
+
+    EXPECT_TRUE(rank_owner_row == rank.row());
+
+    hpx::dataflow(unwrapping([=](auto&& tile) {
+                    EXPECT_EQ(TypeUtil::element(-global_row, 26), tile({0, 0}));
+                  }),
+                  ws_v.read(i_w));
+  }
 }

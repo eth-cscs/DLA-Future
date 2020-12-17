12801d285b420c1636a6b1d7044e8e06c6acb648
lara <lara.querciagrossa@gmail.com>
2020-12-17 18:36:29 +0100

    fixing trmm and gemm uplos, copying V to W, adding a couple of tests

--- include/dlaf/solver/backtransformation/mc.h
+++ include/dlaf/solver/backtransformation/mc.h
@@ -26,7 +26,8 @@
 #include "dlaf/communication/executor.h"
 #include "dlaf/communication/functions_sync.h"
 #include "dlaf/lapack_tile.h"
-#include "dlaf/matrix/copy_tile.h"
+#include "dlaf/matrix/copy.h"
+//#include "dlaf/matrix/copy_tile.h"
 #include "dlaf/matrix/matrix.h"
 #include "dlaf/matrix/distribution.h"
 #include "dlaf/matrix/layout_info.h"
@@ -38,6 +39,9 @@ namespace dlaf {
 namespace solver {
 namespace internal {
 
+  using namespace dlaf::matrix;
+  //  using namespace dlaf::tile;
+  
 // Local implementation of Left Lower NoTrans
 
 // Implementation based on:
@@ -69,7 +73,7 @@ struct BackTransformation<Backend::MC, Device::CPU, T> {
      using hpx::threads::thread_priority_high;
      using hpx::threads::thread_priority_default;
      using hpx::util::unwrapping;
-  
+     
      // Set up executor on the default queue with high priority.
      pool_executor executor_hp("default", thread_priority_high);
      // Set up executor on the default queue with default priority.
@@ -80,43 +84,28 @@ struct BackTransformation<Backend::MC, Device::CPU, T> {
      SizeType mb = mat_c.blockSize().rows();
      SizeType nb = mat_c.blockSize().cols();
 
+     TileElementSize size(mb, nb);
+
+     // CHECK!!
+     auto dist_w = mat_c.distribution();
+     auto layout_w = tileLayout(dist_w.localSize(), size);
+     Matrix<T, Device::CPU> mat_w(std::move(dist_w), layout_w);
+     copy(mat_v, mat_w);
+
+     auto dist_w2 = mat_c.distribution();
+     auto layout_w2 = tileLayout(dist_w2.localSize(), size);
+     Matrix<T, Device::CPU> mat_w2(std::move(dist_w2), layout_w2);
+     matrix::util::set(mat_w2, [](auto&&){return 0;});
+
      // n-1 reflectors
      for (SizeType i = 0; i < (m - 1); ++i) {
-       // Create a temporary matrix to store W2
-       //       matrix::util::set(t, [](auto&&) { return 0; });
-
-       TileElementSize size(mb, nb);
-       
-       auto dist_w = mat_c.distribution();
-       auto layout_w = tileLayout(dist_w.localSize(), size);
-       Matrix<T, Device::CPU> mat_w(std::move(dist_w), layout_w);
-       for (SizeType w_col = 0; w_col < n; ++w_col) {
-	 for (SizeType w_row = 0; w_row < m; ++w_row) {
-	   auto tile_index = LocalTileIndex(w_row, w_col);
-	   auto tile = mat_w(tile_index).get();
-	   for (SizeType w_j = 0; w_j < nb; ++w_j) {
-	     for (SizeType w_i = 0; w_i < mb; ++w_i) {
-	       TileElementIndex index(w_i, w_j);
-	       dlaf::matrix::copy(mat_t(TileElementIndex(w_j, w_j)), std::move(mat_w(index)));
-	     }
-	   }
-	 }
-       }
-       std::cout << "MAT W" << std::endl;
-       printElements(mat_w);
-       //       matrix::copy(mat_t, mat_w);
-
-       auto dist_w2 = mat_c.distribution();
-       auto layout_w2 = tileLayout(dist_w2.localSize(), size);
-       Matrix<T, Device::CPU> mat_w2(std::move(dist_w2), layout_w2);
-       matrix::util::set(mat_w2, [](auto&&){return 0;});
 
        for (SizeType k = i; k < m; ++k) {
 	 auto ki = LocalTileIndex{k, i};
 	 auto ii = LocalTileIndex{i, i};
 	 // W = V T
-	 hpx::dataflow(executor_hp, hpx::util::unwrapping(tile::trmm<T, Device::CPU>), Left, Upper, NoTrans,
-                    NonUnit, 1.0, mat_v.read(ki), std::move(mat_w(ki)));
+	 hpx::dataflow(executor_hp, hpx::util::unwrapping(tile::trmm<T, Device::CPU>), Right, Upper, ConjTrans,
+		       NonUnit, 1.0, mat_t.read(ii), std::move(mat_w(ki)));
        }
 
        for (SizeType k = 0; k < m; ++k) {
@@ -125,7 +114,7 @@ struct BackTransformation<Backend::MC, Device::CPU, T> {
 	   auto ji = LocalTileIndex{j, i};
 	   auto jk = LocalTileIndex{j, k};
 	   // W2 = WH C
-	   hpx::dataflow(executor_normal, hpx::util::unwrapping(tile::gemm<T, Device::CPU>), NoTrans,
+	   hpx::dataflow(executor_normal, hpx::util::unwrapping(tile::gemm<T, Device::CPU>), ConjTrans,
 			 NoTrans, 1.0, std::move(mat_w(ji)), mat_c.read(jk), 1.0, std::move(mat_w2(ik)));
 	 }
        }

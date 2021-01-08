fa09d32e47d49c93df0ebc74a340c65253824bf7
Mikael Simberg <simberg@cscs.ch>
2021-01-08 16:25:35 +0100

    Re-enable ETI for MC trsm

--- src/solver/triangular/mc.cpp
+++ src/solver/triangular/mc.cpp
@@ -4,10 +4,10 @@ namespace dlaf {
 namespace solver {
 namespace internal {
 
-//DLAF_SOLVER_TRIANGULAR_MC_ETI(, float)
-//DLAF_SOLVER_TRIANGULAR_MC_ETI(, double)
-//DLAF_SOLVER_TRIANGULAR_MC_ETI(, std::complex<float>)
-//DLAF_SOLVER_TRIANGULAR_MC_ETI(, std::complex<double>)
+DLAF_SOLVER_TRIANGULAR_MC_ETI(, float)
+DLAF_SOLVER_TRIANGULAR_MC_ETI(, double)
+DLAF_SOLVER_TRIANGULAR_MC_ETI(, std::complex<float>)
+DLAF_SOLVER_TRIANGULAR_MC_ETI(, std::complex<double>)
 
 }
 }

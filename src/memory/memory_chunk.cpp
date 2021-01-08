8aaaac0b4e460ebf8a2c216b7ddf672979acd8d0
Mikael Simberg <simberg@cscs.ch>
2021-01-08 16:24:57 +0100

    Only allocate host memory with Umpire when CUDA is disabled

--- src/memory/memory_chunk.cpp
+++ src/memory/memory_chunk.cpp
@@ -7,6 +7,8 @@
 
 namespace dlaf {
 namespace memory {
+#ifdef DLAF_WITH_CUDA
+  // TODO: Initial pool size must be configurable.
   umpire::Allocator& getHostAllocator() {
     static auto host_allocator = umpire::ResourceManager::getInstance().getAllocator("PINNED");
     static auto pooled_host_allocator = umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::DynamicPool>("PINNED_pool", host_allocator, 4ul << 30);
@@ -20,6 +22,14 @@ namespace memory {
     static auto thread_safe_pooled_device_allocator = umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::ThreadSafeAllocator>("DEVICE_thread_safe_pool", pooled_device_allocator);
     return thread_safe_pooled_device_allocator;
   }
+#else
+  umpire::Allocator& getHostAllocator() {
+    static auto host_allocator = umpire::ResourceManager::getInstance().getAllocator("HOST");
+    static auto pooled_host_allocator = umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::DynamicPool>("HOST_pool", host_allocator, 4ul << 30);
+    static auto thread_safe_pooled_host_allocator = umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::ThreadSafeAllocator>("HOST_thread_safe_pool", pooled_host_allocator);
+    return thread_safe_pooled_host_allocator;
+  }
+#endif
 }
 }
 

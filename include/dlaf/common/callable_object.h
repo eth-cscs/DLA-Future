01d5be16b9296f837ab28d2799baa25aaf065190
Mikael Simberg <simberg@cscs.ch>
2021-01-14 17:07:06 +0100

    Make callable object wrapper const, conditionally noexcept, and SFINAE friendly

--- include/dlaf/common/callable_object.h
+++ include/dlaf/common/callable_object.h
@@ -17,11 +17,12 @@
 /// require explicit selection of the overload when passing the function to
 /// higher-order functions. With this wrapper the overload is selected inside
 /// the wrapper's operator().
-#define DLAF_MAKE_CALLABLE_OBJECT(fname)     \
-  constexpr struct fname##_t {               \
-    template <typename... Ts>                \
-    decltype(auto) operator()(Ts&&... ts) {  \
-      return fname(std::forward<Ts>(ts)...); \
-    }                                        \
-  } fname##_o {                              \
+#define DLAF_MAKE_CALLABLE_OBJECT(fname)                                                 \
+  constexpr struct fname##_t {                                                           \
+    template <typename... Ts>                                                            \
+    auto operator()(Ts&&... ts) const noexcept(noexcept(fname(std::forward<Ts>(ts)...))) \
+        -> decltype(fname(std::forward<Ts>(ts)...)) {                                    \
+      return fname(std::forward<Ts>(ts)...);                                             \
+    }                                                                                    \
+  } fname##_o {                                                                          \
   }

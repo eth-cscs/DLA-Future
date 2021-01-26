47d65a972c01a5ca74e2d52b92295fe4b5c8a96b
Teodor Nikolov <teodor.nikolov22@gmail.com>
2021-01-26 16:14:23 +0100

    Reduce LOCs

--- scripts/miniapps.py
+++ scripts/miniapps.py
@@ -90,50 +90,15 @@ def chol(lib, build_dir, nodes, rpn, m_sz, mb_sz, nruns):
 
     if lib.startswith("dlaf"):
         extra_flags = _dlaf_flags(lib)
-        cmds = (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/miniapp/miniapp_cholesky "
-            f"--matrix-size {m_sz} "
-            f"--block-size {mb_sz} "
-            f"--grid-rows {grid_rows} "
-            f"--grid-cols {grid_cols} "
-            f"--nruns {nruns} "
-            f"--hpx:use-process-mask {extra_flags}"
-            f">> chol_{lib}.out"
-        )
-        return sub(" +", " ", cmds)
+        cmd = f"{build_dir}/miniapp/miniapp_cholesky --matrix-size {m_sz} --block-size {mb_sz} --grid-rows {grid_rows} --grid-cols {grid_cols} --nruns {nruns} --hpx:use-process-mask {extra_flags}"
     elif lib == "slate":
-        return (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/test/slate_test potrf "
-            f"--dim {m_sz}x${m_sz}x0 "
-            f"--nb {mb_sz} "
-            f"--p {grid_rows} "
-            f"--q {grid_cols} "
-            f"--repeat {nruns} "
-            "--check n --ref n --type d "
-            f">> chol_{lib}.out"
-        )
+        cmd = f"{build_dir}/test/slate_test potrf --dim {m_sz}x${m_sz}x0 --nb {mb_sz} --p {grid_rows} --q {grid_cols} --repeat {nruns} --check n --ref n --type d"
     elif lib == "dplasma":
-        return (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/tests/testing_dpotrf "
-            f"-N ${m_sz} "
-            f"--MB ${mb_sz} "
-            f"--NB ${mb_sz} "
-            f"--grid-rows ${grid_rows} "
-            f"--grid-cols ${grid_cols} "
-            "-c 36 -v "
-            f">> chol_{lib}.out"
-        )
+        cmd = f"{build_dir}/tests/testing_dpotrf -N ${m_sz} --MB ${mb_sz} --NB ${mb_sz} --grid-rows ${grid_rows} --grid-cols ${grid_cols} -c 36 -v"
     else:
         raise ValueError(_err_msg(lib))
+
+    return f"\nsrun -n {total_ranks} -c {cpus_per_rank} {cmd} >> chol_{lib}.out"
 
 
 # lib: allowed libraries are dlaf|dlaf_nb_[0|1][0|1][0|1]|slate|dplasma
@@ -152,50 +117,12 @@ def trsm(lib, build_dir, nodes, rpn, m_sz, n_sz, mb_sz, nruns):
 
     if lib.startswith("dlaf"):
         extra_flags = _dlaf_flags(lib)
-        cmds = (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/miniapp/miniapp_triangular_solver "
-            f"--m {m_sz} "
-            f"--n {n_sz} "
-            f"--mb {mb_sz} "
-            f"--nb {mb_sz} "
-            f"--grid-rows {gr} "
-            f"--grid-cols {gc} "
-            f"--nruns {nruns} "
-            f"--hpx:use-process-mask {extra_flags}"
-            f">> trsm_{lib}.out"
-        )
-        return sub(" +", " ", cmds)
+        cmd = f"{build_dir}/miniapp/miniapp_triangular_solver --m {m_sz} --n {n_sz} --mb {mb_sz} --nb {mb_sz} --grid-rows {gr} --grid-cols {gc} --nruns {nruns} --hpx:use-process-mask {extra_flags}"
     elif lib == "slate":
-        return (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/test/slate_test trsm "
-            f"--dim {m_sz}x{n_sz}x0 "
-            f"--nb {mb_sz} "
-            f"--p {gr} "
-            f"--q {gc} "
-            f"--repeat {nruns} "
-            "--alpha 2 --check n --ref n --type d "
-            f">> trsm_{lib}.out"
-        )
+        cmd = f"{build_dir}/test/slate_test trsm --dim {m_sz}x{n_sz}x0 --nb {mb_sz} --p {gr} --q {gc} --repeat {nruns} --alpha 2 --check n --ref n --type d >> trsm_{lib}.out"
     elif lib == "dplasma":
-        return (
-            "\nsrun "
-            f"-n {total_ranks} "
-            f"-c {cpus_per_rank} "
-            f"{build_dir}/tests/testing_dtrsm "
-            f"-M {m_sz} "
-            f"-N {n_sz} "
-            f"--MB {mb_sz} "
-            f"--NB {mb_sz} "
-            f"--grid-rows {gr} "
-            f"--grid-cols {gc} "
-            "-c 36 -v "
-            f">> trsm_{lib}.out"
-        )
+        cmd = f"{build_dir}/tests/testing_dtrsm -M {m_sz} -N {n_sz} --MB {mb_sz} --NB {mb_sz} --grid-rows {gr} --grid-cols {gc} -c 36 -v >> trsm_{lib}.out"
     else:
         raise ValueError(_err_msg(lib))
+
+    return f"\nsrun -n {total_ranks} -c {cpus_per_rank} {cmd} >> trsm_{lib}.out"

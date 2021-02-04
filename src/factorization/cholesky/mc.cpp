e0004a083541db6a730b8ce437e8847e44a489c2
Mikael Simberg <simberg@cscs.ch>
2021-02-04 12:52:09 +0100

    Fix Cholesky MC/CPU ETI

--- src/factorization/cholesky/mc.cpp
+++ src/factorization/cholesky/mc.cpp
@@ -4,10 +4,10 @@ namespace dlaf {
 namespace factorization {
 namespace internal {
 
-DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::GPU, Device::GPU, float)
-DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::GPU, Device::GPU, double)
-DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::GPU, Device::GPU, std::complex<float>)
-DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::GPU, Device::GPU, std::complex<double>)
+DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::MC, Device::CPU, float)
+DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::MC, Device::CPU, double)
+DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::MC, Device::CPU, std::complex<float>)
+DLAF_FACTORIZATION_CHOLESKY_ETI(, Backend::MC, Device::CPU, std::complex<double>)
 }
 }
 }

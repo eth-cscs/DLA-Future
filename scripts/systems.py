4ac3d993c58df6820b893c9d0a77ff4e9c062994
Mikael Simberg <simberg@cscs.ch>
2021-02-05 13:04:13 +0100

    Add daint-gpu to systems.py

--- scripts/systems.py
+++ scripts/systems.py
@@ -27,6 +27,33 @@ printenv > env.txt
 """
 }
 
+cscs["daint-gpu"] = {
+  "Cores": 12,
+  "Threads per core": 2,
+  "Allowed rpns": [1],
+  "Run command": "srun -n {total_ranks} -c {threads_per_rank}",
+  "Batch preamble": """
+#!/bin/bash -l
+#SBATCH --job-name={run_name}_{nodes}
+#SBATCH --time={time_min}
+#SBATCH --nodes={nodes}
+#SBATCH --partition=normal
+#SBATCH --account=csstaff
+#SBATCH --constraint=gpu
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+
+# Env
+export MPICH_MAX_THREAD_SAFETY=multiple
+
+# Debug
+module list &> modules.txt
+printenv > env.txt
+
+# Commands
+"""
+}
+
 cscs["eiger"] = {
   "Cores": 128,
   "Threads per core": 2,

fe0571ff700391e5e6b3eab4d522e58589a2dc9f
Raffaele Solcà <rasolca@cscs.ch>
2021-02-08 13:00:00 +0100

    some changes to commandline environments and allow multiple submissions

--- scripts/miniapps.py
+++ scripts/miniapps.py
@@ -38,7 +38,7 @@ def run_command(system, total_ranks, cpus_per_rank):
 
 # Create the job directory tree and submit jobs.
 #
-def submit_jobs(run_dir, nodes, job_text, suffix="na"):
+def submit_jobs(run_dir, nodes, job_text, suffix="na", submissions=1):
     job_path = expanduser(f"{run_dir}/{nodes}")
     makedirs(job_path, exist_ok=True)
     job_file = f"{job_path}/job_{suffix}.sh"
@@ -46,13 +46,16 @@ def submit_jobs(run_dir, nodes, job_text, suffix="na"):
         f.write(job_text)
 
     print(f"Submitting : {job_file}")
-    system(f"sbatch --chdir={job_path} {job_file}")
+    for i in range(submissions):
+        system(f"sbatch --chdir={job_path} {job_file}")
+        # sleep to not overload the scheduler
+        time.sleep(1)
 
 
 # lib: allowed libraries are dlaf|slate|dplasma
 # rpn: ranks per node
 #
-def chol(system, lib, build_dir, nodes, rpn, m_sz, mb_sz, nruns, suffix="na", extra_flags=""):
+def chol(system, lib, build_dir, nodes, rpn, m_sz, mb_sz, nruns, suffix="na", extra_flags="", env=""):
     _check_ranks_per_node(system, lib, rpn)
 
     total_ranks = nodes * rpn
@@ -60,13 +63,13 @@ def chol(system, lib, build_dir, nodes, rpn, m_sz, mb_sz, nruns, suffix="na", ex
     grid_cols, grid_rows = _sq_factor(total_ranks)
 
     if lib.startswith("dlaf"):
-        env = ""
+        env += " OMP_NUM_THREADS=1"
         cmd = f"{build_dir}/miniapp/miniapp_cholesky --matrix-size {m_sz} --block-size {mb_sz} --grid-rows {grid_rows} --grid-cols {grid_cols} --nruns {nruns} --hpx:use-process-mask {extra_flags}"
     elif lib == "slate":
-        env = f"OMP_NUM_THREADS={cpus_per_rank}"
-        cmd = f"{build_dir}/test/tester potrf --dim {m_sz}x{m_sz}x0 --nb {mb_sz} --p {grid_rows} --q {grid_cols} --repeat {nruns} --check n --ref n --type d {extra_flags}"
+        env += f" OMP_NUM_THREADS={cpus_per_rank}"
+        cmd = f"{build_dir}/test/tester --dim {m_sz}x{m_sz}x0 --nb {mb_sz} --p {grid_rows} --q {grid_cols} --repeat {nruns} --check n --ref n --type d {extra_flags} potrf"
     elif lib == "dplasma":
-        env = "OMP_NUM_THREADS=1"
+        env += " OMP_NUM_THREADS=1"
         cmd = f"{build_dir}/tests/testing_dpotrf -N {m_sz} --MB {mb_sz} --NB {mb_sz} --grid-rows {grid_rows} --grid-cols {grid_cols} -c {cpus_per_rank} -v {extra_flags}"
     else:
         raise ValueError(_err_msg(lib))
@@ -81,7 +84,7 @@ def chol(system, lib, build_dir, nodes, rpn, m_sz, mb_sz, nruns, suffix="na", ex
 # rpn: ranks per node
 #
 def trsm(
-    system, lib, build_dir, nodes, rpn, m_sz, n_sz, mb_sz, nruns, suffix="na", extra_flags=""
+    system, lib, build_dir, nodes, rpn, m_sz, n_sz, mb_sz, nruns, suffix="na", extra_flags="", env=""
 ):
     _check_ranks_per_node(system, lib, rpn)
 
@@ -90,18 +93,18 @@ def trsm(
     gr, gc = _sq_factor(total_ranks)
 
     if lib.startswith("dlaf"):
-        env = ""
+        env += " OMP_NUM_THREADS=1"
         cmd = f"{build_dir}/miniapp/miniapp_triangular_solver --m {m_sz} --n {n_sz} --mb {mb_sz} --nb {mb_sz} --grid-rows {gr} --grid-cols {gc} --nruns {nruns} --hpx:use-process-mask {extra_flags}"
     elif lib == "slate":
-        env = f"OMP_NUM_THREADS={cpus_per_rank}"
-        cmd = f"{build_dir}/test/tester trsm --dim {m_sz}x{n_sz}x0 --nb {mb_sz} --p {gr} --q {gc} --repeat {nruns} --alpha 2 --check n --ref n --type d {extra_flags}"
+        env += f" OMP_NUM_THREADS={cpus_per_rank}"
+        cmd = f"{build_dir}/test/tester --dim {m_sz}x{n_sz}x0 --nb {mb_sz} --p {gr} --q {gc} --repeat {nruns} --alpha 2 --check n --ref n --type d {extra_flags} trsm"
     elif lib == "dplasma":
-        env = "OMP_NUM_THREADS=1"
+        env += " OMP_NUM_THREADS=1"
         cmd = f"{build_dir}/tests/testing_dtrsm -M {m_sz} -N {n_sz} --MB {mb_sz} --NB {mb_sz} --grid-rows {gr} --grid-cols {gc} -c {cpus_per_rank} -v {extra_flags}"
     else:
         raise ValueError(_err_msg(lib))
 
     run_cmd = run_command(system, total_ranks, cpus_per_rank)
     return (
-        "\n" + f"{env} {run_cmd} {cmd} >> trsm_{lib}_{suffix}.out".strip()
+        "\n" + f"{env} {run_cmd} {cmd} >> trsm_{lib}_{suffix}.out 2>&1".strip()
     )

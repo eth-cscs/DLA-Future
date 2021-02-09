64fa3ae0f4cc44cc348533fb771cdd9e7c7cec61
Alberto Invernizzi <invernizzi@cscs.ch>
2021-02-09 12:02:42 +0100

    workaround for original solver

--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -26,7 +26,10 @@ class DlaFuture(CMakePackage, CudaPackage):
     depends_on("blaspp")
     depends_on("lapackpp")
     depends_on("hpx cxxstd=14 networking=none")
-    depends_on("hpx@1.5", when="~cuda")
+    # it would have been "more correct" specifying @1.5 as constraint but, if clingo
+    # solver works as expected, the default solver has problems with that.
+    # Eventually, we opted for specifying the range more explicitly, so it works with both
+    depends_on("hpx@1.5.0:1.5.1", when="~cuda")
     depends_on("hpx@master +cuda", when="+cuda")
 
     depends_on("hpx build_type=Debug", when="build_type=Debug")

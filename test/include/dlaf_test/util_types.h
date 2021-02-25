f7c2e6c9e2fd59db676dea8077cadc0fe9c78819
lara <lara.querciagrossa@gmail.com>
2021-02-25 15:52:56 +0100

    reorganizing test structure

--- test/include/dlaf_test/util_types.h
+++ test/include/dlaf_test/util_types.h
@@ -18,8 +18,9 @@ namespace test {
 
 using ElementTypes =
     ::testing::Types<int, long long, float, double, std::complex<float>, std::complex<double>>;
-using MatrixElementTypes = ::testing::Types<float, double, std::complex<float>, std::complex<double>>;
-
+//using MatrixElementTypes = ::testing::Types<float, double, std::complex<float>, std::complex<double>>;
+ using MatrixElementTypes = ::testing::Types<float, double>;
+ 
 template <class T>
 struct TypeUtilities {
   /// Returns r.
--- test/unit/solver/mc/test_backtransformation.cpp
+++ test/unit/solver/mc/test_backtransformation.cpp
@@ -40,8 +40,7 @@ using namespace testing;
 
 template <typename Type>
 class BackTransformationSolverLocalTest : public ::testing::Test {};
-//TYPED_TEST_SUITE(BackTransformationSolverLocalTest, MatrixElementTypes);
-TYPED_TEST_SUITE(BackTransformationSolverLocalTest, double);
+TYPED_TEST_SUITE(BackTransformationSolverLocalTest, MatrixElementTypes);
 
 //template <typename Type>
 //class BackTransformationSolverDistributedTest : public ::testing::Test {
@@ -52,137 +51,76 @@ TYPED_TEST_SUITE(BackTransformationSolverLocalTest, double);
 //};
 //TYPED_TEST_SUITE(BackTransformationSolverDistributedTest, double);
 
-const std::vector<blas::Side> blas_sides({blas::Side::Left, blas::Side::Right});
-const std::vector<blas::Uplo> blas_uplos({blas::Uplo::Lower, blas::Uplo::Upper});
-const std::vector<blas::Op> blas_ops({blas::Op::NoTrans, blas::Op::Trans, blas::Op::ConjTrans});
-const std::vector<blas::Diag> blas_diags({blas::Diag::NonUnit, blas::Diag::Unit});
-
-//const std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> sizes = {
-//    {0, 0, 1, 1},                                                // m, n = 0
-//    {0, 2, 1, 2}, {7, 0, 2, 1},                                  // m = 0 or n = 0
-//    {2, 2, 5, 5}, {10, 10, 2, 3}, {7, 7, 3, 2},                  // m = n
-//    {3, 2, 7, 7}, {12, 3, 5, 5},  {7, 6, 3, 2}, {15, 7, 3, 5},   // m > n
-//    {2, 3, 7, 7}, {4, 13, 5, 5},  {7, 8, 2, 9}, {19, 25, 6, 5},  // m < n
-//};
-
 GlobalElementSize globalTestSize(const LocalElementSize& size) {
   return {size.rows(), size.cols()};
 }
 
-
-//TYPED_TEST(BackTransformationSolverLocalTest, Correctness_n3_nb1) {
-//  const SizeType m = 3;
-//  const SizeType n = 3;
-//  const SizeType mb = 1;
-//  const SizeType nb = 1;
-//  
-//  // DATA
-//  auto el_C = [](const GlobalElementIndex& index) {
-//    // ColMajor
-//    static const double values[] = {12, 6, -4, -51, 167, 24, 4, -68, -41};
-//    return values[index.row() + 3 * index.col()];
-//  };
-//
-//  auto el_V = [](const GlobalElementIndex& index) {
-//    // ColMajor
-//    static const double values[] = {1, 0.23077, -0.15385, 0, 1, 0.055556, 0, 0, 0};
-//    return values[index.row() + 3 * index.col()];
-//  };
-//
-//  auto el_T = [](const GlobalElementIndex& index) {
-//    // ColMajor
-//    static const double values[] = {1.8571, 0.0, 0.0, -0.82, 1.9938, 0.0, 0., 0., 0.};
-//    return values[index.row() + 3 * index.col()];
-//  };
-//
-//  // RESULT
-//  auto res = [](const GlobalElementIndex& index) {
-//    // ColMajor
-//    static const double values[] = {-14., 0., 0., -21., -175., 0., 14., 70., -35.};
-//    return values[index.row() + 3 * index.col()];
-//  };
-//
-//  LocalElementSize sizeC(m, n);
-//  TileElementSize blockSizeC(mb, nb);
-//  Matrix<double, Device::CPU> mat_c(sizeC, blockSizeC);
-//  set(mat_c, el_C);
-//
-//  LocalElementSize sizeV(m, n);
-//  TileElementSize blockSizeV(mb, nb);
-//  Matrix<double, Device::CPU> mat_v(sizeV, blockSizeV);
-//  set(mat_v, el_V);
-//
-//  LocalElementSize sizeT(m, n);
-//  TileElementSize blockSizeT(mb, nb);
-//  Matrix<double, Device::CPU> mat_t(sizeT, blockSizeT);
-//  set(mat_t, el_T);
-//
-//  solver::backTransformation<Backend::MC>(mat_c, mat_v, mat_t);
-//
-//  double error = 0.1;
-//  CHECK_MATRIX_NEAR(res, mat_c, error, error);
-//}
-//
-//
-
-void set_zero(Matrix<double, Device::CPU>& mat) {
-  set(mat, [](auto&&){return 0;});
+template<class T>
+void set_zero(Matrix<T, Device::CPU>& mat) {
+  set(mat, [](auto&&){return static_cast<T>(0.0);});
 }
+
+const std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> sizes = {
+    {3, 3, 1, 1}, {3, 3, 3, 3},
+    {4, 4, 1, 1}, {4, 4, 4, 4}, {4, 4, 2, 2}, {4, 2, 2, 2}, //{4, 1, 1, 1}, 
+    {6, 6, 3, 3}, {6, 3, 3, 3},   // {6, 1, 3, 3},
+    {12, 2, 2, 2}, // {12, 2, 4, 4}, {12, 2, 3, 3},
+    {12, 12, 2, 2}, {12, 12, 3, 3}, {12, 12, 4, 4},
+    //{12, 24, 2, 2}, {12, 24, 3, 3}, {12, 24, 4, 4},
+    //{24, 12, 4, 4}
+};
 
 template<class T>
 MatrixLocal<T> makeLocal(const Matrix<const T, Device::CPU>& matrix) {
   return {matrix.size(), matrix.distribution().blockSize()};
 }
 
-TYPED_TEST(BackTransformationSolverLocalTest, Correctness_random) {
-  const SizeType m = 16;
-  const SizeType n = 16;
-  const SizeType mb = 4;
-  const SizeType nb = 4;
+template <class T>
+void testBacktransformationEigenv(SizeType m, SizeType n, SizeType mb, SizeType nb) {
 
-  // TODO: only for DOUBLE so far!!!
   LocalElementSize sizeC(m, n);
   TileElementSize blockSizeC(mb, nb);
-  Matrix<double, Device::CPU> mat_c(sizeC, blockSizeC);
+  Matrix<T, Device::CPU> mat_c(sizeC, blockSizeC);
   dlaf::matrix::util::set_random(mat_c);
-  std::cout << "Random matrix C" << std::endl;
-  printElements(mat_c);
+  //std::cout << "Random matrix C" << std::endl;
+  //printElements(mat_c);
 
   LocalElementSize sizeV(m, m);
   TileElementSize blockSizeV(mb, mb);
-  Matrix<double, Device::CPU> mat_v(sizeV, blockSizeV);
+  Matrix<T, Device::CPU> mat_v(sizeV, blockSizeV);
   dlaf::matrix::util::set_random(mat_v);
-  std::cout << "Random matrix V" << std::endl;
-  printElements(mat_v);
+  //std::cout << "Random matrix V" << std::endl;
+  //printElements(mat_v);
   
   // Impose orthogonality: Q = I - v tau v^H is orthogonal (Q Q^H = I)
   // leads to tau = 2/(vT v) for real 
+  // TODO: COMPLEX!!
   LocalElementSize sizeTau(m, 1);
   TileElementSize blockSizeTau(1, 1);
-  Matrix<double, Device::CPU> mat_tau(sizeTau, blockSizeTau);
+  Matrix<T, Device::CPU> mat_tau(sizeTau, blockSizeTau);
   
   LocalElementSize sizeT(mb, m);
   TileElementSize blockSizeT(mb, nb);
-  Matrix<double, Device::CPU> mat_t(sizeT, blockSizeT);
+  Matrix<T, Device::CPU> mat_t(sizeT, blockSizeT);
   set_zero(mat_t);
 
   comm::CommunicatorGrid comm_grid(MPI_COMM_WORLD, 1, 1, common::Ordering::ColumnMajor);
 
   // Copy C matrix locally
-  auto mat_c_loc = dlaf::matrix::test::all_gather<double>(mat_c, comm_grid);
+  auto mat_c_loc = dlaf::matrix::test::all_gather<T>(mat_c, comm_grid);
 
   // Copy T matrix locally
-  auto mat_t_loc = dlaf::matrix::test::all_gather<double>(mat_t, comm_grid);
+  auto mat_t_loc = dlaf::matrix::test::all_gather<T>(mat_t, comm_grid);
 
   // Copy V matrix locally
-  auto mat_v_loc = dlaf::matrix::test::all_gather<double>(mat_v, comm_grid);
+  auto mat_v_loc = dlaf::matrix::test::all_gather<T>(mat_v, comm_grid);
   // Reset diagonal and upper values of V
   lapack::laset(lapack::MatrixType::General, std::min(m,mb), mat_v_loc.size().cols(), 0, 0, mat_v_loc.ptr(), mat_v_loc.ld());
   if (m > mb) {
     lapack::laset(lapack::MatrixType::Upper, mat_v_loc.size().rows()-mb, mat_v_loc.size().cols(), 0, 1, mat_v_loc.ptr(GlobalElementIndex{mb, 0}), mat_v_loc.ld());
   }
 
-  MatrixLocal<double> taus({m, 1}, {1, 1});
+  MatrixLocal<T> taus({m, 1}, {1, 1});
   // Compute taus (real case: tau = 2 / v^H v)
   const int tottaus = (m/mb-1)*mb-1;
   for (SizeType i = tottaus; i > -1; --i) {
@@ -194,10 +132,10 @@ TYPED_TEST(BackTransformationSolverLocalTest, Correctness_random) {
     lapack::larf(lapack::Side::Left, m-i, n, mat_v_loc.ptr(v_offset), 1, tau, mat_c_loc.ptr(GlobalElementIndex{i,0}), mat_c_loc.ld());
   }
 
-  std::cout << " " << std::endl;
-  std::cout << " Result simple way " << std::endl;
-  dlaf::matrix::test::print(format::numpy{}, "mat Cloc ", mat_c_loc, std::cout);
-  std::cout << " " << std::endl;
+//  std::cout << " " << std::endl;
+//  std::cout << " Result simple way " << std::endl;
+//  dlaf::matrix::test::print(format::numpy{}, "mat Cloc ", mat_c_loc, std::cout);
+//  std::cout << " " << std::endl;
   
   for (SizeType i = mat_t.nrTiles().cols()-1; i > -1; --i) {
       const GlobalElementIndex offset{i*nb, i*nb};
@@ -206,14 +144,15 @@ TYPED_TEST(BackTransformationSolverLocalTest, Correctness_random) {
       lapack::larft(lapack::Direction::Forward, lapack::StoreV::Columnwise, mat_v.size().rows()-i*nb, nb, mat_v_loc.ptr(offset), mat_v_loc.ld(), taus.ptr(tau_offset), tile_t.ptr(), tile_t.ld());
   }
   
-  std::cout << " " << std::endl;
-  std::cout << "Matrix T: " << mat_t << std::endl;
-  printElements(mat_t);
+  //std::cout << " " << std::endl;
+  //std::cout << "Matrix T: " << mat_t << std::endl;
+  //printElements(mat_t);
   
-  std::cout << " " << std::endl;
   solver::backTransformation<Backend::MC>(mat_c, mat_v, mat_t);
-  std::cout << "Output " << std::endl;
-  printElements(mat_c);
+
+  //std::cout << " " << std::endl;
+  //std::cout << "Output " << std::endl;
+  //printElements(mat_c);
 
   auto result = [& dist = mat_c.distribution(),
 		 &mat_local = mat_c_loc](const GlobalElementIndex& element) {
@@ -224,4 +163,16 @@ TYPED_TEST(BackTransformationSolverLocalTest, Correctness_random) {
   
   double error = 0.01;
   CHECK_MATRIX_NEAR(result, mat_c, error, error);
+}
+
+
+TYPED_TEST(BackTransformationSolverLocalTest, Correctness_random) {
+
+  SizeType m, n, mb, nb;
+
+  for (auto sz : sizes) {
+    std::tie(m, n, mb, nb) = sz;
+    testBacktransformationEigenv<TypeParam>(m, n, mb, nb);
+  }
+    
 }

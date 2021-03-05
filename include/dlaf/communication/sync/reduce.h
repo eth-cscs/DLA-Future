a071be3936d948c8a1ecc2e4b8bc39caa3881bff
Alberto Invernizzi <invernizzi@cscs.ch>
2021-03-05 09:58:29 +0100

    extend api with in-place + minor renamings

--- include/dlaf/communication/sync/reduce.h
+++ include/dlaf/communication/sync/reduce.h
@@ -25,13 +25,12 @@ namespace sync {
 namespace internal {
 namespace reduce {
 
-/// MPI_Reduce wrapper (sender side).
+/// MPI_Reduce wrapper (collector side, i.e. send and receive).
 ///
 /// MPI Reduce(see MPI documentation for additional info).
-/// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator.
+/// @param reduce_op MPI_Op to perform on @p input data coming from ranks in @p communicator.
 template <class DataIn, class DataOut>
-void collector(Communicator& communicator, MPI_Op reduce_operation, const DataIn input,
-               const DataOut output) {
+void collector(Communicator& communicator, MPI_Op reduce_op, const DataIn input, const DataOut output) {
   using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
 
   // Wayout for single rank communicator, just copy data
@@ -51,22 +50,20 @@ void collector(Communicator& communicator, MPI_Op reduce_operation, const DataIn
     common::copy(input, buffer_in);
 
   DLAF_MPI_CALL(MPI_Reduce(message_input.data(), message_output.data(), message_input.count(),
-                           message_input.mpi_type(), reduce_operation, communicator.rank(),
-                           communicator));
+                           message_input.mpi_type(), reduce_op, communicator.rank(), communicator));
 
   // if the output buffer has been used, copy-back output values
   if (buffer_out)
     common::copy(buffer_out, output);
 }
 
-/// MPI_Reduce wrapper (receiver side).
+/// MPI_Reduce wrapper (sender side).
 ///
 /// MPI Reduce(see MPI documentation for additional info).
 /// @param rank_root  the rank that will collect the result in output,
-/// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator.
+/// @param reduce_op MPI_Op to perform on @p input data coming from ranks in @p communicator.
 template <class DataIn>
-void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operation,
-                 const DataIn input) {
+void participant(IndexT_MPI rank_root, Communicator& communicator, MPI_Op reduce_op, const DataIn input) {
   using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
 
   // Buffers not allocated, just placeholders in case we need to allocate them
@@ -79,28 +76,79 @@ void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operat
     common::copy(input, buffer_in);
 
   DLAF_MPI_CALL(MPI_Reduce(message_input.data(), nullptr, message_input.count(),
-                           message_input.mpi_type(), reduce_operation, rank_root, communicator));
+                           message_input.mpi_type(), reduce_op, rank_root, communicator));
 }
 }
 }
 
-/// MPI_Reduce wrapper.
+/// MPI_Reduce wrapper (both sides).
 ///
 /// MPI Reduce(see MPI documentation for additional info).
 /// @param rank_root the rank that will collect the result in output,
-/// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator,
+/// @param reduce_op MPI_Op to perform on @p input data coming from ranks in @p communicator,
 /// @pre @p rank_root < @p communicator.size(),
 /// @pre @p rank_root != MPI_UNDEFINED.
 template <class DataIn, class DataOut>
-void reduce(const int rank_root, Communicator& communicator, MPI_Op reduce_operation, const DataIn input,
+void reduce(const IndexT_MPI rank_root, Communicator& communicator, MPI_Op reduce_op, const DataIn input,
             const DataOut output) {
   DLAF_ASSERT(rank_root < communicator.size() && rank_root != MPI_UNDEFINED, rank_root,
               communicator.size());
 
   if (rank_root == communicator.rank())
-    internal::reduce::collector(communicator, reduce_operation, input, output);
+    internal::reduce::collector(communicator, reduce_op, input, output);
   else
-    internal::reduce::participant(rank_root, communicator, reduce_operation, input);
+    internal::reduce::participant(rank_root, communicator, reduce_op, input);
+}
+
+/// MPI_Reduce wrapper (collector-side, in-place).
+///
+/// MPI Reduce(see MPI documentation for additional info).
+/// It uses the MPI_IN_PLACE option, so the result of the reduce is stored in the same buffer
+/// used for the input, i.e. @p inout
+///
+/// It must be highlighted that if the buffer is not contiguous, it will get copied to a support
+/// buffer, and then copied back. From the user perspective it is still in-place, but in that specific
+/// case there will be internal memory copies.
+///
+/// @param reduce_op MPI_Op to perform on @p inout data coming from ranks in @p communicator,
+/// @pre @p rank_root < @p communicator.size(),
+template <class DataInOut>
+void reduce(Communicator& communicator, MPI_Op reduce_op, const DataInOut inout) {
+  using T = std::remove_const_t<typename common::data_traits<DataInOut>::element_t>;
+
+  // Wayout for single rank communicator, just copy data
+  if (communicator.size() == 1) {
+    return;
+  }
+
+  // Buffers not allocated, just placeholders in case we need to allocate them
+  common::Buffer<T> buffer_inout;
+
+  auto message_inout = comm::make_message(make_contiguous(inout, buffer_inout));
+
+  // if the input buffer has been used, initialize it with input values
+  if (buffer_inout)
+    common::copy(inout, buffer_inout);
+
+  DLAF_MPI_CALL(MPI_Reduce(MPI_IN_PLACE, message_inout.data(), message_inout.count(),
+                           message_inout.mpi_type(), reduce_op, communicator.rank(), communicator));
+
+  // if the output buffer has been used, copy-back output values
+  if (buffer_inout)
+    common::copy(buffer_inout, inout);
+}
+
+/// MPI_Reduce wrapper (sender side).
+///
+/// MPI Reduce(see MPI documentation for additional info).
+///
+/// This is an helper function for the sender side, with just needed arguments (i.e. output parameter
+/// is meaningful just on the sender side).
+/// @param rank_root  the rank that will collect the result in output,
+/// @param reduce_op MPI_Op to perform on @p input data coming from ranks in @p communicator.
+template <class DataIn>
+void reduce(IndexT_MPI rank_root, Communicator& communicator, MPI_Op reduce_op, const DataIn input) {
+  internal::reduce::participant(rank_root, communicator, reduce_op, input);
 }
 }
 }

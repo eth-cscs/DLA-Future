a15246176e2d0500ff8bfc06418a57477137248a
Alberto Invernizzi <invernizzi@cscs.ch>
2021-03-17 18:46:03 +0100

    add test for allReduceInPlace

--- test/unit/communication/test_all_reduce.cpp
+++ test/unit/communication/test_all_reduce.cpp
@@ -24,6 +24,7 @@ using namespace dlaf::comm::test;
 using namespace dlaf::test;
 
 using AllReduceTest = SplittedCommunicatorsTest;
+using AllReduceInPlaceTest = SplittedCommunicatorsTest;
 
 using TypeParam = std::complex<double>;
 
@@ -188,4 +189,111 @@ TEST_F(AllReduceTest, ContiguousToStrided) {
       EXPECT_LE(std::abs(value * static_cast<TypeParam>(NUM_MPI_RANKS) - data_strided[mem_pos]),
                 NUM_MPI_RANKS * TypeUtilities<TypeParam>::error);
     }
+}
+
+TEST_F(AllReduceInPlaceTest, ValueOnSingleRank) {
+  CommunicatorGrid alone_grid(world, 1, 1, common::Ordering::RowMajor);
+
+  Communicator alone_world = alone_grid.rowCommunicator();
+
+  // just the master rank has to reduce
+  if (alone_world == MPI_COMM_NULL)
+    return;
+
+  constexpr int root = 0;
+  constexpr TypeParam value = TypeUtilities<TypeParam>::element(13, 26);
+  TypeParam result = value;
+
+  ASSERT_EQ(alone_world.rank(), root);
+
+  sync::allReduceInPlace(alone_world, MPI_SUM, common::make_data(&result, 1));
+
+  EXPECT_LE(std::abs(value - result), TypeUtilities<TypeParam>::error);
+}
+
+TEST_F(AllReduceInPlaceTest, CArrayOnSingleRank) {
+  CommunicatorGrid alone_grid(world, 1, 1, common::Ordering::RowMajor);
+
+  Communicator alone_world = alone_grid.rowCommunicator();
+
+  // just the master rank has to reduce
+  if (alone_world == MPI_COMM_NULL)
+    return;
+
+  constexpr int root = 0;
+  constexpr SizeType N = 3;
+  constexpr TypeParam input[N] = {TypeUtilities<TypeParam>::element(0, 1),
+                                  TypeUtilities<TypeParam>::element(1, 2),
+                                  TypeUtilities<TypeParam>::element(2, 3)};
+  TypeParam reduced[N];
+  std::copy(input, input + N, reduced);
+
+  ASSERT_EQ(alone_world.rank(), root);
+
+  sync::allReduceInPlace(alone_world, MPI_SUM, common::make_data(reduced, N));
+
+  for (SizeType index = 0; index < N; ++index)
+    EXPECT_LE(std::abs(input[index] - reduced[index]), TypeUtilities<TypeParam>::error);
+}
+
+TEST_F(AllReduceInPlaceTest, Value) {
+  constexpr TypeParam value = TypeUtilities<TypeParam>::element(13, 26);
+  TypeParam result = value;
+
+  sync::allReduceInPlace(world, MPI_SUM, common::make_data(&result, 1));
+
+  EXPECT_LE(std::abs(value * static_cast<TypeParam>(NUM_MPI_RANKS) - result),
+            NUM_MPI_RANKS * TypeUtilities<TypeParam>::error);
+}
+
+TEST_F(AllReduceInPlaceTest, CArray) {
+  static_assert(NUM_MPI_RANKS >= 2, "This test requires at least two ranks");
+
+  constexpr SizeType N = 3;
+  constexpr TypeParam input[N] = {TypeUtilities<TypeParam>::element(0, 1),
+                                  TypeUtilities<TypeParam>::element(1, 2),
+                                  TypeUtilities<TypeParam>::element(2, 3)};
+  TypeParam reduced[N];
+  std::copy(input, input + N, reduced);
+
+  sync::allReduceInPlace(world, MPI_SUM, common::make_data(reduced, N));
+
+  for (SizeType index = 0; index < N; ++index)
+    EXPECT_LE(std::abs(input[index] * static_cast<TypeParam>(NUM_MPI_RANKS) - reduced[index]),
+              NUM_MPI_RANKS * TypeUtilities<TypeParam>::error);
+}
+
+TEST_F(AllReduceInPlaceTest, WithStrided) {
+  static_assert(NUM_MPI_RANKS >= 2, "This test requires at least two ranks");
+
+  // 3 blocks, 2 elements each, with a distance of 5 elements between start of each block
+  // E E - - - E E - - - E E    (without padding at the end)
+  constexpr SizeType nblocks = 3;
+  constexpr SizeType block_size = 2;
+  constexpr SizeType block_distance = 5;
+
+  constexpr SizeType memory_footprint = (nblocks - 1) * block_distance + block_size;
+  TypeParam data_strided[memory_footprint];
+
+  constexpr TypeParam value = TypeUtilities<TypeParam>::element(13, 26);
+  for (SizeType i_block = 0; i_block < nblocks; ++i_block) {
+    for (SizeType i_element = 0; i_element < block_size; ++i_element) {
+      auto mem_pos = i_block * block_distance + i_element;
+      data_strided[mem_pos] = value;
+    }
+  }
+
+  auto message_inout = common::make_data(data_strided, nblocks, block_size, block_distance);
+
+  sync::allReduceInPlace(world, MPI_SUM, std::move(message_inout));
+
+  const TypeParam expected_result = value * static_cast<TypeParam>(NUM_MPI_RANKS);
+
+  for (SizeType i_block = 0; i_block < nblocks; ++i_block) {
+    for (SizeType i_element = 0; i_element < block_size; ++i_element) {
+      auto mem_pos = i_block * block_distance + i_element;
+      EXPECT_LE(std::abs(expected_result - data_strided[mem_pos]),
+                NUM_MPI_RANKS * TypeUtilities<TypeParam>::error);
+    }
+  }
 }

c242b66fe1d78b27fc7ffb9ae347752ce8772b16
Mikael Simberg <simberg@cscs.ch>
2021-04-21 15:31:57 +0200

    Fix spack package dependencies

--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -26,8 +26,8 @@ class DlaFuture(CMakePackage, CudaPackage):
     depends_on("blaspp")
     depends_on("lapackpp")
     depends_on("umpire~examples")
-    depends_on("umpire+cuda~shared", when="+cuda+umpire")
-    depends_on("hpx cxxstd=14 networking=none")
+    depends_on("umpire+cuda~shared", when="+cuda")
+    depends_on("hpx cxxstd=14 networking=none +async_mpi")
     depends_on("hpx@1.6.0:")
     depends_on("hpx +cuda", when="+cuda")
 

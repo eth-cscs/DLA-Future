10ed2e6b294da6ca03f4c7f588c7e49c2e1f964f
Raffaele Solcà <rasolca@cscs.ch>
2021-04-23 17:16:51 +0200

    Added shared future test

--- test/unit/communication/test_comm_matrix.cpp
+++ test/unit/communication/test_comm_matrix.cpp
@@ -20,7 +20,7 @@
 
 using namespace dlaf;
 
-TEST(BcastMatrixTest, Dataflow) {
+TEST(BcastMatrixTest, DataflowFuture) {
   using namespace std::literals;
   comm::Communicator comm(MPI_COMM_WORLD);
   comm::CommunicatorGrid grid(comm, 1, 2, common::Ordering::ColumnMajor);
@@ -35,6 +35,32 @@ TEST(BcastMatrixTest, Dataflow) {
     dlaf::Matrix<double, Device::CPU> mat({sz, 1}, {sz, 1});
     mat(index).get()({sz - 1, 0}) = 1.;
     hpx::dataflow(ex, hpx::util::unwrapping(comm::sendBcast<double>), mat(index), ccomm());
+    hpx::dataflow(hpx::util::unwrapping([sz](auto tile) { tile({sz - 1, 0}) = 2.; }), mat(index));
+    EXPECT_EQ(2., mat(index).get()({sz - 1, 0}));
+  }
+  else {
+    std::this_thread::sleep_for(50ms);
+    auto tile_f = hpx::dataflow(ex, hpx::util::unwrapping(comm::recvBcastAlloc<double>),
+                                TileElementSize{sz, 1}, root, ccomm());
+    EXPECT_EQ(1., tile_f.get()({sz - 1, 0}));
+  }
+}
+
+TEST(BcastMatrixTest, DataflowSharedFuture) {
+  using namespace std::literals;
+  comm::Communicator comm(MPI_COMM_WORLD);
+  comm::CommunicatorGrid grid(comm, 1, 2, common::Ordering::ColumnMajor);
+  common::Pipeline<comm::Communicator> ccomm(comm);
+  comm::Executor ex{};
+
+  int root = 0;
+  int sz = 10000;
+
+  if (comm.rank() == root) {
+    LocalTileIndex index(0, 0);
+    dlaf::Matrix<double, Device::CPU> mat({sz, 1}, {sz, 1});
+    mat(index).get()({sz - 1, 0}) = 1.;
+    hpx::dataflow(ex, hpx::util::unwrapping(comm::sendBcast<double>), mat.read(index), ccomm());
     hpx::dataflow(hpx::util::unwrapping([sz](auto tile) { tile({sz - 1, 0}) = 2.; }), mat(index));
     EXPECT_EQ(2., mat(index).get()({sz - 1, 0}));
   }

052cd7836066638d94efa0da18eece64fe7913e9
Alberto Invernizzi <invernizzi@cscs.ch>
2021-04-26 13:43:34 +0200

    minor optimization: reserve vector space

--- include/dlaf/matrix/matrix.h
+++ include/dlaf/matrix/matrix.h
@@ -38,6 +38,7 @@ auto selectGeneric(Func&& f, common::IterableRange2D<SizeType, LocalTile_TAG> ra
   using RetT = decltype(f(LocalTileIndex{}));
 
   std::vector<RetT> tiles;
+  tiles.reserve(to_sizet(std::distance(range.begin(), range.end())));
   std::transform(range.begin(), range.end(), std::back_inserter(tiles),
                  [&](auto idx) { return f(idx); });
   return tiles;

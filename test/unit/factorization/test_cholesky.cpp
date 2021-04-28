262ad820c030f987af3cfade6306c22a95ba90b7
Mikael Simberg <simberg@cscs.ch>
2021-04-28 12:34:52 +0200

    Remove unnecessary headers

--- test/unit/factorization/test_cholesky.cpp
+++ test/unit/factorization/test_cholesky.cpp
@@ -7,7 +7,6 @@
 // Please, refer to the LICENSE file in the root directory.
 // SPDX-License-Identifier: BSD-3-Clause
 //
-#include "dlaf/communication/mech.h"
 #include "dlaf/factorization/cholesky.h"
 
 #include "gtest/gtest.h"

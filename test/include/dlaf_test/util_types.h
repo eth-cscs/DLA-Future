a153c61e8c3488d5ab885af43718db0ac8e161f6
Mikael Simberg <simberg@cscs.ch>
2021-04-28 11:19:33 +0200

    Remove unnecessary ComplexMatrixElementTypes

--- test/include/dlaf_test/util_types.h
+++ test/include/dlaf_test/util_types.h
@@ -19,7 +19,6 @@ namespace test {
 using ElementTypes =
     ::testing::Types<int, long long, float, double, std::complex<float>, std::complex<double>>;
 using MatrixElementTypes = ::testing::Types<float, double, std::complex<float>, std::complex<double>>;
-using ComplexMatrixElementTypes = ::testing::Types<std::complex<float>, std::complex<double>>;
 
 template <class T>
 struct TypeUtilities {

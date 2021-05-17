3dbad0764a0f0103d9c63f3c1a1a5e8a9595d19e
Alberto Invernizzi <invernizzi@cscs.ch>
2021-05-17 14:54:52 +0200

    move to a more straightforward logic for index + reserve space for pool

--- include/dlaf/common/round_robin.h
+++ include/dlaf/common/round_robin.h
@@ -17,18 +17,18 @@ namespace common {
 template <class T>
 struct RoundRobin {
   template <class... Args>
-  RoundRobin(std::size_t n, Args&&... args) : next_index_(0) {
+  RoundRobin(std::size_t n, Args... args) : curr_index_(0) {
+    pool_.reserve(n);
     for (std::size_t i = 0; i < n; ++i)
-      pool_.emplace_back(std::forward<Args>(args)...);
+      pool_.emplace_back(args...);
   }
 
   T& nextResource() {
-    auto idx = (next_index_ + 1) % pool_.size();
-    std::swap(idx, next_index_);
-    return pool_[idx];
+    curr_index_ = (curr_index_ + 1) % pool_.size();
+    return pool_[curr_index_];
   }
 
-  std::size_t next_index_;
+  std::size_t curr_index_;
   std::vector<T> pool_;
 };
 

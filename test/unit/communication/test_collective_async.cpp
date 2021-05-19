efc2653bc10329449a1f8712e1449de60be0f97f
Alberto Invernizzi <invernizzi@cscs.ch>
2021-05-19 10:17:07 +0200

    introduce all_reduce in-place

--- test/unit/communication/test_collective_async.cpp
+++ test/unit/communication/test_collective_async.cpp
@@ -120,3 +120,49 @@ TEST(AllReduce, NotContiguous) {
   }
   EXPECT_EQ(result, matrix.read(index_out).get()({0, 0}));
 }
+
+TEST(AllReduceInPlace, Contiguous) {
+  using namespace dlaf;
+  using namespace std::literals;
+
+  comm::Communicator comm(MPI_COMM_WORLD);
+  comm::CommunicatorGrid grid(comm, 1, 2, common::Ordering::ColumnMajor);
+  common::Pipeline<comm::Communicator> chain(comm);
+
+  auto ex_mpi = getMPIExecutor<Backend::MC>();
+
+  int sz = 4;
+
+  LocalTileIndex index(0, 0);
+  dlaf::Matrix<double, Device::CPU> matrix({sz, 1}, {sz, 1});
+  matrix(index).get()({0, 0}) = comm.rank() == 0 ? 1 : 3;
+  const double result = 4.0;
+  {
+    auto t = scheduleAllReduceInPlace(ex_mpi, chain(), MPI_SUM, matrix(index));
+    EXPECT_EQ(result, t.get()({0, 0}));
+  }
+  EXPECT_EQ(result, matrix.read(index).get()({0, 0}));
+}
+
+TEST(AllReduceInPlace, NotContiguous) {
+  using namespace dlaf;
+  using namespace std::literals;
+
+  comm::Communicator comm(MPI_COMM_WORLD);
+  comm::CommunicatorGrid grid(comm, 1, 2, common::Ordering::ColumnMajor);
+  common::Pipeline<comm::Communicator> chain(comm);
+
+  auto ex_mpi = getMPIExecutor<Backend::MC>();
+
+  int sz = 4;
+
+  LocalTileIndex index(0, 0);
+  dlaf::Matrix<double, Device::CPU> matrix({1, sz}, {1, sz});
+  matrix(index).get()({0, 0}) = comm.rank() == 0 ? 1 : 3;
+  const double result = 4.0;
+  {
+    auto t = scheduleAllReduceInPlace(ex_mpi, chain(), MPI_SUM, matrix(index));
+    EXPECT_EQ(result, t.get()({0, 0}));
+  }
+  EXPECT_EQ(result, matrix.read(index).get()({0, 0}));
+}

ff700355b9822dcddde9a2edd70b5ef413d32350
lara <lara.querciagrossa@gmail.com>
2021-05-21 17:10:16 +0200

    CPU in src file

--- src/eigensolver/backtransformation/mc.cpp
+++ src/eigensolver/backtransformation/mc.cpp
@@ -4,10 +4,10 @@ namespace dlaf {
 namespace eigensolver {
 namespace internal {
 
-DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, float)
-DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, double)
-DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, std::complex<float>)
-DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, std::complex<double>)
+DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, Backend::MC, Device::CPU, float)
+DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, Backend::MC, Device::CPU, double)
+DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, Backend::MC, Device::CPU, std::complex<float>)
+DLAF_EIGENSOLVER_BACKTRANSFORMATION_MC_ETI(, Backend::MC, Device::CPU, std::complex<double>)
 
 }
 }

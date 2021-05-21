48798ed062ae4ff7e1ac3c9f15a9a773e957fe1d
lara <lara.querciagrossa@gmail.com>
2021-05-21 12:05:10 +0200

    adding cublas test

--- test/unit/test_cublas_tile.cpp
+++ test/unit/test_cublas_tile.cpp
@@ -96,6 +96,36 @@ TYPED_TEST(HerkTileOperationsTest, Herk) {
   }
 }
 
+TYPED_TEST(TileOperationsTest, Trmm) {
+  using Type = TypeParam;
+
+  SizeType m, n, extra_lda, extra_ldb;
+
+  std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> sizes =
+      {{0, 0, 0, 0},                 // all 0 sizes
+       {0, 5, 1, 0},  {7, 0, 1, 2},  // one 0 size
+       {1, 1, 0, 3},  {1, 12, 1, 0},  {17, 12, 1, 3}, {11, 23, 0, 3},
+       {9, 12, 1, 1}, {32, 32, 0, 0}, {32, 32, 4, 7}};
+
+  for (const auto side : blas_sides) {
+    for (const auto uplo : blas_uplos) {
+      for (const auto op : blas_ops) {
+        for (const auto diag : blas_diags) {
+          for (const auto& size : sizes) {
+            std::tie(m, n, extra_lda, extra_ldb) = size;
+
+            // Test a const Tile.
+            testTrmm<TileElementIndex, Type>(side, uplo, op, diag, m, n, extra_lda, extra_ldb);
+
+            // Test a non const Tile.
+            testTrmm<TileElementIndex, Type, Type>(side, uplo, op, diag, m, n, extra_lda, extra_ldb);
+          }
+        }
+      }
+    }
+  }
+}
+
 TYPED_TEST(TileOperationsTest, Trsm) {
   using Type = TypeParam;
 

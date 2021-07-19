569064a5541a912805202edf1734367b8632fb8e
Lara Querciagrossa <191-lquerci1@users.noreply.gitlab.hpc.cineca.it>
2021-07-20 01:14:01 +0200

    clang format

--- test/unit/eigensolver/mc/test_backtransformation.cpp
+++ test/unit/eigensolver/mc/test_backtransformation.cpp
@@ -43,8 +43,8 @@ GlobalElementSize globalTestSize(const LocalElementSize& size) {
 }
 
 const std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> sizes =
-  // m, n, mb, nb
-  {{3, 0, 1, 1},   {0, 5, 2, 3},  // m, n = 0
+    // m, n, mb, nb
+    {{3, 0, 1, 1},   {0, 5, 2, 3},  // m, n = 0
      {2, 2, 3, 3},   {3, 4, 6, 7},  // m < mb
      {3, 3, 1, 1},   {4, 4, 2, 2}, {6, 3, 3, 3}, {12, 2, 4, 4}, {12, 24, 3, 3},
      {24, 36, 6, 6}, {5, 8, 3, 2}, {4, 6, 2, 3}, {5, 5, 2, 3},  {8, 27, 3, 4},
@@ -98,11 +98,11 @@ void testBacktransformationEigenv(SizeType m, SizeType n, SizeType mb, SizeType 
         tile::set0<T>(v.tile(ij_tile));
       }
       else if (ij_tile.row() == ij_tile.col() + 1) {
-	copy(source_tile, v.tile(ij_tile));
+        copy(source_tile, v.tile(ij_tile));
         tile::laset<T>(lapack::MatrixType::Upper, 0.f, 1.f, v.tile(ij_tile));
       }
       else {
-	copy(source_tile, v.tile(ij_tile));
+        copy(source_tile, v.tile(ij_tile));
       }
     }
 
@@ -117,11 +117,11 @@ void testBacktransformationEigenv(SizeType m, SizeType n, SizeType mb, SizeType 
     common::internal::vector<hpx::shared_future<common::internal::vector<T>>> taus;
 
     common::internal::vector<T> tausloc;
-    
+
     // Impose orthogonality: Q = I - v tau vH is orthogonal (Q QH = I).
     // Real case: tau = 2 / (vH v)
     // Complex case: real part of tau = [1 + sqrt(1 - vH v taui^2)]/(vH v)
-    for (SizeType k = 0; k < tottaus; k+=mb) {
+    for (SizeType k = 0; k < tottaus; k += mb) {
       common::internal::vector<T> t_tile;
       auto seed = 10000 * k / mb + 1;
       dlaf::matrix::util::internal::getter_random<BaseType<T>> random_value(seed);
@@ -134,7 +134,7 @@ void testBacktransformationEigenv(SizeType m, SizeType n, SizeType mb, SizeType 
         }
         T tau;
         getTau(tau, dotprod, tau_i);
-	tausloc.push_back(static_cast<T>(tau));
+        tausloc.push_back(static_cast<T>(tau));
         t_tile.push_back(static_cast<T>(tau));
       }
       taus.push_back(hpx::make_ready_future(t_tile));

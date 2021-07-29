5dc10c27c5774dcad9f95dab2454d64b8a423974
Lara Querciagrossa <44648667+Sely85@users.noreply.github.com>
2021-07-29 17:31:27 +0200

    Upper version of generalized to standard algorithm (#404)

--- include/dlaf/eigensolver/gen_to_std/api.h
+++ include/dlaf/eigensolver/gen_to_std/api.h
@@ -19,6 +19,8 @@ template <Backend backend, Device device, class T>
 struct GenToStd {
   static void call_L(Matrix<T, device>& mat_a, Matrix<T, device>& mat_l);
   static void call_L(comm::CommunicatorGrid grid, Matrix<T, device>& mat_a, Matrix<T, device>& mat_l);
+  static void call_U(Matrix<T, device>& mat_a, Matrix<T, device>& mat_u);
+  static void call_U(comm::CommunicatorGrid grid, Matrix<T, device>& mat_a, Matrix<T, device>& mat_u);
 };
 
 /// ---- ETI

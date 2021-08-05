b3a41ffdef803c90fa8656a0babc7fb752101061
Mikael Simberg <simberg@cscs.ch>
2021-08-05 13:43:58 +0200

    Add constraints to senderified cholesky helper functions

--- include/dlaf/factorization/cholesky/impl.h
+++ include/dlaf/factorization/cholesky/impl.h
@@ -35,33 +35,144 @@ namespace dlaf {
 namespace factorization {
 namespace internal {
 
+// TODO: Move.
+template <typename ValueTypes>
+struct sender_single_value_type_impl {};
+
+template <typename T>
+struct sender_single_value_type_impl<hpx::util::pack<hpx::util::pack<T>>> {
+  using type = T;
+};
+
+// We are only interested in the types wrapped by future and shared_future since
+// we will internally unwrap them.
+template <typename T>
+struct sender_single_value_type_impl<hpx::util::pack<hpx::util::pack<hpx::future<T>>>> {
+  using type = T;
+};
+
+template <typename T>
+struct sender_single_value_type_impl<hpx::util::pack<hpx::util::pack<hpx::shared_future<T>>>> {
+  using type = T;
+};
+
+// Contains a typedef type if and only if the sender sends a single type. If
+// that is the case, type will be defined to the type sent by the sender.
+template <typename Sender>
+struct sender_single_value_type
+    : sender_single_value_type_impl<typename hpx::execution::experimental::sender_traits<
+          Sender>::template value_types<hpx::util::pack, hpx::util::pack>> {};
+
+template <typename Sender>
+using sender_single_value_type_t = typename sender_single_value_type<Sender>::type;
+
+template <typename T>
+struct tile_sender_element_type_impl {
+  using type = typename T::ElementType;
+};
+
+// Contains a typedef type if and only if the sender sends a single Tile. If
+// that is the case, type will be defined to ElementType of the Tile.
+template <typename Sender>
+struct tile_sender_element_type : tile_sender_element_type_impl<sender_single_value_type_t<Sender>> {};
+
+template <typename Sender>
+using tile_sender_element_type_t = typename tile_sender_element_type<Sender>::type;
+
+// Contains a boolean variable value which is true if and only if the given type
+// is a Tile.
+template <typename T>
+struct is_tile : std::false_type {};
+
+template <typename T, Device device>
+struct is_tile<matrix::Tile<T, device>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_tile_v = is_tile<T>::value;
+
+// Contains a boolean variable value which is true if and only if the given type
+// is a Tile with const ElementType.
+template <typename T>
+struct is_const_tile : std::false_type {};
+
+template <typename T, Device device>
+struct is_const_tile<matrix::Tile<const T, device>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_const_tile_v = is_const_tile<T>::value;
+
+template <typename Sender, typename Enable = void>
+struct is_sender_of_tile : std::false_type {};
+
+// Contains a boolean variable value which is true if and only if the given sender
+// sends a Tile.
+template <typename Sender>
+struct is_sender_of_tile<Sender, std::enable_if_t<is_tile_v<sender_single_value_type_t<Sender>>>>
+    : std::true_type {};
+
+template <typename Sender>
+inline constexpr bool is_sender_of_tile_v = is_sender_of_tile<Sender>::value;
+
+// Contains a boolean variable value which is true if and only if the given sender
+// sends a Tile with const element type.
+template <typename Sender, typename Enable = void>
+struct is_sender_of_const_tile : std::false_type {};
+
+template <typename Sender>
+struct is_sender_of_const_tile<Sender,
+                               std::enable_if_t<is_const_tile_v<sender_single_value_type_t<Sender>>>>
+    : std::true_type {};
+
+template <typename Sender>
+inline constexpr bool is_sender_of_const_tile_v = is_sender_of_const_tile<Sender>::value;
+
+// Contains a boolean variable value which is true if and only all given senders
+// send a tile and the element type of the tiles are the same.
+template <typename Sender, typename... Senders>
+struct have_same_element_types
+    : std::conjunction<
+          std::is_same<tile_sender_element_type_t<Sender>, tile_sender_element_type_t<Senders>>...> {};
+
+template <typename... Senders>
+inline constexpr bool have_same_element_types_v = have_same_element_types<Senders...>::value;
+
 namespace cholesky_l {
 template <Backend backend, class MatrixTileSender>
 void potrfDiagTile(MatrixTileSender&& matrix_tile) {
-  // TODO:
-  // - when_all(...) | algo(policy) | detach()?
-  // - algo(policy, when_all(...)) | detach?
-  // - detach(algo(policy, when_all(...)))?
-  tile::potrf(dlaf::internal::Policy<backend>(hpx::threads::thread_priority::normal),
-              dlaf::internal::whenAllLift(blas::Uplo::Lower,
-                                          std::forward<MatrixTileSender>(matrix_tile))) |
+  static_assert(is_sender_of_tile_v<MatrixTileSender>);
+
+  dlaf::internal::whenAllLift(blas::Uplo::Lower, std::forward<MatrixTileSender>(matrix_tile)) |
+      tile::potrf(dlaf::internal::Policy<backend>(hpx::threads::thread_priority::normal)) |
       hpx::execution::experimental::detach();
 }
 
-template <Backend backend, class T, class KKTileSender, class MatrixTileSender>
+template <Backend backend, class KKTileSender, class MatrixTileSender>
 void trsmPanelTile(KKTileSender&& kk_tile, MatrixTileSender&& matrix_tile) {
+  static_assert(is_sender_of_const_tile_v<KKTileSender>);
+  static_assert(is_sender_of_tile_v<MatrixTileSender>);
+  static_assert(have_same_element_types_v<KKTileSender, MatrixTileSender>);
+
+  using element_type = tile_sender_element_type_t<KKTileSender>;
+
   dlaf::internal::whenAllLift(blas::Side::Right, blas::Uplo::Lower, blas::Op::ConjTrans,
-                              blas::Diag::NonUnit, T(1.0), std::forward<KKTileSender>(kk_tile),
+                              blas::Diag::NonUnit, element_type(1.0),
+                              std::forward<KKTileSender>(kk_tile),
                               std::forward<MatrixTileSender>(matrix_tile)) |
       tile::trsm(dlaf::internal::Policy<backend>(hpx::threads::thread_priority::high)) |
       hpx::execution::experimental::detach();
 }
 
-template <Backend backend, class T, class PanelTileSender, class MatrixTileSender>
+template <Backend backend, class PanelTileSender, class MatrixTileSender>
 void herkTrailingDiagTile(hpx::threads::thread_priority priority, PanelTileSender&& panel_tile,
                           MatrixTileSender&& matrix_tile) {
-  dlaf::internal::whenAllLift(blas::Uplo::Lower, blas::Op::NoTrans, BaseType<T>(-1.0),
-                              std::forward<PanelTileSender>(panel_tile), BaseType<T>(1.0),
+  static_assert(is_sender_of_const_tile_v<PanelTileSender>);
+  static_assert(is_sender_of_tile_v<MatrixTileSender>);
+  static_assert(have_same_element_types_v<PanelTileSender, MatrixTileSender>);
+
+  using base_element_type = BaseType<tile_sender_element_type_t<PanelTileSender>>;
+
+  dlaf::internal::whenAllLift(blas::Uplo::Lower, blas::Op::NoTrans, base_element_type(-1.0),
+                              std::forward<PanelTileSender>(panel_tile), base_element_type(1.0),
                               std::forward<MatrixTileSender>(matrix_tile)) |
       tile::herk(dlaf::internal::Policy<backend>(priority)) | hpx::execution::experimental::detach();
 }
@@ -85,20 +196,33 @@ void potrfDiagTile(MatrixTileSender&& matrix_tile) {
       hpx::execution::experimental::detach();
 }
 
-template <Backend backend, class T, class KKTileSender, class MatrixTileSender>
+template <Backend backend, class KKTileSender, class MatrixTileSender>
 void trsmPanelTile(KKTileSender&& kk_tile, MatrixTileSender&& matrix_tile) {
+  static_assert(is_sender_of_const_tile_v<KKTileSender>);
+  static_assert(is_sender_of_tile_v<MatrixTileSender>);
+  static_assert(have_same_element_types_v<KKTileSender, MatrixTileSender>);
+
+  using element_type = tile_sender_element_type_t<KKTileSender>;
+
   dlaf::internal::whenAllLift(blas::Side::Left, blas::Uplo::Upper, blas::Op::ConjTrans,
-                              blas::Diag::NonUnit, T(1.0), std::forward<KKTileSender>(kk_tile),
+                              blas::Diag::NonUnit, element_type(1.0),
+                              std::forward<KKTileSender>(kk_tile),
                               std::forward<MatrixTileSender>(matrix_tile)) |
       tile::trsm(dlaf::internal::Policy<backend>(hpx::threads::thread_priority::high)) |
       hpx::execution::experimental::detach();
 }
 
-template <Backend backend, class T, class PanelTileSender, class MatrixTileSender>
+template <Backend backend, class PanelTileSender, class MatrixTileSender>
 void herkTrailingDiagTile(hpx::threads::thread_priority priority, PanelTileSender&& panel_tile,
                           MatrixTileSender&& matrix_tile) {
-  dlaf::internal::whenAllLift(blas::Uplo::Upper, blas::Op::ConjTrans, BaseType<T>(-1.0),
-                              std::forward<PanelTileSender>(panel_tile), BaseType<T>(1.0),
+  static_assert(is_sender_of_const_tile_v<PanelTileSender>);
+  static_assert(is_sender_of_tile_v<MatrixTileSender>);
+  static_assert(have_same_element_types_v<PanelTileSender, MatrixTileSender>);
+
+  using base_element_type = BaseType<tile_sender_element_type_t<PanelTileSender>>;
+
+  dlaf::internal::whenAllLift(blas::Uplo::Upper, blas::Op::ConjTrans, base_element_type(-1.0),
+                              std::forward<PanelTileSender>(panel_tile), base_element_type(1.0),
                               std::forward<MatrixTileSender>(matrix_tile)) |
       tile::herk(dlaf::internal::Policy<backend>(priority)) | hpx::execution::experimental::detach();
 }
@@ -130,7 +254,7 @@ void Cholesky<backend, device, T>::call_L(Matrix<T, device>& mat_a) {
 
     for (SizeType i = k + 1; i < nrtile; ++i) {
       // Update panel mat_a(i,k) with trsm (blas operation), using data mat_a.read(k,k)
-      trsmPanelTile<backend, T>(mat_a.read_sender(kk), mat_a.readwrite_sender(LocalTileIndex{i, k}));
+      trsmPanelTile<backend>(mat_a.read_sender(kk), mat_a.readwrite_sender(LocalTileIndex{i, k}));
     }
 
     for (SizeType j = k + 1; j < nrtile; ++j) {
@@ -139,8 +263,8 @@ void Cholesky<backend, device, T>::call_L(Matrix<T, device>& mat_a) {
           (j == k + 1) ? hpx::threads::thread_priority::high : hpx::threads::thread_priority::normal;
 
       // Update trailing matrix: diagonal element mat_a(j,j), reading mat_a.read(j,k), using herk (blas operation)
-      herkTrailingDiagTile<backend, T>(trailing_matrix_priority, mat_a.read_sender(LocalTileIndex{j, k}),
-                                       mat_a.readwrite_sender(LocalTileIndex{j, j}));
+      herkTrailingDiagTile<backend>(trailing_matrix_priority, mat_a.read_sender(LocalTileIndex{j, k}),
+                                    mat_a.readwrite_sender(LocalTileIndex{j, j}));
 
       for (SizeType i = j + 1; i < nrtile; ++i) {
         // Update remaining trailing matrix mat_a(i,j), reading mat_a.read(i,k) and mat_a.read(j,k),
@@ -209,7 +333,7 @@ void Cholesky<backend, device, T>::call_L(comm::CommunicatorGrid grid, Matrix<T,
         const LocalTileIndex local_idx(Coord::Row, i);
         const LocalTileIndex ik_idx(i, distr.localTileFromGlobalTile<Coord::Col>(k));
 
-        trsmPanelTile<backend, T>(panelT.read_sender(diag_wp_idx), mat_a.readwrite_sender(ik_idx));
+        trsmPanelTile<backend>(panelT.read_sender(diag_wp_idx), mat_a.readwrite_sender(ik_idx));
 
         panel.setTile(local_idx, mat_a.read(ik_idx));
       }
@@ -235,8 +359,8 @@ void Cholesky<backend, device, T>::call_L(comm::CommunicatorGrid grid, Matrix<T,
       if (this_rank.row() == owner.row()) {
         const auto i = distr.localTileFromGlobalTile<Coord::Row>(jt_idx);
 
-        herkTrailingDiagTile<backend, T>(trailing_matrix_priority, panel.read_sender({Coord::Row, i}),
-                                         mat_a.readwrite_sender(LocalTileIndex{i, j}));
+        herkTrailingDiagTile<backend>(trailing_matrix_priority, panel.read_sender({Coord::Row, i}),
+                                      mat_a.readwrite_sender(LocalTileIndex{i, j}));
       }
 
       for (SizeType i_idx = jt_idx + 1; i_idx < nrtile; ++i_idx) {
@@ -274,15 +398,15 @@ void Cholesky<backend, device, T>::call_U(Matrix<T, device>& mat_a) {
     potrfDiagTile<backend>(mat_a.readwrite_sender(kk));
 
     for (SizeType j = k + 1; j < nrtile; ++j) {
-      trsmPanelTile<backend, T>(mat_a.read_sender(kk), mat_a.readwrite_sender(LocalTileIndex{k, j}));
+      trsmPanelTile<backend>(mat_a.read_sender(kk), mat_a.readwrite_sender(LocalTileIndex{k, j}));
     }
 
     for (SizeType i = k + 1; i < nrtile; ++i) {
       const auto trailing_matrix_priority =
           (i == k + 1) ? hpx::threads::thread_priority::high : hpx::threads::thread_priority::normal;
 
-      herkTrailingDiagTile<backend, T>(trailing_matrix_priority, mat_a.read_sender(LocalTileIndex{k, i}),
-                                       mat_a.readwrite_sender(LocalTileIndex{i, i}));
+      herkTrailingDiagTile<backend>(trailing_matrix_priority, mat_a.read_sender(LocalTileIndex{k, i}),
+                                    mat_a.readwrite_sender(LocalTileIndex{i, i}));
 
       for (SizeType j = i + 1; j < nrtile; ++j) {
         gemmTrailingMatrixTile<backend, T>(hpx::threads::thread_priority::normal,
@@ -349,7 +473,7 @@ void Cholesky<backend, device, T>::call_U(comm::CommunicatorGrid grid, Matrix<T,
         const LocalTileIndex local_idx(Coord::Col, j);
         const LocalTileIndex kj_idx(distr.localTileFromGlobalTile<Coord::Row>(k), j);
 
-        trsmPanelTile<backend, T>(panelT.read_sender(diag_wp_idx), mat_a.readwrite_sender(kj_idx));
+        trsmPanelTile<backend>(panelT.read_sender(diag_wp_idx), mat_a.readwrite_sender(kj_idx));
 
         panel.setTile(local_idx, mat_a.read(kj_idx));
       }
@@ -375,8 +499,8 @@ void Cholesky<backend, device, T>::call_U(comm::CommunicatorGrid grid, Matrix<T,
       if (this_rank.col() == owner.col()) {
         const auto j = distr.localTileFromGlobalTile<Coord::Col>(it_idx);
 
-        herkTrailingDiagTile<backend, T>(trailing_matrix_priority, panel.read_sender({Coord::Col, j}),
-                                         mat_a.readwrite_sender(LocalTileIndex{i, j}));
+        herkTrailingDiagTile<backend>(trailing_matrix_priority, panel.read_sender({Coord::Col, j}),
+                                      mat_a.readwrite_sender(LocalTileIndex{i, j}));
       }
 
       for (SizeType j_idx = it_idx + 1; j_idx < nrtile; ++j_idx) {

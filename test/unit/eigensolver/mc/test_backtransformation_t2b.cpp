1f014d75e39bf6124ed2f36bcb8200710732f58b
Alberto Invernizzi <invernizzi@cscs.ch>
2021-08-30 18:44:16 +0200

    open to more configs

--- test/unit/eigensolver/mc/test_backtransformation_t2b.cpp
+++ test/unit/eigensolver/mc/test_backtransformation_t2b.cpp
@@ -60,68 +60,79 @@ void computeTaus(const bool limit, const SizeType k, matrix::Tile<T, Device::CPU
   }
 }
 
+struct config_t {
+  const SizeType m, n, mb, nb;
+};
+
+std::vector<config_t> configs {
+  {12, 12, 4, 4},
+  {20, 20, 5, 5},
+};
+
 TYPED_TEST(BacktransformationT2BTest, CorrectnessLocal) {
-  const SizeType m = 12;
-  const SizeType n = 12;
-  const SizeType mb = 4;
-  const SizeType nb = 4;
+  for (const auto& config : configs) {
+    const SizeType m = config.m;
+    const SizeType n = config.n;
+    const SizeType mb = config.mb;
+    const SizeType nb = config.nb;
 
-  const SizeType b = mb;
+    const SizeType b = mb;
 
-  const LocalElementSize sz_e(m, n);
-  const TileElementSize bsz_e(mb, nb);
+    const LocalElementSize sz_e(m, n);
+    const TileElementSize bsz_e(mb, nb);
 
-  const LocalElementSize sz_v(m, m);
-  const TileElementSize bsz_v(mb, mb);
+    const LocalElementSize sz_v(m, m);
+    const TileElementSize bsz_v(mb, mb);
 
-  Matrix<TypeParam, Device::CPU> mat_e(sz_e, bsz_e);
-  set_random(mat_e);
-  auto mat_e_local = allGather(lapack::MatrixType::General, mat_e);
+    Matrix<TypeParam, Device::CPU> mat_e(sz_e, bsz_e);
+    set_random(mat_e);
+    auto mat_e_local = allGather(lapack::MatrixType::General, mat_e);
 
-  Matrix<const TypeParam, Device::CPU> mat_v = [sz_v, bsz_v]() {
-    Matrix<TypeParam, Device::CPU> mat_v(sz_v, bsz_v);
-    set_random(mat_v); // TODO ? same seed ==> mat_v == mat_e
+    Matrix<const TypeParam, Device::CPU> mat_v = [b, sz_v, bsz_v]() {
+      Matrix<TypeParam, Device::CPU> mat_v(sz_v, bsz_v);
+      set_random(mat_v); // TODO ? same seed ==> mat_v == mat_e
 
-    for (SizeType j = 0; j < mat_v.distribution().localNrTiles().cols(); ++j) {
-      for (SizeType i = 0; i < j; ++i) {
-        const SizeType k = (i == j) ? b - 2 : b;
-        hpx::dataflow(hpx::unwrapping(computeTaus<TypeParam>), i == j, k, mat_v(LocalTileIndex(i, j)));
+      for (SizeType j = 0; j < mat_v.distribution().localNrTiles().cols(); ++j) {
+        for (SizeType i = 0; i < j; ++i) {
+          const SizeType k = (i == j) ? b - 2 : b;
+          hpx::dataflow(hpx::unwrapping(computeTaus<TypeParam>), i == j, k, mat_v(LocalTileIndex(i, j)));
+        }
+      }
+
+      return mat_v;
+    }();
+
+    MatrixLocal<TypeParam> mat_v_local = allGather(lapack::MatrixType::Lower, mat_v);
+
+    eigensolver::backTransformationT2B<Backend::MC>(mat_e, mat_v);
+
+    for (SizeType j = m - 1; j >= 0; --j) {
+      SizeType j_t = j / mb;
+      for (SizeType i = m - mb + ((j + 1) % mb); i >= j + 1; i -= mb) {
+        SizeType i_t = (i - 1) / mb;
+        SizeType k = j % mb;
+
+        const SizeType size = std::min(mb, m - i);
+
+        if (size == 1) // TODO check this (also for complex)
+          continue;
+
+        auto& tile_v = mat_v_local.tile({i_t, j_t});
+        TypeParam& v_head = *tile_v.ptr({0, k});
+        const TypeParam tau = v_head;
+        v_head = 1;
+
+        lapack::larf(blas::Side::Left, size, n, &v_head, 1, tau, mat_e_local.ptr({i, 0}), mat_e_local.ld());
       }
     }
 
-    return mat_v;
-  }();
+    auto result = [&dist = mat_e.distribution(), &mat_local = mat_e_local](const GlobalElementIndex& element) {
+      const auto tile_index = dist.globalTileIndex(element);
+      const auto tile_element = dist.tileElementIndex(element);
+      return mat_local.tile_read(tile_index)(tile_element);
+    };
 
-  MatrixLocal<TypeParam> mat_v_local = allGather(lapack::MatrixType::Lower, mat_v);
-
-  eigensolver::backTransformationT2B<Backend::MC>(mat_e, mat_v);
-
-  for (SizeType j = m - 1; j >= 0; --j) {
-    SizeType j_t = j / mb;
-    for (SizeType i = m - mb + ((j + 1) % mb); i >= j + 1; i -= mb) {
-      SizeType i_t = (i - 1) / mb;
-      SizeType k = j % mb;
-
-      const SizeType size = std::min(mb, m - i);
-
-      if (size == 1) // TODO check this (also for complex)
-        continue;
-
-      auto& tile_v = mat_v_local.tile({i_t, j_t});
-      TypeParam& v_head = *tile_v.ptr({0, k});
-      const TypeParam tau = v_head;
-      v_head = 1;
-
-      lapack::larf(blas::Side::Left, size, n, &v_head, 1, tau, mat_e_local.ptr({i, 0}), mat_e_local.ld());
-    }
+    const auto error = m * n * TypeUtilities<TypeParam>::error; // TODO how much error
+    CHECK_MATRIX_NEAR(result, mat_e, error, error);
   }
-
-  auto result = [&dist = mat_e.distribution(), &mat_local = mat_e_local](const GlobalElementIndex& element) {
-    const auto tile_index = dist.globalTileIndex(element);
-    const auto tile_element = dist.tileElementIndex(element);
-    return mat_local.tile_read(tile_index)(tile_element);
-  };
-
-  const auto error = m * n * TypeUtilities<TypeParam>::error; // TODO how much error
-  CHECK_MATRIX_NEAR(result, mat_e, error, error);
 }

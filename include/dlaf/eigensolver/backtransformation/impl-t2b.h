e45c486d2c5d18f40ba8e5a2c94e41175c3c4497
Alberto Invernizzi <invernizzi@cscs.ch>
2021-09-14 14:37:52 +0200

    WIP test fixed (stil a problem with complex), implementation still not working property due to W sizes

--- include/dlaf/eigensolver/backtransformation/impl-t2b.h
+++ include/dlaf/eigensolver/backtransformation/impl-t2b.h
@@ -30,6 +30,16 @@ namespace internal {
 template <class T>
 struct BackTransformationT2B<Backend::MC, Device::CPU, T> {
   static void call(Matrix<T, Device::CPU>& mat_e, Matrix<const T, Device::CPU>& mat_i);
+private:
+  static constexpr bool is_complex = std::is_same<T, ComplexType<T>>::value;
+
+  static SizeType nrSweeps(const SizeType m) {
+    return std::max<SizeType>(0, is_complex ? m - 1 : m - 2);
+  }
+
+  static SizeType nrStepsPerSweep(SizeType sweep, SizeType m, SizeType mb) {
+    return std::max<SizeType>(0, sweep == m - 2 ? m - 1 : dlaf::util::ceilDiv(m - sweep - 2, mb));
+  }
 };
 
 template <class T>
@@ -40,13 +50,13 @@ auto setupVWellFormed(SizeType k, hpx::shared_future<matrix::Tile<const T, Devic
     constexpr auto Upper = lapack::MatrixType::Upper;
     constexpr auto Lower = lapack::MatrixType::Lower;
 
-    const auto b = tile_v_compact.size().rows();
+    std::cout << "Vnrefls = " << k << "\n";
 
-    //std::cout << "Vc = ";
-    //print(format::numpy{}, tile_v_compact);
+    std::cout << "Vc = ";
+    print(format::numpy{}, tile_v_compact);
 
-    //std::cout << "Vp = ";
-    //print(format::numpy{}, tile_v);
+    std::cout << "Vp = ";
+    print(format::numpy{}, tile_v);
 
     // TODO this requires W complete (even the bottom one)
     //lacpy(General, b - 1, b,
@@ -54,7 +64,7 @@ auto setupVWellFormed(SizeType k, hpx::shared_future<matrix::Tile<const T, Devic
     //    tile_v.ptr({1, 0}), tile_v.ld() + 1);
 
     for (SizeType j = 0; j < k; ++j) {
-      const auto size = std::min<SizeType>(
+      const SizeType size = std::min<SizeType>(
           tile_v.size().rows() - (1 + j),
           tile_v_compact.size().rows() - 1);
       lacpy(General, size, 1,
@@ -64,11 +74,14 @@ auto setupVWellFormed(SizeType k, hpx::shared_future<matrix::Tile<const T, Devic
 
     // TODO is it needed because W = V . T? or is it enough just setting ones?
     laset(Upper, tile_v.size().rows(), k, T(0), T(1), tile_v.ptr({0, 0}), tile_v.ld());
-    if (tile_v.size().rows() > tile_v_compact.size().rows())
-      laset(Lower, tile_v.size().rows() - b, b - 1, T(0), T(0), tile_v.ptr({b, 0}), tile_v.ld());
 
-    //std::cout << "V = ";
-    //print(format::numpy{}, tile_v);
+    const SizeType mb = tile_v_compact.size().cols();
+    if (tile_v.size().rows() > mb) {
+      laset(Lower, tile_v.size().rows() - mb, k - 1, T(0), T(0), tile_v.ptr({mb, 0}), tile_v.ld());
+    }
+
+    std::cout << "V = ";
+    print(format::numpy{}, tile_v);
 
     return matrix::Tile<const T, Device::CPU>(std::move(tile_v));
   };
@@ -90,7 +103,7 @@ auto computeTFactor(hpx::shared_future<matrix::Tile<const T, Device::CPU>> tile_
 
     const auto k = tile_v.size().cols();
     const auto n = tile_v.size().rows();
-    DLAF_ASSERT_HEAVY((tile_t.size() == TileElementSize(k, k)), tile_t.size());
+    //DLAF_ASSERT_HEAVY((tile_t.size() == TileElementSize(k, k)), tile_t.size());
 
     std::vector<T> taus;
     taus.resize(to_sizet(tile_v.size().cols()));
@@ -193,62 +206,86 @@ void BackTransformationT2B<Backend::MC, Device::CPU, T>::call(Matrix<T, Device::
   const auto& dist_i = mat_i.distribution();
   const TileElementSize w_blocksize(2 * b - 1, b);
 
-  // TODO w last tile is complete anyway, becuase of setup V well formed
+  const SizeType nsweeps = nrSweeps(mat_i.size().cols());
+
+  // TODO w last tile is complete anyway, because of setup V well formed
+  const SizeType w_nrows = [=, last_rows=mat_i.tileSize({m - 1, 0}).rows()]() {
+    if (last_rows == mb)
+      return (m - 1) * w_blocksize.rows() + mb - 1;
+    else
+      return (mb - 1) + 2 * w_blocksize.rows() + (m > 2 ? (m - 2) * w_blocksize.rows() - 1 : 0);
+  }();
   const matrix::Distribution dist_w(
-      {(m - 1) * w_blocksize.rows() + mat_e.tileSize({m - 1, 0}).rows() - 1, w_blocksize.cols()},
+      {w_nrows, w_blocksize.cols()},
       w_blocksize,
       dist_i.commGridSize(), dist_i.rankIndex(), dist_i.sourceRankIndex());
+
+  std::cout << "W: " << dist_w.size() << "\n";
+  std::cout << "infoW:" << nsweeps << " " << mat_e.tileSize({m - 1, 0}).rows() << "\n";
+
+  print(format::numpy{}, "I", mat_i);
 
   constexpr std::size_t n_workspaces = 2;
   RoundRobin<Panel<Coord::Col, T, Device::CPU>> t_panels(n_workspaces, mat_i.distribution());
   RoundRobin<Panel<Coord::Col, T, Device::CPU>> w_panels(n_workspaces, dist_w);
   RoundRobin<Panel<Coord::Row, T, Device::CPU>> w2_panels(n_workspaces, mat_e.distribution());
 
-  for (SizeType j_v = n - 1; j_v >= 0; --j_v) {
-    for (SizeType i_v = j_v; i_v < m; ++i_v) {
-      const LocalTileIndex ij_refls(i_v, j_v);
+  const SizeType last_sweep_tile = (nsweeps - 1) / mb;
+  for (SizeType sweep_tile = last_sweep_tile; sweep_tile >= 0; --sweep_tile) {
+    std::cout << "sweep_tile=" << sweep_tile << "/" << last_sweep_tile << "\n";
 
-      const bool affectsTwoRows = i_v < m - 1;
+    const SizeType steps = nrStepsPerSweep(sweep_tile * mb, mat_i.size().cols(), mb);
+
+    std::cout << "steps=" << steps << "\n";
+
+    auto& mat_w = w_panels.nextResource();
+    auto& mat_t = t_panels.nextResource();
+    auto& mat_w2 = w2_panels.nextResource();
+
+    for (SizeType step = 0; step < steps; ++step) {
+      const SizeType i_v = sweep_tile + step;
+
+      const LocalTileIndex ij_refls(i_v, sweep_tile);
+
+      const bool affectsTwoRows = i_v < (m - 1);
 
       std::array<SizeType, 2> sizes;
-      sizes[0] = mat_i.tileSize({i_v, j_v}).rows() - 1;
+      sizes[0] = mat_i.tileSize({i_v, sweep_tile}).rows() - 1;
       if (affectsTwoRows)
-        sizes[1] = mat_i.tileSize({i_v + 1, j_v}).rows();
+        sizes[1] = mat_i.tileSize({i_v + 1, sweep_tile}).rows();
+      const SizeType w_rows = sizes[0] + (affectsTwoRows ? sizes[1] : 0);
 
-      // Note:
-      // -1 because of offset, -1 because of reflectors size > 1
-      const auto nrefls = affectsTwoRows ? mat_i.tileSize({i_v, j_v}).cols() : mat_i.tileSize({i_v, j_v}).rows() - 2;
+      // TODO fix this
+      const SizeType nrefls = std::min(mb, nsweeps - sweep_tile * mb);
 
-      auto& mat_w = w_panels.nextResource();
-      mat_w.setWidth(nrefls);
-      // TODO mat_w.setRange(GlobalTileIndex(i_v, 0), GlobalTileIndex(i_v + (affectsTwoRows ? 2 : 1), 0));
+      const SizeType k = (nrefls < mb) ? nrefls : std::min(w_rows - 1, mb);
+      std::cout << "REFL: nrefls=" << nrefls << " mb=" << mb << " w_rows=" << w_rows << " k=" << k << "\n";
+
+      mat_w.setWidth(k);
+      mat_t.setWidth(k);
+      mat_w2.setHeight(k);
+
+      // TODO setRange?
 
       const auto& tile_i = mat_i.read(ij_refls);
-      auto tile_v = setupVWellFormed(nrefls, tile_i, mat_w(ij_refls));
-
-      auto& mat_t = t_panels.nextResource();
-      mat_t.setWidth(nrefls);
-      // TODO mat_t.setRange(GlobalTileIndex(i_v, 0), GlobalTileIndex(i_v + 1, 0));
+      auto tile_v = setupVWellFormed(k, tile_i, mat_w(ij_refls));
 
       auto tile_t_full = mat_t(LocalTileIndex(i_v, 0));
-      auto tile_t = computeTFactor(tile_i, tile_v, splitTile(tile_t_full, {{0, 0}, {nrefls, nrefls}}));
+      auto tile_t = computeTFactor(tile_i, tile_v, splitTile(tile_t_full, {{0, 0}, {k, k}}));
 
       // Note:
       // W2 is computed before W, because W is used as temporary storage for V.
-      auto& mat_w2 = w2_panels.nextResource();
-      mat_w2.setHeight(nrefls);
-
       for (SizeType j_e = 0; j_e < n; ++j_e) {
         const auto sz_e = mat_e.tileSize({i_v, j_e});
 
-        auto tile_v_up = splitTile(tile_v, {{0, 0}, {sizes[0], nrefls}});
+        auto tile_v_up = splitTile(tile_v, {{0, 0}, {sizes[0], k}});
         auto tile_e = mat_e.read(LocalTileIndex(i_v, j_e));
         const auto& tile_e_up = splitTile(tile_e, {{1, 0}, {sizes[0], sz_e.cols()}});
 
         computeW2(tile_v_up, tile_e_up, T(0), mat_w2(LocalTileIndex(0, j_e)));
 
         if (affectsTwoRows) {
-          auto tile_v_down = splitTile(tile_v, {{sizes[0], 0}, {sizes[1], nrefls}});
+          auto tile_v_down = splitTile(tile_v, {{sizes[0], 0}, {sizes[1], k}});
           auto tile_e_down = mat_e.read(LocalTileIndex(i_v + 1, j_e));
 
           computeW2(tile_v_down, tile_e_down, T(1), mat_w2(LocalTileIndex(0, j_e)));
@@ -264,14 +301,14 @@ void BackTransformationT2B<Backend::MC, Device::CPU, T>::call(Matrix<T, Device::
 
         auto tile_e = mat_e(idx_e);
         auto tile_e_up = splitTile(tile_e, {{1, 0}, {sizes[0], sz_e.cols()}});
-        auto tile_w_up = splitTile(tile_w, {{0, 0}, {sizes[0], nrefls}});
+        auto tile_w_up = splitTile(tile_w, {{0, 0}, {sizes[0], k}});
         const auto& tile_w2 = mat_w2.read(idx_e);
 
         updateE(tile_w_up, tile_w2, std::move(tile_e_up));
 
         if (affectsTwoRows) {
           auto tile_e_dn = mat_e(LocalTileIndex{i_v + 1, j_e});
-          auto tile_w_dn = splitTile(tile_w, {{sizes[0], 0}, {sizes[1], nrefls}});
+          auto tile_w_dn = splitTile(tile_w, {{sizes[0], 0}, {sizes[1], k}});
 
           updateE(tile_w_dn, tile_w2, std::move(tile_e_dn));
         }

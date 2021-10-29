9b14556e036d7eb2f8831b6458eda9d3ae8b9c37
Sely85 <lara.querciagrossa@gmail.com>
2021-10-29 15:35:44 +0200

    simplifying test cases

--- test/unit/eigensolver/mc/test_backtransformation.cpp
+++ test/unit/eigensolver/mc/test_backtransformation.cpp
@@ -53,11 +53,12 @@ GlobalElementSize globalTestSize(const LocalElementSize& size) {
 
 const std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> sizes =
     // m, n, mb, nb
-    {{3, 0, 1, 1},   {0, 5, 2, 3},  // m, n = 0
-     {2, 2, 3, 3},   {3, 4, 6, 7},  // m < mb
-     {3, 3, 1, 1},   {4, 4, 2, 2}, {6, 3, 3, 3}, {12, 2, 4, 4}, {12, 24, 3, 3},
-     {24, 36, 6, 6}, {5, 8, 3, 2}, {4, 6, 2, 3}, {5, 5, 2, 3},  {8, 27, 3, 4},
-     {15, 34, 4, 6}, {7, 6, 2, 3}, {8, 5, 2, 3}, {7, 5, 2, 3}};
+    {
+        {3, 0, 1, 1}, {0, 5, 2, 3},                                  // m, n = 0
+        {2, 2, 3, 3}, {3, 4, 6, 7},                                  // m < mb
+        {3, 3, 1, 1}, {4, 4, 2, 2},  {12, 2, 4, 4}, {24, 36, 6, 6},  // m = n
+        {5, 8, 3, 2}, {8, 27, 3, 4}, {15, 34, 4, 6}                  // m != n
+};
 
 template <class T>
 MatrixLocal<T> makeLocal(const Matrix<const T, Device::CPU>& matrix) {

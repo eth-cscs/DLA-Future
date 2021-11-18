7215e76ed158c036b90dd385859e26f6a71e306c
Teodor Nikolov <teodor.nikolov22@gmail.com>
2021-11-18 14:13:15 +0100

    Configuration for the Cineca M100 cluster

--- scripts/systems.py
+++ scripts/systems.py
@@ -81,3 +81,34 @@ printenv > env.txt
 # Commands
 """,
 }
+
+# NOTE: `gpu2ranks_ompi` has to be in PATH! (e.g. in `~/bin`)
+cineca["m100-gpu"] = {
+    "Cores": 16,
+    "Threads per core": 2,
+    "Allowed rpns": [2],  # 4?
+    "Run command": "mpirun gpu2ranks_ompi",
+    "Batch preamble": """
+#!/bin/bash -l
+#SBATCH --job-name={run_name}_{nodes}
+#SBATCH --time={time_min}
+#SBATCH --nodes={nodes}
+#SBATCH --partition=m100_usr_prod
+#SBATCH --gres=gpu:4
+#SBATCH --qos=normal
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+
+module purge
+. /m100_work/cin_staff/lquerci1/spack-vv/30mar21/share/spack/setup-env.sh
+
+module load spectrum_mpi/10.3.1--binary
+module load dla-future-develop-gcc-10.2.0-ruvjcr5
+
+# Debug
+module list &> modules.txt
+printenv > env.txt
+
+# Commands
+""",
+}

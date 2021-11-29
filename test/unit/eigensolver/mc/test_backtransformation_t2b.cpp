d889c636f8b838bca5205b8538d385032aff770d
Alberto Invernizzi <invernizzi@cscs.ch>
2021-11-29 11:42:49 +0100

    factor out test

--- test/unit/eigensolver/mc/test_backtransformation_t2b.cpp
+++ test/unit/eigensolver/mc/test_backtransformation_t2b.cpp
@@ -67,77 +67,76 @@ std::vector<config_t> configs{
     {8, 8, 3, 3}, {10, 10, 3, 3}, {12, 12, 5, 5}, {12, 30, 5, 6},
 };
 
-TYPED_TEST(BacktransformationT2BTest, CorrectnessLocal) {
-  for (const auto& config : configs) {
-    const SizeType m = config.m;
-    const SizeType n = config.n;
-    const SizeType mb = config.mb;
-    const SizeType nb = config.nb;
+template <class T>
+void testBacktransformation(SizeType m, SizeType n, SizeType mb, SizeType nb) {
+  Matrix<T, Device::CPU> mat_e({m, n}, {mb, nb});
+  set_random(mat_e);
+  auto mat_e_local = allGather(lapack::MatrixType::General, mat_e);
 
-    Matrix<TypeParam, Device::CPU> mat_e({m, n}, {mb, nb});
-    set_random(mat_e);
-    auto mat_e_local = allGather(lapack::MatrixType::General, mat_e);
+  auto nrSweeps = [m]() {
+    const bool is_complex = std::is_same<T, ComplexType<T>>::value;
+    return std::max<SizeType>(0, is_complex ? m - 1 : m - 2);
+  };
 
-    auto nrSweeps = [m]() {
-      const bool is_complex = std::is_same<TypeParam, ComplexType<TypeParam>>::value;
-      return std::max<SizeType>(0, is_complex ? m - 1 : m - 2);
-    };
+  auto nrStepsPerSweep = [m, mb](const SizeType sweep) {
+    return std::max<SizeType>(0, sweep == m - 2 ? 1 : dlaf::util::ceilDiv(m - sweep - 2, mb));
+  };
 
-    auto nrStepsPerSweep = [m, mb](const SizeType sweep) {
-      return std::max<SizeType>(0, sweep == m - 2 ? 1 : dlaf::util::ceilDiv(m - sweep - 2, mb));
-    };
+  Matrix<const T, Device::CPU> mat_i = [m, mb]() {
+    Matrix<T, Device::CPU> mat_i({m, m}, {mb, mb});
+    set_random(mat_i);  // TODO ? same seed ==> mat_i == mat_e
 
-    Matrix<const TypeParam, Device::CPU> mat_i = [m, mb]() {
-      Matrix<TypeParam, Device::CPU> mat_i({m, m}, {mb, mb});
-      set_random(mat_i);  // TODO ? same seed ==> mat_i == mat_e
-
-      const auto m = mat_i.distribution().localNrTiles().cols();
-      for (SizeType j = 0; j < m; ++j) {
-        for (SizeType i = j; i < m; ++i) {
-          const bool affectsTwoRows = i < m - 1;
-          const SizeType k =
-              affectsTwoRows ? mat_i.tileSize({i, j}).cols() : mat_i.tileSize({i, j}).rows() - 2;
-          const SizeType n = mat_i.tileSize({i, j}).rows() - 1 +
-                             (affectsTwoRows ? mat_i.tileSize({i + 1, j}).rows() : 0);
-          if (k <= 0)
-            continue;
-          hpx::dataflow(hpx::unwrapping(computeTaus<TypeParam>), n, k, mat_i(LocalTileIndex(i, j)));
-        }
-      }
-
-      return mat_i;
-    }();
-
-    MatrixLocal<TypeParam> mat_i_local = allGather(lapack::MatrixType::Lower, mat_i);
-
-    eigensolver::backTransformationT2B<Backend::MC>(mat_e, mat_i);
-
-    for (SizeType sweep = nrSweeps() - 1; sweep >= 0; --sweep) {
-      for (SizeType step = nrStepsPerSweep(sweep) - 1; step >= 0; --step) {
-        const SizeType j = sweep;
-        const SizeType i = j + 1 + step * mb;
-
-        const SizeType size = std::min(mb, m - i);
-        const SizeType i_v = (i - 1) / mb * mb;
-
-        TypeParam& v_head = *mat_i_local.ptr({i_v, j});
-        const TypeParam tau = v_head;
-        v_head = 1;
-
-        using blas::Side;
-        lapack::larf(Side::Left, size, n, &v_head, 1, tau, mat_e_local.ptr({i, 0}), mat_e_local.ld());
+    const auto m = mat_i.distribution().localNrTiles().cols();
+    for (SizeType j = 0; j < m; ++j) {
+      for (SizeType i = j; i < m; ++i) {
+        const bool affectsTwoRows = i < m - 1;
+        const SizeType k =
+            affectsTwoRows ? mat_i.tileSize({i, j}).cols() : mat_i.tileSize({i, j}).rows() - 2;
+        const SizeType n =
+            mat_i.tileSize({i, j}).rows() - 1 + (affectsTwoRows ? mat_i.tileSize({i + 1, j}).rows() : 0);
+        if (k <= 0)
+          continue;
+        hpx::dataflow(hpx::unwrapping(computeTaus<T>), n, k, mat_i(LocalTileIndex(i, j)));
       }
     }
 
-    auto result = [&dist = mat_e.distribution(),
-                   &mat_local = mat_e_local](const GlobalElementIndex& element) {
-      const auto tile_index = dist.globalTileIndex(element);
-      const auto tile_element = dist.tileElementIndex(element);
-      return mat_local.tile_read(tile_index)(tile_element);
-    };
+    return mat_i;
+  }();
 
-    // TODO how much error
-    const auto error = std::max<SizeType>(1, 40 * m * n) * TypeUtilities<TypeParam>::error;
-    CHECK_MATRIX_NEAR(result, mat_e, error, error);
+  MatrixLocal<T> mat_i_local = allGather(lapack::MatrixType::Lower, mat_i);
+
+  eigensolver::backTransformationT2B<Backend::MC>(mat_e, mat_i);
+
+  for (SizeType sweep = nrSweeps() - 1; sweep >= 0; --sweep) {
+    for (SizeType step = nrStepsPerSweep(sweep) - 1; step >= 0; --step) {
+      const SizeType j = sweep;
+      const SizeType i = j + 1 + step * mb;
+
+      const SizeType size = std::min(mb, m - i);
+      const SizeType i_v = (i - 1) / mb * mb;
+
+      T& v_head = *mat_i_local.ptr({i_v, j});
+      const T tau = v_head;
+      v_head = 1;
+
+      using blas::Side;
+      lapack::larf(Side::Left, size, n, &v_head, 1, tau, mat_e_local.ptr({i, 0}), mat_e_local.ld());
+    }
   }
+
+  auto result = [&dist = mat_e.distribution(),
+                 &mat_local = mat_e_local](const GlobalElementIndex& element) {
+    const auto tile_index = dist.globalTileIndex(element);
+    const auto tile_element = dist.tileElementIndex(element);
+    return mat_local.tile_read(tile_index)(tile_element);
+  };
+
+  // TODO how much error
+  const auto error = std::max<SizeType>(1, 40 * m * n) * TypeUtilities<T>::error;
+  CHECK_MATRIX_NEAR(result, mat_e, error, error);
+}
+
+TYPED_TEST(BacktransformationT2BTest, CorrectnessLocal) {
+  for (const auto& [m, n, mb, nb] : configs)
+    testBacktransformation<TypeParam>(m, n, mb, nb);
 }

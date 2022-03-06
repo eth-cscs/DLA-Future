b44ed234abcfbe9f6c06f72ab40d825c5a960d16
Teodor Nikolov <teodor.nikolov22@gmail.com>
2022-03-06 12:20:34 +0100

    Add more docs

--- include/dlaf/eigensolver/tridiag_solver.h
+++ include/dlaf/eigensolver/tridiag_solver.h
@@ -20,11 +20,32 @@ namespace eigensolver {
 
 /// Finds the eigenvalues and eigenvectors of a symmetric tridiagonal matrix.
 ///
+/// Notation:
+/// nb - the block/tile size of all matrices and vectors
+/// n - the dimension of the full tridiagonal matrix
+/// d - the diagonal of the full tridiagonal matrix
+/// e - the subdiagonal of the full tridiagonal matrix
+/// Q1 - (n1 x n1) the orthogonal matrix of the top subproblem
+/// Q2 - (n2 x n2) the orthogonal matrix of the bottom subproblem
+///      ┌───┬───┐
+///      │Q1 │   │
+/// Q := ├───┼───┤
+///      │   │Q2 │
+///      └───┴───┘
+/// The following holds for each n1 and n2: `n2 = n1` or `n2 = n1 + nb`.
+///
+///                          ┌───┬──┬─┐
+///                          │Q1'│  │ │
+/// Q-U multiplication form: ├──┬┴──┤ │
+///                          │  │Q2'│ │
+///                          └──┴───┴─┘
+///
 /// @param mat_a  [in/out] `n x 2` matrix with the diagonal and off-diagonal of the symmetric tridiagonal
 /// matrix in the first column and second columns respectively. The last entry of the second column is
 /// not used. On exit the eigenvalues are saved in the first column.
 /// @param mat_ev [out]    `n x n` matrix holding the eigenvectors of the the symmetric tridiagonal
 /// matrix on exit.
+///
 ///
 /// @pre mat_a and mat_ev are local matrices
 /// @pre mat_a has 2 columns
@@ -51,16 +72,27 @@ void tridiagSolver(Matrix<T, device>& mat_a, Matrix<T, device>& mat_ev) {
   TileElementSize vec_tile_size(distr.blockSize().rows(), 1);
   // Holds the diagonal elements of the tridiagonal matrix
   Matrix<T, device> d(vec_size, vec_tile_size);
+  // Holds the values of the deflated diagonal sorted in ascending order
+  Matrix<T, device> d_defl(vec_size, vec_tile_size);
   // Holds the values of Cuppen's rank-1 vector
   Matrix<T, device> z(vec_size, vec_tile_size);
-  // Holds the global indices of the vector `d`
-  Matrix<SizeType, Device::CPU> index(vec_size, vec_tile_size);
+  // Holds the values of the rank-1 update vector sorted corresponding to `d_defl`
+  Matrix<T, device> z_defl(vec_size, vec_tile_size);
+  // Holds indices/permutations of elements of the diagonal sorted in ascending order.
+  Matrix<SizeType, Device::CPU> perm_d(vec_size, vec_tile_size);
+  // Holds indices/permutations of the rows of U that bring it in Q-U matrix multiplication form
+  Matrix<SizeType, Device::CPU> perm_u(vec_size, vec_tile_size);
+  // Holds indices/permutations of the columns of Q that bring it in Q-U matrix multiplication form
+  Matrix<SizeType, Device::CPU> perm_q(vec_size, vec_tile_size);
+  // Assigns a type to each column of Q which is used to calculate the permutation indices for Q and U
+  // that bring them in matrix multiplication form.
+  Matrix<internal::ColType, Device::CPU> coltypes(vec_size, vec_tile_size);
 
   // Tile indices of the first and last diagonal tiles
   SizeType i_begin = 0;
   SizeType i_end = SizeType(distr.nrTiles().rows() - 1);
 
-  internal::TridiagSolver<backend, device, T>::call(mat_a, i_begin, i_end, d, z, index, mat_ws, mat_ev);
+  internal::TridiagSolver<backend, device, T>::call(mat_a, i_begin, i_end, d, z, perm_d, mat_ws, mat_ev);
 }
 
 /// TODO: more info on the distributed version

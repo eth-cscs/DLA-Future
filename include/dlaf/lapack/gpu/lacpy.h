d6d5149775f54b3e40b7801e36d909784111db08
Alberto Invernizzi <invernizzi@cscs.ch>
2022-03-29 09:21:23 +0200

    add basic implementation for lacpy

---
+++ include/dlaf/lapack/gpu/lacpy.h
@@ -0,0 +1,36 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2022, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#ifdef DLAF_WITH_CUDA
+
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+#include "dlaf/types.h"
+
+namespace dlaf::gpulapack {
+
+template <class T>
+void lacpy(cublasFillMode_t uplo, SizeType m, SizeType n, const T* a, SizeType lda, T* b, SizeType ldb,
+           cudaStream_t stream);
+
+#define DLAF_CUBLAS_LACPY_ETI(kword, Type)                                                              \
+  kword template void lacpy(cublasFillMode_t uplo, SizeType m, SizeType n, const Type* a, SizeType lda, \
+                            Type* b, SizeType ldb, cudaStream_t stream)
+
+DLAF_CUBLAS_LACPY_ETI(extern, float);
+DLAF_CUBLAS_LACPY_ETI(extern, double);
+DLAF_CUBLAS_LACPY_ETI(extern, std::complex<float>);
+DLAF_CUBLAS_LACPY_ETI(extern, std::complex<double>);
+}
+
+#endif
---
+++ src/lapack/gpu/lacpy.cu
@@ -0,0 +1,125 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2022, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#include "dlaf/cuda/assert.cu.h"
+#include "dlaf/lapack/gpu/lacpy.h"
+#include "dlaf/util_cublas.h"
+#include "dlaf/util_math.h"
+
+namespace dlaf::gpulapack {
+namespace kernels {
+
+using namespace dlaf::util::cuda_operators;
+
+struct LacpyParams {
+  static constexpr unsigned kernel_tile_size_rows = 64;
+  static constexpr unsigned kernel_tile_size_cols = 16;
+};
+
+template <class T>
+__device__ void copyAll(const unsigned m, const unsigned n, const T* a, const unsigned lda, T* b,
+                        const unsigned ldb) {
+  constexpr unsigned kernel_tile_size_rows = LacpyParams::kernel_tile_size_rows;
+  constexpr unsigned kernel_tile_size_cols = LacpyParams::kernel_tile_size_cols;
+
+  const unsigned i = blockIdx.x * kernel_tile_size_rows + threadIdx.x;
+  const unsigned j = blockIdx.y * kernel_tile_size_cols;
+
+  if (i >= m)
+    return;
+
+  const unsigned k_max = min(j + kernel_tile_size_cols, n);
+
+  for (unsigned k = j; k < k_max; ++k)
+    b[i + k * ldb] = a[i + k * lda];
+}
+
+template <bool (*comp)(unsigned, unsigned), class T>
+__device__ void copyDiag(const unsigned m, const unsigned n, const T* a, const unsigned lda, T* b,
+                         const unsigned ldb) {
+  constexpr unsigned kernel_tile_size_rows = LacpyParams::kernel_tile_size_rows;
+  constexpr unsigned kernel_tile_size_cols = LacpyParams::kernel_tile_size_cols;
+
+  const unsigned i = blockIdx.x * kernel_tile_size_rows + threadIdx.x;
+  const unsigned j = blockIdx.y * kernel_tile_size_cols;
+
+  if (i >= m)
+    return;
+
+  const unsigned k_max = min(j + kernel_tile_size_cols, n);
+
+  for (unsigned k = j; k < k_max; ++k)
+    if (i == k or (comp(i, k)))
+      b[i + k * ldb] = a[i + k * lda];
+}
+
+template <class T>
+__global__ void lacpy(cublasFillMode_t uplo, const unsigned m, const unsigned n, const T* a,
+                      const unsigned lda, T* b, const unsigned ldb) {
+  constexpr unsigned kernel_tile_size_rows = LacpyParams::kernel_tile_size_rows;
+  constexpr unsigned kernel_tile_size_cols = LacpyParams::kernel_tile_size_cols;
+
+  DLAF_GPU_ASSERT_HEAVY(kernel_tile_size_rows % kernel_tile_size_cols == 0);
+  DLAF_GPU_ASSERT_HEAVY(kernel_tile_size_rows == blockDim.x);
+  DLAF_GPU_ASSERT_HEAVY(1 == blockDim.y);
+  DLAF_GPU_ASSERT_HEAVY(1 == blockDim.z);
+  DLAF_GPU_ASSERT_HEAVY(gridDim.x == ceilDiv(m, kernel_tile_size_rows));
+  DLAF_GPU_ASSERT_HEAVY(gridDim.y == ceilDiv(n, kernel_tile_size_cols));
+  DLAF_GPU_ASSERT_HEAVY(1 == gridDim.z);
+
+  const unsigned i = blockIdx.x;
+  const unsigned j = blockIdx.y * kernel_tile_size_cols / kernel_tile_size_rows;
+
+  // Note: if (i == j) the kernel tile contains parts of the diagonal
+
+  switch (uplo) {
+    case CUBLAS_FILL_MODE_LOWER:
+      if (i == j)
+        copyDiag<dlaf::util::isLower>(m, n, a, lda, b, ldb);
+      else if (i > j)
+        copyAll(m, n, a, lda, b, ldb);
+      break;
+    case CUBLAS_FILL_MODE_UPPER:
+      if (i == j)
+        copyDiag<dlaf::util::isUpper>(m, n, a, lda, b, ldb);
+      else if (i < j)
+        copyAll(m, n, a, lda, b, ldb);
+      break;
+    case CUBLAS_FILL_MODE_FULL:
+      copyAll(m, n, a, lda, b, ldb);
+      break;
+  }
+}
+}
+
+template <class T>
+void lacpy(cublasFillMode_t uplo, SizeType m, SizeType n, const T* a, SizeType lda, T* b, SizeType ldb,
+           cudaStream_t stream) {
+  DLAF_ASSERT_HEAVY(m >= lda, m, lda);
+  DLAF_ASSERT_HEAVY(m >= ldb, m, ldb);
+
+  constexpr unsigned kernel_tile_size_rows = kernels::LacpyParams::kernel_tile_size_rows;
+  constexpr unsigned kernel_tile_size_cols = kernels::LacpyParams::kernel_tile_size_cols;
+
+  const unsigned um = to_uint(m);
+  const unsigned un = to_uint(n);
+
+  dim3 nr_threads(kernel_tile_size_rows, 1);
+  dim3 nr_blocks(util::ceilDiv(um, kernel_tile_size_rows), util::ceilDiv(un, kernel_tile_size_cols));
+  kernels::lacpy<<<nr_blocks, nr_threads, 0, stream>>>(uplo, um, un, util::cppToCudaCast(a),
+                                                       to_uint(lda), util::cppToCudaCast(b),
+                                                       to_uint(ldb));
+}
+
+DLAF_CUBLAS_LACPY_ETI(, float);
+DLAF_CUBLAS_LACPY_ETI(, double);
+DLAF_CUBLAS_LACPY_ETI(, std::complex<float>);
+DLAF_CUBLAS_LACPY_ETI(, std::complex<double>);
+}

2c6da55e216ed2c3e707e18b3ae768cf1150e15f
Mikael Simberg <mikael.simberg@iki.if>
2022-04-27 11:53:19 +0200

    Disable use of hints for schedulers (prefer is broken for with_hint)

--- include/dlaf/schedulers.h
+++ include/dlaf/schedulers.h
@@ -36,9 +36,12 @@ auto getBackendScheduler() {
 
 template <Backend backend>
 auto getBackendScheduler(const pika::threads::thread_priority priority,
-                         const pika::threads::thread_schedule_hint hint) {
+                         const pika::threads::thread_schedule_hint) {
   namespace ex = pika::execution::experimental;
-  return ex::with_hint(ex::with_priority(getBackendScheduler<backend>(), priority), hint);
+  using pika::experimental::prefer;
+  // TODO: prefer is broken with with_hint. Try to fix for pika 0.4.0?
+  // return prefer(ex::with_hint, ex::with_priority(getBackendScheduler<backend>(), priority), hint);
+  return ex::with_priority(getBackendScheduler<backend>(), priority);
 }
 
 inline auto getMPIScheduler() {

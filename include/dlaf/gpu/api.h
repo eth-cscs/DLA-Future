400052064a3ac2146e84d2fb2bc59765038d8416
Alberto Invernizzi <invernizzi@cscs.ch>
2022-05-06 10:12:17 +0200

    some other cuda-hip translations

--- include/dlaf/gpu/api.h
+++ include/dlaf/gpu/api.h
@@ -16,21 +16,24 @@
 
 #include <hip/hip_runtime.h>
 
+#define cublasGetStream                  hipblasGetStream
 #define cudaDeviceGetStreamPriorityRange hipDeviceGetStreamPriorityRange
 #define cudaDeviceSynchronize            hipDeviceSynchronize
 #define cudaError_t                      hipError_t
 #define cudaGetErrorString               hipGetErrorString
 #define cudaMemcpy2D                     hipMemcpy2D
 #define cudaMemcpy2DAsync                hipMemcpy2DAsync
+#define cudaMemcpyDefault                hipMemcpyDefault
 #define cudaMemcpyDeviceToDevice         hipMemcpyDeviceToDevice
 #define cudaMemcpyDeviceToHost           hipMemcpyDeviceToHost
 #define cudaMemcpyHostToDevice           hipMemcpyHostToDevice
 #define cudaMemset2DAsync                hipMemset2DAsync
+#define cudaPointerAttributes            hipPointerAttribute_t
 #define cudaSetDevice                    hipSetDevice
-#define cudaStream_t                     hipStream_t
 #define cudaStreamCreateWithPriority     hipStreamCreateWithPriority
 #define cudaStreamDestroy                hipStreamDestroy
 #define cudaStreamNonBlocking            hipStreamNonBlocking
+#define cudaStream_t                     hipStream_t
 #define cudaSuccess                      hipSuccess
 
 #elif defined(DLAF_WITH_CUDA)

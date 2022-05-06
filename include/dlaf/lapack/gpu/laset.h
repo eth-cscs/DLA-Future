201d6359c49da88ae8f6d730ba59418358f9302c
Alberto Invernizzi <invernizzi@cscs.ch>
2022-05-06 10:27:10 +0200

    WIP: laset+lacpy implement/adapt for ROCm

--- include/dlaf/lapack/gpu/laset.h
+++ include/dlaf/lapack/gpu/laset.h
@@ -22,6 +22,7 @@ template <class T>
 void laset(blas::Uplo uplo, SizeType m, SizeType n, T alpha, T beta, T* a, SizeType lda,
            cudaStream_t stream);
 
+#if defined DLAF_WITH_CUDA
 #define DLAF_CUBLAS_LASET_ETI(kword, Type)                                                           \
   kword template void laset(blas::Uplo uplo, SizeType m, SizeType n, Type alpha, Type beta, Type* a, \
                             SizeType lda, cudaStream_t stream)
@@ -30,6 +31,11 @@ DLAF_CUBLAS_LASET_ETI(extern, float);
 DLAF_CUBLAS_LASET_ETI(extern, double);
 DLAF_CUBLAS_LASET_ETI(extern, std::complex<float>);
 DLAF_CUBLAS_LASET_ETI(extern, std::complex<double>);
+#elif defined DLAF_WITH_HIP
+template <class T>
+void laset(blas::Uplo uplo, SizeType m, SizeType n, T alpha, T beta, T* a, SizeType lda,
+           cudaStream_t stream) {}
+#endif
 }
 
 #endif

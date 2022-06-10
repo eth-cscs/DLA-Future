c5bcf713d5e61468cb1865f4dcdd2988693d230c
Mikael Simberg <simberg@cscs.ch>
2022-06-10 16:28:29 +0200

    TEMP: Disable ADL for callable objects
    We assume that the wrapped function is always in the same namespace.

--- include/dlaf/common/callable_object.h
+++ include/dlaf/common/callable_object.h
@@ -17,12 +17,14 @@
 /// require explicit selection of the overload when passing the function to
 /// higher-order functions. With this wrapper the overload is selected inside
 /// the wrapper's operator().
-#define DLAF_MAKE_CALLABLE_OBJECT(fname)                                                 \
-  constexpr struct fname##_t {                                                           \
-    template <typename... Ts>                                                            \
-    auto operator()(Ts&&... ts) const noexcept(noexcept(fname(std::forward<Ts>(ts)...))) \
-        -> decltype(fname(std::forward<Ts>(ts)...)) {                                    \
-      return fname(std::forward<Ts>(ts)...);                                             \
-    }                                                                                    \
-  } fname##_o {                                                                          \
+///
+/// TODO: Aaargh, (f)(...) turns off ADL!? That's great, but too obscure?
+#define DLAF_MAKE_CALLABLE_OBJECT(fname)                                                   \
+  constexpr struct fname##_t {                                                             \
+    template <typename... Ts>                                                              \
+    auto operator()(Ts&&... ts) const noexcept(noexcept((fname)(std::forward<Ts>(ts)...))) \
+        -> decltype((fname)(std::forward<Ts>(ts)...)) {                                    \
+      return (fname)(std::forward<Ts>(ts)...);                                             \
+    }                                                                                      \
+  } fname##_o {                                                                            \
   }

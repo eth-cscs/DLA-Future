1d16e00f31893d3eff1827e197c31ed1ba14913f
Mikael Simberg <simbergm@cscs.ch>
2022-06-21 15:48:46 +0200

    Fix macros and ifdefs after merge

--- include/dlaf/multiplication/general/api.h
+++ include/dlaf/multiplication/general/api.h
@@ -34,7 +34,7 @@ DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::MC, Device::CPU, double)
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::MC, Device::CPU, std::complex<float>)
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::MC, Device::CPU, std::complex<double>)
 
-#ifdef DLAF_WITH_CUDA
+#ifdef DLAF_WITH_GPU
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::GPU, Device::GPU, float)
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::GPU, Device::GPU, double)
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::GPU, Device::GPU, std::complex<float>)

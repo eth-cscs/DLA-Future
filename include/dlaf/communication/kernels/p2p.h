ccf4994b8327eb8bb1a74973b18c4f4542274243
Alberto Invernizzi <invernizzi@cscs.ch>
2022-07-07 16:50:07 +0200

    add cuda rdma check

--- include/dlaf/communication/kernels/p2p.h
+++ include/dlaf/communication/kernels/p2p.h
@@ -31,6 +31,10 @@ namespace internal {
 template <class T, Device D>
 void send(const Communicator& comm, const matrix::Tile<const T, D>& tile, IndexT_MPI dest,
           IndexT_MPI tag, MPI_Request* req) {
+#if !defined(DLAF_WITH_CUDA_RDMA)
+  static_assert(D == Device::CPU, "DLAF_WITH_CUDA_RDMA=off, MPI accepts just CPU memory.");
+#endif
+
   auto msg = comm::make_message(common::make_data(tile));
   DLAF_MPI_CHECK_ERROR(
       MPI_Isend(const_cast<T*>(msg.data()), msg.count(), msg.mpi_type(), dest, tag, comm, req));
@@ -42,6 +46,10 @@ DLAF_MAKE_CALLABLE_OBJECT(send);
 template <class T, Device D>
 auto recv(const Communicator& comm, const matrix::Tile<T, D>& tile, IndexT_MPI source, IndexT_MPI tag,
           MPI_Request* req) {
+#if !defined(DLAF_WITH_CUDA_RDMA)
+  static_assert(D == Device::CPU, "DLAF_WITH_CUDA_RDMA=off, MPI accepts just CPU memory.");
+#endif
+
   auto msg = comm::make_message(common::make_data(tile));
   DLAF_MPI_CHECK_ERROR(MPI_Irecv(msg.data(), msg.count(), msg.mpi_type(), source, tag, comm, req));
 }
@@ -54,8 +62,8 @@ void scheduleSend(IndexT_MPI dest, CommSender&& pcomm, IndexT_MPI tag, Sender&& 
   using dlaf::internal::whenAllLift;
   using pika::execution::experimental::start_detached;
 
-  whenAllLift(std::forward<CommSender>(pcomm), std::forward<Sender>(tile), dest, tag) |
-      internal::transformMPI(internal::send_o) | pika::execution::experimental::start_detached();
+  start_detached(whenAllLift(std::forward<CommSender>(pcomm), std::forward<Sender>(tile), dest, tag) |
+                 internal::transformMPI(internal::send_o));
 }
 
 template <class CommSender, class Sender>
@@ -63,7 +71,7 @@ auto scheduleRecv(IndexT_MPI source, CommSender&& pcomm, IndexT_MPI tag, Sender&
   using dlaf::internal::whenAllLift;
   using pika::execution::experimental::start_detached;
 
-  whenAllLift(std::forward<CommSender>(pcomm), std::forward<Sender>(tile), source, tag) |
-      internal::transformMPI(internal::recv_o) | pika::execution::experimental::start_detached();
+  start_detached(whenAllLift(std::forward<CommSender>(pcomm), std::forward<Sender>(tile), source, tag) |
+                 internal::transformMPI(internal::recv_o));
 }
 }

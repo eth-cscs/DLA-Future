63fe9e561b661d59cb75583c748499512d96c154
Mikael Simberg <simberg@cscs.ch>
2022-09-19 13:41:19 +0200

    Move MPI_Comm_split to separate std::threads

--- src/communication/communicator_grid.cpp
+++ src/communication/communicator_grid.cpp
@@ -33,9 +33,15 @@ CommunicatorGrid::CommunicatorGrid(Communicator comm, IndexT_MPI nrows, IndexT_M
   }
 
   MPI_Comm mpi_full, mpi_col, mpi_row;
-  DLAF_MPI_CHECK_ERROR(MPI_Comm_split(comm, is_in_grid ? 0 : MPI_UNDEFINED, key_full, &mpi_full));
-  DLAF_MPI_CHECK_ERROR(MPI_Comm_split(comm, index_row, key, &mpi_row));
-  DLAF_MPI_CHECK_ERROR(MPI_Comm_split(comm, index_col, key, &mpi_col));
+  // TODO
+  namespace ex = pika::execution::experimental;
+  namespace tt = pika::this_thread::experimental;
+  tt::sync_wait(ex::schedule(ex::std_thread_scheduler{}) | ex::then([&]() {
+                  DLAF_MPI_CHECK_ERROR(
+                      MPI_Comm_split(comm, is_in_grid ? 0 : MPI_UNDEFINED, key_full, &mpi_full));
+                  DLAF_MPI_CHECK_ERROR(MPI_Comm_split(comm, index_row, key, &mpi_row));
+                  DLAF_MPI_CHECK_ERROR(MPI_Comm_split(comm, index_col, key, &mpi_col));
+                }));
 
   if (!is_in_grid)
     return;
--- test/include/dlaf_test/comm_grids/grids_6_ranks.h
+++ test/include/dlaf_test/comm_grids/grids_6_ranks.h
@@ -54,7 +54,12 @@ public:
         color = 3;
       }
       MPI_Comm split_comm;
-      DLAF_MPI_CHECK_ERROR(MPI_Comm_split(world, color, world.rank(), &split_comm));
+      // TODO
+      namespace ex = pika::execution::experimental;
+      namespace tt = pika::this_thread::experimental;
+      tt::sync_wait(ex::schedule(ex::std_thread_scheduler{}) | ex::then([&]() {
+                      DLAF_MPI_CHECK_ERROR(MPI_Comm_split(world, color, world.rank(), &split_comm));
+                    }));
 
       comm::Communicator comm(split_comm);
       comm_grids.emplace_back(comm, rows, cols, common::Ordering::ColumnMajor);

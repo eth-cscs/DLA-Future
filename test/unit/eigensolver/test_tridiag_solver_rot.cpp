d4f02886dc96ba880b6bad578aa7a92a82d5bde8
Alberto Invernizzi <invernizzi@cscs.ch>
2022-10-13 09:48:22 +0200

    basic implementation for test

--- test/unit/eigensolver/test_tridiag_solver_rot.cpp
+++ test/unit/eigensolver/test_tridiag_solver_rot.cpp
@@ -8,15 +8,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
-#include "dlaf/communication/communicator_grid.h"
-#include "dlaf/eigensolver/tridiag_solver/merge.h"
 #include "dlaf/eigensolver/tridiag_solver/rot.h"
 
 #include <gtest/gtest.h>
 
+#include "dlaf/communication/communicator_grid.h"
 #include "dlaf/matrix/distribution.h"
 #include "dlaf/matrix/matrix.h"
+
 #include "dlaf_test/comm_grids/grids_6_ranks.h"
+#include "dlaf_test/matrix/matrix_local.h"
+#include "dlaf_test/matrix/util_matrix.h"
+#include "dlaf_test/matrix/util_matrix_local.h"
 #include "dlaf_test/util_types.h"
 
 using namespace dlaf;
@@ -40,20 +43,34 @@ template <class T>
 void testApplyGivenRotations(comm::CommunicatorGrid grid, const SizeType m, const SizeType mb,
                              const SizeType idx_begin, const SizeType idx_last,
                              std::vector<di::GivensRotation<T>> rots) {
+  // TODO check about "independent" rots?
+
   using dlaf::eigensolver::internal::applyGivensRotationsToMatrixColumns;
 
   matrix::Distribution dist({m, m}, {mb, mb}, grid.size(), grid.rank(), {0, 0});
 
-  // TODO initialize matrix randomly
-  matrix::Matrix<T, Device::CPU> matrix(dist);
+  matrix::Matrix<T, Device::CPU> mat(dist);
+  matrix::util::set_random(mat);
 
-  auto rots_fut = ex::just(rots);
+  matrix::test::MatrixLocal<T> mat_loc = matrix::test::allGather(blas::Uplo::General, mat, grid);
 
-  applyGivensRotationsToMatrixColumns(grid, idx_begin, idx_last, std::move(rots_fut), matrix);
+  applyGivensRotationsToMatrixColumns(grid, idx_begin, idx_last, ex::just(rots), mat);
 
-  // TODO collect locally mat
-  // TODO apply blas::rot
-  // TODO check equality
+  // Apply Given Rotations
+  for (auto rot : rots) {
+    const SizeType n = mat_loc.size().rows();
+    T* x = mat_loc.ptr({0, rot.i});
+    T* y = mat_loc.ptr({0, rot.j});
+    blas::rot(n, x, 1, y, 1, rot.c, rot.s);
+  }
+
+  auto result = [&dist = mat.distribution(), &mat_local = mat_loc](const GlobalElementIndex& element) {
+    const auto tile_index = dist.globalTileIndex(element);
+    const auto tile_element = dist.tileElementIndex(element);
+    return mat_local.tile_read(tile_index)(tile_element);
+  };
+
+  CHECK_MATRIX_NEAR(result, mat, m * TypeUtilities<T>::error, m * TypeUtilities<T>::error);
 }
 
 template <class T>
@@ -70,7 +87,7 @@ TYPED_TEST(TridiagEigensolverRotTest, ApplyGivenRotations) {
                                                3,
                                                0,
                                                2,
-                                               {di::GivensRotation<TypeParam>{0, 8, 0.5f, 0.5f},
+                                               {// di::GivensRotation<TypeParam>{0, 8, 0.5f, 0.5f},
                                                 di::GivensRotation<TypeParam>{0, 2, 0.5f, 0.5f}}}};
 
   for (const auto& grid : this->commGrids()) {

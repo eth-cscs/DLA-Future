50abfea11c5e1e8fde100b6e61f0ff775d8e5544
Alberto Invernizzi <9337627+albestro@users.noreply.github.com>
2022-10-13 09:58:51 +0200

    algo: Distributed sub-Gemm (trisolver use-case) (#659)
    * add gemm test-case on sub-matrix (there were just full matrix use-cases)
    
    * double bug fix! non-init value was wrong in ref matrix + polar magnitude > 0
    
    * structure + test + WIP impl
    
    * add range (still all reflectors) + bug fixes
    
    * WIP fixes
    
    * bug fixes + minimize panels allocation
    
    * comments + refactoring + avoid splitTile when possible
    
    * add reference to the paper for the implementation
    
    * enable GPU
    
    * fix root rank for communication
    
    * fix problems with partial k in distributed runs
    
    * test distributed + remove superfluous parameters + add incomplete test-cases
    
    * add assert for polar magnitude
    
    * remove k parameter for now
    
    * minor fixes
    
    * rename internal function
    
    * document and improve constraints check
    
    * basic naming proposal
    
    * add documentation
    
    * fix last todo in test about source rank index
    
    * forgot to revert gpu test

--- include/dlaf/multiplication/general.h
+++ include/dlaf/multiplication/general.h
@@ -13,6 +13,9 @@
 #include <blas.hh>
 
 #include "dlaf/common/assert.h"
+#include "dlaf/communication/communicator_grid.h"
+#include "dlaf/matrix/distribution.h"
+#include "dlaf/matrix/index.h"
 #include "dlaf/matrix/matrix.h"
 #include "dlaf/multiplication/general/api.h"
 #include "dlaf/util_matrix.h"
@@ -35,7 +38,7 @@ namespace dlaf::multiplication {
 ///         overwritten with the result, while others are left untouched.
 ///         Only tiles whose both row and col tile coords are in the closed range [a,b] are accessed.
 /// @pre mat_a, mat_b and mat_c have the same square block size,
-/// @pre mat_a, mat_b and mat_c have the same square size,
+/// @pre mat_a, mat_b and mat_c have the same size,
 /// @pre mat_a, mat_b and mat_c are not distributed,
 /// @pre a <= b < mat_a.nrTiles().rows()
 template <Backend B, Device D, class T>
@@ -46,14 +49,18 @@ void generalSubMatrix(const SizeType a, const SizeType b, const blas::Op opA, co
   DLAF_ASSERT(dlaf::matrix::square_blocksize(mat_b), mat_b);
   DLAF_ASSERT(dlaf::matrix::square_blocksize(mat_c), mat_c);
 
-  // TODO check assertions. these are superflous
-  DLAF_ASSERT(dlaf::matrix::square_size(mat_a), mat_a);
-  DLAF_ASSERT(dlaf::matrix::square_size(mat_b), mat_b);
-  DLAF_ASSERT(dlaf::matrix::square_size(mat_c), mat_c);
-
   DLAF_ASSERT(matrix::local_matrix(mat_a), mat_a);
   DLAF_ASSERT(matrix::local_matrix(mat_b), mat_b);
   DLAF_ASSERT(matrix::local_matrix(mat_c), mat_c);
+
+  // Note:
+  // This is an over-constraint, since the algorithm just cares about the sub-matrix size.
+  // It simplifies next check about [a,b] range validity, that otherwise would require it to be
+  // validated against every single sub-matrix in a, b and c that might have different element sizes.
+  //
+  // At the moment, we don't have this use-case, so let's keep it simple.
+  DLAF_ASSERT(equal_size(mat_a, mat_b), mat_a, mat_b);
+  DLAF_ASSERT(equal_size(mat_a, mat_c), mat_a, mat_c);
 
   const SizeType m = mat_a.nrTiles().rows();
   DLAF_ASSERT(a <= b, a, b);
@@ -66,4 +73,49 @@ void generalSubMatrix(const SizeType a, const SizeType b, const blas::Op opA, co
   else
     DLAF_UNIMPLEMENTED(opA, opB);
 }
+
+/// General sub-matrix distributed multiplication, computing
+/// C[a:b][a:b] = alpha * A[a:b][a:b] * B[a:b][a:b] + beta * C[a:b][a:b]
+/// where [a:b] is the range of tiles starting from tile index @p a to tile index @p b (included)
+///
+/// @param  mat_a contains the input matrix A. Only tiles whose both row and col tile coords are in
+///         the closed range [a,b] are accessed in read-only mode (elements are not modified)
+/// @param  mat_b contains the input matrix B. Only tiles whose both row and col tile coords are in
+///         the closed range [a,b] are accessed in read-only mode (elements are not modified)
+/// @param  mat_c On entry it contains the input matrix C. On exit matrix tiles in the range will be
+///         overwritten with the result, while others are left untouched.
+///         Only tiles whose both row and col tile coords are in the closed range [a,b] are accessed.
+/// @pre mat_a, mat_b and mat_c are distributed in the same way,
+/// @pre mat_a, mat_b and mat_c have the same square block size,
+/// @pre mat_a, mat_b and mat_c have the same size,
+/// @pre a <= b < mat_a.nrTiles().rows()
+template <Backend B, Device D, class T>
+void generalSubMatrix(comm::CommunicatorGrid grid, const SizeType a, const SizeType b, const T alpha,
+                      Matrix<const T, D>& mat_a, Matrix<const T, D>& mat_b, const T beta,
+                      Matrix<T, D>& mat_c) {
+  DLAF_ASSERT(equal_process_grid(mat_a, grid), mat_a, grid);
+  DLAF_ASSERT(equal_process_grid(mat_b, grid), mat_a, grid);
+  DLAF_ASSERT(equal_process_grid(mat_c, grid), mat_a, grid);
+
+  DLAF_ASSERT(dlaf::matrix::square_blocksize(mat_a), mat_a);
+  DLAF_ASSERT(dlaf::matrix::square_blocksize(mat_b), mat_b);
+  DLAF_ASSERT(dlaf::matrix::square_blocksize(mat_c), mat_c);
+
+  // Note:
+  // This is an over-constraint, since the algorithm just cares about the sub-matrix size (and its
+  // distribution).
+  // It simplifies next check about [a,b] range validity, that otherwise would require it to be
+  // validated against every single sub-matrix in a, b and c that might have different element sizes.
+  //
+  // At the moment, we don't have this use-case, so let's keep it simple.
+  DLAF_ASSERT(equal_size(mat_a, mat_b), mat_a, mat_b);
+  DLAF_ASSERT(equal_size(mat_a, mat_c), mat_a, mat_c);
+
+  const SizeType m = mat_a.nrTiles().rows();
+  DLAF_ASSERT(a <= b, a, b);
+  DLAF_ASSERT(a >= 0 && a < m, a, m);
+
+  internal::GeneralSub<B, D, T>::callNN(grid, a, b, alpha, mat_a, mat_b, beta, mat_c);
+}
+
 }

f258fc81c17296afb5bc0b5d4b8b7695d01039ee
Mikael Simberg <mikael.simberg@iki.fi>
2022-10-20 10:15:44 +0200

    Move SingleThreadedOmpScope definitions to source file

--- include/dlaf/common/single_threaded_omp.h
+++ include/dlaf/common/single_threaded_omp.h
@@ -10,31 +10,12 @@
 
 #pragma once
 
-#ifdef DLAF_WITH_OPENMP
-#include <omp.h>
-#endif
-#ifdef DLAF_WITH_MKL
-#include <mkl_service.h>
-#endif
-
 // TODO: internal?
 namespace dlaf::common::internal {
 class [[nodiscard]] SingleThreadedOmpScope {
 public:
-  SingleThreadedOmpScope() {
-#ifdef DLAF_WITH_OPENMP
-    omp_set_num_threads(1);
-#endif
-  }
-  ~SingleThreadedOmpScope() {
-#ifdef DLAF_WITH_OPENMP
-    omp_set_num_threads(omp_num_threads);
-#endif
-#ifdef DLAF_WITH_MKL
-    mkl_set_num_threads_local(mkl_num_threads);
-#endif
-  }
-
+  SingleThreadedOmpScope();
+  ~SingleThreadedOmpScope();
   SingleThreadedOmpScope(SingleThreadedOmpScope&&) = delete;
   SingleThreadedOmpScope(SingleThreadedOmpScope const&) = delete;
   SingleThreadedOmpScope& operator=(SingleThreadedOmpScope&&) = delete;
@@ -42,10 +23,10 @@ public:
 
 private:
 #ifdef DLAF_WITH_OPENMP
-  int omp_num_threads = omp_get_max_threads();
+  int omp_num_threads;
 #endif
 #ifdef DLAF_WITH_MKL
-  int mkl_num_threads = mkl_set_num_threads_local(1);
+  int mkl_num_threads;
 #endif
 };
 }
---
+++ src/common/single_threaded_omp.cpp
@@ -0,0 +1,39 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2022, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#ifdef DLAF_WITH_OPENMP
+#include <omp.h>
+#endif
+#ifdef DLAF_WITH_MKL
+#include <mkl_service.h>
+#endif
+
+#include <dlaf/common/single_threaded_omp.h>
+
+namespace dlaf::common::internal {
+SingleThreadedOmpScope::SingleThreadedOmpScope() {
+#ifdef DLAF_WITH_OPENMP
+  omp_num_threads = omp_get_max_threads();
+  omp_set_num_threads(1);
+#endif
+#ifdef DLAF_WITH_MKL
+  mkl_num_threads = mkl_set_num_threads_local(1);
+#endif
+}
+
+SingleThreadedOmpScope::~SingleThreadedOmpScope() {
+#ifdef DLAF_WITH_OPENMP
+  omp_set_num_threads(omp_num_threads);
+#endif
+#ifdef DLAF_WITH_MKL
+  mkl_set_num_threads_local(mkl_num_threads);
+#endif
+}
+}

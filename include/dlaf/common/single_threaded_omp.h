00416e6817625e70df283778427ffd067b550e5f
Mikael Simberg <mikael.simberg@iki.fi>
2022-10-20 10:06:55 +0200

    Make OpenMP conditional through CMake variable

--- include/dlaf/common/single_threaded_omp.h
+++ include/dlaf/common/single_threaded_omp.h
@@ -10,40 +10,42 @@
 
 #pragma once
 
+#ifdef DLAF_WITH_OPENMP
+#include <omp.h>
+#endif
 #ifdef DLAF_WITH_MKL
 #include <mkl_service.h>
 #endif
-#include <omp.h>
 
 // TODO: internal?
 namespace dlaf::common::internal {
 class [[nodiscard]] SingleThreadedOmpScope {
 public:
-  SingleThreadedOmpScope()
-      : omp_num_threads(omp_get_max_threads())
-#ifdef DLAF_WITH_MKL
-        ,
-        mkl_num_threads(mkl_set_num_threads_local(1))
-#endif
-  {
+  SingleThreadedOmpScope() {
+#ifdef DLAF_WITH_OPENMP
     omp_set_num_threads(1);
+#endif
   }
   ~SingleThreadedOmpScope() {
+#ifdef DLAF_WITH_OPENMP
     omp_set_num_threads(omp_num_threads);
+#endif
 #ifdef DLAF_WITH_MKL
     mkl_set_num_threads_local(mkl_num_threads);
 #endif
   }
 
-  SingleThreadedOmpScope(SingleThreadedOmpScope &&) = delete;
+  SingleThreadedOmpScope(SingleThreadedOmpScope&&) = delete;
   SingleThreadedOmpScope(SingleThreadedOmpScope const&) = delete;
   SingleThreadedOmpScope& operator=(SingleThreadedOmpScope&&) = delete;
   SingleThreadedOmpScope& operator=(SingleThreadedOmpScope const&) = delete;
 
 private:
-  int omp_num_threads;
+#ifdef DLAF_WITH_OPENMP
+  int omp_num_threads = omp_get_max_threads();
+#endif
 #ifdef DLAF_WITH_MKL
-  int mkl_num_threads;
+  int mkl_num_threads = mkl_set_num_threads_local(1);
 #endif
 };
 }

318e3fcedbaa8f65604eea79da612861a83b78e1
Alberto Invernizzi <invernizzi@cscs.ch>
2022-10-26 20:05:57 +0200

    generalize test

--- test/unit/eigensolver/test_tridiag_solver_rot.cpp
+++ test/unit/eigensolver/test_tridiag_solver_rot.cpp
@@ -15,6 +15,7 @@
 #include "dlaf/communication/communicator_grid.h"
 #include "dlaf/matrix/distribution.h"
 #include "dlaf/matrix/matrix.h"
+#include "dlaf/matrix/matrix_mirror.h"
 
 #include "dlaf_test/comm_grids/grids_6_ranks.h"
 #include "dlaf_test/matrix/matrix_local.h"
@@ -32,11 +33,26 @@ namespace di = dlaf::eigensolver::internal;
     ::testing::AddGlobalTestEnvironment(new CommunicatorGrid6RanksEnvironment);
 
 template <typename T>
-class TridiagEigensolverRotTest : public TestWithCommGrids {};
+struct TridiagEigensolverRotTest : public TestWithCommGrids {
+  template <class U>
+  struct config_t {
+    SizeType m;
+    SizeType mb;
+    SizeType i_begin;
+    SizeType i_last;
+    std::vector<di::GivensRotation<U>> rots;
+  };
 
-TYPED_TEST_SUITE(TridiagEigensolverRotTest, RealMatrixElementTypes);
+  const std::vector<config_t<T>> configs{
+      {9, 3, 0, 2, {di::GivensRotation<T>{0, 8, 0.5f, 0.5f}, di::GivensRotation<T>{0, 2, 0.5f, 0.5f}}}};
+};
 
-template <class T>
+template <typename T>
+using TridiagEigensolverRotMCTest = TridiagEigensolverRotTest<T>;
+
+TYPED_TEST_SUITE(TridiagEigensolverRotMCTest, RealMatrixElementTypes);
+
+template <class T, Device D>
 void testApplyGivenRotations(comm::CommunicatorGrid grid, const SizeType m, const SizeType mb,
                              const SizeType idx_begin, const SizeType idx_last,
                              std::vector<di::GivensRotation<T>> rots) {
@@ -46,12 +62,16 @@ void testApplyGivenRotations(comm::CommunicatorGrid grid, const SizeType m, cons
 
   matrix::Distribution dist({m, m}, {mb, mb}, grid.size(), grid.rank(), {0, 0});
 
-  matrix::Matrix<T, Device::CPU> mat(dist);
-  matrix::util::set_random(mat);
+  matrix::Matrix<T, Device::CPU> mat_h(dist);
+  matrix::util::set_random(mat_h);
 
-  matrix::test::MatrixLocal<T> mat_loc = matrix::test::allGather(blas::Uplo::General, mat, grid);
+  matrix::test::MatrixLocal<T> mat_loc = matrix::test::allGather(blas::Uplo::General, mat_h, grid);
 
-  applyGivensRotationsToMatrixColumns(grid.rowCommunicator(), idx_begin, idx_last, ex::just(rots), mat);
+  {
+    matrix::MatrixMirror<T, D, Device::CPU> mat(mat_h);
+    applyGivensRotationsToMatrixColumns(grid.rowCommunicator(), idx_begin, idx_last, ex::just(rots),
+                                        mat.get());
+  }
 
   // Apply Given Rotations
   for (auto rot : rots) {
@@ -61,35 +81,19 @@ void testApplyGivenRotations(comm::CommunicatorGrid grid, const SizeType m, cons
     blas::rot(n, x, 1, y, 1, rot.c, rot.s);
   }
 
-  auto result = [&dist = mat.distribution(), &mat_local = mat_loc](const GlobalElementIndex& element) {
+  auto result = [&dist = mat_h.distribution(), &mat_local = mat_loc](const GlobalElementIndex& element) {
     const auto tile_index = dist.globalTileIndex(element);
     const auto tile_element = dist.tileElementIndex(element);
     return mat_local.tile_read(tile_index)(tile_element);
   };
 
-  CHECK_MATRIX_NEAR(result, mat, m * TypeUtilities<T>::error, m * TypeUtilities<T>::error);
+  CHECK_MATRIX_NEAR(result, mat_h, m * TypeUtilities<T>::error, m * TypeUtilities<T>::error);
 }
 
-template <class T>
-struct config_t {
-  SizeType m;
-  SizeType mb;
-  SizeType i_begin;
-  SizeType i_last;
-  std::vector<di::GivensRotation<T>> rots;
-};
-
-TYPED_TEST(TridiagEigensolverRotTest, ApplyGivenRotations) {
-  std::vector<config_t<TypeParam>> configs = {{9,
-                                               3,
-                                               0,
-                                               2,
-                                               {di::GivensRotation<TypeParam>{0, 8, 0.5f, 0.5f},
-                                                di::GivensRotation<TypeParam>{0, 2, 0.5f, 0.5f}}}};
-
+TYPED_TEST(TridiagEigensolverRotMCTest, ApplyGivenRotations) {
   for (const auto& grid : this->commGrids()) {
-    for (const auto& [m, mb, idx_begin, idx_last, rots] : configs) {
-      testApplyGivenRotations<TypeParam>(grid, m, mb, idx_begin, idx_last, rots);
+    for (const auto& [m, mb, idx_begin, idx_last, rots] : this->configs) {
+      testApplyGivenRotations<TypeParam, Device::CPU>(grid, m, mb, idx_begin, idx_last, rots);
     }
   }
 }

1f24ba1ee6d3c8980a152acceaf49ee5c48537cf
Alberto Invernizzi <invernizzi@cscs.ch>
2022-11-03 14:21:07 +0100

    improve tilecollector

--- include/dlaf/eigensolver/tridiag_solver/merge.h
+++ include/dlaf/eigensolver/tridiag_solver/merge.h
@@ -225,33 +225,37 @@ pika::future<T> calcTolerance(SizeType i_begin, SizeType i_end, Matrix<const T, 
          di::transform(di::Policy<Backend::MC>(), std::move(tol_fn)) | ex::make_future();
 }
 
-struct TileCollector {
+/// TileCollector is an helper that, given included boundaries of a range [idx_begin, idx_last],
+/// allows to obtain access to all tiles in that range for a matrix.
+///
+/// The range specified is generally considered a square, i.e. the range is applied to both rows and
+/// columns.
+/// Instead, if the matrix has a single column of tiles, the range is considered linear over rows.
+class TileCollector {
   SizeType idx_begin;
   SizeType idx_last;
 
-private:
-  std::pair<GlobalTileIndex, GlobalTileSize> getRange(const matrix::Distribution& dist) const {
-    SizeType ntiles = idx_last - idx_begin + 1;
-    bool is_col_matrix = dist.size().cols() == 1;
-    SizeType col_begin = (is_col_matrix) ? 0 : idx_begin;
-    SizeType col_sz = (is_col_matrix) ? 1 : ntiles;
-
-    return std::make_pair(GlobalTileIndex{idx_begin, col_begin}, GlobalTileSize{ntiles, col_sz});
-  }
-
-public:
   auto iteratorLocal(const matrix::Distribution& dist) const {
-    auto [g_begin, g_size] = getRange(dist);
+    const bool is_col_matrix = dist.nrTiles().cols() == 1;
+
+    const GlobalTileIndex g_begin(idx_begin, is_col_matrix ? 0 : idx_begin);
+    const GlobalTileIndex g_end(idx_last + 1, is_col_matrix ? 1 : idx_last + 1);
+
     const LocalTileIndex begin{
         dist.template nextLocalTileFromGlobalTile<Coord::Row>(g_begin.row()),
         dist.template nextLocalTileFromGlobalTile<Coord::Col>(g_begin.col()),
     };
-    const LocalTileSize size{
-        dist.template nextLocalTileFromGlobalTile<Coord::Row>(g_size.rows()),
-        dist.template nextLocalTileFromGlobalTile<Coord::Col>(g_size.cols()),
+    const LocalTileIndex end{
+        dist.template nextLocalTileFromGlobalTile<Coord::Row>(g_end.row()),
+        dist.template nextLocalTileFromGlobalTile<Coord::Col>(g_end.col()),
     };
+    const LocalTileSize size = end - begin;
+
     return std::make_pair(begin, size);
   }
+
+public:
+  TileCollector(SizeType i_begin, SizeType i_last) : idx_begin(i_begin), idx_last(i_last) {}
 
   template <class T, Device D>
   auto read(Matrix<const T, D>& mat) const {

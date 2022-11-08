9638e84983a877402bb0cb31b296575b8703052e
Alberto Invernizzi <invernizzi@cscs.ch>
2022-11-08 12:12:32 +0100

    add eigensolver distributed pipeline + test

--- test/unit/eigensolver/test_eigensolver.cpp
+++ test/unit/eigensolver/test_eigensolver.cpp
@@ -15,10 +15,12 @@
 
 #include <gtest/gtest.h>
 
+#include "dlaf/communication/communicator_grid.h"
 #include "dlaf/matrix/copy.h"
 #include "dlaf/matrix/matrix.h"
 #include "dlaf/matrix/matrix_mirror.h"
 #include "dlaf/types.h"
+#include "dlaf_test/comm_grids/grids_6_ranks.h"
 #include "dlaf_test/matrix/matrix_local.h"
 #include "dlaf_test/matrix/util_matrix.h"
 #include "dlaf_test/matrix/util_matrix_local.h"
@@ -31,8 +33,11 @@ using namespace dlaf::matrix::test;
 using namespace dlaf::test;
 using namespace testing;
 
+::testing::Environment* const comm_grids_env =
+    ::testing::AddGlobalTestEnvironment(new CommunicatorGrid6RanksEnvironment);
+
 template <typename Type>
-class EigensolverTest : public ::testing::Test {};
+class EigensolverTest : public TestWithCommGrids {};
 
 template <class T>
 using EigensolverTestMC = EigensolverTest<T>;
@@ -116,11 +121,89 @@ void testEigensolver(const blas::Uplo uplo, const SizeType m, const SizeType mb)
   CHECK_MATRIX_NEAR(res, workspace, 2 * m * TypeUtilities<T>::error, 2 * m * TypeUtilities<T>::error);
 }
 
+template <class T, Backend B, Device D>
+void testEigensolver(comm::CommunicatorGrid grid, const blas::Uplo uplo, const SizeType m,
+                     const SizeType mb) {
+  const GlobalElementSize size(m, m);
+  const TileElementSize block_size(mb, mb);
+
+  Matrix<const T, Device::CPU> reference = [&]() {
+    Matrix<T, Device::CPU> reference(size, block_size, grid);
+    matrix::util::set_random_hermitian(reference);
+    return reference;
+  }();
+
+  Matrix<T, Device::CPU> mat_a_h(reference.distribution());
+  copy(reference, mat_a_h);
+
+  auto ret = [&]() {
+    MatrixMirror<T, D, Device::CPU> mat_a(mat_a_h);
+    return eigensolver::eigensolver<B>(grid, uplo, mat_a.get());
+  }();
+
+  if (mat_a_h.size().isEmpty())
+    return;
+
+  auto mat_a_local = allGather(blas::Uplo::General, reference, grid);
+  auto mat_evalues_local = [&]() {
+    MatrixMirror<const BaseType<T>, Device::CPU, D> mat_evals(ret.eigenvalues);
+    return allGather(blas::Uplo::General, mat_evals.get(), grid);
+  }();
+  auto mat_e_local = [&]() {
+    MatrixMirror<const T, Device::CPU, D> mat_e(ret.eigenvectors);
+    return allGather(blas::Uplo::General, mat_e.get(), grid);
+  }();
+
+  MatrixLocal<T> workspace({m, m}, block_size);
+
+  // Check eigenvectors orthogonality (E^H E == Id)
+  blas::gemm(blas::Layout::ColMajor, blas::Op::ConjTrans, blas::Op::NoTrans, m, m, m, T{1},
+             mat_e_local.ptr(), mat_e_local.ld(), mat_e_local.ptr(), mat_e_local.ld(), T{0},
+             workspace.ptr(), workspace.ld());
+
+  auto id = [](GlobalElementIndex index) {
+    if (index.row() == index.col())
+      return T{1};
+    return T{0};
+  };
+  CHECK_MATRIX_NEAR(id, workspace, m * TypeUtilities<T>::error, 10 * m * TypeUtilities<T>::error);
+
+  // Check Ax = lambda x
+  // Compute A E
+  blas::hemm(blas::Layout::ColMajor, blas::Side::Left, uplo, m, m, T{1}, mat_a_local.ptr(),
+             mat_a_local.ld(), mat_e_local.ptr(), mat_e_local.ld(), T{0}, workspace.ptr(),
+             workspace.ld());
+
+  // Compute Lambda E (in place in mat_e_local)
+  for (SizeType j = 0; j < m; ++j) {
+    blas::scal(m, mat_evalues_local({j, 0}), mat_e_local.ptr({0, j}), 1);
+  }
+
+  // Check A E == Lambda E
+  auto res = [&mat_e_local](GlobalElementIndex index) { return mat_e_local(index); };
+  CHECK_MATRIX_NEAR(res, workspace, 2 * m * TypeUtilities<T>::error, 2 * m * TypeUtilities<T>::error);
+}
+
 TYPED_TEST(EigensolverTestMC, CorrectnessLocal) {
   for (auto uplo : blas_uplos) {
     for (auto sz : sizes) {
       const auto& [m, mb] = sz;
       testEigensolver<TypeParam, Backend::MC, Device::CPU>(uplo, m, mb);
+    }
+  }
+}
+
+TYPED_TEST(EigensolverTestMC, CorrectnessDistributed) {
+  for (const comm::CommunicatorGrid grid : this->commGrids()) {
+    // TODO not all algorithms ready for a real distributed
+    if (grid.size() != comm::Size2D(1, 1))
+      continue;
+
+    for (auto uplo : blas_uplos) {
+      for (auto sz : sizes) {
+        const auto& [m, mb] = sz;
+        testEigensolver<TypeParam, Backend::MC, Device::CPU>(grid, uplo, m, mb);
+      }
     }
   }
 }

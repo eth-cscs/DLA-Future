ccbc4140f78090ead87af0b54b8870015326db3a
John Biddiscombe <biddisco@cscs.ch>
2022-11-17 18:43:45 +0100

    script tweaks for JB setup

--- scripts/compare_commits.sh
+++ scripts/compare_commits.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+SCRATCH=$( pwd )
+GIT_DIR=$HOME/src/dlaf/
+GCC=11.2.0
+ARCH=cray-cnl7-haswell
+ARCH=linux-pop22-skylake
+
+
 set -euo pipefail
 
 date=$(date -Iseconds)
@@ -21,10 +28,10 @@ for configuration in "${configurations[@]}"; do
     echo "Handling configuration \"${configuration}\""
 
     if [[ "${configuration}" == "mc" ]]; then
-        spec="dla-future%gcc@9.3.0 arch=cray-cnl7-haswell build_type=Release ~cuda ^intel-mkl"
+        spec="dla-future%gcc@$GCC arch=$ARCH build_type=Release ~cuda ^intel-mkl"
         cmake_flags="-DCMAKE_BUILD_TYPE=Release -DDLAF_WITH_CUDA=OFF -DDLAF_WITH_MKL=ON -DDLAF_BUILD_MINIAPPS=ON"
     elif [[ "${configuration}" == "gpu" ]]; then
-        spec="dla-future%gcc@9.3.0 arch=cray-cnl7-haswell build_type=Release +cuda ^intel-mkl"
+        spec="dla-future%gcc@$GCC arch=$ARCH build_type=Release +cuda ^intel-mkl"
         cmake_flags="-DCMAKE_BUILD_TYPE=Release -DDLAF_WITH_CUDA=ON -DDLAF_WITH_MKL=ON -DDLAF_BUILD_MINIAPPS=ON"
     else
         echo "configuration should be \"mc\" or \"gpu\" (\"${configuration}\" given)"
@@ -32,19 +39,22 @@ for configuration in "${configurations[@]}"; do
 
     paths_array=()
     for commitish in "$@"; do
-        commit=$(git rev-parse ${commitish})
+        commit=$(git -C $GIT_DIR rev-parse ${commitish})
 
         # create worktree
         worktree_path=$SCRATCH/dlaf-comparisons/${commit}
-        [[ ! -d "${worktree_path}" ]] && git worktree add "${worktree_path}" "${commit}"
+        [[ ! -d "${worktree_path}" ]] && git -C $GIT_DIR worktree add "${worktree_path}" "${commit}"
 
         # create spack repository
         repo_path="${worktree_path}/spack-${commit}"
         [[ ! -L "${repo_path}" ]] && ln -s "${worktree_path}/spack" "${repo_path}"
 
         spack_cmd="spack -c repos:[\"${repo_path}\"]"
+        echo "spack command is $spack_cmd"
 
         spec_hash=$(${spack_cmd} spec --json ${spec} | jq --raw-output '.spec.nodes[] | select(.name == "dla-future") | .full_hash')
+        echo "spec commmand ${spack_cmd} spec --json ${spec} | jq --raw-output '.spec.nodes[] | select(.name == \"dla-future\") | .full_hash'"
+
         cmake_flags_hash=$(echo ${cmake_flags} | sha1sum | head -c 16)
         build_dir="${worktree_path}/build/${spec_hash}/${cmake_flags_hash}"
 
@@ -60,13 +70,14 @@ for configuration in "${configurations[@]}"; do
 #        rm -rf "${build_dir}"
 #        mkdir -p "${build_dir}"
 #
+        echo "spec commmand ${spack_cmd} spec --fresh -lI ${spec}"
         ${spack_cmd} spec --fresh -lI ${spec}
 #        # install dependencies
 #        ${spack_cmd} install --fresh --only dependencies ${spec}
 #
 #        # build dla-future
-#        ${spack_cmd} build-env --fresh ${spec} -- cmake ${worktree_path} -B${build_dir} ${cmake_flags}
-#        ${spack_cmd} build-env --fresh ${spec} -- cmake --build ${build_dir} --target miniapp_{cholesky,triangular_solver,gen_to_std}
+#        ${spack_cmd} -e dev build-env ${spec} -- cmake ${worktree_path} -B${build_dir} ${cmake_flags}
+#        ${spack_cmd} -e dev build-env ${spec} -- cmake --build ${build_dir} --target miniapp_{cholesky,triangular_solver,gen_to_std}
 #
 #        paths_array+=(${build_dir})
     done

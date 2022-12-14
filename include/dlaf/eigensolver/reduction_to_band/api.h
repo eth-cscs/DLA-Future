1214f50233f00874f9e35504a4b0496d9372bc0b
Mikael Simberg <simberg@cscs.ch>
2022-12-14 11:21:04 +0100

    Don't include impl.h header in public reduction to band header

--- include/dlaf/eigensolver/reduction_to_band/api.h
+++ include/dlaf/eigensolver/reduction_to_band/api.h
@@ -22,4 +22,19 @@ struct ReductionToBand {
       comm::CommunicatorGrid grid, Matrix<T, D>& mat_a);
 };
 
+/// ---- ETI
+#define DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(KWORD, BACKEND, DEVICE, DATATYPE) \
+  KWORD template struct ReductionToBand<BACKEND, DEVICE, DATATYPE>;
+
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::MC, Device::CPU, float)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::MC, Device::CPU, double)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::MC, Device::CPU, std::complex<float>)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::MC, Device::CPU, std::complex<double>)
+
+#ifdef DLAF_WITH_GPU
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::GPU, Device::GPU, float)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::GPU, Device::GPU, double)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::GPU, Device::GPU, std::complex<float>)
+DLAF_EIGENSOLVER_REDUCTION_TO_BAND_ETI(extern, Backend::GPU, Device::GPU, std::complex<double>)
+#endif
 }

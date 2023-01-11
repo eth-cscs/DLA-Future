827bf06868c952ef89809e7afd189ab743c3da91
Raffaele Solcà <rasolca@cscs.ch>
2023-01-11 19:27:04 +0100

    general max norm

--- src/auxiliary/norm/mc.cpp
+++ src/auxiliary/norm/mc.cpp
@@ -10,15 +10,10 @@
 
 #include "dlaf/auxiliary/norm/mc.h"
 
-namespace dlaf {
-namespace auxiliary {
-namespace internal {
+namespace dlaf::auxiliary::internal {
 
 DLAF_NORM_ETI(, float)
 DLAF_NORM_ETI(, double)
 DLAF_NORM_ETI(, std::complex<float>)
 DLAF_NORM_ETI(, std::complex<double>)
-
-}
-}
 }

33c92d93f3ac702ab94b6aba7b8d410e072fd881
Raffaele Solcà <rasolca@cscs.ch>
2023-01-13 10:52:42 +0100

    review suggestion

--- include/dlaf/multiplication/hermitian.h
+++ include/dlaf/multiplication/hermitian.h
@@ -22,12 +22,13 @@ namespace dlaf::multiplication {
 /// (when side == Left) or C + alpha B A (when side == Right), where A is a Hermitian matrix.
 ///
 /// @param side specifies whether A appears on the \a Left or on the \a Right of matrix B,
-/// @param uplo specifies if the elements of the Hermitian matrix to be referenced are the elements in
+/// @param uplo specifies if the elements of the Hermitian matrix A to be referenced are the elements in
+/// the lower or upper triangular part,
 /// @param mat_a contains the hermitian matrix A. Only the tiles of the matrix which contain the upper or
 /// the lower triangular part which represent the Hermitian matrix (depending on the value of uplo)
 /// are accessed in read-only mode (the elements are not modified),
 /// @param mat_b contains the matrix B accessed in read-only mode (the elements are not modified),
-/// @param mat_ic on entry it contains the matrix C, on exit the matrix elements are overwritten with the
+/// @param mat_c on entry it contains the matrix C, on exit the matrix elements are overwritten with the
 /// elements of the result.
 /// @pre mat_a has a square size,
 /// @pre mat_a has a square block size,
@@ -40,6 +41,7 @@ void hermitian(blas::Side side, blas::Uplo uplo, const T alpha, Matrix<const T, 
   DLAF_ASSERT(matrix::square_blocksize(mat_a), mat_a);
   DLAF_ASSERT(matrix::local_matrix(mat_a), mat_a);
   DLAF_ASSERT(matrix::local_matrix(mat_b), mat_b);
+  DLAF_ASSERT(matrix::local_matrix(mat_c), mat_c);
 
   if (side == blas::Side::Left) {
     DLAF_ASSERT(matrix::multipliable(mat_a, mat_b, mat_c, blas::Op::NoTrans, blas::Op::NoTrans), mat_a,
@@ -68,12 +70,13 @@ void hermitian(blas::Side side, blas::Uplo uplo, const T alpha, Matrix<const T, 
 /// where A is a Hermitian matrix.
 ///
 /// @param side specifies whether A appears on the \a Left or on the \a Right of matrix B,
-/// @param uplo specifies if the elements of the Hermitian matrix to be referenced are the elements in
+/// @param uplo specifies if the elements of the Hermitian matrix A to be referenced are the elements in
+/// the lower or upper triangular part,
 /// @param mat_a contains the hermitian matrix A. Only the tiles of the matrix which contain the upper or
 /// the lower triangular part which represent the Hermitian matrix (depending on the value of uplo)
 /// are accessed in read-only mode (the elements are not modified),
 /// @param mat_b contains the matrix B accessed in read-only mode (the elements are not modified),
-/// @param mat_ic on entry it contains the matrix C, on exit the matrix elements are overwritten with the
+/// @param mat_c on entry it contains the matrix C, on exit the matrix elements are overwritten with the
 /// elements of the result.
 /// @pre mat_a has a square size,
 /// @pre mat_a has a square block size,

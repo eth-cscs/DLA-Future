bff9a6fc47b06018eff5d5c011a1376c7cc44ce8
Raffaele Solcà <rasolca@cscs.ch>
2023-01-13 12:08:55 +0100

    hermitian matrix multiplication (left lower) (#774)

---
+++ src/multiplication/hermitian/mc.cpp
@@ -0,0 +1,19 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2022, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#include "dlaf/multiplication/hermitian/impl.h"
+
+namespace dlaf::multiplication::internal {
+
+DLAF_MULTIPLICATION_HERMITIAN_ETI(, Backend::MC, Device::CPU, float)
+DLAF_MULTIPLICATION_HERMITIAN_ETI(, Backend::MC, Device::CPU, double)
+DLAF_MULTIPLICATION_HERMITIAN_ETI(, Backend::MC, Device::CPU, std::complex<float>)
+DLAF_MULTIPLICATION_HERMITIAN_ETI(, Backend::MC, Device::CPU, std::complex<double>)
+}

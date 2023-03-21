1e1a7b97a580f2682abc604682703c25d2e047ef
Alberto Invernizzi <invernizzi@cscs.ch>
2023-03-21 16:09:02 +0100

    clang-format

--- src/permutations/general/perms.cu
+++ src/permutations/general/perms.cu
@@ -49,14 +49,16 @@ MatrixLayout getMatrixLayout(const matrix::Distribution& distr,
 }
 
 template <class T>
-MatrixLayout getMatrixLayout(const matrix::Distribution& distr,
-                             const std::vector<pika::shared_future<matrix::Tile<const T, Device::GPU>>>& tiles) {
+MatrixLayout getMatrixLayout(
+    const matrix::Distribution& distr,
+    const std::vector<pika::shared_future<matrix::Tile<const T, Device::GPU>>>& tiles) {
   const LocalTileSize tile_sz = distr.localNrTiles();
   MatrixLayout layout;
   layout.nb = distr.blockSize().rows();
   layout.ld = tiles[0].get().ld();
   layout.row_offset = (tile_sz.rows() > 1) ? tiles[1].get().ptr() - tiles[0].get().ptr() : 0;
-  layout.col_offset = (tile_sz.cols() > 1) ? tiles[to_sizet(tile_sz.rows())].get().ptr() - tiles[0].get().ptr() : 0;
+  layout.col_offset =
+      (tile_sz.cols() > 1) ? tiles[to_sizet(tile_sz.rows())].get().ptr() - tiles[0].get().ptr() : 0;
   return layout;
 }
 
@@ -97,11 +99,11 @@ __global__ void applyPermutationsOnDevice(SizeType out_begin_row, SizeType out_b
 }
 
 template <class T, Coord coord>
-void applyPermutationsOnDevice(GlobalElementIndex out_begin, GlobalElementSize sz, SizeType in_offset,
-                               const matrix::Distribution& distr, const SizeType* perms,
-                               const std::vector<pika::shared_future<matrix::Tile<const T, Device::GPU>>>& in_tiles_fut,
-                               const std::vector<matrix::Tile<T, Device::GPU>>& out_tiles,
-                               whip::stream_t stream) {
+void applyPermutationsOnDevice(
+    GlobalElementIndex out_begin, GlobalElementSize sz, SizeType in_offset,
+    const matrix::Distribution& distr, const SizeType* perms,
+    const std::vector<pika::shared_future<matrix::Tile<const T, Device::GPU>>>& in_tiles_fut,
+    const std::vector<matrix::Tile<T, Device::GPU>>& out_tiles, whip::stream_t stream) {
   MatrixLayout in_layout = getMatrixLayout(distr, in_tiles_fut);
   MatrixLayout out_layout = getMatrixLayout(distr, out_tiles);
   const T* in = in_tiles_fut[0].get().ptr();

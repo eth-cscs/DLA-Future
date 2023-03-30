6c78e973cdd9703949e2bb37e3eabcf9a6943f9f
Mikael Simberg <simberg@cscs.ch>
2023-03-30 16:23:50 +0200

    Remove MatrixView

--- test/unit/matrix/test_matrix_mirror.cpp
+++ test/unit/matrix/test_matrix_mirror.cpp
@@ -8,8 +8,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
-#include "dlaf/matrix/matrix_view.h"
-
 #include <vector>
 #include "gtest/gtest.h"
 #include "dlaf/communication/communicator_grid.h"

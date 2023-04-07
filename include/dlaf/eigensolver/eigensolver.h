a33ea60f9c887bb73798663d4fb7a6ab5ae06bb3
Rocco Meli <r.meli@bluemail.ch>
2023-04-07 15:40:50 +0200

    remove merge duplication

--- include/dlaf/eigensolver/eigensolver.h
+++ include/dlaf/eigensolver/eigensolver.h
@@ -61,38 +61,6 @@ void eigensolver(blas::Uplo uplo, Matrix<T, D>& mat, Matrix<BaseType<T>, D>& eig
 ///
 /// Implementation on local memory.
 ///
-/// @param uplo specifies if upper or lower triangular part of @p mat will be referenced
-/// @param mat contains the Hermitian matrix A
-/// @param eigenvectors matrix of eigenvectors
-/// @param eigenvalues vector of eigenvalues
-template <Backend B, Device D, class T>
-void eigensolver(blas::Uplo uplo, Matrix<T, D>& mat, Matrix<BaseType<T>, D>& eigenvalues,
-                 Matrix<T, D>& eigenvectors) {
-  DLAF_ASSERT(matrix::local_matrix(mat), mat);
-  DLAF_ASSERT(square_size(mat), mat);
-  DLAF_ASSERT(square_blocksize(mat), mat);
-  DLAF_ASSERT(matrix::local_matrix(eigenvalues), eigenvalues);
-  DLAF_ASSERT(eigenvalues.size().rows() == eigenvectors.size().rows(), eigenvalues, eigenvectors);
-  DLAF_ASSERT(eigenvalues.blockSize().rows() == eigenvectors.blockSize().rows(), eigenvalues,
-              eigenvectors);
-  DLAF_ASSERT(matrix::local_matrix(eigenvectors), eigenvectors);
-  DLAF_ASSERT(square_size(eigenvectors), eigenvectors);
-  DLAF_ASSERT(square_blocksize(eigenvectors), eigenvectors);
-  DLAF_ASSERT(eigenvectors.size() == mat.size(), eigenvectors, mat);
-  DLAF_ASSERT(eigenvectors.blockSize() == mat.blockSize(), eigenvectors, mat);
-
-  internal::Eigensolver<B, D, T>::call(uplo, mat, eigenvalues, eigenvectors);
-}
-
-/// Standard Eigensolver.
-///
-/// It solves the standard eigenvalue problem A * x = lambda * x.
-///
-/// On exit, the lower triangle or the upper triangle (depending on @p uplo) of @p mat_a,
-/// including the diagonal, is destroyed.
-///
-/// Implementation on local memory.
-///
 /// @return struct ReturnEigensolverType with eigenvalues, as a vector<T>, and eigenvectors as a Matrix
 /// @param uplo specifies if upper or lower triangular part of @p mat will be referenced
 /// @param mat contains the Hermitian matrix A

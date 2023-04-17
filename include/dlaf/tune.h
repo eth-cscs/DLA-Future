2b308e3e9d625d61c770d188bcbc99e4c75ebd11
Alberto Invernizzi <9337627+albestro@users.noreply.github.com>
2023-04-17 10:57:15 +0200

    ReductionToBand parallelise panel computation with bulk (both MC and GPU, both Local and Distributed) (#797)
    This PR increases parallelism with bulk for the computation of the panel in the ReductionToBand algorithm, which happens on Backend::MC for both Backend:MC and Backend::GPU versions of it.
    - add tuneable parameter for number of threads to use for the computation
    - add custom c'tor for common::internal::vector

--- include/dlaf/tune.h
+++ include/dlaf/tune.h
@@ -9,18 +9,21 @@
 //
 #pragma once
 
-#include <pika/program_options.hpp>
-
+#include <pika/runtime.hpp>
 #include <dlaf/types.h>
 
 namespace dlaf {
 /// DLA-Future tuning parameters.
 ///
 /// Holds the value of the parameters that can be used to tune DLA-Future.
+/// - red2band_panel_nworkers: number of threads to use for computing the panel in the reduction to band algorithm.
 /// - eigensolver_min_band: The minimun value to start looking for a divisor of the block size.
 ///                         Set with --dlaf:eigensolver-min-band or env variable DLAF_EIGENSOLVER_MIN_BAND.
 /// Note to developers: Users can change these values, therefore consistency has to be ensured by algorithms.
 struct TuneParameters {
+  size_t red2band_panel_nworkers =
+      std::max<size_t>(1, pika::resource::get_thread_pool("default").get_os_thread_count() / 2);
+
   SizeType eigensolver_min_band = 100;
   SizeType band_to_tridiag_1d_block_size_base = 8192;
 };

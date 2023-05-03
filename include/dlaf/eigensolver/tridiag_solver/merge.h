3fa1774aaf0f38768ca23b2c60f0406da32427af
Rocco Meli <r.meli@bluemail.ch>
2023-05-03 21:38:08 +0200

    lapack single threaded

--- include/dlaf/eigensolver/tridiag_solver/merge.h
+++ include/dlaf/eigensolver/tridiag_solver/merge.h
@@ -13,6 +13,7 @@
 #include <pika/execution.hpp>
 
 #include "dlaf/common/range2d.h"
+#include "dlaf/common/single_threaded_blas.h"
 #include "dlaf/communication/kernels.h"
 #include "dlaf/eigensolver/tridiag_solver/coltype.h"
 #include "dlaf/eigensolver/tridiag_solver/kernels.h"
@@ -643,6 +644,7 @@ void solveRank1Problem(SizeType i_begin, SizeType i_end, KSender&& k, RhoSender&
       SizeType i_col = distr.tileElementFromGlobalElement<Coord::Col>(i);
       T* delta = evec_tiles[to_sizet(i_tile)].ptr(TileElementIndex(0, i_col));
 
+      common::internal::SingleThreadedBlasScope single;
       lapack::laed4(static_cast<int>(k), static_cast<int>(i), d_ptr, z_ptr, delta, rho, &eigenval);
     });
   };
@@ -1066,6 +1068,8 @@ void solveRank1ProblemDist(SizeType i_begin, SizeType i_end, LocalTileIndex idx_
                    dist](const auto& k, const auto& rho, const auto& d_sfut_tile_arr,
                          const auto& z_sfut_tile_arr, const auto& eval_tiles, const auto& delta_tile_arr,
                          const auto& i2_tile_arr, const auto& evec_tile_arr) {
+    common::internal::SingleThreadedBlasScope single;
+
     const SizeType n = problemSize(i_begin, i_end, dist);
     const T* d_ptr = d_sfut_tile_arr[0].get().ptr();
     const T* z_ptr = z_sfut_tile_arr[0].get().ptr();

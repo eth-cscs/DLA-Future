983f06367370b057c72901116a5303f6e07211f9
Rocco Meli <r.meli@bluemail.ch>
2023-05-05 09:24:02 +0200

    refactor single-threaded test

--- test/unit/common/test_single_threaded_blas.cpp
+++ test/unit/common/test_single_threaded_blas.cpp
@@ -19,34 +19,41 @@
 
 #include <gtest/gtest.h>
 
-TEST(SingleThreadedBlas, Basic) {
+#ifdef DLAF_WITH_MKL
+TEST(SingleThreadedBlas, MKL) {
   // Set the number of threads to something bigger than 1 so that we can
   // actually observe if SingleThreadedBlasScope has an effect.
-  [[maybe_unused]] constexpr int num_threads = 2;
-#ifdef DLAF_WITH_OPENMP
-  omp_set_num_threads(num_threads);
-#endif
-#ifdef DLAF_WITH_MKL
+  constexpr int num_threads = 2;
+
   mkl_set_num_threads(num_threads);
+
   // If MKL is sequential mkl_set_num_threads will have no effect
   bool mkl_set_num_threads_applied = mkl_get_max_threads() == 2;
-#endif
+
   {
     dlaf::common::internal::SingleThreadedBlasScope single;
-#ifdef DLAF_WITH_OPENMP
-    EXPECT_EQ(omp_get_max_threads(), 1);
-#endif
-#ifdef DLAF_WITH_MKL
     EXPECT_EQ(mkl_get_max_threads(), 1);
-#endif
   }
 
-#ifdef DLAF_WITH_OPENMP
-  EXPECT_EQ(omp_get_max_threads(), num_threads);
-#endif
-#ifdef DLAF_WITH_MKL
   if (mkl_set_num_threads_applied) {
     EXPECT_EQ(mkl_get_max_threads(), num_threads);
   }
-#endif
 }
+#endif
+
+#ifdef DLAF_WITH_OPENMP
+TEST(SingleThreadedBlas, OpenMP) {
+  // Set the number of threads to something bigger than 1 so that we can
+  // actually observe if SingleThreadedBlasScope has an effect.
+  constexpr int num_threads = 2;
+
+  omp_set_num_threads(num_threads);
+
+  {
+    dlaf::common::internal::SingleThreadedBlasScope single;
+    EXPECT_EQ(omp_get_max_threads(), 1);
+  }
+
+  EXPECT_EQ(omp_get_max_threads(), num_threads);
+}
+#endif

44c9387458b523314b6730c38bafda0f2cbf71c0
Alberto Invernizzi <invernizzi@cscs.ch>
2023-05-05 16:28:06 +0200

    use matrix directly without redundantly getting their distribution

--- include/dlaf/permutations/general.h
+++ include/dlaf/permutations/general.h
@@ -36,10 +36,6 @@ namespace dlaf::permutations {
 template <Backend B, Device D, class T, Coord coord>
 void permute(SizeType i_begin, SizeType i_end, Matrix<const SizeType, D>& perms,
              Matrix<const T, D>& mat_in, Matrix<T, D>& mat_out) {
-  const matrix::Distribution& distr_perms = perms.distribution();
-  const matrix::Distribution& distr_in = mat_in.distribution();
-  const matrix::Distribution& distr_out = mat_out.distribution();
-
   DLAF_ASSERT(matrix::local_matrix(perms), perms);
   DLAF_ASSERT(matrix::local_matrix(mat_in), mat_in);
   DLAF_ASSERT(matrix::local_matrix(mat_out), mat_out);
@@ -56,13 +52,12 @@ void permute(SizeType i_begin, SizeType i_end, Matrix<const SizeType, D>& perms,
 
   DLAF_ASSERT(i_begin >= 0 && i_begin <= i_end, i_begin, i_end);
 
-  DLAF_ASSERT(i_end <= distr_perms.nrTiles().rows(), i_end, perms);
-  DLAF_ASSERT(i_end <= distr_in.nrTiles().rows() && i_end <= distr_in.nrTiles().cols(), i_end, mat_in);
-  DLAF_ASSERT(i_end <= distr_out.nrTiles().rows() && i_end <= distr_out.nrTiles().cols(), i_end,
-              mat_out);
+  DLAF_ASSERT(i_end <= perms.nrTiles().rows(), i_end, perms);
+  DLAF_ASSERT(i_end <= mat_in.nrTiles().rows() && i_end <= mat_in.nrTiles().cols(), i_end, mat_in);
+  DLAF_ASSERT(i_end <= mat_out.nrTiles().rows() && i_end <= mat_out.nrTiles().cols(), i_end, mat_out);
 
   DLAF_ASSERT(perms.size().cols() == 1, perms);
-  DLAF_ASSERT(distr_in.blockSize().get<coord>() == distr_perms.blockSize().rows(), mat_in, perms);
+  DLAF_ASSERT(mat_in.blockSize().template get<coord>() == perms.blockSize().rows(), mat_in, perms);
 
   internal::Permutations<B, D, T, coord>::call(i_begin, i_end, perms, mat_in, mat_out);
 }
@@ -91,10 +86,6 @@ template <Backend B, Device D, class T, Coord coord>
 void permute(comm::CommunicatorGrid grid, common::Pipeline<comm::Communicator>& sub_task_chain,
              SizeType i_begin, SizeType i_end, Matrix<const SizeType, D>& perms,
              Matrix<const T, D>& mat_in, Matrix<T, D>& mat_out) {
-  const matrix::Distribution& distr_perms = perms.distribution();
-  const matrix::Distribution& distr_in = mat_in.distribution();
-  const matrix::Distribution& distr_out = mat_out.distribution();
-
   DLAF_ASSERT(matrix::local_matrix(perms), perms);
   DLAF_ASSERT(matrix::equal_process_grid(mat_in, grid), mat_in, grid);
   DLAF_ASSERT(matrix::equal_process_grid(mat_out, grid), mat_out, grid);
@@ -110,12 +101,12 @@ void permute(comm::CommunicatorGrid grid, common::Pipeline<comm::Communicator>& 
   DLAF_ASSERT(matrix::equal_blocksize(mat_in, mat_out), mat_in);
 
   DLAF_ASSERT(i_begin >= 0 && i_begin <= i_end, i_begin, i_end);
-  DLAF_ASSERT(i_end <= distr_perms.nrTiles().rows(), i_end, perms);
-  DLAF_ASSERT(i_end <= distr_in.nrTiles().rows() && i_end <= distr_in.nrTiles().cols(), i_end, mat_in);
-  DLAF_ASSERT(i_end <= distr_out.nrTiles().rows() && i_end <= distr_out.nrTiles().cols(), i_end, mat_out);
+  DLAF_ASSERT(i_end <= perms.nrTiles().rows(), i_end, perms);
+  DLAF_ASSERT(i_end <= mat_in.nrTiles().rows() && i_end <= mat_in.nrTiles().cols(), i_end, mat_in);
+  DLAF_ASSERT(i_end <= mat_out.nrTiles().rows() && i_end <= mat_out.nrTiles().cols(), i_end, mat_out);
 
-  DLAF_ASSERT(distr_perms.size().cols() == 1, perms);
-  DLAF_ASSERT(distr_in.blockSize().get<coord>() == distr_perms.blockSize().rows(), mat_in, perms);
+  DLAF_ASSERT(perms.size().cols() == 1, perms);
+  DLAF_ASSERT(mat_in.blockSize().template get<coord>() == perms.blockSize().rows(), mat_in, perms);
 
   internal::Permutations<B, D, T, coord>::call(sub_task_chain, i_begin, i_end, perms, mat_in, mat_out);
 }

c94e0a973d8bfad13368aaca2f470c9f7bab2499
Mikael Simberg <mikael.simberg@iki.fi>
2023-05-15 13:40:40 +0200

    Reenable single threaded blas scope

--- src/common/single_threaded_blas.cpp
+++ src/common/single_threaded_blas.cpp
@@ -19,21 +19,21 @@
 
 namespace dlaf::common::internal {
 SingleThreadedBlasScope::SingleThreadedBlasScope() {
-// #ifdef DLAF_WITH_OPENMP
-//   omp_num_threads = omp_get_max_threads();
-//   omp_set_num_threads(1);
-// #endif
-// #ifdef DLAF_WITH_MKL
-//   mkl_num_threads = mkl_set_num_threads_local(1);
-// #endif
+#ifdef DLAF_WITH_OPENMP
+  omp_num_threads = omp_get_max_threads();
+  omp_set_num_threads(1);
+#endif
+#ifdef DLAF_WITH_MKL
+  mkl_num_threads = mkl_set_num_threads_local(1);
+#endif
 }
 
 SingleThreadedBlasScope::~SingleThreadedBlasScope() {
-// #ifdef DLAF_WITH_OPENMP
-//   omp_set_num_threads(omp_num_threads);
-// #endif
-// #ifdef DLAF_WITH_MKL
-//   mkl_set_num_threads_local(mkl_num_threads);
-// #endif
+#ifdef DLAF_WITH_OPENMP
+  omp_set_num_threads(omp_num_threads);
+#endif
+#ifdef DLAF_WITH_MKL
+  mkl_set_num_threads_local(mkl_num_threads);
+#endif
 }
 }

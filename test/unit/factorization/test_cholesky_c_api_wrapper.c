1f8f7feef98e58cb0811ea7d972a3bd7a4bb56f6
Rocco Meli <r.meli@bluemail.ch>
2023-06-01 08:25:37 +0200

    fix license

--- test/unit/factorization/test_cholesky_c_api_wrapper.c
+++ test/unit/factorization/test_cholesky_c_api_wrapper.c
@@ -1,7 +1,16 @@
-#include "test_cholesky_c_api_wrapper.h"
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2023, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
 
+#include "test_cholesky_c_api_wrapper.h"
 #include <dlaf_c/factorization/cholesky.h>
 
-void C_dlaf_pdpotrf(char uplo, int n, double* a, int ia, int ja, int* desca, int* info){
+void C_dlaf_pdpotrf(char uplo, int n, double* a, int ia, int ja, int* desca, int* info) {
   dlaf_pdpotrf(uplo, n, a, ia, ja, desca, info);
 }

4f38fd659cd99d795dc0772dcdf8e2a8277d8d12
Rocco Meli <rocco.meli@cscs.ch>
2023-06-20 08:45:13 +0200

    fix license

--- test/unit/c_api/grid/test_grid_c_api_wrapper.h
+++ test/unit/c_api/grid/test_grid_c_api_wrapper.h
@@ -1,4 +1,15 @@
-#include <dlaf_c/utils.h>
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2023, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
 #include <mpi.h>
+
+#include <dlaf_c/utils.h>
 
 DLAF_EXTERN_C char C_grid_ordering(MPI_Comm comm, int nprow, int npcol, int myprow, int mypcol);

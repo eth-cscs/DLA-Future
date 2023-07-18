7998b4c88a5f1da10a11c251833419bb2e06bf48
Rocco Meli <rocco.meli@cscs.ch>
2023-07-18 08:43:18 +0200

    remove try/catch

--- src/c_api/eigensolver/eigensolver.h
+++ src/c_api/eigensolver/eigensolver.h
@@ -30,62 +30,51 @@
 template <typename T>
 int eigensolver(int dlaf_context, char uplo, T* a, DLAF_descriptor dlaf_desca, dlaf::BaseType<T>* w,
                 T* z, DLAF_descriptor dlaf_descz) {
-  try {
-    using MatrixHost = dlaf::matrix::Matrix<T, dlaf::Device::CPU>;
-    using MatrixMirror = dlaf::matrix::MatrixMirror<T, dlaf::Device::Default, dlaf::Device::CPU>;
-    using MatrixBaseMirror =
-        dlaf::matrix::MatrixMirror<dlaf::BaseType<T>, dlaf::Device::Default, dlaf::Device::CPU>;
+  using MatrixHost = dlaf::matrix::Matrix<T, dlaf::Device::CPU>;
+  using MatrixMirror = dlaf::matrix::MatrixMirror<T, dlaf::Device::Default, dlaf::Device::CPU>;
+  using MatrixBaseMirror =
+      dlaf::matrix::MatrixMirror<dlaf::BaseType<T>, dlaf::Device::Default, dlaf::Device::CPU>;
 
-    DLAF_ASSERT(dlaf_desca.i == 0, dlaf_desca.i);
-    DLAF_ASSERT(dlaf_desca.j == 0, dlaf_desca.j);
-    DLAF_ASSERT(dlaf_descz.i == 0, dlaf_descz.i);
-    DLAF_ASSERT(dlaf_descz.j == 0, dlaf_descz.j);
+  DLAF_ASSERT(dlaf_desca.i == 0, dlaf_desca.i);
+  DLAF_ASSERT(dlaf_desca.j == 0, dlaf_desca.j);
+  DLAF_ASSERT(dlaf_descz.i == 0, dlaf_descz.i);
+  DLAF_ASSERT(dlaf_descz.j == 0, dlaf_descz.j);
 
-    pika::resume();
+  pika::resume();
 
-    auto communicator_grid = dlaf_grids.at(dlaf_context);
+  auto communicator_grid = dlaf_grids.at(dlaf_context);
 
-    auto [distribution_a, layout_a] = distribution_and_layout(dlaf_desca, communicator_grid);
-    auto [distribution_z, layout_z] = distribution_and_layout(dlaf_desca, communicator_grid);
+  auto [distribution_a, layout_a] = distribution_and_layout(dlaf_desca, communicator_grid);
+  auto [distribution_z, layout_z] = distribution_and_layout(dlaf_desca, communicator_grid);
 
-    MatrixHost matrix_host(distribution_a, layout_a, a);
-    MatrixHost eigenvectors_host(distribution_z, layout_z, z);
-    auto eigenvalues_host =
-        dlaf::matrix::createMatrixFromColMajor<dlaf::Device::CPU>({dlaf_descz.m, 1}, {dlaf_descz.mb, 1},
-                                                                  std::max(dlaf_descz.m, 1), w);
+  MatrixHost matrix_host(distribution_a, layout_a, a);
+  MatrixHost eigenvectors_host(distribution_z, layout_z, z);
+  auto eigenvalues_host =
+      dlaf::matrix::createMatrixFromColMajor<dlaf::Device::CPU>({dlaf_descz.m, 1}, {dlaf_descz.mb, 1},
+                                                                std::max(dlaf_descz.m, 1), w);
 
-    {
-      MatrixMirror matrix(matrix_host);
-      MatrixMirror eigenvectors(eigenvectors_host);
-      MatrixBaseMirror eigenvalues(eigenvalues_host);
+  {
+    MatrixMirror matrix(matrix_host);
+    MatrixMirror eigenvectors(eigenvectors_host);
+    MatrixBaseMirror eigenvalues(eigenvalues_host);
 
-      dlaf::eigensolver::eigensolver<dlaf::Backend::Default, dlaf::Device::Default, T>(
-          communicator_grid, blas::char2uplo(uplo), matrix.get(), eigenvalues.get(), eigenvectors.get());
-    }  // Destroy mirror
+    dlaf::eigensolver::eigensolver<dlaf::Backend::Default, dlaf::Device::Default, T>(
+        communicator_grid, blas::char2uplo(uplo), matrix.get(), eigenvalues.get(), eigenvectors.get());
+  }  // Destroy mirror
 
-    // Ensure data is copied back to the host
-    eigenvalues_host.waitLocalTiles();
-    eigenvectors_host.waitLocalTiles();
+  // Ensure data is copied back to the host
+  eigenvalues_host.waitLocalTiles();
+  eigenvectors_host.waitLocalTiles();
 
-    pika::suspend();
-    return 0;
-  }
-  catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-    return -1;
-  }
-  catch (...) {
-    std::cerr << "ERROR: Unknown exception caught in DLA-Future's eigensolver." << '\n';
-    return -1;
-  }
+  pika::suspend();
+  return 0;
 }
 
 #ifdef DLAF_WITH_SCALAPACK
 
 template <typename T>
-void pxheevd(char uplo, int m, T* a, [[maybe_unused]] int ia, [[maybe_unused]] int ja, int* desca,
-             dlaf::BaseType<T>* w, T* z, [[maybe_unused]] int iz, [[maybe_unused]] int jz, int* descz,
-             int& info) {
+void pxheevd(char uplo, int m, T* a, int ia, int ja, int* desca, dlaf::BaseType<T>* w, T* z, int iz,
+             int jz, int* descz, int& info) {
   DLAF_ASSERT(desca[0] == 1, desca[0]);
   DLAF_ASSERT(descz[0] == 1, descz[0]);
   DLAF_ASSERT(desca[1] == descz[1], desca[1], descz[1]);
--- src/c_api/factorization/cholesky.h
+++ src/c_api/factorization/cholesky.h
@@ -28,49 +28,38 @@
 #include "../utils.h"
 
 template <typename T>
-int cholesky(int dlaf_context, char uplo, T* a, DLAF_descriptor dlaf_desca) noexcept {
-  try {
-    using MatrixMirror = dlaf::matrix::MatrixMirror<T, dlaf::Device::Default, dlaf::Device::CPU>;
+int cholesky(int dlaf_context, char uplo, T* a, DLAF_descriptor dlaf_desca) {
+  using MatrixMirror = dlaf::matrix::MatrixMirror<T, dlaf::Device::Default, dlaf::Device::CPU>;
 
-    DLAF_ASSERT(dlaf_desca.i == 0, dlaf_desca.i);
-    DLAF_ASSERT(dlaf_desca.j == 0, dlaf_desca.j);
+  DLAF_ASSERT(dlaf_desca.i == 0, dlaf_desca.i);
+  DLAF_ASSERT(dlaf_desca.j == 0, dlaf_desca.j);
 
-    pika::resume();
+  pika::resume();
 
-    auto communicator_grid = dlaf_grids.at(dlaf_context);
+  auto communicator_grid = dlaf_grids.at(dlaf_context);
 
-    auto [distribution, layout] = distribution_and_layout(dlaf_desca, communicator_grid);
+  auto [distribution, layout] = distribution_and_layout(dlaf_desca, communicator_grid);
 
-    dlaf::matrix::Matrix<T, dlaf::Device::CPU> matrix_host(std::move(distribution), layout, a);
+  dlaf::matrix::Matrix<T, dlaf::Device::CPU> matrix_host(std::move(distribution), layout, a);
 
-    {
-      MatrixMirror matrix(matrix_host);
+  {
+    MatrixMirror matrix(matrix_host);
 
-      dlaf::factorization::cholesky<dlaf::Backend::Default, dlaf::Device::Default, T>(
-          communicator_grid, blas::char2uplo(uplo), matrix.get());
-    }  // Destroy mirror
+    dlaf::factorization::cholesky<dlaf::Backend::Default, dlaf::Device::Default, T>(
+        communicator_grid, blas::char2uplo(uplo), matrix.get());
+  }  // Destroy mirror
 
-    matrix_host.waitLocalTiles();
+  matrix_host.waitLocalTiles();
 
-    pika::suspend();
+  pika::suspend();
 
-    return 0;
-  }
-  catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-    return -1;
-  }
-  catch (...) {
-    std::cerr << "ERROR: Unknown exception caught in DLA-Future's Cholesky decomposition." << '\n';
-    return -1;
-  }
+  return 0;
 }
 
 #ifdef DLAF_WITH_SCALAPACK
 
 template <typename T>
-void pxpotrf(char uplo, int n, T* a, [[maybe_unused]] int ia, [[maybe_unused]] int ja, int* desca,
-             int& info) noexcept {
+void pxpotrf(char uplo, int n, T* a, int ia, int ja, int* desca, int& info) {
   DLAF_ASSERT(desca[0] == 1, desca[0]);
   DLAF_ASSERT(ia == 1, ia);
   DLAF_ASSERT(ja == 1, ja);

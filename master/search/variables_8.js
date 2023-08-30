34fc0612d2611737274c4c2d173aebae3e313f00
Doc CI Action <rasolca@users.noreply.github.com>
2023-08-30 11:43:24 +0000

    Doc Reimplementation of distributed band-to-tridiagonal (#946)

--- master/search/variables_8.js
+++ master/search/variables_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['n_1170',['n',['../struct_d_l_a_f__descriptor.html#ad2c386c95272a2a492dfa7e04b14fcd8',1,'DLAF_descriptor']]],
-  ['nb_1171',['nb',['../struct_d_l_a_f__descriptor.html#aa7e6f8e576e4e498b677617399297195',1,'DLAF_descriptor']]]
+  ['n_1171',['n',['../struct_d_l_a_f__descriptor.html#ad2c386c95272a2a492dfa7e04b14fcd8',1,'DLAF_descriptor']]],
+  ['nb_1172',['nb',['../struct_d_l_a_f__descriptor.html#aa7e6f8e576e4e498b677617399297195',1,'DLAF_descriptor']]]
 ];

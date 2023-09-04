60df966a45759399368f9c6915d2f942382874fe
Doc CI Action <rasolca@users.noreply.github.com>
2023-09-04 16:20:26 +0000

    Doc TridiagSolver (local): sort eigenvalues by column type for rank1 solver (#943)

--- master/search/functions_b.js
+++ master/search/functions_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['nblocks_1029',['nblocks',['../structdlaf_1_1common_1_1_data_descriptor.html#a583161cb3c61e3af88fe66552a123866',1,'dlaf::common::DataDescriptor']]],
-  ['nextlocaltilefromglobalelement_1030',['nextLocalTileFromGlobalElement',['../classdlaf_1_1matrix_1_1_distribution.html#a559caf9af0f658507a6a8fe949e09a93',1,'dlaf::matrix::Distribution']]],
-  ['nextlocaltilefromglobaltile_1031',['nextLocalTileFromGlobalTile',['../classdlaf_1_1matrix_1_1_distribution.html#afcd76386ddd1319978242aaf54412825',1,'dlaf::matrix::Distribution::nextLocalTileFromGlobalTile()'],['../util__distribution_8h.html#ad6d38066f4c7c00e41fc2d8830efb77f',1,'dlaf::util::matrix::nextLocalTileFromGlobalTile()']]],
-  ['nrtiles_1032',['nrTiles',['../classdlaf_1_1matrix_1_1_distribution.html#acc85a62e6350bcac93a8e442d4603516',1,'dlaf::matrix::Distribution::nrTiles()'],['../classdlaf_1_1matrix_1_1internal_1_1_matrix_base.html#a068e58f7f6c2956f92e69fd7c8d8f68d',1,'dlaf::matrix::internal::MatrixBase::nrTiles()']]]
+  ['nblocks_1028',['nblocks',['../structdlaf_1_1common_1_1_data_descriptor.html#a583161cb3c61e3af88fe66552a123866',1,'dlaf::common::DataDescriptor']]],
+  ['nextlocaltilefromglobalelement_1029',['nextLocalTileFromGlobalElement',['../classdlaf_1_1matrix_1_1_distribution.html#a559caf9af0f658507a6a8fe949e09a93',1,'dlaf::matrix::Distribution']]],
+  ['nextlocaltilefromglobaltile_1030',['nextLocalTileFromGlobalTile',['../classdlaf_1_1matrix_1_1_distribution.html#afcd76386ddd1319978242aaf54412825',1,'dlaf::matrix::Distribution::nextLocalTileFromGlobalTile()'],['../util__distribution_8h.html#ad6d38066f4c7c00e41fc2d8830efb77f',1,'dlaf::util::matrix::nextLocalTileFromGlobalTile()']]],
+  ['nrtiles_1031',['nrTiles',['../classdlaf_1_1matrix_1_1_distribution.html#acc85a62e6350bcac93a8e442d4603516',1,'dlaf::matrix::Distribution::nrTiles()'],['../classdlaf_1_1matrix_1_1internal_1_1_matrix_base.html#a068e58f7f6c2956f92e69fd7c8d8f68d',1,'dlaf::matrix::internal::MatrixBase::nrTiles()']]]
 ];

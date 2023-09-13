e83a2305ca11e1be80b0c26b2626062c51106ff7
Rocco Meli <r.meli@bluemail.ch>
2023-09-13 15:06:55 +0200

    fix gpu test

--- test/unit/eigensolver/test_reduction_to_band.cpp
+++ test/unit/eigensolver/test_reduction_to_band.cpp
@@ -464,9 +464,9 @@ TYPED_TEST(ReductionToBandTestGPU, CorrectnessDistributedSubBand) {
 
 TYPED_TEST(ReductionToBandTestGPU, Issue974) {
   for (auto&& comm_grid : this->commGrids()) {
-    testReductionToBandMatrix<TypeParam, Device::GPU, Backend::GPU>(comm_grid, {46, 46}, {32, 32}, 32,
+    testReductionToBand<TypeParam, Device::GPU, Backend::GPU>(comm_grid, {46, 46}, {32, 32}, 32,
                                                                     true);
-    testReductionToBandMatrix<TypeParam, Device::GPU, Backend::GPU>(comm_grid, {46, 46}, {46, 46}, 46,
+    testReductionToBand<TypeParam, Device::GPU, Backend::GPU>(comm_grid, {46, 46}, {46, 46}, 46,
                                                                     true);
   }
 }

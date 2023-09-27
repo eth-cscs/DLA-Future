e69d0a4745334384a5c81085cb840a3e8ce641b5
Rocco Meli <rocco.meli@cscs.ch>
2023-09-27 09:16:31 +0200

    move to correct container

--- ci/docker/codecov.Dockerfile
+++ ci/docker/codecov.Dockerfile
@@ -30,7 +30,7 @@ RUN spack repo rm --scope site dlaf && \
     spack -e ci concretize -f && \
     mkdir ${BUILD} && \
     ln -s ${BUILD} `spack -e ci location -b dla-future` && \
-    spack -e ci --config "config:flags:keep_werror:all" install --jobs ${NUM_PROCS} --keep-stage --verbose
+    spack -e ci --config "config:flags:keep_werror:all" --config "packages:dla-future:variants:test-output-path=${CACHE_FOLDER}" install --jobs ${NUM_PROCS} --keep-stage --verbose
 
 # Prune and bundle binaries
 RUN mkdir ${BUILD}-tmp && cd ${BUILD} && \
--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -120,7 +120,7 @@ class DlaFuture(CMakePackage, CudaPackage, ROCmPackage):
     variant("ci-test", default=False, description="Build for CI (Advanced usage).")
     conflicts("~miniapps", when="+ci-test")
     
-    variant("test_output_path", default="/tmp", when="+ci-test +hdf5", description="Output path for tests")
+    variant("test-output-path", default="/tmp", when="+ci-test +hdf5", description="Output path for tests")
 
     variant(
         "ci-check-threads",
@@ -210,7 +210,7 @@ class DlaFuture(CMakePackage, CudaPackage, ROCmPackage):
             args.append(self.define("DLAF_BUILD_TESTING", True))
             args.append(self.define("DLAF_BUILD_TESTING_HEADER", True))
             args.append(self.define("DLAF_CI_RUNNER_USES_MPIRUN", True))
-            args.append(self.define_from_variant("DLAF_TEST_OUTPUT_PATH", "test_output_path"))
+            args.append(self.define_from_variant("DLAF_TEST_OUTPUT_PATH", "test-output-path"))
         else:
             # TEST
             args.append(self.define("DLAF_BUILD_TESTING", self.run_tests))

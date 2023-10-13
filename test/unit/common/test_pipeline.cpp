6c2822a0ec051f513ed9550c6bc85cbd681242d2
Mikael Simberg <simberg@cscs.ch>
2023-10-13 10:15:35 +0200

    Add random access test to test_pipeline

--- test/unit/common/test_pipeline.cpp
+++ test/unit/common/test_pipeline.cpp
@@ -10,8 +10,13 @@
 
 #include <atomic>
 #include <chrono>
+#include <cstddef>
+#include <mutex>
+#include <random>
+#include <vector>
 
 #include <pika/execution.hpp>
+#include <pika/mutex.hpp>
 #include <pika/thread.hpp>
 
 #include <dlaf/common/pipeline.h>
@@ -195,4 +200,321 @@ TEST(SubPipeline, Basic) {
                      });
 
   tt::sync_wait(ex::when_all(std::move(checkpoint0), std::move(checkpoint1), std::move(checkpoint2)));
+}
+
+TEST(SubPipeline, BasicParentAccess) {
+  // A subpipeline will not start executing if the parent hasn't released its accesses
+  Pipeline<int> pipeline(26);
+
+  auto first_parent_sender = pipeline();
+  Pipeline<int> sub_pipeline = pipeline.sub_pipeline();
+  auto last_parent_sender = pipeline();
+
+  std::atomic<bool> first_parent_access_done{false};
+  std::atomic<bool> first_access_done{false};
+  std::atomic<bool> second_access_done{false};
+  std::atomic<bool> third_access_done{false};
+  std::atomic<bool> last_parent_access_done{false};
+
+  auto checkpointparent_first = std::move(first_parent_sender) | ex::then([&](auto&& wrapper) {
+                                  EXPECT_FALSE(first_parent_access_done);
+                                  EXPECT_FALSE(first_access_done);
+                                  EXPECT_FALSE(second_access_done);
+                                  EXPECT_FALSE(third_access_done);
+                                  EXPECT_FALSE(last_parent_access_done);
+                                  first_parent_access_done = true;
+                                  auto local = std::move(wrapper);
+                                  dlaf::internal::silenceUnusedWarningFor(local);
+                                });
+
+  auto checkpoint0 = sub_pipeline() | ex::then([&](auto&& wrapper) {
+                       EXPECT_TRUE(first_parent_access_done);
+                       EXPECT_FALSE(first_access_done);
+                       EXPECT_FALSE(second_access_done);
+                       EXPECT_FALSE(third_access_done);
+                       EXPECT_FALSE(last_parent_access_done);
+                       first_access_done = true;
+                       auto local = std::move(wrapper);
+                       dlaf::internal::silenceUnusedWarningFor(local);
+                     });
+  auto checkpoint1 = sub_pipeline() | ex::then([&](auto&& wrapper) {
+                       EXPECT_TRUE(first_parent_access_done);
+                       EXPECT_TRUE(first_access_done);
+                       EXPECT_FALSE(second_access_done);
+                       EXPECT_FALSE(third_access_done);
+                       EXPECT_FALSE(last_parent_access_done);
+                       second_access_done = true;
+                       auto local = std::move(wrapper);
+                       dlaf::internal::silenceUnusedWarningFor(local);
+                     });
+  auto checkpoint2 = sub_pipeline() | ex::then([&](auto&& wrapper) {
+                       EXPECT_TRUE(first_parent_access_done);
+                       EXPECT_TRUE(first_access_done);
+                       EXPECT_TRUE(second_access_done);
+                       EXPECT_FALSE(third_access_done);
+                       EXPECT_FALSE(last_parent_access_done);
+                       third_access_done = true;
+                       auto local = std::move(wrapper);
+                       dlaf::internal::silenceUnusedWarningFor(local);
+                     });
+  auto checkpointparent_last = std::move(last_parent_sender) | ex::then([&](auto&& wrapper) {
+                                 EXPECT_TRUE(first_parent_access_done);
+                                 EXPECT_TRUE(first_access_done);
+                                 EXPECT_TRUE(second_access_done);
+                                 EXPECT_TRUE(third_access_done);
+                                 EXPECT_FALSE(last_parent_access_done);
+                                 last_parent_access_done = true;
+                                 auto local = std::move(wrapper);
+                                 dlaf::internal::silenceUnusedWarningFor(local);
+                               });
+
+  // The first parent access and all sub pipeline accesses should complete here. The last parent access
+  // should not complete until the sub pipeline has been reset.
+  auto checkpointparent_last_started = ex::ensure_started(std::move(checkpointparent_last));
+  tt::sync_wait(ex::when_all(std::move(checkpoint0), std::move(checkpoint1), std::move(checkpoint2),
+                             std::move(checkpointparent_first)));
+  // Since the last parent access will be run as an inline continuation and the access was eagerly
+  // started, it should be triggered by the reset of the sub pipeline even without a sync_wait.
+  EXPECT_FALSE(last_parent_access_done);
+  sub_pipeline.reset();
+  EXPECT_TRUE(last_parent_access_done);
+}
+
+// A nullable int is "emptied" on move construction and assignment (i.e. reset to zero) so that we can
+// detect in the test if we're using a moved-from int at some point.
+class nullable_size_t {
+  // For testing purposes we cheat and make x mutable so that we can increment it through const
+  // references. Since we externally protect access to it we will not be concurrently modifying it even
+  // though the Pipeline itself gives concurrent (read-only) access.
+  mutable std::size_t x = 0;
+
+public:
+  nullable_size_t() = default;
+  nullable_size_t(std::size_t x) : x(x) {}
+
+  nullable_size_t(nullable_size_t&& other) : x(std::exchange(other.x, 0)) {}
+  nullable_size_t& operator=(nullable_size_t&& other) {
+    x = std::exchange(other.x, 0);
+    return *this;
+  }
+
+  nullable_size_t(const nullable_size_t& other) = default;
+  nullable_size_t& operator=(const nullable_size_t& other) = default;
+
+  std::size_t get() const noexcept {
+    return x;
+  }
+  std::size_t operator++() const noexcept {
+    return ++x;
+  }
+};
+
+enum class SubPipelineAccessType { inline_access, new_task };
+
+struct PipelineTestConfig {
+  std::size_t min_readwrite_accesses;
+  std::size_t max_readwrite_accesses;
+  std::size_t min_readonly_accesses;
+  std::size_t max_readonly_accesses;
+  SubPipelineAccessType access_type;
+};
+
+const std::vector<PipelineTestConfig> pipeline_tests({
+    // No nested access
+    {0, 0, 0, 0, SubPipelineAccessType::inline_access},
+
+    // Read-write access
+    {0, 1, 0, 0, SubPipelineAccessType::inline_access},
+    {0, 5, 0, 0, SubPipelineAccessType::inline_access},
+    {5, 10, 0, 0, SubPipelineAccessType::inline_access},
+
+    // Read-only access
+    {0, 0, 0, 1, SubPipelineAccessType::inline_access},
+    {0, 0, 0, 5, SubPipelineAccessType::inline_access},
+    {0, 0, 5, 10, SubPipelineAccessType::inline_access},
+
+    // Mixed
+    {0, 1, 0, 1, SubPipelineAccessType::inline_access},
+    {0, 1, 1, 5, SubPipelineAccessType::inline_access},
+    {1, 5, 0, 1, SubPipelineAccessType::inline_access},
+    {5, 10, 5, 10, SubPipelineAccessType::inline_access},
+
+    // Same with subpipelines in new tasks
+    {0, 0, 0, 0, SubPipelineAccessType::new_task},
+
+    // Read-write access
+    {0, 1, 0, 0, SubPipelineAccessType::new_task},
+    {0, 5, 0, 0, SubPipelineAccessType::new_task},
+    {5, 10, 0, 0, SubPipelineAccessType::new_task},
+
+    // Read-only access
+    {0, 0, 0, 1, SubPipelineAccessType::new_task},
+    {0, 0, 0, 5, SubPipelineAccessType::new_task},
+    {0, 0, 5, 10, SubPipelineAccessType::new_task},
+
+    // Mixed
+    {0, 1, 0, 1, SubPipelineAccessType::new_task},
+    {0, 1, 1, 5, SubPipelineAccessType::new_task},
+    {1, 5, 0, 1, SubPipelineAccessType::new_task},
+    {5, 10, 5, 10, SubPipelineAccessType::new_task},
+});
+
+struct PipelineTestState {
+  std::atomic<std::size_t> spawn_count{0};
+  std::atomic<std::size_t> access_count{0};
+  std::vector<ex::unique_any_sender<>> accesses{};
+  pika::mutex accesses_mutex;
+  pika::mutex count_mutex;
+};
+
+using PipelineInt = Pipeline<nullable_size_t>;
+using Generator = std::mt19937;
+
+void spawn_work_readwrite(PipelineInt& pipeline, PipelineTestState& state, Generator& gen,
+                          const PipelineTestConfig& test) {
+  std::uniform_int_distribution<std::size_t> dist(test.min_readwrite_accesses,
+                                                  test.max_readwrite_accesses);
+
+  for (std::size_t i = 0; i < dist(gen); ++i) {
+    ++state.spawn_count;
+    auto s = pipeline() |
+             dlaf::internal::transform(dlaf::internal::Policy<dlaf::Backend::MC>(),
+                                       [&state, spawn_count = state.spawn_count.load()](auto& count) {
+                                         ++state.access_count;
+                                         ++count;
+
+                                         EXPECT_EQ(state.access_count.load(), spawn_count);
+                                         EXPECT_EQ(count.get(), spawn_count);
+                                       }) |
+             ex::ensure_started();
+
+    std::lock_guard l(state.accesses_mutex);
+    state.accesses.emplace_back(std::move(s));
+  }
+}
+
+void spawn_work_readonly(PipelineInt& pipeline, PipelineTestState& state, Generator& gen,
+                         const PipelineTestConfig& test) {
+  std::uniform_int_distribution<std::size_t> dist(test.min_readonly_accesses,
+                                                  test.max_readonly_accesses);
+
+  const std::size_t num_readonly_accesses = dist(gen);
+  const std::size_t min_expected_count = state.spawn_count + 1;
+  const std::size_t max_expected_count = state.spawn_count + 1 + num_readonly_accesses;
+  for (std::size_t i = 0; i < num_readonly_accesses; ++i) {
+    ++state.spawn_count;
+    auto s = pipeline.read() |
+             dlaf::internal::transform(dlaf::internal::Policy<dlaf::Backend::MC>(),
+                                       [&state, min_expected_count, max_expected_count,
+                                        spawn_count = state.spawn_count.load()](auto& count) {
+                                         const auto current_access_count = ++state.access_count;
+
+                                         const auto current_count = [&] {
+                                           std::lock_guard l(state.count_mutex);
+                                           return ++count;
+                                         }();
+
+                                         EXPECT_GE(current_access_count, min_expected_count);
+                                         EXPECT_LT(current_access_count, max_expected_count);
+                                         EXPECT_GE(current_count, min_expected_count);
+                                         EXPECT_LT(current_count, max_expected_count);
+                                       }) |
+             ex::ensure_started();
+
+    std::lock_guard l(state.accesses_mutex);
+    state.accesses.emplace_back(std::move(s));
+  }
+}
+
+void spawn_work(PipelineInt& pipeline, PipelineTestState& state, Generator& gen,
+                const PipelineTestConfig& test) {
+  // Note that any of the blocks below may spawn zero accesses depending the test configuration
+  spawn_work_readwrite(pipeline, state, gen, test);
+  spawn_work_readonly(pipeline, state, gen, test);
+  spawn_work_readwrite(pipeline, state, gen, test);
+}
+
+void test_recurse_sub_pipeline(PipelineInt& pipeline, PipelineTestState& state, Generator& gen,
+                               const PipelineTestConfig& test, std::size_t remaining_depth) {
+  if (remaining_depth == 0) {
+    return;
+  }
+
+  spawn_work(pipeline, state, gen, test);
+
+  auto recurse_f = [&state, &test, gen, remaining_depth,
+                    sub_pipeline = pipeline.sub_pipeline()]() mutable {
+    test_recurse_sub_pipeline(sub_pipeline, state, gen, test, remaining_depth - 1);
+  };
+
+  switch (test.access_type) {
+    case SubPipelineAccessType::inline_access:
+      recurse_f();
+      break;
+    case SubPipelineAccessType::new_task:
+      ex::start_detached(ex::just() |
+                         dlaf::internal::transform(dlaf::internal::Policy<dlaf::Backend::MC>(),
+                                                   std::move(recurse_f)));
+      break;
+  }
+
+  // If we are using subpipelines in new tasks we can't keep track of the correct counts anymore after
+  // spawning the task with the subpipeline
+  if (test.access_type == SubPipelineAccessType::inline_access) {
+    spawn_work(pipeline, state, gen, test);
+  }
+}
+
+TEST(SubPipeline, RandomAccess) {
+  constexpr std::size_t depth = 5;
+  constexpr std::size_t num_seeds = 10;
+
+  for (const auto& test : pipeline_tests) {
+    for (std::size_t seed = 0; seed < num_seeds; ++seed) {
+      std::mt19937 gen(seed);
+
+      PipelineTestState state{};
+      PipelineInt pipeline(0);
+
+      // Make the first access to the parent pipeline, but do not eagerly start it
+      ++state.spawn_count;
+      auto first_access = pipeline() | ex::then([&state](auto wrapper) {
+                            ++state.access_count;
+                            ++wrapper.get();
+
+                            EXPECT_EQ(state.access_count.load(), 1);
+                            EXPECT_EQ(wrapper.get().get(), 1);
+                          });
+
+      test_recurse_sub_pipeline(pipeline, state, gen, test, depth);
+
+      // After the work has been created the spawn count should be at least one and equal to the number
+      // of senders stored in accesses. No accesses should be done yet at this point since they depend
+      // on the first access completing.
+      ASSERT_GE(state.spawn_count, 1);
+      ASSERT_GE(state.spawn_count, state.accesses.size());
+      EXPECT_EQ(state.access_count.load(), 0);
+
+      // After the first access is done there can be one or more accesses done
+      tt::sync_wait(std::move(first_access));
+      EXPECT_GE(state.access_count.load(), 1);
+
+      // After all accesses have completed we expect there to have been more than one access. We can
+      // only check the following if we are spawning work inline.
+      if (test.access_type == SubPipelineAccessType::inline_access) {
+        tt::sync_wait(ex::when_all_vector(std::move(state.accesses)));
+
+        if (test.min_readwrite_accesses > 0 || test.min_readonly_accesses) {
+          EXPECT_GT(state.access_count.load(), 1);
+        }
+
+        // We should have accessed the pipeline value the same number of times as spawns
+        EXPECT_EQ(state.access_count.load(), state.spawn_count);
+      }
+
+      // The count held by the pipeline should have been incremented spawn_count times by now
+      auto final_count = tt::sync_wait(pipeline()).get().get();
+      EXPECT_EQ(state.spawn_count, final_count);
+    }
+  }
 }

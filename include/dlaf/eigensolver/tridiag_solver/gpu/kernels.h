c4e5bba7904d5dc2d72d1e5ac6f1022818c87a08
Raffaele Solcà <rasolca@cscs.ch>
2023-11-22 19:06:08 +0100

    Restructure the code such that it can be compiled with nvcc 11.2 (#1045)

---
+++ include/dlaf/eigensolver/tridiag_solver/gpu/kernels.h
@@ -0,0 +1,60 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2023, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#ifdef DLAF_WITH_GPU
+
+#include <whip.hpp>
+
+#include <dlaf/eigensolver/tridiag_solver/coltype.h>
+#include <dlaf/gpu/lapack/api.h>
+#include <dlaf/types.h>
+
+namespace dlaf::eigensolver::internal::gpu {
+
+template <class T>
+void castToComplex(const SizeType m, const SizeType n, const T* in, const SizeType ld_in,
+                   std::complex<T>* out, const SizeType ld_out, whip::stream_t stream);
+
+#define DLAF_GPU_CAST_TO_COMPLEX_ETI(kword, Type)                                       \
+  kword template void castToComplex(const SizeType m, const SizeType n, const Type* in, \
+                                    const SizeType ld_in, std::complex<Type>* out,      \
+                                    const SizeType ld_out, whip::stream_t stream)
+
+DLAF_GPU_CAST_TO_COMPLEX_ETI(extern, float);
+DLAF_GPU_CAST_TO_COMPLEX_ETI(extern, double);
+
+template <class T>
+void assembleRank1UpdateVectorTile(bool is_top_tile, T rho, const SizeType m, const SizeType n,
+                                   const T* evecs, const SizeType ld_evecs, T* rank1,
+                                   whip::stream_t stream);
+
+#define DLAF_GPU_ASSEMBLE_RANK1_UPDATE_VECTOR_TILE_ETI(kword, Type)                               \
+  kword template void assembleRank1UpdateVectorTile(bool is_top_tile, Type rho, const SizeType m, \
+                                                    const SizeType n, const Type* evecs,          \
+                                                    const SizeType ld_evecs, Type* rank1,         \
+                                                    whip::stream_t stream)
+
+DLAF_GPU_ASSEMBLE_RANK1_UPDATE_VECTOR_TILE_ETI(extern, float);
+DLAF_GPU_ASSEMBLE_RANK1_UPDATE_VECTOR_TILE_ETI(extern, double);
+
+template <class T>
+void givensRotationOnDevice(SizeType len, T* x, T* y, T c, T s, whip::stream_t stream);
+
+#define DLAF_GIVENS_ROT_ETI(kword, Type)                                                     \
+  kword template void givensRotationOnDevice(SizeType len, Type* x, Type* y, Type c, Type s, \
+                                             whip::stream_t stream)
+
+DLAF_GIVENS_ROT_ETI(extern, float);
+DLAF_GIVENS_ROT_ETI(extern, double);
+
+}
+#endif
--- include/dlaf/eigensolver/tridiag_solver/impl.h
+++ include/dlaf/eigensolver/tridiag_solver/impl.h
@@ -21,7 +21,7 @@
 #include <dlaf/common/callable_object.h>
 #include <dlaf/communication/communicator_grid.h>
 #include <dlaf/eigensolver/tridiag_solver/api.h>
-#include <dlaf/eigensolver/tridiag_solver/kernels.h>
+#include <dlaf/eigensolver/tridiag_solver/kernels_async.h>
 #include <dlaf/eigensolver/tridiag_solver/merge.h>
 #include <dlaf/lapack/tile.h>
 #include <dlaf/matrix/copy_tile.h>
@@ -94,8 +94,7 @@ template <class T>
 void solveLeaf(Matrix<T, Device::CPU>& tridiag, Matrix<T, Device::CPU>& evecs) {
   const SizeType ntiles = tridiag.distribution().nrTiles().rows();
   for (SizeType i = 0; i < ntiles; ++i) {
-    stedcAsync<Device::CPU>(tridiag.readwrite(LocalTileIndex(i, 0)),
-                            evecs.readwrite(LocalTileIndex(i, i)));
+    stedcAsync(tridiag.readwrite(LocalTileIndex(i, 0)), evecs.readwrite(LocalTileIndex(i, i)));
   }
 }
 
@@ -113,7 +112,7 @@ void solveLeaf(Matrix<T, Device::CPU>& tridiag, Matrix<T, Device::GPU>& evecs,
     const auto id_tr = LocalTileIndex(i, 0);
     const auto id_ev = LocalTileIndex(i, i);
 
-    stedcAsync<Device::CPU>(tridiag.readwrite(id_tr), h_evecs.readwrite(id_ev));
+    stedcAsync(tridiag.readwrite(id_tr), h_evecs.readwrite(id_ev));
     ex::start_detached(ex::when_all(h_evecs.read(id_ev), evecs.readwrite(id_ev)) | copy(cp_policy));
   }
 }
@@ -295,7 +294,7 @@ void solveDistLeaf(comm::CommunicatorGrid grid, common::Pipeline<comm::Communica
     const comm::Index2D ii_rank = dist.rankGlobalTile(ii_tile);
     const GlobalTileIndex id_tr(i, 0);
     if (ii_rank == this_rank) {
-      stedcAsync<Device::CPU>(tridiag.readwrite(id_tr), evecs.readwrite(ii_tile));
+      stedcAsync(tridiag.readwrite(id_tr), evecs.readwrite(ii_tile));
       ex::start_detached(comm::scheduleSendBcast(full_task_chain(), tridiag.read(id_tr)));
     }
     else {
@@ -324,7 +323,7 @@ void solveDistLeaf(comm::CommunicatorGrid grid, common::Pipeline<comm::Communica
     const comm::Index2D ii_rank = dist.rankGlobalTile(ii_tile);
     const GlobalTileIndex id_tr(i, 0);
     if (ii_rank == this_rank) {
-      stedcAsync<Device::CPU>(tridiag.readwrite(id_tr), h_evecs.readwrite(ii_tile));
+      stedcAsync(tridiag.readwrite(id_tr), h_evecs.readwrite(ii_tile));
       ex::start_detached(ex::when_all(h_evecs.read(ii_tile), evecs.readwrite(ii_tile)) |
                          copy(cp_policy));
       ex::start_detached(comm::scheduleSendBcast(full_task_chain(), tridiag.read(id_tr)));

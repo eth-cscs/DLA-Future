dbd5503bcbbc06e5b05b47743a34106921cce4d0
Doc CI Action <rasolca@users.noreply.github.com>
2023-11-27 16:47:36 +0000

    Doc: Panel: add limited functionality for Matrix with offset (#1047)

--- master/search/variables_4.js
+++ master/search/variables_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['i_1230',['i',['../struct_d_l_a_f__descriptor.html#a35753e2a24f31d479019b186e71f919d',1,'DLAF_descriptor']]],
-  ['internal_5f_1231',['internal_',['../structdlaf_1_1matrix_1_1_panel_3_01axis_00_01const_01_t_00_01_d_00_01_store_transposed_1_1_no_01_4.html#adf84f8c3806212547305082bf21ac006',1,'dlaf::matrix::Panel&lt; axis, const T, D, StoreTransposed::No &gt;']]],
-  ['isrc_1232',['isrc',['../struct_d_l_a_f__descriptor.html#aaf3ffdca2bf377353f1248f2339d9249',1,'DLAF_descriptor']]]
+  ['i_1231',['i',['../struct_d_l_a_f__descriptor.html#a35753e2a24f31d479019b186e71f919d',1,'DLAF_descriptor']]],
+  ['internal_5f_1232',['internal_',['../structdlaf_1_1matrix_1_1_panel_3_01axis_00_01const_01_t_00_01_d_00_01_store_transposed_1_1_no_01_4.html#adf84f8c3806212547305082bf21ac006',1,'dlaf::matrix::Panel&lt; axis, const T, D, StoreTransposed::No &gt;']]],
+  ['isrc_1233',['isrc',['../struct_d_l_a_f__descriptor.html#aaf3ffdca2bf377353f1248f2339d9249',1,'DLAF_descriptor']]]
 ];
--- master/search/variables_7.js
+++ master/search/variables_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['m_1236',['m',['../struct_d_l_a_f__descriptor.html#a662ff55ddd73a8f07fa474221fe120fe',1,'DLAF_descriptor']]],
-  ['mb_1237',['mb',['../struct_d_l_a_f__descriptor.html#ad57b91c440fcdc67cf2ee27c99a1409f',1,'DLAF_descriptor']]]
+  ['m_1237',['m',['../struct_d_l_a_f__descriptor.html#a662ff55ddd73a8f07fa474221fe120fe',1,'DLAF_descriptor']]],
+  ['mb_1238',['mb',['../struct_d_l_a_f__descriptor.html#ad57b91c440fcdc67cf2ee27c99a1409f',1,'DLAF_descriptor']]]
 ];

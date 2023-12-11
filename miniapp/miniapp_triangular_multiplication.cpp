a3510873971c0dd5aa8d5534a173ff75cb1406b7
Guglielmo Gagliardi <guglielmo.gagliardi@cscs.ch>
2023-12-11 11:32:34 +0100

    complete

--- miniapp/miniapp_triangular_multiplication.cpp
+++ miniapp/miniapp_triangular_multiplication.cpp
@@ -118,6 +118,8 @@ struct triangularMultiplicationMiniapp {
     DLAF_ASSERT(side == blas::Side::Left, side);
 
     const auto side = opts.side;
+
+    const auto side = opts.side;
     const auto uplo = opts.uplo;
     const auto op = opts.op;
     const auto diag = opts.diag;

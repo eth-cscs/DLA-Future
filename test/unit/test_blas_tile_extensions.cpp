abab9bbab772237b09c0dc1e5be517b4be389b3f
Guglielmo Gagliardi <guglielmo.gagliardi@cscs.ch>
2024-01-16 11:57:08 +0100

    add the add_sizes e scal_sizes and refactor the parameter

--- test/unit/test_blas_tile_extensions.cpp
+++ test/unit/test_blas_tile_extensions.cpp
@@ -42,9 +42,27 @@ std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType, SizeType, SizeTyp
     {6, 9, 12, 1, 1, 1}, {32, 32, 32, 0, 0, 0}, {32, 32, 32, 4, 5, 7}, {128, 128, 128, 0, 0, 0},
 };
 
+// Tuple elements:  m, n, extra_lda
+std::vector<std::tuple<SizeType, SizeType, SizeType>> scal_sizes = {
+    {0, 0, 0, 0, 0, 0},                                               // all 0 sizes
+    {7, 0, 0, 3, 1, 0},  {0, 5, 0, 0, 0, 1},    {0, 0, 11, 1, 1, 2},  // two 0 sizes
+    {0, 5, 13, 1, 0, 1}, {7, 0, 4, 1, 2, 0},    {3, 11, 0, 0, 1, 0},  // one 0 size
+    {1, 1, 1, 0, 3, 0},  {1, 12, 1, 1, 0, 7},   {17, 12, 16, 1, 3, 0}, {11, 23, 8, 0, 3, 4},
+    {6, 9, 12, 1, 1, 1}, {32, 32, 32, 0, 0, 0}, {32, 32, 32, 4, 5, 7}, {128, 128, 128, 0, 0, 0},
+};
+
+// Tuple elements:  m, n, k, extra_lda, extra_ldb
+std::vector<std::tuple<SizeType, SizeType, SizeType, SizeType>> add_sizes = {
+    {0, 0, 0, 0, 0, 0},                                               // all 0 sizes
+    {7, 0, 0, 3, 1, 0},  {0, 5, 0, 0, 0, 1},    {0, 0, 11, 1, 1, 2},  // two 0 sizes
+    {0, 5, 13, 1, 0, 1}, {7, 0, 4, 1, 2, 0},    {3, 11, 0, 0, 1, 0},  // one 0 size
+    {1, 1, 1, 0, 3, 0},  {1, 12, 1, 1, 0, 7},   {17, 12, 16, 1, 3, 0}, {11, 23, 8, 0, 3, 4},
+    {6, 9, 12, 1, 1, 1}, {32, 32, 32, 0, 0, 0}, {32, 32, 32, 4, 5, 7}, {128, 128, 128, 0, 0, 0},
+};
+
 TYPED_TEST(TileOperationsExtensionsTestMC, Scal) {
   using Type = TypeParam;
-  for (const auto& [m, n, k, extra_lda, extra_ldb, extra_ldc] : gemm_sizes) {
+  for (const auto& [m, n, k, extra_lda] : scal_sizes) {
     // Test a and b const Tiles.
     dlaf::test::testScal<Device::CPU, Type>(m, n, extra_lda);
     // Test a and b non const Tiles.
@@ -56,7 +74,7 @@ TYPED_TEST(TileOperationsExtensionsTestMC, Scal) {
 TYPED_TEST(TileOperationsExtensionsTestGPU, Scal) {
   using Type = TypeParam;
 
-  for (const auto& [m, n, k, extra_lda, extra_ldb, extra_ldc] : gemm_sizes) {
+  for (const auto& [m, n, k, extra_lda] : scal_sizes) {
     // Test a and b const Tiles.
     dlaf::test::testScal<Device::GPU, Type>(m, n, extra_lda);
 
@@ -68,7 +86,7 @@ TYPED_TEST(TileOperationsExtensionsTestGPU, Scal) {
 
 TYPED_TEST(TileOperationsExtensionsTestMC, Add) {
   using Type = TypeParam;
-  for (const auto& [m, n, k, extra_lda, extra_ldb, extra_ldc] : gemm_sizes) {
+  for (const auto& [m, n, k, extra_lda, extra_ldb] : add_sizes) {
     // Test a and b const Tiles.
     dlaf::test::testAdd<Device::CPU, Type>(m, n, extra_lda, extra_ldb);
     // Test a and b non const Tiles.
@@ -80,7 +98,7 @@ TYPED_TEST(TileOperationsExtensionsTestMC, Add) {
 TYPED_TEST(TileOperationsExtensionsTestGPU, Add) {
   using Type = TypeParam;
 
-  for (const auto& [m, n, k, extra_lda, extra_ldb, extra_ldc] : gemm_sizes) {
+  for (const auto& [m, n, k, extra_lda, extra_ldb] : add_sizes) {
     // Test a and b const Tiles.
     dlaf::test::testAdd<Device::GPU, Type>(m, n, extra_lda, extra_ldb);
 

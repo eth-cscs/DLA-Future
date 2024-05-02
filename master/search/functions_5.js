f5a1b2bdd8efe56d161e11f6cc702bc851f24807
Doc CI Action <rasolca@users.noreply.github.com>
2024-05-02 15:20:27 +0000

    Doc: Instrument distributed eigensolver with HDF5 (#1016)

--- master/search/functions_5.js
+++ master/search/functions_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['finalize_1032',['finalize',['../dlaf_2init_8h.html#abf5448f6d8cff5d741a7680123a7082c',1,'dlaf']]],
-  ['full_5fcommunicator_5fpipeline_1033',['full_communicator_pipeline',['../classdlaf_1_1comm_1_1_communicator_grid.html#aff6495095473aef3f010d97c5a6786ae',1,'dlaf::comm::CommunicatorGrid']]],
-  ['fullcommunicator_1034',['fullCommunicator',['../classdlaf_1_1comm_1_1_communicator_grid.html#a1cb5842312d3e88e298819ea192d286d',1,'dlaf::comm::CommunicatorGrid']]],
-  ['fullindex_1035',['fullIndex',['../structdlaf_1_1matrix_1_1_panel_3_01axis_00_01const_01_t_00_01_d_00_01_store_transposed_1_1_no_01_4.html#a709e5ba882750052d23a5a8eb1eff21a',1,'dlaf::matrix::Panel&lt; axis, const T, D, StoreTransposed::No &gt;']]]
+  ['finalize_1031',['finalize',['../dlaf_2init_8h.html#abf5448f6d8cff5d741a7680123a7082c',1,'dlaf']]],
+  ['full_5fcommunicator_5fpipeline_1032',['full_communicator_pipeline',['../classdlaf_1_1comm_1_1_communicator_grid.html#aff6495095473aef3f010d97c5a6786ae',1,'dlaf::comm::CommunicatorGrid']]],
+  ['fullcommunicator_1033',['fullCommunicator',['../classdlaf_1_1comm_1_1_communicator_grid.html#a1cb5842312d3e88e298819ea192d286d',1,'dlaf::comm::CommunicatorGrid']]],
+  ['fullindex_1034',['fullIndex',['../structdlaf_1_1matrix_1_1_panel_3_01axis_00_01const_01_t_00_01_d_00_01_store_transposed_1_1_no_01_4.html#a709e5ba882750052d23a5a8eb1eff21a',1,'dlaf::matrix::Panel&lt; axis, const T, D, StoreTransposed::No &gt;']]]
 ];
--- master/search/typedefs_2.js
+++ master/search/typedefs_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['index2d_1288',['Index2D',['../communication_2index_8h.html#af5204c68f1436b89ad06c653b710432e',1,'dlaf::comm']]],
-  ['indext_5fmpi_1289',['IndexT_MPI',['../communication_2index_8h.html#a2b48f62607e5e128dff19ceac22093e9',1,'dlaf::comm']]]
+  ['index2d_1287',['Index2D',['../communication_2index_8h.html#af5204c68f1436b89ad06c653b710432e',1,'dlaf::comm']]],
+  ['indext_5fmpi_1288',['IndexT_MPI',['../communication_2index_8h.html#a2b48f62607e5e128dff19ceac22093e9',1,'dlaf::comm']]]
 ];

ed8bde7fe83ebeb5880d017179776776374d02b5
Raffaele Solcà <rasolca@cscs.ch>
2024-05-23 18:07:47 +0200

    remove multiplication not used anymore

--- include/dlaf/multiplication/general/api.h
+++ include/dlaf/multiplication/general/api.h
@@ -32,21 +32,8 @@ struct General {
                      const T beta, MatrixRef<T, D>& mat_c);
 };
 
-template <Backend B, Device D, class T>
-struct GeneralSub {
-  static void callNN(const SizeType i_tile_from, const SizeType i_tile_to, const blas::Op opA,
-                     const blas::Op opB, const T alpha, Matrix<const T, D>& mat_a,
-                     Matrix<const T, D>& mat_b, const T beta, Matrix<T, D>& mat_c);
-  static void callNN(comm::CommunicatorPipeline<comm::CommunicatorType::Row>& row_task_chain,
-                     comm::CommunicatorPipeline<comm::CommunicatorType::Col>& col_task_chain,
-                     const SizeType i_tile_from, const SizeType i_tile_to, const T alpha,
-                     Matrix<const T, D>& mat_a, Matrix<const T, D>& mat_b, const T beta,
-                     Matrix<T, D>& mat_c);
-};
-
 #define DLAF_MULTIPLICATION_GENERAL_ETI(KWORD, BACKEND, DEVICE, DATATYPE) \
-  KWORD template struct General<BACKEND, DEVICE, DATATYPE>;               \
-  KWORD template struct GeneralSub<BACKEND, DEVICE, DATATYPE>;
+  KWORD template struct General<BACKEND, DEVICE, DATATYPE>;
 
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::MC, Device::CPU, float)
 DLAF_MULTIPLICATION_GENERAL_ETI(extern, Backend::MC, Device::CPU, double)

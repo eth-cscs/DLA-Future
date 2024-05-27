39e414c6dacbd46d07b82878c3eaf5476b2e61b5
Doc CI Action <rasolca@users.noreply.github.com>
2024-05-27 09:48:52 +0000

    Doc: Remove `multiplication::generalSubMatrix` (#1148)

--- master/search/classes_6.js
+++ master/search/classes_6.js
@@ -3,9 +3,8 @@ var searchData=
   ['gemmsizes_0',['gemmSizes',['../structdlaf_1_1tile_1_1internal_1_1gemm_sizes.html',1,'dlaf::tile::internal']]],
   ['geneigensolver_1',['GenEigensolver',['../structdlaf_1_1eigensolver_1_1internal_1_1_gen_eigensolver.html',1,'dlaf::eigensolver::internal']]],
   ['general_2',['General',['../structdlaf_1_1multiplication_1_1internal_1_1_general.html',1,'dlaf::multiplication::internal']]],
-  ['generalsub_3',['GeneralSub',['../structdlaf_1_1multiplication_1_1internal_1_1_general_sub.html',1,'dlaf::multiplication::internal']]],
-  ['gentostd_4',['GenToStd',['../structdlaf_1_1eigensolver_1_1internal_1_1_gen_to_std.html',1,'dlaf::eigensolver::internal']]],
-  ['getter_5frandom_5',['getter_random',['../classdlaf_1_1matrix_1_1util_1_1internal_1_1getter__random.html',1,'dlaf::matrix::util::internal']]],
-  ['getter_5frandom_3c_20std_3a_3acomplex_3c_20t_20_3e_20_3e_6',['getter_random&lt; std::complex&lt; T &gt; &gt;',['../classdlaf_1_1matrix_1_1util_1_1internal_1_1getter__random_3_01std_1_1complex_3_01_t_01_4_01_4.html',1,'dlaf::matrix::util::internal']]],
-  ['givensrotation_7',['GivensRotation',['../structdlaf_1_1eigensolver_1_1internal_1_1_givens_rotation.html',1,'dlaf::eigensolver::internal']]]
+  ['gentostd_3',['GenToStd',['../structdlaf_1_1eigensolver_1_1internal_1_1_gen_to_std.html',1,'dlaf::eigensolver::internal']]],
+  ['getter_5frandom_4',['getter_random',['../classdlaf_1_1matrix_1_1util_1_1internal_1_1getter__random.html',1,'dlaf::matrix::util::internal']]],
+  ['getter_5frandom_3c_20std_3a_3acomplex_3c_20t_20_3e_20_3e_5',['getter_random&lt; std::complex&lt; T &gt; &gt;',['../classdlaf_1_1matrix_1_1util_1_1internal_1_1getter__random_3_01std_1_1complex_3_01_t_01_4_01_4.html',1,'dlaf::matrix::util::internal']]],
+  ['givensrotation_6',['GivensRotation',['../structdlaf_1_1eigensolver_1_1internal_1_1_givens_rotation.html',1,'dlaf::eigensolver::internal']]]
 ];

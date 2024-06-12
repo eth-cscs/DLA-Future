7bfbf9cc67c127270cc3b7ebea4b3175d76f0d1d
guglielmogagliardi <94535690+gulivarese@users.noreply.github.com>
2024-06-12 09:46:29 +0200

    Update miniapp_triangular_multiplication.cpp

--- miniapp/miniapp_triangular_multiplication.cpp
+++ miniapp/miniapp_triangular_multiplication.cpp
@@ -180,8 +180,7 @@ struct triangularMultiplicationMiniapp {
         double gigaflops = total_ops / elapsed_time / 1e9;
 
         std::cout << "[" << run_index << "]" << " " << elapsed_time << "s" << " " << gigaflops
-                  << "GFlop/s"
-                  << " " << dlaf::internal::FormatShort{opts.type}
+                  << "GFlop/s" << " " << dlaf::internal::FormatShort{opts.type}
                   << dlaf::internal::FormatShort{opts.side} << dlaf::internal::FormatShort{opts.uplo}
                   << dlaf::internal::FormatShort{opts.op} << dlaf::internal::FormatShort{opts.diag}
                   << " " << bh.size() << " " << bh.blockSize() << " " << comm_grid.size() << " "

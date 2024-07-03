e1fc3e73025bb4124ed69fe31c74569485606dcf
Doc CI Action <rasolca@users.noreply.github.com>
2024-07-03 12:49:54 +0000

    Doc: Update README.md (#1173)

--- master/search/all_4.js
+++ master/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['eigensolver_0',['Eigensolver',['../structdlaf_1_1eigensolver_1_1internal_1_1_eigensolver.html',1,'dlaf::eigensolver::internal']]],
-  ['eigensolver_2eh_1',['eigensolver.h',['../dlaf__c_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf_2eigensolver_8h.html',1,'(Global Namespace)']]],
+  ['eigensolver_2eh_1',['eigensolver.h',['../dlaf_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf__c_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)']]],
   ['eigensolverresult_2',['EigensolverResult',['../structdlaf_1_1_eigensolver_result.html',1,'dlaf']]],
   ['element_5ffrom_5ftile_5fand_5ftile_5felement_3',['element_from_tile_and_tile_element',['../util__distribution_8h.html#a46416badc561a4959ed5a78359d45674',1,'dlaf::util::matrix']]],
   ['emptyenv_4',['EmptyEnv',['../structdlaf_1_1internal_1_1_empty_env.html',1,'dlaf::internal']]],
@@ -11,7 +11,7 @@ var searchData=
   ['enum_5foutput_2eh_8',['enum_output.h',['../blas_2enum__output_8h.html',1,'']]],
   ['equal_5fblocksize_9',['equal_blocksize',['../util__matrix_8h.html#a0bb54ace6dfcb245ec387c1af75e63fe',1,'dlaf::matrix']]],
   ['equal_5fdistributions_10',['equal_distributions',['../util__matrix_8h.html#a8af8a409c1af4f1c7cdbed9b4f6b24a2',1,'dlaf::matrix']]],
-  ['equal_5fprocess_5fgrid_11',['equal_process_grid',['../util__matrix_8h.html#a8a0a99aa23eccc3a51062915dfc55abe',1,'dlaf::matrix::equal_process_grid(const comm::CommunicatorPipeline&lt; CT1 &gt; &amp;p1, const comm::CommunicatorPipeline&lt; CT2 &gt; &amp;p2) noexcept'],['../util__matrix_8h.html#a128a1f3ccd354d095a9c227cd44db814',1,'dlaf::matrix::equal_process_grid(const MatrixLike&lt; const T, D &gt; &amp;m, const comm::CommunicatorPipeline&lt; CT &gt; &amp;p) noexcept'],['../util__matrix_8h.html#a48e5bc8a263576b58a3b9c92dc464d82',1,'dlaf::matrix::equal_process_grid(const MatrixLike&lt; const T, D &gt; &amp;m, const comm::CommunicatorGrid &amp;g) noexcept']]],
+  ['equal_5fprocess_5fgrid_11',['equal_process_grid',['../util__matrix_8h.html#a48e5bc8a263576b58a3b9c92dc464d82',1,'dlaf::matrix::equal_process_grid(const MatrixLike&lt; const T, D &gt; &amp;m, const comm::CommunicatorGrid &amp;g) noexcept'],['../util__matrix_8h.html#a128a1f3ccd354d095a9c227cd44db814',1,'dlaf::matrix::equal_process_grid(const MatrixLike&lt; const T, D &gt; &amp;m, const comm::CommunicatorPipeline&lt; CT &gt; &amp;p) noexcept'],['../util__matrix_8h.html#a8a0a99aa23eccc3a51062915dfc55abe',1,'dlaf::matrix::equal_process_grid(const comm::CommunicatorPipeline&lt; CT1 &gt; &amp;p1, const comm::CommunicatorPipeline&lt; CT2 &gt; &amp;p2) noexcept']]],
   ['equal_5fsize_12',['equal_size',['../util__matrix_8h.html#ada06af4450b10ed70a5830c0d8db0302',1,'dlaf::matrix']]],
   ['error_2eh_13',['error.h',['../communication_2error_8h.html',1,'']]],
   ['eti_2eh_14',['eti.h',['../eti_8h.html',1,'']]],

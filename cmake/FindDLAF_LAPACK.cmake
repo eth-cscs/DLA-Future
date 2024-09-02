c98e3343ff5e32f8c3ec87608b260fbc75d29dde
Mikael Simberg <mikael.simberg@iki.fi>
2024-09-02 16:38:57 +0200

    Namespace (SCA)LAPACK CMake modules with DLAF to avoid conflicts with other packages (#1178)

--- cmake/FindDLAF_LAPACK.cmake
+++ cmake/FindDLAF_LAPACK.cmake
@@ -16,14 +16,14 @@
 #
 # Users can manually specify next variables (even by setting them empty to force use of
 # the compiler implicit linking) to control which implementation they want to use:
-#   LAPACK_LIBRARY
+#   DLAF_LAPACK_LIBRARY
 #       ;-list of {lib name, lib filepath, -Llibrary_folder}
 #
-#   LAPACK_INCLUDE_DIR
+#   DLAF_LAPACK_INCLUDE_DIR
 #       ;-list of include folders
 #
 # This module sets the following variables:
-#   LAPACK_FOUND - set to true if a library implementing the LAPACK interface is found
+#   DLAF_LAPACK_FOUND - set to true if a library implementing the LAPACK interface is found
 #
 # If LAPACK symbols got found, it creates target DLAF::LAPACK
 
@@ -35,19 +35,19 @@ macro(_lapack_check_is_working)
 
   set(CMAKE_REQUIRED_QUIET TRUE)
 
-  if(NOT LAPACK_LIBRARY STREQUAL "LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
-    list(APPEND CMAKE_REQUIRED_LIBRARIES ${LAPACK_LIBRARY})
+  if(NOT DLAF_LAPACK_LIBRARY STREQUAL "DLAF_LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
+    list(APPEND CMAKE_REQUIRED_LIBRARIES ${DLAF_LAPACK_LIBRARY})
   endif()
 
-  unset(_LAPACK_CHECK_BLAS CACHE)
-  check_function_exists(dgemm_ _LAPACK_CHECK_BLAS)
-  if(NOT _LAPACK_CHECK_BLAS)
+  unset(_DLAF_LAPACK_CHECK_BLAS CACHE)
+  check_function_exists(dgemm_ _DLAF_LAPACK_CHECK_BLAS)
+  if(NOT _DLAF_LAPACK_CHECK_BLAS)
     message(FATAL_ERROR "BLAS symbol not found with this configuration")
   endif()
 
-  unset(_LAPACK_CHECK CACHE)
-  check_function_exists(dpotrf_ _LAPACK_CHECK)
-  if(NOT _LAPACK_CHECK)
+  unset(_DLAF_LAPACK_CHECK CACHE)
+  check_function_exists(dpotrf_ _DLAF_LAPACK_CHECK)
+  if(NOT _DLAF_LAPACK_CHECK)
     message(FATAL_ERROR "LAPACK symbol not found with this configuration")
   endif()
 
@@ -57,40 +57,41 @@ endmacro()
 # Dependencies
 set(_DEPS "")
 
-if(NOT LAPACK_LIBRARY)
-  set(LAPACK_LIBRARY "LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
+if(NOT DLAF_LAPACK_LIBRARY)
+  set(DLAF_LAPACK_LIBRARY "DLAF_LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
 endif()
 
-mark_as_advanced(LAPACK_LIBRARY)
+mark_as_advanced(DLAF_LAPACK_LIBRARY)
 
 _lapack_check_is_working()
 
-if(NOT LAPACK_INCLUDE_DIR)
-  set(LAPACK_INCLUDE_DIR "LAPACK_INCLUDE_DIR-PLACEHOLDER-FOR-EMPTY-INCLUDE-DIR")
+if(NOT DLAF_LAPACK_INCLUDE_DIR)
+  set(DLAF_LAPACK_INCLUDE_DIR "DLAF_LAPACK_INCLUDE_DIR-PLACEHOLDER-FOR-EMPTY-INCLUDE-DIR")
 endif()
 
-mark_as_advanced(LAPACK_INCLUDE_DIR)
+mark_as_advanced(DLAF_LAPACK_INCLUDE_DIR)
 
 ### Package
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  LAPACK DEFAULT_MSG LAPACK_LIBRARY LAPACK_INCLUDE_DIR _LAPACK_CHECK _LAPACK_CHECK_BLAS
+  DLAF_LAPACK DEFAULT_MSG DLAF_LAPACK_LIBRARY DLAF_LAPACK_INCLUDE_DIR _DLAF_LAPACK_CHECK
+  _DLAF_LAPACK_CHECK_BLAS
 )
 
 # Remove the placeholders
-if(LAPACK_LIBRARY STREQUAL "LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
-  set(LAPACK_LIBRARY "")
+if(DLAF_LAPACK_LIBRARY STREQUAL "DLAF_LAPACK_LIBRARIES-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
+  set(DLAF_LAPACK_LIBRARY "")
 endif()
 
-if(LAPACK_INCLUDE_DIR STREQUAL "LAPACK_INCLUDE_DIR-PLACEHOLDER-FOR-EMPTY-INCLUDE-DIR")
-  set(LAPACK_INCLUDE_DIR "")
+if(DLAF_LAPACK_INCLUDE_DIR STREQUAL "DLAF_LAPACK_INCLUDE_DIR-PLACEHOLDER-FOR-EMPTY-INCLUDE-DIR")
+  set(DLAF_LAPACK_INCLUDE_DIR "")
 endif()
 
-if(LAPACK_FOUND)
+if(DLAF_LAPACK_FOUND)
   if(NOT TARGET DLAF::LAPACK)
     add_library(DLAF::LAPACK INTERFACE IMPORTED GLOBAL)
   endif()
 
-  target_link_libraries(DLAF::LAPACK INTERFACE "${LAPACK_LIBRARY}" "${_DEPS}")
-  target_include_directories(DLAF::LAPACK INTERFACE "${LAPACK_INCLUDE_DIR}")
+  target_link_libraries(DLAF::LAPACK INTERFACE "${DLAF_LAPACK_LIBRARY}" "${_DEPS}")
+  target_include_directories(DLAF::LAPACK INTERFACE "${DLAF_LAPACK_INCLUDE_DIR}")
 endif()

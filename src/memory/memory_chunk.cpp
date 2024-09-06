b5d23d4f6ceec0de48f28325b9b13d35937a5e1a
Mikael Simberg <simberg@cscs.ch>
2024-09-06 16:41:52 +0200

    Add custom coalescing heuristic to umpire memory pools

--- src/memory/memory_chunk.cpp
+++ src/memory/memory_chunk.cpp
@@ -11,6 +11,7 @@
 #include <cstddef>
 
 #include <umpire/ResourceManager.hpp>
+#include <umpire/strategy/PoolCoalesceHeuristic.hpp>
 #include <umpire/strategy/QuickPool.hpp>
 #include <umpire/strategy/ThreadSafeAllocator.hpp>
 
@@ -36,7 +37,40 @@ umpire::Allocator& getUmpireHostAllocator() {
 }
 #endif
 
-void initializeUmpireHostAllocator(std::size_t initial_bytes) {
+using PoolType = umpire::strategy::QuickPool;
+using CoalesceHeuristicType = umpire::strategy::PoolCoalesceHeuristic<PoolType>;
+
+// This is a modified version of the "percent_releasable" coalescing heuristic
+// from Umpire. This version allows choosing what ratio of the actual size to
+// reallocate when coalescing.
+//
+// A free ratio of 1.0 means that the pool will be coalesced only when all
+// blocks are unused. A free ratio of 0.5 means that the pool will be coalesced
+// when at least 50% of the pool's memory is unused. A ratio of 0.0 means that
+// the pool will be coalesced as soon as any two free blocks are available. A
+// ratio of more than 1.0 will make the pool never coalesce.
+//
+// A reallocation ratio of 1.0 simply coalesces all the free memory into a new
+// block. A ratio of 0.5 will attempt to shrink the pool to half its previous
+// size. A ratio of 1.5 will allocate 50% more than the previous pool size.
+//
+// A single free block is never "coalesced" to keep things simple. In theory a
+// single block could be shrunk or grown to match the reallocation ratio but
+// this can lead to strange reallocations, so we simply avoid that case. Two or
+// more blocks are always coalesced to one block, so no reallocation will
+// happen immediately after coalescing two or more blocks.
+static CoalesceHeuristicType get_coalesce_heuristic(double coalesce_free_ratio, double coalesce_reallocation_ratio) {
+  return [=](const PoolType& pool) {
+    std::size_t threshold = static_cast<std::size_t>(coalesce_free_ratio * pool.getActualSize());
+    if (pool.getReleasableBlocks() >= 2 && pool.getReleasableSize() >= threshold) {
+      return static_cast<std::size_t>(coalesce_reallocation_ratio * pool.getActualSize());
+    } else {
+      return static_cast<std::size_t>(0);
+    }
+  };
+}
+
+void initializeUmpireHostAllocator(std::size_t initial_bytes, std::size_t next_bytes, std::size_t alignment_bytes, double coalesce_free_ratio, double coalesce_reallocation_ratio) {
 #ifdef DLAF_WITH_GPU
   static bool initialized = false;
 
@@ -47,7 +81,7 @@ void initializeUmpireHostAllocator(std::size_t initial_bytes) {
     auto pooled_host_allocator =
         umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::QuickPool>("PINNED_pool",
                                                                                           host_allocator,
-                                                                                          initial_bytes);
+                                                                                          initial_bytes, next_bytes, alignment_bytes, get_coalesce_heuristic(coalesce_free_ratio, coalesce_reallocation_ratio));
     auto thread_safe_pooled_host_allocator =
         umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::ThreadSafeAllocator>(
             "PINNED_thread_safe_pool", pooled_host_allocator);
@@ -64,7 +98,7 @@ void initializeUmpireHostAllocator(std::size_t initial_bytes) {
 void finalizeUmpireHostAllocator() {}
 
 #ifdef DLAF_WITH_GPU
-void initializeUmpireDeviceAllocator(std::size_t initial_bytes) {
+void initializeUmpireDeviceAllocator(std::size_t initial_bytes, std::size_t next_bytes, std::size_t alignment_bytes, double coalesce_free_ratio, double coalesce_reallocation_ratio) {
   static bool initialized = false;
 
   // Umpire pools cannot be released, so we keep the pools around even when
@@ -73,7 +107,7 @@ void initializeUmpireDeviceAllocator(std::size_t initial_bytes) {
     auto device_allocator = umpire::ResourceManager::getInstance().getAllocator("DEVICE");
     auto pooled_device_allocator =
         umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::QuickPool>(
-            "DEVICE_pool", device_allocator, initial_bytes);
+            "DEVICE_pool", device_allocator, initial_bytes, next_bytes, alignment_bytes, get_coalesce_heuristic(coalesce_free_ratio, coalesce_reallocation_ratio));
     auto thread_safe_pooled_device_allocator =
         umpire::ResourceManager::getInstance().makeAllocator<umpire::strategy::ThreadSafeAllocator>(
             "DEVICE_thread_safe_pool", pooled_device_allocator);

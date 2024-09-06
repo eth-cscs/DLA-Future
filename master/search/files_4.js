59de7da35a4a9908808bbe1e2c43f6cbd36eb8c7
Doc CI Action <rasolca@users.noreply.github.com>
2024-09-06 15:00:45 +0000

    Doc: Fix blaspp/lapackpp deprecations of enum conversions (Uplo, Op, ...) from and to string (#1181)

--- master/search/files_4.js
+++ master/search/files_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['eigensolver_2eh_0',['eigensolver.h',['../dlaf_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)'],['../dlaf__c_2eigensolver_2eigensolver_8h.html',1,'(Global Namespace)']]],
   ['enum_5foutput_2eh_1',['enum_output.h',['../blas_2enum__output_8h.html',1,'']]],
-  ['error_2eh_2',['error.h',['../communication_2error_8h.html',1,'']]],
-  ['eti_2eh_3',['eti.h',['../eti_8h.html',1,'']]]
+  ['enum_5fparse_2eh_2',['enum_parse.h',['../enum__parse_8h.html',1,'']]],
+  ['error_2eh_3',['error.h',['../communication_2error_8h.html',1,'']]],
+  ['eti_2eh_4',['eti.h',['../eti_8h.html',1,'']]]
 ];

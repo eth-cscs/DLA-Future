5362f7f4ff25676c7067d3414e52693f87efa2ff
Mikael Simberg <mikael.simberg@iki.fi>
2024-12-02 10:54:01 +0100

    Add configuration option for choosing number of GPU streams when they're not per-thread (#1222)

--- test/unit/init/test_init.cpp
+++ test/unit/init/test_init.cpp
@@ -21,8 +21,8 @@
 #include <gtest/gtest.h>
 
 static const char* binary_name = "init_test";
-static const char* env_var_name = "DLAF_NUM_HP_GPU_STREAMS_PER_THREAD";
-static const char* command_line_option_name = "--dlaf:num-hp-gpu-streams-per-thread";
+static const char* env_var_name = "DLAF_NUM_GPU_BLAS_HANDLES";
+static const char* command_line_option_name = "--dlaf:num-gpu-blas-handles";
 static const char* print_bind = "--pika:print-bind";
 
 static int argc_without_option = 1;
@@ -91,10 +91,10 @@ class InitTest : public ::testing::TestWithParam<InitializerType> {};
 
 int precedence_main(int, char*[]) {
   const dlaf::configuration default_cfg;
-  const std::size_t default_val = default_cfg.num_hp_gpu_streams_per_thread;
-  // Note that this test doesn't mean that the default value has to be 3. It is
+  const std::size_t default_val = default_cfg.num_gpu_blas_handles;
+  // Note that this test doesn't mean that the default value has to be 16. It is
   // included to help catch unexpected changes in the configuration handling.
-  EXPECT_EQ(default_val, 3);
+  EXPECT_EQ(default_val, 16);
 
   // Make sure environment is clean for the test.
   unsetenv(env_var_name);
@@ -103,37 +103,37 @@ int precedence_main(int, char*[]) {
   {
     InitializeTester init(current_initializer_type, argc_without_option, argv_without_option);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(default_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(default_val, cfg.num_gpu_blas_handles);
   }
 
   // User configuration should take precedence over default configuration.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
 
     InitializeTester init(current_initializer_type, argc_without_option, argv_without_option, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(user_cfg.num_hp_gpu_streams_per_thread, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(user_cfg.num_gpu_blas_handles, cfg.num_gpu_blas_handles);
   }
 
   // Environment variables should take precedence over user configuration.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
-    const std::size_t env_var_val = user_cfg.num_hp_gpu_streams_per_thread + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
+    const std::size_t env_var_val = user_cfg.num_gpu_blas_handles + 1;
     const std::string env_var_val_str = std::to_string(env_var_val);
     setenv(env_var_name, env_var_val_str.c_str(), 1);
 
     InitializeTester init(current_initializer_type, argc_without_option, argv_without_option, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(env_var_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(env_var_val, cfg.num_gpu_blas_handles);
   }
 
   // Command-line options should take precedence over environment variables.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
-    const std::size_t env_var_val = user_cfg.num_hp_gpu_streams_per_thread + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
+    const std::size_t env_var_val = user_cfg.num_gpu_blas_handles + 1;
     const std::string env_var_val_str = std::to_string(env_var_val);
     setenv(env_var_name, env_var_val_str.c_str(), 1);
     const std::size_t command_line_option_val = env_var_val + 1;
@@ -145,7 +145,7 @@ int precedence_main(int, char*[]) {
 
     InitializeTester init(current_initializer_type, argc_with_option, argv_with_option, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(command_line_option_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(command_line_option_val, cfg.num_gpu_blas_handles);
   }
 
   pika::finalize();
@@ -160,10 +160,10 @@ TEST_P(InitTest, Precedence) {
 
 int vm_no_command_line_option_main(pika::program_options::variables_map& vm) {
   const dlaf::configuration default_cfg;
-  const std::size_t default_val = default_cfg.num_hp_gpu_streams_per_thread;
-  // Note that this test doesn't mean that the default value has to be 3. It is
+  const std::size_t default_val = default_cfg.num_gpu_blas_handles;
+  // Note that this test doesn't mean that the default value has to be 16. It is
   // included to help catch unexpected changes in the configuration handling.
-  EXPECT_EQ(default_val, 3);
+  EXPECT_EQ(default_val, 16);
 
   // Make sure environment is clean for the test.
   unsetenv(env_var_name);
@@ -172,37 +172,37 @@ int vm_no_command_line_option_main(pika::program_options::variables_map& vm) {
   {
     InitializeTester init(current_initializer_type, vm);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(default_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(default_val, cfg.num_gpu_blas_handles);
   }
 
   // User configuration should take precedence over default configuration.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
 
     InitializeTester init(current_initializer_type, vm, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(user_cfg.num_hp_gpu_streams_per_thread, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(user_cfg.num_gpu_blas_handles, cfg.num_gpu_blas_handles);
   }
 
   // Environment variables should take precedence over user configuration.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
-    const std::size_t env_var_val = user_cfg.num_hp_gpu_streams_per_thread + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
+    const std::size_t env_var_val = user_cfg.num_gpu_blas_handles + 1;
     const std::string env_var_val_str = std::to_string(env_var_val);
     setenv(env_var_name, env_var_val_str.c_str(), 1);
 
     InitializeTester init(current_initializer_type, vm, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(env_var_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(env_var_val, cfg.num_gpu_blas_handles);
   }
 
   // Command-line options should take precedence over environment variables.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
-    const std::size_t env_var_val = user_cfg.num_hp_gpu_streams_per_thread + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
+    const std::size_t env_var_val = user_cfg.num_gpu_blas_handles + 1;
     const std::string env_var_val_str = std::to_string(env_var_val);
     setenv(env_var_name, env_var_val_str.c_str(), 1);
     const std::size_t command_line_option_val = env_var_val + 1;
@@ -214,7 +214,7 @@ int vm_no_command_line_option_main(pika::program_options::variables_map& vm) {
 
     InitializeTester init(current_initializer_type, argc_with_option, argv_with_option, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(command_line_option_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(command_line_option_val, cfg.num_gpu_blas_handles);
   }
 
   pika::finalize();
@@ -229,20 +229,20 @@ TEST_P(InitTest, VariablesMapNoCommandLineOption) {
 
 int vm_command_line_option_main(pika::program_options::variables_map& vm) {
   dlaf::configuration default_cfg;
-  const std::size_t default_val = default_cfg.num_hp_gpu_streams_per_thread;
+  const std::size_t default_val = default_cfg.num_gpu_blas_handles;
 
   // Command-line options should take precedence over everything else.
   {
     dlaf::configuration user_cfg = default_cfg;
-    user_cfg.num_hp_gpu_streams_per_thread = default_val + 1;
-    const std::size_t env_var_val = user_cfg.num_hp_gpu_streams_per_thread + 1;
+    user_cfg.num_gpu_blas_handles = default_val + 1;
+    const std::size_t env_var_val = user_cfg.num_gpu_blas_handles + 1;
     const std::string env_var_val_str = std::to_string(env_var_val);
     setenv(env_var_name, env_var_val_str.c_str(), 1);
     const std::size_t command_line_option_val = env_var_val + 1;
 
     InitializeTester init(current_initializer_type, vm, user_cfg);
     dlaf::configuration cfg = dlaf::internal::getConfiguration();
-    EXPECT_EQ(command_line_option_val, cfg.num_hp_gpu_streams_per_thread);
+    EXPECT_EQ(command_line_option_val, cfg.num_gpu_blas_handles);
   }
 
   pika::finalize();
@@ -259,10 +259,10 @@ TEST_P(InitTest, VariablesMapCommandLineOption) {
   p.desc_cmdline = options;
 
   dlaf::configuration default_cfg;
-  const std::size_t default_val = default_cfg.num_hp_gpu_streams_per_thread;
-  // Note that this test doesn't mean that the default value has to be 3. It is
+  const std::size_t default_val = default_cfg.num_gpu_blas_handles;
+  // Note that this test doesn't mean that the default value has to be 16. It is
   // included to help catch unexpected changes in the configuration handling.
-  EXPECT_EQ(default_val, 3);
+  EXPECT_EQ(default_val, 16);
 
   std::size_t command_line_option_val = default_val + 3;
   std::string command_line_option_val_str = std::to_string(command_line_option_val);

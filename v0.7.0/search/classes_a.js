91f414ba125904feaaa5bded58ec4d435b323a98
Doc CI Action <rasolca@users.noreply.github.com>
2024-12-13 09:43:42 +0000

    Doc: add v0.7.0 documentation

---
+++ v0.7.0/search/classes_a.js
@@ -0,0 +1,27 @@
+var searchData=
+[
+  ['matrix_0',['Matrix',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20const_20t_2c_20d_20_3e_1',['Matrix&lt; const T, D &gt;',['../classdlaf_1_1matrix_1_1_matrix_3_01const_01_t_00_01_d_01_4.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20const_20t_2c_20source_20_3e_2',['Matrix&lt; const T, Source &gt;',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20const_20t_2c_20sourcetarget_20_3e_3',['Matrix&lt; const T, SourceTarget &gt;',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20t_2c_20source_20_3e_4',['Matrix&lt; T, Source &gt;',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20t_2c_20sourcetarget_20_3e_5',['Matrix&lt; T, SourceTarget &gt;',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrix_3c_20t_2c_20target_20_3e_6',['Matrix&lt; T, Target &gt;',['../classdlaf_1_1matrix_1_1_matrix.html',1,'dlaf::matrix']]],
+  ['matrixbase_7',['MatrixBase',['../classdlaf_1_1matrix_1_1internal_1_1_matrix_base.html',1,'dlaf::matrix::internal']]],
+  ['matrixlayout_8',['MatrixLayout',['../structdlaf_1_1permutations_1_1internal_1_1_matrix_layout.html',1,'dlaf::permutations::internal']]],
+  ['matrixmirror_9',['MatrixMirror',['../classdlaf_1_1matrix_1_1_matrix_mirror.html',1,'dlaf::matrix']]],
+  ['matrixmirror_3c_20const_20t_2c_20sourcetarget_2c_20sourcetarget_20_3e_10',['MatrixMirror&lt; const T, SourceTarget, SourceTarget &gt;',['../classdlaf_1_1matrix_1_1_matrix_mirror_3_01const_01_t_00_01_source_target_00_01_source_target_01_4.html',1,'dlaf::matrix']]],
+  ['matrixmirror_3c_20const_20t_2c_20target_2c_20source_20_3e_11',['MatrixMirror&lt; const T, Target, Source &gt;',['../classdlaf_1_1matrix_1_1_matrix_mirror_3_01const_01_t_00_01_target_00_01_source_01_4.html',1,'dlaf::matrix']]],
+  ['matrixmirror_3c_20t_2c_20sourcetarget_2c_20sourcetarget_20_3e_12',['MatrixMirror&lt; T, SourceTarget, SourceTarget &gt;',['../classdlaf_1_1matrix_1_1_matrix_mirror_3_01_t_00_01_source_target_00_01_source_target_01_4.html',1,'dlaf::matrix']]],
+  ['matrixref_13',['MatrixRef',['../classdlaf_1_1matrix_1_1internal_1_1_matrix_ref.html',1,'dlaf::matrix::internal']]],
+  ['matrixref_3c_20const_20t_2c_20d_20_3e_14',['MatrixRef&lt; const T, D &gt;',['../classdlaf_1_1matrix_1_1internal_1_1_matrix_ref_3_01const_01_t_00_01_d_01_4.html',1,'dlaf::matrix::internal']]],
+  ['memorychunk_15',['MemoryChunk',['../classdlaf_1_1memory_1_1_memory_chunk.html',1,'dlaf::memory']]],
+  ['memoryview_16',['MemoryView',['../classdlaf_1_1memory_1_1_memory_view.html',1,'dlaf::memory']]],
+  ['memoryview_3c_20t_2c_20device_3a_3acpu_20_3e_17',['MemoryView&lt; T, Device::CPU &gt;',['../classdlaf_1_1memory_1_1_memory_view.html',1,'dlaf::memory']]],
+  ['message_18',['Message',['../classdlaf_1_1comm_1_1_message.html',1,'dlaf::comm']]],
+  ['movenonconsttile_19',['moveNonConstTile',['../structdlaf_1_1internal_1_1move_non_const_tile.html',1,'dlaf::internal']]],
+  ['mpi_5fdatatype_20',['mpi_datatype',['../structdlaf_1_1comm_1_1mpi__datatype.html',1,'dlaf::comm']]],
+  ['mpi_5fdatatype_3c_20const_20t_20_3e_21',['mpi_datatype&lt; const T &gt;',['../structdlaf_1_1comm_1_1mpi__datatype_3_01const_01_t_01_4.html',1,'dlaf::comm']]],
+  ['mpi_5finit_22',['mpi_init',['../structdlaf_1_1comm_1_1mpi__init.html',1,'dlaf::comm']]],
+  ['mpicallhelper_23',['MPICallHelper',['../structdlaf_1_1comm_1_1internal_1_1_m_p_i_call_helper.html',1,'dlaf::comm::internal']]]
+];

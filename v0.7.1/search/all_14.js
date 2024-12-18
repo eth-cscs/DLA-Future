8a1fc2c12b671a89b025e97da27a46cf0dc192d1
Doc CI Action <rasolca@users.noreply.github.com>
2024-12-18 15:15:42 +0000

    Doc: add v0.7.1 documentation

---
+++ v0.7.1/search/all_14.js
@@ -0,0 +1,28 @@
+var searchData=
+[
+  ['unique_0',['Unique',['../structdlaf_1_1internal_1_1_unique.html',1,'dlaf::internal']]],
+  ['unique_3c_20pack_3c_20ts_2e_2e_2e_20_3e_20_3e_1',['Unique&lt; Pack&lt; Ts... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_3_01_pack_3_01_ts_8_8_8_01_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquehelper_2',['UniqueHelper',['../structdlaf_1_1internal_1_1_unique_helper.html',1,'dlaf::internal']]],
+  ['uniquehelper_3c_20pack_3c_20ts_2e_2e_2e_20_3e_2c_20pack_3c_20u_2c_20us_2e_2e_2e_20_3e_20_3e_3',['UniqueHelper&lt; Pack&lt; Ts... &gt;, Pack&lt; U, Us... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_helper_3_01_pack_3_01_ts_8_8_8_01_4_00_01_pack_3_01_u_00_01_us_8_8_8_01_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquehelper_3c_20pack_3c_20ts_2e_2e_2e_20_3e_2c_20pack_3c_3e_20_3e_4',['UniqueHelper&lt; Pack&lt; Ts... &gt;, Pack&lt;&gt; &gt;',['../structdlaf_1_1internal_1_1_unique_helper_3_01_pack_3_01_ts_8_8_8_01_4_00_01_pack_3_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquehelper_3c_20pack_3c_3e_2c_20pack_3c_20ts_2e_2e_2e_20_3e_20_3e_5',['UniqueHelper&lt; Pack&lt;&gt;, Pack&lt; Ts... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_helper.html',1,'dlaf::internal']]],
+  ['uniquepack_6',['UniquePack',['../structdlaf_1_1internal_1_1_unique_pack.html',1,'dlaf::internal']]],
+  ['uniquepack_3c_20pack_3c_20ts_2e_2e_2e_20_3e_20_3e_7',['UniquePack&lt; Pack&lt; Ts... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_pack_3_01_pack_3_01_ts_8_8_8_01_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquepackhelper_8',['UniquePackHelper',['../structdlaf_1_1internal_1_1_unique_pack_helper.html',1,'dlaf::internal']]],
+  ['uniquepackhelper_3c_20pack_3c_20ts_2e_2e_2e_20_3e_2c_20pack_3c_20u_2c_20us_2e_2e_2e_20_3e_20_3e_9',['UniquePackHelper&lt; Pack&lt; Ts... &gt;, Pack&lt; U, Us... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_pack_helper_3_01_pack_3_01_ts_8_8_8_01_4_00_01_pack_3_01_u_00_01_us_8_8_8_01_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquepackhelper_3c_20pack_3c_20ts_2e_2e_2e_20_3e_2c_20pack_3c_3e_20_3e_10',['UniquePackHelper&lt; Pack&lt; Ts... &gt;, Pack&lt;&gt; &gt;',['../structdlaf_1_1internal_1_1_unique_pack_helper_3_01_pack_3_01_ts_8_8_8_01_4_00_01_pack_3_4_01_4.html',1,'dlaf::internal']]],
+  ['uniquepackhelper_3c_20pack_3c_3e_2c_20pack_3c_20ts_2e_2e_2e_20_3e_20_3e_11',['UniquePackHelper&lt; Pack&lt;&gt;, Pack&lt; Ts... &gt; &gt;',['../structdlaf_1_1internal_1_1_unique_pack_helper.html',1,'dlaf::internal']]],
+  ['unwrap_12',['unwrap',['../unwrap_8h.html#a8da70a5535ac987a8f6ca7f8b195547d',1,'dlaf::common::internal']]],
+  ['unwrap_2eh_13',['unwrap.h',['../unwrap_8h.html',1,'']]],
+  ['unwrapper_14',['Unwrapper',['../structdlaf_1_1common_1_1internal_1_1_unwrapper.html',1,'dlaf::common::internal']]],
+  ['unwrapper_3c_20pika_3a_3aexecution_3a_3aexperimental_3a_3aasync_5frw_5fmutex_5faccess_5fwrapper_3c_20t1_2c_20t2_2c_20at_20_3e_20_3e_15',['Unwrapper&lt; pika::execution::experimental::async_rw_mutex_access_wrapper&lt; T1, T2, at &gt; &gt;',['../structdlaf_1_1common_1_1internal_1_1_unwrapper_3_01pika_1_1execution_1_1experimental_1_1async__r5be7b0afcb71e6df73ebcc954d80821d.html',1,'dlaf::common::internal']]],
+  ['unwrapper_3c_20std_3a_3areference_5fwrapper_3c_20t_20_3e_20_3e_16',['Unwrapper&lt; std::reference_wrapper&lt; T &gt; &gt;',['../structdlaf_1_1common_1_1internal_1_1_unwrapper_3_01std_1_1reference__wrapper_3_01_t_01_4_01_4.html',1,'dlaf::common::internal']]],
+  ['unwrapping_17',['Unwrapping',['../structdlaf_1_1common_1_1internal_1_1_unwrapping.html',1,'dlaf::common::internal']]],
+  ['util_5fblas_2eh_18',['util_blas.h',['../util__blas_8h.html',1,'']]],
+  ['util_5fdistribution_2eh_19',['util_distribution.h',['../util__distribution_8h.html',1,'']]],
+  ['util_5flapack_2eh_20',['util_lapack.h',['../util__lapack_8h.html',1,'']]],
+  ['util_5fmath_2eh_21',['util_math.h',['../util__math_8h.html',1,'']]],
+  ['util_5fmatrix_2eh_22',['util_matrix.h',['../util__matrix_8h.html',1,'']]],
+  ['util_5ftile_2eh_23',['util_tile.h',['../util__tile_8h.html',1,'']]],
+  ['utils_2eh_24',['utils.h',['../utils_8h.html',1,'']]]
+];
---
+++ v0.7.1/search/files_14.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['with_5fresult_5fof_2eh_0',['with_result_of.h',['../with__result__of_8h.html',1,'']]]
+];

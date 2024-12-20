9a5f5237e7e29654de442854af7b8f8ccca66a44
Doc CI Action <rasolca@users.noreply.github.com>
2024-12-20 14:01:00 +0000

    Doc: add v0.7.3 documentation

---
+++ v0.7.3/search/files_13.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['vector_2eh_0',['vector.h',['../vector_8h.html',1,'']]],
+  ['views_2eh_1',['views.h',['../views_8h.html',1,'']]]
+];
---
+++ v0.7.3/search/functions_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['finalize_0',['finalize',['../dlaf_2init_8h.html#abf5448f6d8cff5d741a7680123a7082c',1,'dlaf']]],
+  ['full_5fcommunicator_5fpipeline_1',['full_communicator_pipeline',['../classdlaf_1_1comm_1_1_communicator_grid.html#aa14622db356b247aad0e0f460a281822',1,'dlaf::comm::CommunicatorGrid']]],
+  ['fullcommunicator_2',['fullCommunicator',['../classdlaf_1_1comm_1_1_communicator_grid.html#abf3d66e3e9d0267f22dfa395d57c4974',1,'dlaf::comm::CommunicatorGrid']]],
+  ['fullindex_3',['fullIndex',['../structdlaf_1_1matrix_1_1_panel_3_01axis_00_01const_01_t_00_01_d_00_01_store_transposed_1_1_no_01_4.html#a709e5ba882750052d23a5a8eb1eff21a',1,'dlaf::matrix::Panel&lt; axis, const T, D, StoreTransposed::No &gt;']]]
+];

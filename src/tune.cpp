a9abb819957c2ddc3801080d34cf4c3177725a86
Alberto Invernizzi <9337627+albestro@users.noreply.github.com>
2025-02-18 19:59:37 +0100

    Rename parameters from "nworkers" to "num-threads" (#1270)

--- src/tune.cpp
+++ src/tune.cpp
@@ -22,9 +22,9 @@ std::ostream& operator<<(std::ostream& os, const TuneParameters& params) {
   os << "  tfactor_num_threads = " << params.tfactor_num_threads << std::endl;
   os << "  tfactor_num_streams = " << params.tfactor_num_streams << std::endl;
   os << "  tfactor_barrier_busy_wait_us = " << params.tfactor_barrier_busy_wait_us << std::endl;
-  os << "  red2band_panel_nworkers = " << params.red2band_panel_nworkers << std::endl;
+  os << "  red2band_panel_num_threads = " << params.red2band_panel_num_threads << std::endl;
   os << "  red2band_barrier_busy_wait_us = " << params.red2band_barrier_busy_wait_us << std::endl;
-  os << "  tridiag_rank1_nworkers = " << params.tridiag_rank1_nworkers << std::endl;
+  os << "  tridiag_rank1_num_threads = " << params.tridiag_rank1_num_threads << std::endl;
   os << "  tridiag_rank1_barrier_busy_wait_us = " << params.tridiag_rank1_barrier_busy_wait_us
      << std::endl;
   os << "  eigensolver_min_band = " << params.eigensolver_min_band << std::endl;

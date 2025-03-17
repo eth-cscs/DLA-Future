ebd4b9a1a2322c12ff021249cc521bb047ccbe66
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-03-17 16:41:55 +0100

    upgrade license: no more years

--- cmake/FindDLAF_LAPACK.cmake
+++ cmake/FindDLAF_LAPACK.cmake
@@ -1,7 +1,7 @@
 #
 # CMake recipes
 #
-# Copyright (c) 2018-2024, ETH Zurich
+# Copyright (c) ETH Zurich
 # BSD 3-Clause License. All rights reserved.
 #
 # author: Alberto Invernizzi (a.invernizzi@cscs.ch)

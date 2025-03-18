8a851e32d7811e65f1399c59820f5fe729d58c60
Alberto Invernizzi <9337627+albestro@users.noreply.github.com>
2025-03-18 11:46:20 +0100

    Update license header (#1303)

--- ci/ctest_to_gitlab.sh
+++ ci/ctest_to_gitlab.sh
@@ -3,7 +3,7 @@
 #
 # Distributed Linear Algebra with Future (DLAF)
 #
-# Copyright (c) 2018-2024, ETH Zurich
+# Copyright (c) ETH Zurich
 # All rights reserved.
 #
 # Please, refer to the LICENSE file in the root directory.

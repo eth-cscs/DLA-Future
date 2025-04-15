3666f5abbf2ae4a26223e583cda3bbfbf24493e2
Doc CI Action <rasolca@users.noreply.github.com>
2025-04-15 11:16:08 +0000

    Doc: add v0.9.0 documentation

---
+++ v0.9.0/search/files_e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['qr_2eh_0',['qr.h',['../qr_8h.html',1,'']]]
+];
---
+++ v0.9.0/search/variables_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ld_0',['ld',['../struct_d_l_a_f__descriptor.html#a323ad31facefd124f573fe5a23388ac5',1,'DLAF_descriptor']]]
+];

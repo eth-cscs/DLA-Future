7b33eb374fe2e7c1b3f9e763096427d0623d26b5
Raffaele Solcà <rasolca@cscs.ch>
2025-04-30 17:53:54 +0200

    add structure and partial implementation (local, MC, Lower) and tests for potri

---
+++ include/dlaf/inverse/cholesky/impl.h
@@ -0,0 +1,253 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#pragma once
+
+#include <cstddef>
+#include <utility>
+
+#ifdef DLAF_WITH_HDF5
+#include <atomic>
+#include <sstream>
+#endif
+
+#include <pika/execution.hpp>
+
+#include <dlaf/blas/tile.h>
+#include <dlaf/common/index2d.h>
+#include <dlaf/common/pipeline.h>
+#include <dlaf/common/range2d.h>
+#include <dlaf/common/round_robin.h>
+#include <dlaf/communication/broadcast_panel.h>
+#include <dlaf/communication/communicator.h>
+#include <dlaf/communication/communicator_grid.h>
+#include <dlaf/communication/kernels.h>
+#include <dlaf/inverse/cholesky/api.h>
+#include <dlaf/lapack/tile.h>
+#include <dlaf/matrix/distribution.h>
+#include <dlaf/matrix/hdf5.h>
+#include <dlaf/matrix/index.h>
+#include <dlaf/matrix/matrix.h>
+#include <dlaf/matrix/panel.h>
+#include <dlaf/matrix/tile.h>
+#include <dlaf/sender/traits.h>
+#include <dlaf/util_matrix.h>
+
+namespace dlaf::inverse::internal {
+
+namespace assemble_cholesky_inv_l {
+template <Backend backend, class MatrixTileSender>
+void assemble_diag_tile(pika::execution::thread_priority priority, MatrixTileSender&& matrix_tile) {
+  using pika::execution::thread_stacksize;
+
+  pika::execution::experimental::start_detached(
+      dlaf::internal::whenAllLift(blas::Uplo::Lower, std::forward<MatrixTileSender>(matrix_tile)) |
+      tile::lauum(dlaf::internal::Policy<backend>(priority, thread_stacksize::nostack)));
+}
+
+template <Backend backend, class KKTileSender, class MatrixTileSender>
+void trmm_row_panel_tile(pika::execution::thread_priority priority, KKTileSender&& kk_tile,
+                         MatrixTileSender&& matrix_tile) {
+  using ElementType = dlaf::internal::SenderElementType<KKTileSender>;
+  using pika::execution::thread_stacksize;
+
+  pika::execution::experimental::start_detached(
+      dlaf::internal::whenAllLift(blas::Side::Left, blas::Uplo::Lower, blas::Op::ConjTrans,
+                                  blas::Diag::NonUnit, ElementType(1.0),
+                                  std::forward<KKTileSender>(kk_tile),
+                                  std::forward<MatrixTileSender>(matrix_tile)) |
+      tile::trmm(dlaf::internal::Policy<backend>(priority, thread_stacksize::nostack)));
+}
+
+template <Backend backend, class PanelTileSender, class MatrixTileSender>
+void herk_matrix_tile(pika::execution::thread_priority priority, PanelTileSender&& panel_tile,
+                      MatrixTileSender&& matrix_tile) {
+  using BaseElementType = BaseType<dlaf::internal::SenderElementType<MatrixTileSender>>;
+  using pika::execution::thread_stacksize;
+
+  pika::execution::experimental::start_detached(
+      dlaf::internal::whenAllLift(blas::Uplo::Lower, blas::Op::ConjTrans, BaseElementType(1.0),
+                                  std::forward<PanelTileSender>(panel_tile), BaseElementType(1.0),
+                                  std::forward<MatrixTileSender>(matrix_tile)) |
+      tile::herk(dlaf::internal::Policy<backend>(priority, thread_stacksize::nostack)));
+}
+
+template <Backend backend, class RowPanelTileSender, class ColPanelTileSender, class MatrixTileSender>
+void gemm_matrix_tile(pika::execution::thread_priority priority, RowPanelTileSender&& row_tile,
+                      ColPanelTileSender&& col_tile, MatrixTileSender&& matrix_tile) {
+  using ElementType = dlaf::internal::SenderElementType<MatrixTileSender>;
+  using pika::execution::thread_stacksize;
+
+  pika::execution::experimental::start_detached(
+      dlaf::internal::whenAllLift(blas::Op::ConjTrans, blas::Op::NoTrans, ElementType(1.0),
+                                  std::forward<RowPanelTileSender>(row_tile),
+                                  std::forward<ColPanelTileSender>(col_tile), ElementType(1.0),
+                                  std::forward<MatrixTileSender>(matrix_tile)) |
+      tile::gemm(dlaf::internal::Policy<backend>(priority, thread_stacksize::nostack)));
+}
+}
+
+// Local implementation of Lower Cholesky factorization.
+template <Backend backend, Device device, class T>
+void AssembleCholeskyInverse<backend, device, T>::call_L(Matrix<T, device>& mat_a) {
+  using namespace assemble_cholesky_inv_l;
+  using pika::execution::thread_priority;
+
+  // Number of tile (rows = cols)
+  SizeType nrtiles = mat_a.nr_tiles().cols();
+
+  for (SizeType k = 0; k < nrtiles; ++k) {
+    auto kk = LocalTileIndex{k, k};
+
+    for (SizeType i = 0; i < k; ++i) {
+      for (SizeType j = 0; j < i; ++j) {
+        gemm_matrix_tile<backend>(thread_priority::normal, mat_a.read(LocalTileIndex{k, i}),
+                                  mat_a.read(LocalTileIndex{k, j}),
+                                  mat_a.readwrite(LocalTileIndex{i, j}));
+      }
+
+      herk_matrix_tile<backend>(thread_priority::normal, mat_a.read(LocalTileIndex{k, i}),
+                                mat_a.readwrite(LocalTileIndex{i, i}));
+    }
+
+    for (SizeType j = 0; j < k; ++j) {
+      trmm_row_panel_tile<backend>(thread_priority::high, mat_a.read(kk),
+                                   mat_a.readwrite(LocalTileIndex{k, j}));
+    }
+
+    assemble_diag_tile<backend>(thread_priority::high, mat_a.readwrite(kk));
+  }
+}
+
+/*template <Backend backend, Device device, class T>
+void Cholesky<backend, device, T>::call_L(comm::CommunicatorGrid& grid, Matrix<T, device>& mat_a) {
+  using namespace cholesky_l;
+  using pika::execution::thread_priority;
+
+#ifdef DLAF_WITH_HDF5
+  static std::atomic<size_t> num_cholesky_calls = 0;
+  std::stringstream fname;
+  fname << "cholesky-factorization-" << matrix::internal::TypeToString_v<T> << "-"
+        << std::to_string(num_cholesky_calls) << ".h5";
+  std::optional<matrix::internal::FileHDF5> file;
+
+  if (getTuneParameters().debug_dump_cholesky_factorization_data) {
+    file = matrix::internal::FileHDF5(grid.fullCommunicator(), fname.str());
+    file->write(mat_a, "/input");
+  }
+#endif
+
+  // Set up MPI executor pipelines
+  auto mpi_row_task_chain = grid.row_communicator_pipeline();
+  auto mpi_col_task_chain = grid.col_communicator_pipeline();
+
+  const comm::Index2D this_rank = grid.rank();
+
+  const matrix::Distribution& distr = mat_a.distribution();
+  const SizeType nrtile = mat_a.nrTiles().cols();
+
+  constexpr std::size_t n_workspaces = 2;
+  common::RoundRobin<matrix::Panel<Coord::Col, T, device>> panels(n_workspaces, distr);
+  common::RoundRobin<matrix::Panel<Coord::Row, T, device, matrix::StoreTransposed::Yes>> panelsT(
+      n_workspaces, distr);
+
+  for (SizeType k = 0; k < nrtile; ++k) {
+    const GlobalTileIndex kk_idx(k, k);
+    const comm::Index2D kk_rank = distr.rankGlobalTile(kk_idx);
+
+    // Factorization of diagonal tile and broadcast it along the k-th column
+    if (kk_rank == this_rank)
+      potrfDiagTile<backend>(thread_priority::high, mat_a.readwrite(kk_idx));
+
+    // If there is no trailing matrix
+    const SizeType kt = k + 1;
+    if (kt == nrtile)
+      continue;
+
+    auto& panel = panels.nextResource();
+    auto& panelT = panelsT.nextResource();
+
+    panel.setRangeStart(GlobalTileIndex(kt, kt));
+
+    if (kk_rank.col() == this_rank.col()) {
+      const LocalTileIndex diag_wp_idx{0, distr.localTileFromGlobalTile<Coord::Col>(k)};
+
+      // Note:
+      // panelT shrinked to a single tile for temporarly storing and communicating the diagonal
+      // tile used for the column update
+      panelT.setRange({k, k}, {kt, kt});
+
+      if (kk_rank.row() == this_rank.row())
+        panelT.setTile(diag_wp_idx, mat_a.read(kk_idx));
+      broadcast(kk_rank.row(), panelT, mpi_col_task_chain);
+
+      // COLUMN UPDATE
+      for (SizeType i = distr.nextLocalTileFromGlobalTile<Coord::Row>(kt);
+           i < distr.localNrTiles().rows(); ++i) {
+        const LocalTileIndex local_idx(Coord::Row, i);
+        const LocalTileIndex ik_idx(i, distr.localTileFromGlobalTile<Coord::Col>(k));
+
+        trsmPanelTile<backend>(thread_priority::high, panelT.read(diag_wp_idx), mat_a.readwrite(ik_idx));
+
+        panel.setTile(local_idx, mat_a.read(ik_idx));
+      }
+
+      // row panel has been used for temporary storage of diagonal panel for column update
+      panelT.reset();
+    }
+
+    panelT.setRange({kt, kt}, indexFromOrigin(distr.nrTiles()));
+
+    broadcast(kk_rank.col(), panel, panelT, mpi_row_task_chain, mpi_col_task_chain);
+
+    // TRAILING MATRIX
+    for (SizeType jt_idx = kt; jt_idx < nrtile; ++jt_idx) {
+      const auto owner = distr.rankGlobalTile({jt_idx, jt_idx});
+
+      if (owner.col() != this_rank.col())
+        continue;
+
+      const auto j = distr.localTileFromGlobalTile<Coord::Col>(jt_idx);
+      const auto trailing_matrix_priority =
+          (jt_idx == kt) ? thread_priority::high : thread_priority::normal;
+      if (this_rank.row() == owner.row()) {
+        const auto i = distr.localTileFromGlobalTile<Coord::Row>(jt_idx);
+
+        herkTrailingDiagTile<backend>(trailing_matrix_priority, panel.read({Coord::Row, i}),
+                                      mat_a.readwrite(LocalTileIndex{i, j}));
+      }
+
+      for (SizeType i_idx = jt_idx + 1; i_idx < nrtile; ++i_idx) {
+        const auto owner_row = distr.rankGlobalTile<Coord::Row>(i_idx);
+
+        if (owner_row != this_rank.row())
+          continue;
+
+        const auto i = distr.localTileFromGlobalTile<Coord::Row>(i_idx);
+        gemmTrailingMatrixTile<backend>(trailing_matrix_priority, panel.read({Coord::Row, i}),
+                                        panelT.read({Coord::Col, j}),
+                                        mat_a.readwrite(LocalTileIndex{i, j}));
+      }
+    }
+
+    panel.reset();
+    panelT.reset();
+  }
+
+#ifdef DLAF_WITH_HDF5
+  if (getTuneParameters().debug_dump_cholesky_factorization_data) {
+    file->write(mat_a, "/cholesky");
+  }
+
+  num_cholesky_calls++;
+#endif
+}
+*/
+}
--- test/include/dlaf_test/matrix/util_generic_lapack.h
+++ test/include/dlaf_test/matrix/util_generic_lapack.h
@@ -148,6 +148,135 @@ auto getGenToStdElementSetters(SizeType n, int itype, blas::Uplo uplo, BaseType<
   return std::make_tuple(el_t, el_a, el_b);
 }
 
+/// Returns a tuple of element generators of two matrices A(n x n) and T(n x n).
+/// such that A = inv(T^H) inv(T) if @p uplo == Lower
+/// or        A = inv(T) inv(T^H) if @p uplo == Upper
+///
+/// The tile elements are chosen such that:
+/// t_ij = 1                    in the diagonal
+/// t_ij = -0.5 * exp(I*(-i+j)) in the offdiagonal,
+/// where I = 0 for real types or I is the complex unit for complex types.
+/// i.e.
+/// inv(t)_ij = 1 / 2^(|i-j|) * exp(I*(-i+j)),
+/// By definition A should be:
+/// a_lh = Sum_k(res_ik * ConjTrans(res)_kj) =
+///      = Sum_k(1 / 2^(|i-k| + |j-k|) * exp(I*(-i+j))),
+/// where k = 0 .. min(i,j), l = n-1-i, h = n-1-j
+/// Therefore,
+/// a_lh = (4^(min(i,j)+1) - 1) / (3 * 2^(i+j)) * exp(I*(i-j))
+template <class ElementIndex, class T>
+auto get_inverse_cholesky_factor_setters(SizeType n, blas::Uplo uplo) {
+  using dlaf::test::TypeUtilities;
+
+  std::function<T(const ElementIndex&)> el_t = [uplo](const ElementIndex& index) {
+    if ((uplo == blas::Uplo::Lower && index.row() < index.col()) ||
+        (uplo == blas::Uplo::Upper && index.row() > index.col()))
+      return TypeUtilities<T>::element(-9.9, 0);
+
+    const double i = index.row();
+    const double j = index.col();
+
+    if (i == j)
+      return TypeUtilities<T>::polar(1, 0);
+    if (i == j + 1 || i == j - 1)
+      return -TypeUtilities<T>::polar(0.5, -i + j);
+    return TypeUtilities<T>::polar(0, 0);
+  };
+
+  // Analytical results
+  std::function<T(const ElementIndex&)> el_a = [n, uplo](const ElementIndex& index) {
+    if ((uplo == blas::Uplo::Lower && index.row() < index.col()) ||
+        (uplo == blas::Uplo::Upper && index.row() > index.col()))
+      return TypeUtilities<T>::element(-9.9, 0);
+
+    const double i = n - 1 - index.row();
+    const double j = n - 1 - index.col();
+
+    return TypeUtilities<T>::polar(std::exp2(-(i + j)) / 3 * (std::exp2(2 * (std::min(i, j) + 1)) - 1),
+                                   i - j);
+  };
+
+  return std::make_tuple(el_t, el_a);
+}
+
+/// Returns a tuple of element generators of two triangular matrices A(n x n) and B(n x n).
+/// such that A = inv(B)
+///
+/// The tile elements are chosen such that:
+/// diag == Unit (i.e. diagonal element are not set but read 1)
+/// a_ij = 1 / 2^(|i-j|) * exp(I*(-i+j)),
+/// b_ij = 1                    in the diagonal
+/// b_ij = -0.5 * exp(I*(-i+j)) in the offdiagonal,
+/// where I = 0 for real types or I is the complex unit for complex types.
+/// diag == NonUnit
+/// a_ij = 4 / 2^(|i-j|) * exp(I*(-i+j)),
+/// b_ij = 1 / 4                  in the diagonal
+/// b_ij = -1 / 8 * exp(I*(-i+j)) in the offdiagonal,
+/// where I = 0 for real types or I is the complex unit for complex types.
+template <class ElementIndex, class T>
+auto get_triangular_inverse_setters(blas::Uplo uplo, blas::Diag diag) {
+  using dlaf::test::TypeUtilities;
+
+  if (diag == blas::Diag::Unit) {
+    std::function<T(const ElementIndex&)> el_a = [uplo](const ElementIndex& index) {
+      if ((uplo == blas::Uplo::Lower && index.row() <= index.col()) ||
+          (uplo == blas::Uplo::Upper && index.row() >= index.col()))
+        return TypeUtilities<T>::element(-9.9, 0);
+
+      const double i = index.row();
+      const double j = index.col();
+
+      return TypeUtilities<T>::polar(std::exp2(-std::abs(i - j)), -i + j);
+    };
+
+    // Analytical results
+    std::function<T(const ElementIndex&)> res_b = [uplo](const ElementIndex& index) {
+      if ((uplo == blas::Uplo::Lower && index.row() <= index.col()) ||
+          (uplo == blas::Uplo::Upper && index.row() >= index.col()))
+        return TypeUtilities<T>::element(-9.9, 0);
+
+      const double i = index.row();
+      const double j = index.col();
+
+      if (i == j + 1 || i == j - 1)
+        return -TypeUtilities<T>::polar(0.5, -i + j);
+      return TypeUtilities<T>::polar(0, 0);
+    };
+
+    return std::make_tuple(el_a, res_b);
+  }
+  else {
+    std::function<T(const ElementIndex&)> el_a = [uplo](const ElementIndex& index) {
+      if ((uplo == blas::Uplo::Lower && index.row() < index.col()) ||
+          (uplo == blas::Uplo::Upper && index.row() > index.col()))
+        return TypeUtilities<T>::element(-9.9, 0);
+
+      const double i = index.row();
+      const double j = index.col();
+
+      return TypeUtilities<T>::polar(4 * std::exp2(-std::abs(i - j)), -i + j);
+    };
+
+    // Analytical results
+    std::function<T(const ElementIndex&)> res_b = [uplo](const ElementIndex& index) {
+      if ((uplo == blas::Uplo::Lower && index.row() < index.col()) ||
+          (uplo == blas::Uplo::Upper && index.row() > index.col()))
+        return TypeUtilities<T>::element(-9.9, 0);
+
+      const double i = index.row();
+      const double j = index.col();
+
+      if (i == j)
+        return TypeUtilities<T>::polar(0.25, 0);
+      if (i == j + 1 || i == j - 1)
+        return -TypeUtilities<T>::polar(0.125, -i + j);
+      return TypeUtilities<T>::polar(0, 0);
+    };
+
+    return std::make_tuple(el_a, res_b);
+  }
+}
+
 }
 }
 }
